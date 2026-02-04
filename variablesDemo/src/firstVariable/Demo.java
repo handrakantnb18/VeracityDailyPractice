d8669ba9f6be7fc2260ee8c7cdad29782f87d4f5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-04 20:24:54 +0530

    Variable project firstVariable Demo

---
+++ variablesDemo/src/firstVariable/Demo.java
@@ -0,0 +1,18 @@
+package firstVariable;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int id = 101;
+		String name = "Chadnraknt";
+		float marks = 81.50f;
+		double salary = 40000;
+		String Address = "Pune";
+		
+		System.out.println("ID : "+id);
+		System.out.println("Name : "+name);
+		System.out.println("Marks : " +marks);
+		System.out.println("Salary : "+salary);
+		System.out.println("Address : "+salary);
+	}
+}
