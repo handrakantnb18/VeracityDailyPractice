1648c8ad9e2682609d3c73c339486d7f4a18e058
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-04 20:24:14 +0530

    Variable project Employee Demo

---
+++ variablesDemo/src/employee/Demo.java
@@ -0,0 +1,20 @@
+package employee;
+
+public class Demo {
+	public static void main(String[] args) {
+		int empId = 1;
+		String name = "Vaibhav";
+		String dept = "Admin";
+		int salary = 25000;
+		String address = "Pune";
+		int age = 26;
+		
+		System.out.println("User Id       : "+empId);
+		System.out.println("User Name     : " +name);
+		System.out.println("Email         : " +dept);
+		System.out.println("Age           : "+salary);
+		System.out.println("Address       : "+address);
+		System.out.println("Mobile number : " +age);
+	}
+
+}
