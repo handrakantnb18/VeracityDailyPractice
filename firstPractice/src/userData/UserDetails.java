580fe6607548e05a4b435e2c481c500e7b76d9ab
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-04 22:46:05 +0530

    five users added

---
+++ firstPractice/src/userData/UserDetails.java
@@ -0,0 +1,13 @@
+package userData;
+
+public class UserDetails {
+	public static void main(String[] args) {
+		int userId = 101;
+		String userName = "Ram";
+		String email = "ram@gmail.com";
+		int age = 26;
+		String address = "Pune";
+		
+	}
+
+}
