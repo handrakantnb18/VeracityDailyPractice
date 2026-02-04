54954d78ed15267415f5f82e2857afc3de59b377
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-04 20:25:56 +0530

    Variable project Product Demo

---
+++ variablesDemo/src/product/Demo.java
@@ -0,0 +1,20 @@
+package product;
+
+public class Demo {
+	public static void main(String[] args) {
+		int userId = 101;
+		String userName = "Vijay";
+		String email = "vijaysbc@gmail.com";
+		int age = 25;
+		String address = "Pune";
+		double contactNo = 987654321;
+		
+		System.out.println("User Id       : "+userId);
+		System.out.println("User Name     : " +userName);
+		System.out.println("Email         : " +email);
+		System.out.println("Age           : "+age);
+		System.out.println("Address       : "+address);
+		System.out.println("Mobile number : " +contactNo);
+	}
+
+}
