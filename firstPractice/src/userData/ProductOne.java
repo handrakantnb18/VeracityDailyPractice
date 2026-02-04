0f034804b6fd15932f76712bac5e69e218fec89e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-04 23:15:58 +0530

    product one data uploaded

---
+++ firstPractice/src/userData/ProductOne.java
@@ -0,0 +1,90 @@
+package userData;
+
+public class ProductOne {
+public static void main(String[] args) {
+		
+		// first Product
+		int pid = 111;
+		String pName = "Samsung";
+		int price = 25000;
+		int qty = 2;
+		String catagory = "abc";
+		
+		System.out.println("\nFirst Product :");
+		System.out.println("Product id : "+pid);
+		System.out.println("Product Name : "+pName);
+		System.out.println("Price : " +price);
+		System.out.println("Quantity : "+qty);
+		System.out.println("Catagory : "+catagory);
+		
+		// Second Product
+		int pid1 = 112;
+		String pName1 = "Vivo";
+		int price1 = 30000;
+		int qty1 = 3;
+		String catagory1 = "abcjhn";
+				
+		System.out.println("\nSecond Product :");
+		System.out.println("Product id : "+pid1);
+		System.out.println("Product Name : "+pName1);
+		System.out.println("Price : " +price1);
+		System.out.println("Quantity : "+qty1);
+		System.out.println("Catagory : "+catagory1);
+		
+		// Third Product
+		int pid2 = 113;
+		String pName2 = "Oppo";
+		int price2 = 40000;
+		int qty2 = 4;
+		String catagory2 = "abcjoiofjhdv";
+				
+		System.out.println("\nThird Product :");
+		System.out.println("Product id : "+pid2);
+		System.out.println("Product Name : "+pName2);
+		System.out.println("Price : " +price2);
+		System.out.println("Quantity : "+qty2);
+		System.out.println("Catagory : "+catagory2);
+		
+		// fourth Product
+		int pid3 = 114;
+		String pName3 = "OnePlus";
+		int price3 = 50000;
+		int qty3 = 5;
+		String catagory3 = "fdskjfabc";
+				
+		System.out.println("\nFourth Product :");
+		System.out.println("Product id : "+pid3);
+		System.out.println("Product Name : "+pName3);
+		System.out.println("Price : " +price3);
+		System.out.println("Quantity : "+qty3);
+		System.out.println("Catagory : "+catagory3);
+		
+		// fifth Product
+		int pid4 = 115;
+		String pName4 = "Nokia";
+		int price4 = 70000;
+		int qty4 = 10;
+		String catagory4 = "kljvnxvabc";
+				
+		System.out.println("\nFifth Product :");
+		System.out.println("Product id : "+pid4);
+		System.out.println("Product Name : "+pName4);
+		System.out.println("Price : " +price4);
+		System.out.println("Quantity : "+qty4);
+		System.out.println("Catagory : "+catagory4);
+		
+		// Sixth Product
+		int pid5 = 116;
+		String pName5 = "Acer";
+		int price5 = 170000;
+		int qty5 = 8;
+		String catagory5 = "kljvnxvfef5655abc";
+						
+		System.out.println("\nSixth Product :");
+		System.out.println("Product id : "+pid5);
+		System.out.println("Product Name : "+pName5);
+		System.out.println("Price : " +price5);
+		System.out.println("Quantity : "+qty5);
+		System.out.println("Catagory : "+catagory5);
+	}
+}
