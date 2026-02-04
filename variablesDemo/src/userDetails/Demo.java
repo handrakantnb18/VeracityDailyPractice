6ec64e425f3e747572e1c2bd83b25c5d75601ecc
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-04 20:26:23 +0530

    Variable project userDetails Demo

---
+++ variablesDemo/src/userDetails/Demo.java
@@ -0,0 +1,20 @@
+package userDetails;
+
+public class Demo {
+	public static void main(String[] args) {
+		int pId = 11;
+		String pName = "Samsung";
+		
+		int price = 25000;
+		int quantity = 2;
+		String catagory = "S1";
+		
+		System.out.println("Product Id       : "+pId);
+		System.out.println("Product Name     : " +pName);
+		
+		System.out.println("Product price    : "+price);
+		System.out.println("Product quantity : "+quantity);
+		System.out.println("Product catagory : " +catagory);
+	}
+
+}
