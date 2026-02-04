860076d92e079121db1c6ed7acae038ed44c44d9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-04 22:45:05 +0530

    Twenty variables uploded

---
+++ firstPractice/src/userData/Demo.java
@@ -0,0 +1,75 @@
+package userData;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int id = 101;
+		int userId = 90456;
+		String dept = "Admin";
+		
+		String userName = "Chandrakant";
+		String city = "Sangli";
+		String currentAddress = "Pune";
+		int pinCode = 411028;
+		
+		String productLocation = "Pune";
+		
+		double contact = 987654321;
+		int salary = 50000;
+		int dra = 4500;
+		
+		double empsalary = 50000;
+		double student_marks = 78.90;
+		double productPrice = 225;
+		double productQty = 50;
+		double productDiscount = 0.05;
+		
+		int std_id = 100;
+		float height = 6.1f;
+		float weight = 50.56f;
+		float mg = 2.30f;
+		float kg = 50.10f;
+		
+		boolean isMarried = false;
+		boolean vote = true;
+		boolean pass = true;
+		boolean gatePass = false;
+		boolean win = true;
+		
+		System.out.println(id);
+		System.out.println(userId);
+		System.out.println(dept);
+		
+		System.out.println(userName);
+		System.out.println(city);
+		System.out.println(currentAddress);
+		System.out.println(pinCode);
+		
+		System.out.println(productLocation);
+		System.out.println(contact);
+		
+		System.out.println(salary + 2000);
+		System.out.println(dra);
+		System.out.println(empsalary);
+		
+		System.out.println(student_marks);
+		System.out.println(productPrice);
+		System.out.println(productQty);
+
+		System.out.println(productDiscount);
+		
+		System.out.println(std_id);
+		System.out.println(height);
+		System.out.println(weight);
+		System.out.println(mg);
+		System.out.println(kg);
+		
+		System.out.println(isMarried);
+		System.out.println(vote);
+		System.out.println(pass);
+		System.out.println(gatePass);
+		System.out.println(win);
+		
+	}
+	
+}
