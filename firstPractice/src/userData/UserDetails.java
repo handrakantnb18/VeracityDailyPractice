27e9c4d304c6acb1674d28bd0e57df1f3887347a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-04 23:11:06 +0530

    user Deails updated successfully

--- firstPractice/src/userData/UserDetails.java
+++ firstPractice/src/userData/UserDetails.java
@@ -2,12 +2,86 @@ package userData;
 
 public class UserDetails {
 	public static void main(String[] args) {
+		
+		// first User details
 		int userId = 101;
 		String userName = "Ram";
 		String email = "ram@gmail.com";
 		int age = 26;
 		String address = "Pune";
+		long mobileNO = 9876543210l;
 		
+		System.out.println("\nFirst User  : ");
+		System.out.println("User Id       : "+userId);
+		System.out.println("User Name     : "+userName);
+		System.out.println("Email         : "+email);
+		System.out.println("Age           : "+age);
+		System.out.println("Address       : "+address);
+		System.out.println("Mobile Number : "+mobileNO);
+		
+		// second User details
+		int userId1 = 102;
+		String userName1 = "Sivam";
+		String email1 = "shivamk@gmail.com";
+		int age1 = 25;
+		String address1 = "Latur";
+		long mobileNO1 = 895576543210l;
+		
+		System.out.println("\nSecond User : ");
+		System.out.println("User Id       : "+userId1);
+		System.out.println("User Name     : "+userName1);
+		System.out.println("Email         : "+email1);
+		System.out.println("Age           : "+age1);
+		System.out.println("Address       : "+address1);
+		System.out.println("Mobile Number : "+mobileNO1);
+		
+		// third User details
+		int userId2 = 103;
+		String userName2 = "Stivan";
+		String email2 = "stivans@example.com";
+		int age2 = 29;
+		String address2 = "Nagpur";
+		long mobileNO2 = 98985576543210l;
+				
+		System.out.println("\nThird User : ");
+		System.out.println("User Id       : "+userId2);
+		System.out.println("User Name     : "+userName2);
+		System.out.println("Email         : "+email2);
+		System.out.println("Age           : "+age2);
+		System.out.println("Address       : "+address2);
+		System.out.println("Mobile Number : "+mobileNO2);
+		
+		// four User details
+		int userId3 = 104;
+		String userName3 = "Ajit";
+		String email3 = "ajit.a@gmail.com";
+		int age3 = 33;
+		String address3 = "Thane";
+		long mobileNO3 = 7878543210l;
+				
+		System.out.println("\nFour User  : ");
+		System.out.println("User Id       : "+userId3);
+		System.out.println("User Name     : "+userName3);
+		System.out.println("Email         : "+email3);
+		System.out.println("Age           : "+age3);
+		System.out.println("Address       : "+address3);
+		System.out.println("Mobile Number : "+mobileNO3);
+		
+		// five User details
+		int userId4 = 104;
+		String userName4 = "Vijay";
+		String email4 = "vijay.b@gmail.com";
+		int age4 = 30;
+		String address4 = "Solapur";
+		long mobileNO4 = 879005543210l;
+						
+		System.out.println("\nFour User  : ");
+		System.out.println("User Id       : "+userId4);
+		System.out.println("User Name     : "+userName4);
+		System.out.println("Email         : "+email4);
+		System.out.println("Age           : "+age4);
+		System.out.println("Address       : "+address4);
+		System.out.println("Mobile Number : "+mobileNO4);
 	}
 
 }
