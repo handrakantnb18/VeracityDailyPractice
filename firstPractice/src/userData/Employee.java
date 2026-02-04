b9c6b97d5a6a6ab9c33b0f4ba8cf5257972bc6fa
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-04 23:35:41 +0530

    salary and id name exp added

--- firstPractice/src/userData/Employee.java
+++ firstPractice/src/userData/Employee.java
@@ -3,11 +3,20 @@ package userData;
 public class Employee {
 
 	public static void main(String[] args) {
-		int a =10, b = 50;
+//		int a =10, b = 50;
+//		
+//		System.out.println("Addition : "+(a+b));
+//		System.out.println("Substraction : "+(a-b));
+//		System.out.println("Division : "+(a/b));
+//		System.out.println("Multiplication : "+(a*b));
 		
-		System.out.println("Addition : "+(a+b));
-		System.out.println("Substraction : "+(a-b));
-		System.out.println("Division : "+(a/b));
-		System.out.println("Multiplication : "+(a*b));
+		int id = 1010;
+		String name = "Rahul";
+		int exp = 4;
+		long salary = 450000l;
+		
+		System.out.println(name);
+		System.out.println(salary);
+		
 	}
 }
