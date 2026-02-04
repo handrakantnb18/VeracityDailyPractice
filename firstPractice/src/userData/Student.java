0f7da7a982996e22f9ea2eaf21c60821582907f3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-04 22:44:22 +0530

    Five student data has been updated

---
+++ firstPractice/src/userData/Student.java
@@ -0,0 +1,93 @@
+package userData;
+
+public class Student {
+
+	public static void main(String[] args) {
+		
+		// first Student
+		int id = 1;
+		String name = "Chandrakant";
+		float marks = 65.33f;
+		long contact = 96638527400000l;
+		char gender = 'M';
+		String city = "Pune";
+		
+		System.out.println("First Student : ");
+		
+		System.out.println("ID            : "+id);
+		System.out.println("Name          : "+name);
+		System.out.println("Marks         : "+marks);
+		System.out.println("Mobile Number : "+contact);
+		System.out.println("Gender        : "+gender);
+		System.out.println("City          : "+city);
+		
+		// second student
+		int id1 = 2;
+		String name1 = "Vijay";
+		float marks1 = 55.66f;
+		long contact1 = 96635464566l;
+		char gender1 = 'M';
+		String city1 = "Mumbai";
+		
+		
+		System.out.println("\nSecond Student : ");
+		System.out.println("ID            : "+id1);
+		System.out.println("Name          : "+name1);
+		System.out.println("Marks         : "+marks1);
+		System.out.println("Mobile Number : "+contact1);
+		System.out.println("Gender        : "+gender1);
+		System.out.println("City          : "+city1);
+		
+		
+		// Third Student
+		int id2 = 3;
+		String name2 = "Viraj";
+		float marks2 = 85.90f;
+		long contact2 = 9663595894566l;
+		char gender2 = 'M';
+		String city2 = "Sangli";
+		
+		
+		System.out.println("\nThird Student : ");
+		System.out.println("ID            : "+id2);
+		System.out.println("Name          : "+name2);
+		System.out.println("Marks         : "+marks2);
+		System.out.println("Mobile Number : "+contact2);
+		System.out.println("Gender        : "+gender2);
+		System.out.println("City          : "+city2);
+		
+		// Fourth Student
+		int id3 = 4;
+		String name3 = "Priyanka";
+		float marks3 = 70.80f;
+		long contact3 = 78223595894566l;
+		char gender3 = 'F';
+		String city3 = "Kolhapur";
+				
+				
+		System.out.println("\nThird Student : ");
+		System.out.println("ID            : "+id3);
+		System.out.println("Name          : "+name3);
+		System.out.println("Marks         : "+marks3);
+		System.out.println("Mobile Number : "+contact3);
+		System.out.println("Gender        : "+gender3);
+		System.out.println("City          : "+city3);
+		
+		// fifth Student
+		int id4 = 5;
+		String name4 = "Neha";
+		float marks4 = 95.33f;
+		long contact4 = 8787877400000l;
+		char gender4 = 'F';
+		String city4 = "Satara";
+		
+		System.out.println("\nFirst Student : ");
+		
+		System.out.println("ID            : "+id4);
+		System.out.println("Name          : "+name4);
+		System.out.println("Marks         : "+marks4);
+		System.out.println("Mobile Number : "+contact4);
+		System.out.println("Gender        : "+gender4);
+		System.out.println("City          : "+city4);
+	}
+}
