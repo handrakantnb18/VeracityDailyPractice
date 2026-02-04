ad364579424e3abbba49c73de62e77d495be11dd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-04 19:57:17 +0530

    first java project 27 jan 2026 start practice First

---
+++ FirstJavaProject/src/one/One.java
@@ -0,0 +1,10 @@
+package one;
+
+public class One {
+	public static void main(String[] args) {
+		String name = "Vaibhav";
+		int year = 2026;
+		
+		System.out.println("Hello!."+ name+ " year is" + year);
+	}
+}
