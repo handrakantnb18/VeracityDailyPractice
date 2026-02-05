10dc2c334841d0fbd7f51f11e34822e1926059f9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-05 21:22:49 +0530

    arithmatic operatorchecking + - / *

--- operatorsAll/src/arithmaticOperators/Demo.java
+++ operatorsAll/src/arithmaticOperators/Demo.java
@@ -5,14 +5,14 @@ public class Demo {
 	public static void main(String[] args) {
 		int a = 10, b = 5;
 
-		System.out.println("Addition is       : "+(a+b));
-		
-		System.out.println("Substraction is   : "+(a-b));
-		
-		System.out.println("Multiplication is : "+(a *b));
-		
-		System.out.println("Division is       : "+(a/b));
-		
+		System.out.println("Addition is       : " + (a + b));
+
+		System.out.println("Substraction is   : " + (a - b));
+
+		System.out.println("Multiplication is : " + (a * b));
+
+		System.out.println("Division is       : " + (a / b));
+
 	}
-		
+
 }
