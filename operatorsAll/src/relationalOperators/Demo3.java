b01f41a9636b87db8f9fbc4d529014afbd6d102b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-05 21:25:35 +0530

    relational operator == != !isMaried or not checked boolean value

--- operatorsAll/src/relationalOperators/Demo3.java
+++ operatorsAll/src/relationalOperators/Demo3.java
@@ -4,18 +4,22 @@ public class Demo3 {
 
 	public static void main(String[] args) {
 		int i = 100;
-		
-		System.out.println( i == i);
-		
-		System.out.println( !(i == i));
-		
+
+		System.out.println(i == i);
+
+		System.out.println(!(i == i));
+
 		System.out.println("\n");
-		
-		
+
 		boolean isMarried = true;
-		
+
 		System.out.println(isMarried);
 		System.out.println(!isMarried);
+
+		// userName, password, = login success
+
+		// skills java, python, age = 10 = JOB
+
 	}
-	
+
 }
