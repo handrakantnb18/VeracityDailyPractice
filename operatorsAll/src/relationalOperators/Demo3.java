8fb375b9fcf5edf8e32242a0ba676405ffd9759a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-05 21:08:24 +0530

    != operator is used checking the boolean result

---
+++ operatorsAll/src/relationalOperators/Demo3.java
@@ -0,0 +1,13 @@
+package relationalOperators;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		int i = 100;
+		
+		System.out.println( i == i);
+		
+		System.out.println( !(i == i));
+	}
+	
+}
