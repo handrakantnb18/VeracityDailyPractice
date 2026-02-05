cf8236bea9a2e60ebbe0d5a3c9d1b36b28532700
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-05 21:45:17 +0530

    logical operators == != < > boolean value true and false

---
+++ operatorsAll/src/logicalOperators/Demo.java
@@ -0,0 +1,18 @@
+package logicalOperators;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		int a = 10, b = 20;
+		
+		System.out.println(a == b); // false
+		
+		System.out.println(a != b);  // true
+		
+		System.out.println(a > b);  // false
+		
+		System.out.println(a  < b);  // true
+		
+	}
+}
