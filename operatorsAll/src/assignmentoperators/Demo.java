24c7aee69c899d67825b596235766021f736c22d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-06 22:41:11 +0530

    checking the condition of assignment operators

--- operatorsAll/src/assignmentoperators/Demo.java
+++ operatorsAll/src/assignmentoperators/Demo.java
@@ -1,19 +1,27 @@
 package assignmentoperators;
 
 public class Demo {
-	
+
 	public static void main(String[] args) {
+
 		
+		// checking the condition of the assignment operator 
 		int a = 100;
+
+		// a = a + 20; // initialize + assignments operators
+
+		// a += 20; // asignment operator
+
+		// a -= 20;
 		
-		// a = a + 20; // initialize  + assignments operators
+		// a *= 2;
 		
-		//a += 20; // asignment operator
+		// a /= 50;
 		
-		a -= 20;
-		
+		a %= 2;
+
 		System.out.println(a);
-		
+
 //		int a = 100; // initialize
 //		
 //		a = 200; // reinitialize
