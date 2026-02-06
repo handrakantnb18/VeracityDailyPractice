9e2327ab465739c81c6508ed2b78d52c44731b4a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-06 22:36:48 +0530

    veriables checked $, _ is used do not use space, before variable num not use

---
+++ operatorsAll/src/variablesRules/Demo.java
@@ -0,0 +1,23 @@
+package variablesRules;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		// variable rules
+		
+		// int 12ab = 10; // invalid
+		
+		String a = "Chandrakant"; // valid
+		
+		int $marks = 20;  // valid
+		
+		String student_name = "Ram"; // valid
+		
+		// String emp name = "Vibhav"; // invalid
+		
+		// int i@d = 25; // invalid
+		
+	}
+
+}
