08c09e3a1f08d4e3150ca43f86cf010f41a876f2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-06 22:37:47 +0530

    Assignment operator is used for checking =, +=, -=, *=, %=

---
+++ operatorsAll/src/assignmentoperators/Demo.java
@@ -0,0 +1,26 @@
+package assignmentoperators;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		int a = 100;
+		
+		// a = a + 20; // initialize  + assignments operators
+		
+		//a += 20; // asignment operator
+		
+		a -= 20;
+		
+		System.out.println(a);
+		
+//		int a = 100; // initialize
+//		
+//		a = 200; // reinitialize
+//		
+//		a = 250; // reinitialize
+//		
+//		System.out.println(a);
+	}
+
+}
