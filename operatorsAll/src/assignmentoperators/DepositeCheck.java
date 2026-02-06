1e7c2036c4f69aee231e86be3b8bcb7b7044e06b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-06 23:34:23 +0530

    check the deposite in the memory += operator

---
+++ operatorsAll/src/assignmentoperators/DepositeCheck.java
@@ -0,0 +1,18 @@
+package assignmentoperators;
+
+public class DepositeCheck {
+	
+	public static void main(String[] args) {
+		
+		double balance = 3000;
+		
+		double deposite = 1000;
+		
+		// balance = balance + deposite;
+		
+		balance += deposite;
+		
+		System.out.println(balance);
+	}
+
+}
