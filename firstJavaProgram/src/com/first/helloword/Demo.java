cfe06930fa2d845f859553d4ad7f8c027b81ac62
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-06 11:48:54 +0530

    Starting My Java Journey with Hello World simple

---
+++ firstJavaProgram/src/com/first/helloword/Demo.java
@@ -0,0 +1,9 @@
+package com.first.helloword;
+
+public class Demo {
+	
+	 public static void main(String[] args) {
+		System.out.println("Hello, Word");
+	}
+
+}
---
+++ firstJavaProgram/src/com/first/helloword/HelloWord.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.first.helloword;
+
+/**
+ * 
+ */
+public class HelloWord {
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to java program, everyone");
+	}
+}
