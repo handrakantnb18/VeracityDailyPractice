7a9938e74d068648a1c971e4ea31151765d35365
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-06 23:35:16 +0530

    unary + and unary - value checked first

---
+++ operatorsAll/src/unaryoperator/Demo.java
@@ -0,0 +1,34 @@
+package unaryoperator;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		int a = 100;
+		
+		System.out.println(a++); // compiler is print value first  .increment will happen in memory
+		
+		System.out.println(a++); // store in memory
+
+//		a++;
+//		
+//		a++;
+//		
+		// ++a;
+
+		// ++a;
+		
+		
+
+		//System.out.println(a);
+		
+		// int x = +100;
+
+		// System.out.println(+x); // unary(+) operator
+		// + + = +
+		// System.out.println(-x); // unary(-) operator
+
+		// - + = -
+
+	}
+}
