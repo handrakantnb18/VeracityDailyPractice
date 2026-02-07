739813a746ce7bb9ef550cd6594b25ba96a756d7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-07 22:18:53 +0530

    simple Number is positive or nigative check

---
+++ javafullstack/src/com/javapractice/day1/assignment1/PositiveNumber.java
@@ -0,0 +1,23 @@
+package com.javapractice.day1.assignment1;
+
+public class PositiveNumber {
+
+	public static void main(String[] args) {
+		
+		// simple Number is positive
+		
+		int number = -10;
+		
+		if (number > 0) {
+			System.out.println("Number is Positive...");
+			
+		} else {
+			System.out.println("Number is Negative..");
+		}
+		
+		
+		
+	}
+	
+	
+}
