4af0e49f20f7b91af8d3110b5b424740ae132efd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-07 22:45:07 +0530

    Write a program to check if a number is positive

--- javafullstack/src/com/javapractice/day1/assignment1/PositiveNumber.java
+++ javafullstack/src/com/javapractice/day1/assignment1/PositiveNumber.java
@@ -2,21 +2,20 @@ package com.javapractice.day1.assignment1;
 
 public class PositiveNumber {
 
+	// Write a program to check if a number is positive
+	
 	public static void main(String[] args) {
 		
 		// simple Number is positive
 		
-		int number = -10;
+		int number = 10;
 		
-		if (number > 0) {
+		if (number < 0) {
 			System.out.println("Number is Positive...");
 			
-		} else {
-			System.out.println("Number is Negative..");
-		}
+		} 		
 		
-		
-		
+		System.out.println("Hello, word");
 	}
 	
 	
