3ed67c3ee23c6ed25d8d4e66ed7488629d68fe58
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-08 21:11:32 +0530

    simple Number is positive and negative number

--- javafullstack/src/com/javapractice/day1/assignment1/PositiveNumber.java
+++ javafullstack/src/com/javapractice/day1/assignment1/PositiveNumber.java
@@ -6,16 +6,64 @@ public class PositiveNumber {
 	
 	public static void main(String[] args) {
 		
-		// simple Number is positive
+		// 1) simple Number is positive
 		
 		int number = 10;
 		
-		if (number < 0) {
+		
+		
+		if (number > 0) {
 			System.out.println("Number is Positive...");
 			
-		} 		
+		} 	
 		
-		System.out.println("Hello, word");
+		if (number > 0) {
+			System.out.println("\nNumber is Negative.");
+		}	
+		
+		// 2) simple Number is positive
+		
+		int number1 = -10;
+		
+		if ( number1 < 0) {
+			System.out.println("\nNumber is Negative.");
+		}	
+		
+		
+		// 3) Number is positive or nigetive
+		
+		// 1
+		int num1 = 10;
+		int num2 = -10;
+		
+		if (num1 < 0 && num2 > 0) {
+			System.out.println("\nNumber is positive.");
+			
+		}else {
+			System.out.println("\nNumber is negative");
+		}
+		
+		// 2
+		if (num1 > 0 && num2 < 0) {
+			System.out.println("\nNumber is positive");
+		}
+		else {
+			System.out.println("\nNumber is nagetive.");
+		}
+		
+		// 4) Number is po
+		
+		int a = 50;
+		
+		if (a < 18 && a > 18) {
+			System.out.println("\nEllagible for the voting");
+		}
+		else {
+			System.out.println("\nNot Eligible.");
+		}
+		
+		//
+		
 	}
 	
 	
