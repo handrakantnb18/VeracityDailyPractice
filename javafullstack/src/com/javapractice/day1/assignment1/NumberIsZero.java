b13ac4699478d8affed900715937997d38b7c50f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-08 22:07:45 +0530

    Number is zero or not check

---
+++ javafullstack/src/com/javapractice/day1/assignment1/NumberIsZero.java
@@ -0,0 +1,21 @@
+package com.javapractice.day1.assignment1;
+
+public class NumberIsZero {
+	public static void main(String[] args) {
+		// Write a program to check if a number is zero.
+		
+		int num1 = 0;
+		
+		System.out.println("Number is - "+num1);
+		
+		// Number is zero or not check
+		int num2 = 0;
+		
+		if (true && false) {
+			System.out.println("Number is Zero"+num2);
+		}
+		
+		// Number is zero
+	}
+
+}
