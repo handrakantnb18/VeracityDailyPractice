d5208f507f9155947a9e0ffbb4df387b1acf9ef8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-08 21:12:41 +0530

    simple Number is positive || operator is used

---
+++ javafullstack/src/com/javapractice/day1/assignment1/PositiveNumber1.java
@@ -0,0 +1,22 @@
+package com.javapractice.day1.assignment1;
+
+public class PositiveNumber1 {
+	
+	public static void main(String[] args) {
+		
+		// 1) Positive Number
+		
+		int num = 20;
+		
+		if (true) {
+			System.out.println("Number is positve.");
+		}
+		
+		int num1 = 30;
+		
+		if (num > 0 || num < 0) {
+			System.out.println();
+		}
+	}
+
+}
