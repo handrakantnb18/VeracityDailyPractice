1b7e69f849c8184d4f59a4abeaa94bd58063c5be
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-08 22:10:32 +0530

    Number is zero if else

--- javafullstack/src/com/javapractice/day1/assignment1/NumberIsZero.java
+++ javafullstack/src/com/javapractice/day1/assignment1/NumberIsZero.java
@@ -12,10 +12,20 @@ public class NumberIsZero {
 		int num2 = 0;
 		
 		if (true && false) {
-			System.out.println("Number is Zero"+num2);
+			System.out.println("\nNumber is Zero"+num2);
 		}
+		System.out.println("\nNumber is not zero"+num2);
 		
-		// Number is zero
+		// Number is zero if else
+		
+		int a = 18;
+		
+		if (a > 0 && a < 0) {
+			System.out.println("\nNumber is greater than zero");
+		}
+		else {
+			System.out.println("\nNumber is not greater than zero.");
+		}
 	}
 
 }
