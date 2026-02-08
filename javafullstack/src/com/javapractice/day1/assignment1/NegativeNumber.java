318ae9eef10d9bf83b2c451535364cb8f40d0891
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-08 21:59:24 +0530

    Negative number check true or false

---
+++ javafullstack/src/com/javapractice/day1/assignment1/NegativeNumber.java
@@ -0,0 +1,16 @@
+package com.javapractice.day1.assignment1;
+
+public class NegativeNumber {
+	
+	public static void main(String[] args) {
+		
+		// Negative number
+		
+		int a = -10;
+		
+		if(true) {
+			System.out.println("Number is negative");
+		}
+	}
+
+}
