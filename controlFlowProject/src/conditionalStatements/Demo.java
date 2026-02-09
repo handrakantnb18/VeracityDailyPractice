c03909f58d7edc229b9fc8ea0cc38244cc3db2db
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-09 17:59:51 +0530

    If statement checkinig the condition true or false

--- controlFlowProject/src/conditionalStatements/Demo.java
+++ controlFlowProject/src/conditionalStatements/Demo.java
@@ -1,13 +1,18 @@
 package conditionalStatements;
 
+import java.util.Iterator;
+
 public class Demo {
 
 	public static void main(String[] args) {
 	
 		// If statement checkinig the condition 
 		
-		System.out.println("Hello, priyanka");
-		System.out.println("How are you.");
+		if (false) {
+			System.out.println("Hello, priyanka");
+			System.out.println("How are you.");
+		}
+		
 		System.out.println("condition good");
 		System.out.println("Where is chandrakant");
 	}
