b642d8285d422be88a7debab07376eff2672f5e4
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-09 11:56:44 +0530

    Eligible for Voting check

---
+++ operatorsAll/src/unaryoperator/FindVotingElig.java
@@ -0,0 +1,15 @@
+package unaryoperator;
+
+public class FindVotingElig {
+
+	public static void main(String[] args) {
+		
+		int age = 25;
+		
+		String a = (age >= 18)? "Eligible for Voting" : "Not eleigible for Voting";
+		
+		System.out.println(a);
+		
+		
+	}
+}
