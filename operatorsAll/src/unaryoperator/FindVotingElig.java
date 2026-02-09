6e2345440eb559bafa551fa51c544a059ba1d9ed
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-09 11:58:14 +0530

    Not eligible for voting for check

--- operatorsAll/src/unaryoperator/FindVotingElig.java
+++ operatorsAll/src/unaryoperator/FindVotingElig.java
@@ -4,12 +4,16 @@ public class FindVotingElig {
 
 	public static void main(String[] args) {
 		
+		// Checking Eligible for the voting
 		int age = 25;
 		
 		String a = (age >= 18)? "Eligible for Voting" : "Not eleigible for Voting";
 		
 		System.out.println(a);
 		
+		String b = (age <= 18)? "Eligible for the Voting" : "Not eligible for voting";
+		
+		System.out.println("\n"+b);
 		
 	}
 }
