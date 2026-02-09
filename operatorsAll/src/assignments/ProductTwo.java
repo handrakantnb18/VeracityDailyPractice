053a5800a6d0cdc7ac08303b904e02c5e7379c85
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-09 14:16:33 +0530

    Second check Product - id, name, category, price, qty

---
+++ operatorsAll/src/assignments/ProductTwo.java
@@ -0,0 +1,38 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class ProductTwo {
+
+	public static void main(String[] args) {
+		// Product - id, name, category, price, qty
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter id : ");
+		int id = sc.nextInt();
+
+		System.out.println("Enter Name : ");
+		String name = sc.next();
+
+		System.out.println("Enter Catagory : ");
+		String catagory = sc.next();
+
+		System.out.println("Enter price : ");
+		int price = sc.nextInt();
+
+		System.out.println("Enter Quantity : ");
+		int qty = sc.nextInt();
+
+		System.out.println("\nId " + id);
+
+		System.out.println("\nName : " + name);
+
+		System.out.println("\nCategory : " + catagory);
+
+		System.out.println("\nPrice : " + price);
+
+		System.out.println("\nQuantity : " + qty);
+
+	}
+}
