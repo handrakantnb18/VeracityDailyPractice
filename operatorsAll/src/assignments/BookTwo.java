e28f0f8b2f5a5c81ea9e113c3f81d3ca87d99b03
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-09 13:58:22 +0530

    BookTwo Book - id, name, auther, price

---
+++ operatorsAll/src/assignments/BookTwo.java
@@ -0,0 +1,36 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class BookTwo {
+
+	public static void main(String[] args) {
+		// Book - id, name, auther, price
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter id : ");
+		int id = sc.nextInt();
+
+		System.out.println("Enter Name : ");
+		String name = sc.next();
+
+		System.out.println("Enter Auther : ");
+		String auther = sc.next();
+
+		System.out.println("Enter Price : ");
+		int price = sc.nextInt();
+
+		System.out.println("Enter Id : "+id);
+		
+		System.out.println("Enter Name : "+name);
+		
+		System.out.println("Enter Auther : "+auther);
+		
+		System.out.println("Enter Price : "+price);
+		
+		String result = ("Id : "+id+" Name : "+name+" Auther : "+auther+" Price : "+price);
+		
+		System.out.println(result);
+	}
+}
