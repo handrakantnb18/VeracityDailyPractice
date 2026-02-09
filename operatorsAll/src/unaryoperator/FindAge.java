4541aadc094289ea72e919598838800828ce6c15
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-09 11:31:52 +0530

    find age eligibility for Driving

---
+++ operatorsAll/src/unaryoperator/FindAge.java
@@ -0,0 +1,14 @@
+package unaryoperator;
+
+public class FindAge {
+	
+	public static void main(String[] args) {
+		
+		int age = 25;
+		
+		String s = (age >= 18) ? "You are eligible for Driving" : "You are not eligible for Driving";
+		
+		System.out.println(s);
+	}
+
+}
