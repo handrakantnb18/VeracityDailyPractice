1a027b010fbd2a13ccefe9d4a6ac5397b47068b7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-09 17:56:29 +0530

    If statement checkinig the condition

--- controlFlowProject/src/conditionalStatements/Demo.java
+++ controlFlowProject/src/conditionalStatements/Demo.java
@@ -3,6 +3,12 @@ package conditionalStatements;
 public class Demo {
 
 	public static void main(String[] args) {
+	
+		// If statement checkinig the condition 
 		
+		System.out.println("Hello, priyanka");
+		System.out.println("How are you.");
+		System.out.println("condition good");
+		System.out.println("Where is chandrakant");
 	}
 }
