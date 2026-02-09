8e7411a4ed9c1d7e1f5f536f91d2c73bc2dcd70a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-09 20:42:43 +0530

    check the number is odd or Even

--- controlFlowProject/src/conditionalStatements/SimpleTwo.java
+++ controlFlowProject/src/conditionalStatements/SimpleTwo.java
@@ -5,26 +5,40 @@ import java.util.Scanner;
 public class SimpleTwo {
 
 	public static void main(String[] args) {
-		
-		
-		// check the number is positive or Negative number
-		
+
+		// check the number is odd or Even
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Enter the Number : ");
-		
+
 		int num = sc.nextInt();
-		
-		if (num > 0) {
-			System.out.println("Number is positive");
+
+		if (num % 2 == 0) {
+			System.out.println("Even Number.");
 		}
-		
-		if (num < 0) {
-			System.out.println("Number is Negative.");
+
+		if (num % 2 != 0) {
+			System.out.println("Odd Number");
 		}
-		
-		if (num == 0) {
-			System.out.println("Number is Zero.");
-		}
+
+		// check the number is positive or Negative number
+
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("Enter the Number : ");
+//		
+//		int num = sc.nextInt();
+//		
+//		if (num > 0) {
+//			System.out.println("Number is positive");
+//		}
+//		
+//		if (num < 0) {
+//			System.out.println("Number is Negative.");
+//		}
+//		
+//		if (num == 0) {
+//			System.out.println("Number is Zero.");
+//		}
 	}
 }
