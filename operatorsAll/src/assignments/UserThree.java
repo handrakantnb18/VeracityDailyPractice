2f4c14216a714e8d7ace6993ad75ebcfa2621015
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-09 14:42:16 +0530

    Details checked hard level for User - id, name, city, contact, email;

---
+++ operatorsAll/src/assignments/UserThree.java
@@ -0,0 +1,43 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class UserThree {
+	
+	public static void main(String[] args) {
+		// User - id, name, city, contact, email;
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("User id : ");
+		int id = sc.nextInt();
+
+		System.out.println("User name : ");
+		String name = sc.next();
+
+		System.out.println("User City : ");
+		String city = sc.next();
+
+		System.out.println("Contact : ");
+		String contact = sc.next();
+
+		System.out.println("Email : ");
+		String email = sc.next();
+		
+		System.out.println("\nUser Id      : "+id);
+		
+		System.out.println("\nUser Name    : "+name);
+		
+		System.out.println("\nUser City    : "+city);
+		
+		System.out.println("\nUser Contact : "+contact);
+		
+		System.out.println("\nUser Email   :"+email);
+	
+		String res = ("User ID : "+id+" User Name : "+name+" User City : "+city+" User Contact : "+contact+" User Email : "+email);
+		
+		System.out.println(res);
+		
+	}
+
+}
