a31b2be8bb1b1a77fe735b330a2ec9ae4d344a3c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-09 20:22:11 +0530

    10 > 20 condition true or false boolean value

---
+++ controlFlowProject/src/conditionalStatements/Simple.java
@@ -0,0 +1,27 @@
+package conditionalStatements;
+
+public class Simple {
+	
+	public static void main(String[] args) {
+		 
+		// 10 > 20 condition true or false boolean value
+		
+		if (10 < 20) {
+			System.out.println("Hello");
+		}
+		
+		if (10 > 20) {
+			System.out.println("Good");
+		}
+		
+		System.out.println("Welcome");
+		
+		int age = 25;
+		
+		if (age > 18) {
+			System.out.println("Your are eleigible");
+		}
+	}
+	
+
+}
