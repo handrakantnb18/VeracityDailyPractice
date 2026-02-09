975373a003d4b13eba79ff02073039990d3b1737
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-09 11:29:38 +0530

    find salary with experience and check eligibility

---
+++ operatorsAll/src/unaryoperator/FindSalary.java
@@ -0,0 +1,15 @@
+package unaryoperator;
+
+public class FindSalary {
+	
+	public static void main(String[] args) {
+		
+		int exp = 4;
+		
+		double salary = 40000;
+		
+		double findSalary = (exp >=5 ) ? salary + 5000: salary + 2000;
+		
+		System.out.println(findSalary);
+	}
+}
