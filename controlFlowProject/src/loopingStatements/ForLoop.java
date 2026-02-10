95e87e8e18358dd3c6665d163477508929756d3f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-10 14:17:33 +0530

    print 10 to 1 reverse

--- controlFlowProject/src/loopingStatements/ForLoop.java
+++ controlFlowProject/src/loopingStatements/ForLoop.java
@@ -11,7 +11,14 @@ public class ForLoop {
 //			System.out.println("Server is down "+i);
 //		}
 		
-		for (int i = 1; i <= 10; i++) {
+		// 1 to 10 print
+//		for (int i = 1; i <= 10; i++) {
+//			System.out.println(i);
+//		}
+		
+		// print 10 to 1
+		
+		for (int i = 10; i >=1; i--) {
 			System.out.println(i);
 		}
 
