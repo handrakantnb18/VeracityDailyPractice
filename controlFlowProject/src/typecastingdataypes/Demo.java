71de5f7efe039419c5362307c1a351e78d7c9262
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-10 12:42:41 +0530

    implicit typecasting

---
+++ controlFlowProject/src/typecastingdataypes/Demo.java
@@ -0,0 +1,24 @@
+package typecastingdataypes;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		// implicit typecasting
+		byte b = 100;
+		
+		short s = b;
+		
+		int i = s;
+		
+		long l = i;
+		
+		float f = l;
+		
+		double d= f;
+		
+		// char , boolean - special typecasting need
+		
+		System.out.println(f);
+	}
+}
