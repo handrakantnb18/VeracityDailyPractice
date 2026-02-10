2c7f186416d7f6a5d4e03096b31e86a6e3dfdb55
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-10 22:39:48 +0530

    9. Write a program to check if salary is greater than 25,000

---
+++ javafullstack/src/com/javapractice/day1/assignment1/SalaryGreater.java
@@ -0,0 +1,24 @@
+package com.javapractice.day1.assignment1;
+
+import java.util.Scanner;
+
+public class SalaryGreater {
+
+	public static void main(String[] args) {
+		
+		// 9. Write a program to check if salary is greater than 25,000
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Check Salary ");
+		
+		double salaryCheck = sc.nextDouble();
+		
+		if (salaryCheck >= 25000) {
+			System.out.println("Salary is greater. ");
+		}
+		
+		else {
+			System.out.println("Salary is smaller");
+		}
+	}
+}
