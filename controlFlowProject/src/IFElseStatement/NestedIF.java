1f836ed6041bb37df3cd13d2cc4ab90d0cefe8be
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-10 11:46:58 +0530

    check the If condition all true the work

---
+++ controlFlowProject/src/IFElseStatement/NestedIF.java
@@ -0,0 +1,26 @@
+package IFElseStatement;
+
+public class NestedIF {
+	public static void main(String[] args) {
+
+		
+		// check the If condition all true the work
+		// if first condition false then donot run the program
+		// all are dipend upon the each other
+		
+		// program 1
+		if (true) {
+			System.out.println("If 1");
+			if (true) {
+				System.out.println("If 2");
+				if (true) {
+					System.out.println("If 3");
+				}
+			}
+		}
+		else {
+		System.out.println("Outer if");
+		}
+	}
+
+}
