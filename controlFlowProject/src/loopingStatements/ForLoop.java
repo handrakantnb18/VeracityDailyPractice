19a468a1782693021e50150d9726f17e18420384
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-10 14:19:51 +0530

    checked Even numbers

--- controlFlowProject/src/loopingStatements/ForLoop.java
+++ controlFlowProject/src/loopingStatements/ForLoop.java
@@ -10,18 +10,25 @@ public class ForLoop {
 //		for (int i = 1; i < 10; i++) {
 //			System.out.println("Server is down "+i);
 //		}
-		
+
 		// 1 to 10 print
 //		for (int i = 1; i <= 10; i++) {
 //			System.out.println(i);
 //		}
-		
-		// print 10 to 1
-		
-		for (int i = 10; i >=1; i--) {
-			System.out.println(i);
-		}
 
+		// print 10 to 1
+
+//		for (int i = 10; i >=1; i--) {
+//			System.out.println(i);
+//		}
+
+		// checked Even numbers
+		for (int i = 1; i <= 10; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+
+		}
 	}
 
 }
