bbf37cb2ba0f8e08f5662fad6176b35d48058ee6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-10 13:28:03 +0530

    explicite typecasting check char = byte, int, short, long, float, double,

--- controlFlowProject/src/typecastingdataypes/ExplicitDemo.java
+++ controlFlowProject/src/typecastingdataypes/ExplicitDemo.java
@@ -7,27 +7,35 @@ public class ExplicitDemo {
 		
 		// char = byte, int, short, long, float, double, 
 		
-		//char c = 66;
+		char c = 66;
+		
+		System.out.println(c);
+		
+		char cc = (char)65.45;
+		
+		System.out.println(cc);
+		
+		// char x = (char)true; // can not cast char to boolean
 		
 		// char = int, short, byte, long, float, double
 		
-		char c = 'A';  // 2 byte
-		
-		byte b = (byte)c;  // 1 byte  Explicit
-		
-		short s = (short)c;  // 2 byte  Explicit
-		
-		int i = c; // 4 byte  Implicit
-		
-		long l = (long)c; // 8 byte  Implicit
-		
-		float f = c;  // implicit
-		
-		double d = c; // Implicit
-		
-		
-		System.out.println(d);
-		
+//		char c = 'A';  // 2 byte
+//		
+//		byte b = (byte)c;  // 1 byte  Explicit
+//		
+//		short s = (short)c;  // 2 byte  Explicit
+//		
+//		int i = c; // 4 byte  Implicit
+//		
+//		long l = (long)c; // 8 byte  Implicit
+//		
+//		float f = c;  // implicit
+//		
+//		double d = c; // Implicit
+//		
+//		
+//		System.out.println(d);
+//		
 	}
 
 }
