4a161ebf7d9c6b7ad4c289e1fe00647fd8e4cf66
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-10 11:57:45 +0530

    check balance, Deposite, Withdraw, Check Balance, Exit

---
+++ controlFlowProject/src/IFElseStatement/NestedIfUser.java
@@ -0,0 +1,37 @@
+package IFElseStatement;
+
+import java.util.Scanner;
+
+public class NestedIfUser {
+
+	public static void main(String[] args) {
+
+		// check balance, Deposite, Withdraw, Check Balance, Exit
+
+		double bankBalance = 1000.00;
+
+		System.out.println("1. Deposite");
+		System.out.println("2. Withdraw");
+		System.out.println("3.Check Balance");
+		System.out.println("4.Exit");
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter your choice 1 - 4 ");
+
+		int choice = sc.nextInt();
+
+		if (choice == 1) {
+			System.out.println("Enter the amount for deposite. ");
+
+			double amount = sc.nextDouble();
+
+			bankBalance += amount;
+			
+			System.out.println("Deposite success : "+bankBalance);
+
+		}
+
+	}
+
+}
