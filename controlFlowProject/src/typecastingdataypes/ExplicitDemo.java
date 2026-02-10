c2de1be5157ffd883aa787b89e4c985826269817
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-10 13:25:08 +0530

    convert into Explicit char = int, short, byte, long, float, double

---
+++ controlFlowProject/src/typecastingdataypes/ExplicitDemo.java
@@ -0,0 +1,33 @@
+package typecastingdataypes;
+
+public class ExplicitDemo {
+	
+	public static void main(String[] args) {
+		
+		
+		// char = byte, int, short, long, float, double, 
+		
+		//char c = 66;
+		
+		// char = int, short, byte, long, float, double
+		
+		char c = 'A';  // 2 byte
+		
+		byte b = (byte)c;  // 1 byte  Explicit
+		
+		short s = (short)c;  // 2 byte  Explicit
+		
+		int i = c; // 4 byte  Implicit
+		
+		long l = (long)c; // 8 byte  Implicit
+		
+		float f = c;  // implicit
+		
+		double d = c; // Implicit
+		
+		
+		System.out.println(d);
+		
+	}
+
+}
