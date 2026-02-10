b88c7b3713bdcb58fdbff4e00fa660f271e76a1e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-10 12:21:28 +0530

    check balance, Deposite, Withdraw, Check Balance, Exit 2. Withdraw amount 3. check the bank balance 4. invalid credintial

--- controlFlowProject/src/IFElseStatement/NestedIfUser.java
+++ controlFlowProject/src/IFElseStatement/NestedIfUser.java
@@ -21,6 +21,7 @@ public class NestedIfUser {
 
 		int choice = sc.nextInt();
 
+		// 1 Deposite amount
 		if (choice == 1) {
 			System.out.println("Enter the amount for deposite. ");
 
@@ -33,23 +34,48 @@ public class NestedIfUser {
 			} else {
 				System.out.println("Invalid amount.");
 			}
-		} else if (choice == 2) {
+		}
+		// 2. Withdraw amount
+
+		else if (choice == 2) {
 			System.out.println("Enter the withdraw amount ");
 
 			double amount = sc.nextDouble();
 
 			if (amount > 0) {
-				bankBalance -= amount;
+				if (amount <= bankBalance) {
+					bankBalance -= amount;
 
-				System.out.println("Withdraw success.");
+					System.out.println("Withdraw success." + bankBalance);
+
+				}
+
+				else {
+					System.out.println("invalid funds");
+				}
 
 			} else {
 				System.out.println("Invalid amount.");
 			}
 
 		}
-		
 
+		// 3. check the bank balance
+
+		else if (choice == 3) {
+			System.out.println("Your bank balance is = " + bankBalance);
+		}
+
+		// exit from the app
+		else if (choice == 4) {
+			System.out.println("Thank you for using the app");
+		}
+
+		// 4. invalid credintial
+
+		else {
+			System.out.println("Invalid choice please enter choice between 1 - 4.");
+		}
 	}
 
 }
