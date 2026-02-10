5aa79bbf145d680f06c4960bd4b22f9fd4f721e4
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-10 22:03:37 +0530

    Write a program to check if a number is odd.

--- javafullstack/src/com/javapractice/day1/assignment1/OddNumber.java
+++ javafullstack/src/com/javapractice/day1/assignment1/OddNumber.java
@@ -6,27 +6,25 @@ public class OddNumber {
 
 	public static void main(String[] args) {
 		// Write a program to check if a number is odd.
-		
+
 		// 1. print odd number
 		int num1 = 25;
-		
+
 		if (num1 % 2 == 0) {
-			System.out.println("Number is Odd. "+num1);
+			System.out.println("Number is Odd. " + num1);
+		} else {
+			System.out.println("Number is Even " + num1);
 		}
-		else {
-			System.out.println("Number is Even "+num1);
-		}
-		
+
 		// 2. print odd number using Scanner class
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter number : ");
 		int num2 = sc.nextInt();
-		
+
 		if (num2 % 2 == 0) {
 			System.out.println("Even Number.");
-		}
-		else {
+		} else {
 			System.out.println("Odd Number.");
 		}
 	}
