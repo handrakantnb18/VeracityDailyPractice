8dcb6b41ab6c7e2fd32319d4827fd850c9230daf
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-10 12:50:51 +0530

    Explicit typecasting developer responsible

--- controlFlowProject/src/typecastingdataypes/Demo.java
+++ controlFlowProject/src/typecastingdataypes/Demo.java
@@ -1,24 +1,51 @@
 package typecastingdataypes;
 
+import javax.sound.midi.Soundbank;
+
 public class Demo {
 
 	public static void main(String[] args) {
 		
-		// implicit typecasting
-		byte b = 100;
+//		// implicit typecasting
+//		byte b = 100;
+//		
+//		short s = b;
+//		
+//		int i = s;
+//		
+//		long l = i;
+//		
+//		float f = l;
+//		
+//		double d= f;
+//		
+//		// char , boolean - special typecasting need
+//		
+//		System.out.println(f);
 		
-		short s = b;
 		
-		int i = s;
+		// Explicit typecasting developer responsible 
 		
-		long l = i;
+		double balance = 450000.56;  // convert into string
 		
-		float f = l;
+		float f = (float)balance;
 		
-		double d= f;
+		long l = (long)f;
 		
-		// char , boolean - special typecasting need
+		int i = (int)l;
 		
-		System.out.println(f);
+		short s = (short)i;
+		
+		byte b= (byte)s;
+		
+		System.out.println(f); // no data loss
+		
+		System.out.println(l); // data lose
+		
+		System.out.println(i); // data is loss
+		
+		System.out.println(s); // data is loss;
+		
+		System.out.println(b); // data loss
 	}
 }
