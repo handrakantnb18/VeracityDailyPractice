5e10257d3681a0bf0eb9636c063ff4c913d71be8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-10 14:11:37 +0530

    print 1 to 10 numbers

--- controlFlowProject/src/loopingStatements/ForLoop.java
+++ controlFlowProject/src/loopingStatements/ForLoop.java
@@ -3,15 +3,18 @@ package loopingStatements;
 import java.util.Iterator;
 
 public class ForLoop {
-	
+
 	public static void main(String[] args) {
-		  // 1 lakh line code in = 1 line  = looping statement
+		// 1 lakh line code in = 1 line = looping statement
+
+//		for (int i = 1; i < 10; i++) {
+//			System.out.println("Server is down "+i);
+//		}
 		
-		for (int i = 1; i < 1000; i++) {
-			System.out.println("Server is down");
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
 		}
-		
-		
+
 	}
 
 }
