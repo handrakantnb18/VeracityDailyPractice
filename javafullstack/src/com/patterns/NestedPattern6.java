0cfdaff9682d64c0a81756c2aecda14a9080f1e8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-11 20:33:17 +0530

    Date Added

---
+++ javafullstack/src/com/patterns/NestedPattern6.java
@@ -0,0 +1,19 @@
+package com.patterns;
+// 11/02/2026
+
+public class NestedPattern6 {
+
+	public static void main(String[] args) {
+		
+		// print in ROW 1 to 20 numbers
+		
+		for (int i = 1; i <= 5; i++)
+		{
+			for (int j = 1; j <= 5; j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+}
