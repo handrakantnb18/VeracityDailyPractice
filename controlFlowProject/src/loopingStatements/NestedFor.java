be0e2de10658a32b630481cc1b79e77dda0de571
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-11 11:56:01 +0530

    print Odd numbers $ sign 1 - 3 - 5- 7

--- controlFlowProject/src/loopingStatements/NestedFor.java
+++ controlFlowProject/src/loopingStatements/NestedFor.java
@@ -68,19 +68,16 @@ public class NestedFor {
 //			System.out.println();
 //		}
 		
-		// print Odd numbers $ sign
+		// print Odd numbers $ sign 1 - 3 - 5- 7
 		
 		for (int i = 1; i <= 5; i++) {
-
 			for (int j = 1; j <= 5; j++) {
-				
 				if (j % 2 != 0) {
 					System.out.print("$ ");
 				}
 				else {
 					System.out.print("* ");
-				}				
-				
+				}
 			}
 			System.out.println();
 		}
