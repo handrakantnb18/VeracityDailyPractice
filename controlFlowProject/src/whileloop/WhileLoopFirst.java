b54bf0ab907c54e307b83e5b25c7c56465f80808
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-11 22:13:07 +0530

    Multiplication of the enter any number

--- controlFlowProject/src/whileloop/WhileLoopFirst.java
+++ controlFlowProject/src/whileloop/WhileLoopFirst.java
@@ -1,15 +1,38 @@
 package whileloop;
 
+import java.util.Scanner;
+
 public class WhileLoopFirst {
 
 	public static void main(String[] args) {
 		
 		// while loop used for the reuse the condition
-		int i = 1;
+//		int i = 1;
+//		
+//		while(i <= 10) {
+//			System.out.println("Ram");
+//			i++;
+//		}
 		
-		while(i <= 10) {
-			System.out.println("Ram");
-			i++;
+		// Multiplication of the enter any number
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any Number : ");
+		
+		int num = sc.nextInt();
+		
+		if (num < 0) {
+			System.out.println("Invalid number..");
 		}
+		else {
+			int i = 1;
+			while (i <= 10) {
+				System.out.println(num * i);
+				i++;
+			}
+		}
+		
+		// 
+		
 	}
 }
