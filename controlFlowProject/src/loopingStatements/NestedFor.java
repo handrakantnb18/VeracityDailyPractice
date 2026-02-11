28f7f07f18a1653b0efa4e01037af8db6d42ebe2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-11 11:28:59 +0530

    Nested for loop first patter program

---
+++ controlFlowProject/src/loopingStatements/NestedFor.java
@@ -0,0 +1,20 @@
+package loopingStatements;
+
+import java.util.Iterator;
+
+public class NestedFor {
+
+	public static void main(String[] args) {
+
+		// Nested for loop first patter program
+
+		for (int i = 1; i <= 5; i++) {
+
+			for (int j = 1; j <= 5; j++) {
+				System.out.print("A | ");
+			}
+			System.out.println();
+		}
+	}
+
+}
