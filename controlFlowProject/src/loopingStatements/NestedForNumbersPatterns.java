2e634157efb0befd7eebbc43c574f3a955d9ae51
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-11 12:13:49 +0530

    checking the condition print 3rd line * and &

--- controlFlowProject/src/loopingStatements/NestedForNumbersPatterns.java
+++ controlFlowProject/src/loopingStatements/NestedForNumbersPatterns.java
@@ -8,9 +8,28 @@ public class NestedForNumbersPatterns {
 		// numbers patterns
 		// print 1 to 5 all line with debugging
 		
+//		for (int i = 1; i <=5; i++) {
+//			for(int j = 1; j <= 5; j++) {
+//				System.out.print(j+ " ");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		// checking the condition print 3rd line * and &
+		
 		for (int i = 1; i <=5; i++) {
 			for(int j = 1; j <= 5; j++) {
-				System.out.print(j+ " ");
+				if( i == 3) {
+					System.out.print("* ");
+				}
+				else if(j == 3) {
+					System.out.print("& ");
+				}
+				else {
+					System.out.print(j+ " ");
+				}
+				
 			}
 			System.out.println();
 		}
