a3045bc88df432e21c96fa67c5def709d6abcc7f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-11 13:35:50 +0530

    print * and four last column print capital 'A' A * * * A * * * * * * * * * * * * * * * A * * * A

---
+++ javafullstack/src/com/patterns/NestedPattern2.java
@@ -0,0 +1,31 @@
+package com.patterns;
+
+public class NestedPattern2 {
+	
+	public static void main(String[] args) {
+		
+		// print * and four last column print capital 'A'
+		
+		for (int i = 1; i <= 5; i++)
+		{
+			for (int j = 1; j <= 5; j++)
+			{
+				if(i == 1 && j != 2 && j != 3 && j != 4)
+				{
+					System.out.print("A ");
+				}
+				else if (i == 5 && j != 2 && j != 3 && j != 4)
+				{
+					System.out.print("A ");
+				}
+				else
+				{
+					System.out.print("* ");
+				}
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
