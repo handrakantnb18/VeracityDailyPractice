d1537b1c32b49009ebfed3ec6d655dc90b2cfa84
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-11 22:05:12 +0530

    while loop used for the reuse the condition

---
+++ controlFlowProject/src/whileloop/WhileLoopFirst.java
@@ -0,0 +1,15 @@
+package whileloop;
+
+public class WhileLoopFirst {
+
+	public static void main(String[] args) {
+		
+		// while loop used for the reuse the condition
+		int i = 1;
+		
+		while(i <= 10) {
+			System.out.println("Ram");
+			i++;
+		}
+	}
+}
