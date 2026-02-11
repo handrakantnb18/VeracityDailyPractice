643a4d7fdee7867e2e0728e73127e09f353d728f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-11 11:51:53 +0530

    print even number $ sign

--- controlFlowProject/src/loopingStatements/NestedFor.java
+++ controlFlowProject/src/loopingStatements/NestedFor.java
@@ -29,22 +29,40 @@ public class NestedFor {
 		// using if statement change the ROW or COLUMN diffrent conditons
 				
 		// change the conditon for 1 and 3 and 5 number line
+//		for (int i = 1; i <= 5; i++) {
+//
+//			for (int j = 1; j <= 5; j++) {
+//				
+//				if (j == 3) {
+//					System.out.print("0 ");
+//				}
+//				else if (j == 5) {
+//					System.out.print("# ");
+//				}
+//				else if (j == 1) {
+//					System.out.print("@ ");
+//				}
+//				else {
+//					System.out.print("* ");
+//				}
+//				
+//			}
+//			System.out.println();
+//		}
+		
+		
+		// print even number $ sign
+		
 		for (int i = 1; i <= 5; i++) {
 
 			for (int j = 1; j <= 5; j++) {
 				
-				if (j == 3) {
-					System.out.print("0 ");
-				}
-				else if (j == 5) {
-					System.out.print("# ");
-				}
-				else if (j == 1) {
-					System.out.print("@ ");
+				if (j % 2 == 0) {
+					System.out.print("$ ");
 				}
 				else {
 					System.out.print("* ");
-				}
+				}				
 				
 			}
 			System.out.println();
