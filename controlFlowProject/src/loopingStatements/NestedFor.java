26c5e6c44448d83005fa8a0b758be4c84868d59d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-11 11:43:19 +0530

    change the conditon for 1 and 3 and 5 number line

--- controlFlowProject/src/loopingStatements/NestedFor.java
+++ controlFlowProject/src/loopingStatements/NestedFor.java
@@ -28,12 +28,19 @@ public class NestedFor {
 		
 		// using if statement change the ROW or COLUMN diffrent conditons
 				
+		// change the conditon for 1 and 3 and 5 number line
 		for (int i = 1; i <= 5; i++) {
 
 			for (int j = 1; j <= 5; j++) {
 				
-				if (i == 3) {
+				if (j == 3) {
 					System.out.print("0 ");
+				}
+				else if (j == 5) {
+					System.out.print("# ");
+				}
+				else if (j == 1) {
+					System.out.print("@ ");
 				}
 				else {
 					System.out.print("* ");
