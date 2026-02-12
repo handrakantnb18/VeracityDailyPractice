6fc88046cf38921504a0fb60b2bea592a6f0e05b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-12 22:51:59 +0530

    Print even numbers from 1 to 20

---
+++ practice/src/com/Day1/practice/PrintEvenNumbers.java
@@ -0,0 +1,16 @@
+package com.Day1.practice;
+
+public class PrintEvenNumbers {
+
+	public static void main(String[] args) {
+		
+		// Print even numbers from 1 to 20
+		
+		int i =2;
+		do {
+			System.out.println(i);
+			i += 2;
+		}while(i <= 20);
+		
+	}
+}
