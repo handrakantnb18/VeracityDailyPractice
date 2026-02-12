983bf9498c347c4481b210a60ce8a272e960e705
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-12 22:13:30 +0530

    Print numbers from 1 to 5

---
+++ practice/src/com/Day1/practice/PRintNumber.java
@@ -0,0 +1,17 @@
+package com.Day1.practice;
+
+public class PRintNumber {
+
+	public static void main(String[] args) {
+		
+		// Print numbers from 1 to 5 
+		
+		int i = 1;
+		
+		do {
+			System.out.println(i);
+			i++;
+		}
+		while(i <= 5);
+	}
+}
