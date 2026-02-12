affcff04a423d5d53d2946256c725ffb0d2b9ed9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-12 22:15:46 +0530

    Print numbers from 10 to 1

---
+++ practice/src/com/Day1/practice/PrintNumber2.java
@@ -0,0 +1,17 @@
+package com.Day1.practice;
+
+public class PrintNumber2 {
+
+	public static void main(String[] args) {
+		
+		// Print numbers from 10 to 1
+		
+		int i = 10;
+		
+		do {
+			System.out.println(i);
+			i--;
+		}
+		while(i >= 1);
+	}
+}
