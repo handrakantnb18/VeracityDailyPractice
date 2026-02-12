c6d1752ad2c0a2bb2a8d7cf4b5dcf8244747b1b4
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-12 22:53:33 +0530

    Print Odd numbers from 1 to 25

---
+++ practice/src/com/Day1/practice/PrintOddNumbers.java
@@ -0,0 +1,16 @@
+package com.Day1.practice;
+
+public class PrintOddNumbers {
+
+	public static void main(String[] args) {
+		
+		// Print Odd numbers from 1 to 25
+		
+		int i = 1;
+		
+		do {
+			System.out.println(i);
+			i += 2;
+		}while(i <= 25);
+	}
+}
