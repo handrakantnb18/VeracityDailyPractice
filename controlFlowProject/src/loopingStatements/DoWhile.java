0f01ba5c711447cd66ee975f5702b50dde410323
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-13 12:04:12 +0530

    Do While loop  check condition ins infinites

--- controlFlowProject/src/loopingStatements/DoWhile.java
+++ controlFlowProject/src/loopingStatements/DoWhile.java
@@ -5,9 +5,18 @@ public class DoWhile {
 	public static void main(String[] args) {
 		// entry control point
 		
-		for (int i=1; i<=10; i++) // ENTRY Point
-		{
+//		for (int i=1; i<=10; i++) // ENTRY Point
+//		{
+//			System.out.println("Hello");
+//		}
+		
+		// Do while loop is Exit control loop
+		
+		
+		int i = 1;
+		
+		do {
 			System.out.println("Hello");
-		}
+		}while(i <= 10);
 	}
 }
