aea6d8a16929f63b7eef2b9cdcc65331b20b7aaa
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-13 13:43:50 +0530

    Movie Booking ticket all complated

--- controlFlowProject/src/loopingStatements/OnlineTicketBookGlobal.java
+++ controlFlowProject/src/loopingStatements/OnlineTicketBookGlobal.java
@@ -10,6 +10,16 @@ public class OnlineTicketBookGlobal {
 		
 		// Array, Collection, Object == we can store multiple book  movie
 
+		// Student management system
+		//1. Add Student = id, name, marks - -45, 245, address - 20, age > 10, CollegeName  1. student only - > // validate = 
+		//2. Appy the Grade > 90 - A, 80 > B
+		//3. Display Student Data - sysout (id, name
+		//4. Exit
+		
+		// validate
+		
+		// do while loop
+		
 		String movie = " ";
 		int qty = 0;
 		double ticketPrice = 0;
