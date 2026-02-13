76c4ae82cb5f962d2c2efa8497e6dc2978828092
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-13 14:18:39 +0530

    Break => break is used to end the loop, for, while, do in java

---
+++ controlFlowProject/src/loopingStatements/BreakDemo.java
@@ -0,0 +1,27 @@
+package loopingStatements;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		
+		// Break => break is used to end the loop, for, while, do in java
+		// you can use loop only
+		
+		for(int i=1; i<=10; i++)
+		{
+			
+			if (i == 4)
+			{
+				break;
+			}
+			System.out.println(i);
+			
+		}
+		
+		
+		
+		// continue statement
+		
+		
+	}
+}
