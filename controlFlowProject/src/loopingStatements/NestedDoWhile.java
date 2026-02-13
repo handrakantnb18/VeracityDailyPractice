d0acd98837f3e77cf756f7b5f3f25bb3e01a5db2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-13 13:51:57 +0530

    print the 2 to 10 multiplication

--- controlFlowProject/src/loopingStatements/NestedDoWhile.java
+++ controlFlowProject/src/loopingStatements/NestedDoWhile.java
@@ -3,22 +3,42 @@ package loopingStatements;
 public class NestedDoWhile {
 
 	public static void main(String[] args) {
-		
+
 		// Nested Do While Check the condition
-		
+
+		// check condition only
+
+//		int i = 1;
+//		
+//		do {
+//			
+//			System.out.println("Chandr");
+//			i++;
+//			
+//			int j = 1;
+//			do {
+//				System.out.println("Bhosale");
+//				j++;
+//			}while (j <= 5);
+//			
+//		}while (i <= 5);
+
+		// print the 2 to 10 multiplication
+
 		int i = 1;
-		
+
 		do {
+
 			
-			System.out.println("Chandr");
-			i++;
-			
-			int j = 1;
+			int j = 2;
 			do {
-				System.out.println("Bhosale");
+				System.out.print(i * j + "\t");
 				j++;
-			}while (j <= 5);
-			
-		}while (i <= 5);
+			} while (j <= 10);
+
+			System.out.println();
+			i++;
+
+		} while (i <= 10);
 	}
 }
