375fada4fc808ed1f45a89a03fb6aa1cc9d81598
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-13 11:54:21 +0530

    entry control point

---
+++ controlFlowProject/src/loopingStatements/DoWhile.java
@@ -0,0 +1,13 @@
+package loopingStatements;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		// entry control point
+		
+		for (int i=1; i<=10; i++) // ENTRY Point
+		{
+			System.out.println("Hello");
+		}
+	}
+}
