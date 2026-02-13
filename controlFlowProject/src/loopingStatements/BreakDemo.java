8d09de81ea61578d9607b44bbd40a55909ea8024
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-13 14:23:31 +0530

    continue statement

--- controlFlowProject/src/loopingStatements/BreakDemo.java
+++ controlFlowProject/src/loopingStatements/BreakDemo.java
@@ -3,10 +3,10 @@ package loopingStatements;
 public class BreakDemo {
 
 	public static void main(String[] args) {
-		
+
 		// Break => break is used to end the loop, for, while, do in java
 		// you can use loop only
-		
+
 //		for(int i=1; i<=10; i++)
 //		{
 //			
@@ -18,22 +18,18 @@ public class BreakDemo {
 //			
 //		}
 //		
-		
-		
+
 		// continue statement
 		// continue is used to skip the condition / iteration and go for next iteration
-		
-		for(int i=1; i<=10; i++)
-		{
-			
-			if (i == 4 || i == 6)
-			{
-				continue;  // END
+
+		for (int i = 1; i <= 10; i++) {
+
+			if (i == 4 || i == 6) {
+				continue; // END
 			}
 			System.out.println(i);
-			
+
 		}
-		
-		
+
 	}
 }
