38da224676cbd2e5231addff228ed65677aefd90
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-19 18:15:57 +0530

    Create a calculator using switch case for +, -, *, / operations.

---
+++ javaAssignmentQuestionsTwo/src/switchCaseOneToTen/CalculaterTwo.java
@@ -0,0 +1,23 @@
+package switchCaseOneToTen;
+
+import java.util.Scanner;
+
+public class CalculaterTwo {
+
+	public static void main(String[] args) {
+		// Create a calculator using switch case for +, -, *, / operations.
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num1 = sc.nextInt();	
+		System.out.println("Enter number One : ");
+		
+		int num2  =sc.nextInt();
+		System.out.println("Enter Number Two : ");
+		
+		switch (num1)
+		{
+		
+		}
+	}
+}
