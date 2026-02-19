d21611f191bed74b7681c7e4877c19ab770b47cb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-19 16:03:18 +0530

    Write a Java program to print day name using day number (1–7) using switch case.

---
+++ javaAssignmentQuestionsTwo/src/switchCaseOneToTen/PrintDayName.java
@@ -0,0 +1,11 @@
+package switchCaseOneToTen;
+
+public class PrintDayName {
+	
+	public static void main(String[] args) {
+		// 1. Write a Java program to print day name using day number (1–7) using switch case.
+		
+		
+	}
+
+}
