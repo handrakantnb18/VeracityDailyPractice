ff3cb473e97cf8e57727b3002b6be5f55bdab455
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-19 20:35:37 +0530

    Create a calculator using switch case for +, -, *, / operations.

--- javaAssignmentQuestionsTwo/src/switchCaseOneToTen/CalculaterTwo.java
+++ javaAssignmentQuestionsTwo/src/switchCaseOneToTen/CalculaterTwo.java
@@ -6,18 +6,45 @@ public class CalculaterTwo {
 
 	public static void main(String[] args) {
 		// Create a calculator using switch case for +, -, *, / operations.
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		int num1 = sc.nextInt();	
+
 		System.out.println("Enter number One : ");
-		
-		int num2  =sc.nextInt();
+		double num1 = sc.nextDouble();
+
 		System.out.println("Enter Number Two : ");
+		double num2 = sc.nextDouble();
+
+		System.out.println("Operator is : ");
+		char operator = sc.next().charAt(0);
 		
-		switch (num1)
-		{
-		
+
+		double res;
+
+		switch (operator) {
+
+		case '+':
+			res = num1 + num2;
+			System.out.println("Addition is : " + res);
+			break;
+
+		case '-':
+			res = num1 - num2;
+			System.out.println("Substraction is : " + res);
+			break;
+
+		case '/':
+			res = num1 / num2;
+			System.out.println("Substraction is : " + res);
+			break;
+
+		case '*':
+			res = num1 * num2;
+			System.out.println("Multiplication is : " + res);
+
+		default:
+			System.out.println("Please Enter this Operators +, /, * ,-");
+			break;
 		}
 	}
 }
