cdb0eef110ea33983ade8a479e013a37bcda79ec
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-20 21:28:09 +0530

    19. Print squares of numbers from 1 to 10 using for loop.

---
+++ javaAssignmentQuestionsTwo/src/ForLoopElevenToTwenty/PrintSquaresNumbers.java
@@ -0,0 +1,14 @@
+package ForLoopElevenToTwenty;
+
+public class PrintSquaresNumbers {
+
+	public static void main(String[] args) {
+		
+		// 19. Print squares of numbers from 1 to 10 using for loop.
+
+		for (int i = 1; i <= 10; i++)
+		{
+			System.out.println("Squares numbers of : "+ i +" is "+(i * i));
+		}
+	}
+}
