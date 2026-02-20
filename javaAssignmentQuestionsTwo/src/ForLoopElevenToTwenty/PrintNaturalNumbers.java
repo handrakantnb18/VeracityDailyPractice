20055e38f0f29acf0c3e688dd71676806b29fa38
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-20 21:03:28 +0530

    18. Print first 10 natural numbers using for loop.

---
+++ javaAssignmentQuestionsTwo/src/ForLoopElevenToTwenty/PrintNaturalNumbers.java
@@ -0,0 +1,15 @@
+package ForLoopElevenToTwenty;
+
+public class PrintNaturalNumbers {
+
+	public static void main(String[] args) {
+		
+		// 18. Print first 10 natural numbers using for loop.
+
+		for (int i = 1; i <= 10; i++)
+		{
+			System.out.println(i);
+		}
+		
+	}
+}
