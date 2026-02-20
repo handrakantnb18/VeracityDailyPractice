fa0a9af0f9cee0f68cb0fee2285b43b95b6a053e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-20 17:39:41 +0530

    Print all even numbers between 1 and 50 using for loop.

---
+++ javaAssignmentQuestionsTwo/src/ForLoopElevenToTwenty/EvenOddNumbers.java
@@ -0,0 +1,21 @@
+package ForLoopElevenToTwenty;
+
+public class EvenOddNumbers {
+
+	public static void main(String[] args) {
+
+		// 13. Print all even numbers between 1 and 50 using for loop.
+
+		for (int i = 1; i <= 50; i++) {
+			if (i % 2 == 0) {
+				System.out.print("Even Number : " + i);
+
+			}
+			else
+			{
+				System.out.print("Odd number  : "+i);
+			}
+			System.out.println();
+		}
+	}
+}
