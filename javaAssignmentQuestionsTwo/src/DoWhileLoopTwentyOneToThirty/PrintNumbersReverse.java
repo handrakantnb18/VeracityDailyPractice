e43be6bb687a721f8083b3ca945fe52bf29ee4eb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-20 21:44:28 +0530

    22. Print numbers from 50 to 1 using while loop.

---
+++ javaAssignmentQuestionsTwo/src/DoWhileLoopTwentyOneToThirty/PrintNumbersReverse.java
@@ -0,0 +1,17 @@
+package DoWhileLoopTwentyOneToThirty;
+
+public class PrintNumbersReverse {
+
+	public static void main(String[] args) {
+		
+		// 22. Print numbers from 50 to 1 using while loop.
+
+		int i = 50;
+		
+		while (i >= 1)
+		{
+			System.out.println(i);
+			i--;
+		}
+	}
+}
