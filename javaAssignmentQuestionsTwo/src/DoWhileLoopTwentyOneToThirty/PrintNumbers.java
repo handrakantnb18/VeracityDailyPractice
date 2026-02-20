3b21eb6a5c7921ad48c8501da7ee1f57fd14ccce
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-20 21:39:41 +0530

    21. Print numbers from 1 to 50 using while loop.

---
+++ javaAssignmentQuestionsTwo/src/DoWhileLoopTwentyOneToThirty/PrintNumbers.java
@@ -0,0 +1,28 @@
+package DoWhileLoopTwentyOneToThirty;
+
+public class PrintNumbers {
+
+	public static void main(String[] args) {
+		
+		// 21. Print numbers from 1 to 50 using while loop.
+
+		int i = 1;
+		
+		while(i <= 50)
+		{
+			System.out.print(" | "+i);
+			i++;
+		}
+		
+		// do while
+//		
+//		do
+//		{
+//			for (int i = 1; i <= 50; i++)
+//			{
+//				System.out.println(i);
+//			}
+//			
+//		}while(true);
+	}
+}
