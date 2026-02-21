a44cdfd8f2b3991e6bf45389a296b2aca7f4e9aa
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-21 22:58:40 +0530

    35. Print multiplication table using do-while loop.

--- javaAssignmentQuestionsTwo/src/DoWhileLoopThirtyOneToFourty/PrintMultiplication.java
+++ javaAssignmentQuestionsTwo/src/DoWhileLoopThirtyOneToFourty/PrintMultiplication.java
@@ -1,21 +1,34 @@
 package DoWhileLoopThirtyOneToFourty;
 
+import java.util.Scanner;
+
 public class PrintMultiplication {
 
 	public static void main(String[] args) {
 		
 		// 35. Print multiplication table using do-while loop.
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number : ");
 		
-		// simple way to print numbers
-		
-		int num = 5;
-		int i = 1;
+		int a = 9;
+		int b = 1;
 		
 		do
 		{
-			System.out.println("Sum of "+num+" X "+i+" of : "+(num * i));
-			i++;
-		}while(i <= 10);
+			System.out.println("Multiplication of "+a+" X "+b+" = "+(a * b));
+			b++;
+		}while(b <= 10);
+		
+		// simple way to print numbers
+		
+//		int num = 5;
+//		int i = 1;
+//		
+//		do
+//		{
+//			System.out.println("Sum of "+num+" X "+i+" of : "+(num * i));
+//			i++;
+//		}while(i <= 10);
 	}
 }
