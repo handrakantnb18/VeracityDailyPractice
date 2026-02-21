21917c0b0e3b9ff20a8ccea39236152c6aaa55e6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-21 22:54:16 +0530

    simple way to print numbers

---
+++ javaAssignmentQuestionsTwo/src/DoWhileLoopThirtyOneToFourty/PrintMultiplication.java
@@ -0,0 +1,21 @@
+package DoWhileLoopThirtyOneToFourty;
+
+public class PrintMultiplication {
+
+	public static void main(String[] args) {
+		
+		// 35. Print multiplication table using do-while loop.
+
+		
+		// simple way to print numbers
+		
+		int num = 5;
+		int i = 1;
+		
+		do
+		{
+			System.out.println("Sum of "+num+" X "+i+" of : "+(num * i));
+			i++;
+		}while(i <= 10);
+	}
+}
