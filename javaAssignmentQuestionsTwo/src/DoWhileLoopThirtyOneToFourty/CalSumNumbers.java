c9763eb3621ba639ec1414e52fab6cf4579a3e92
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-21 22:45:36 +0530

    34. Calculate sum of numbers from 1 to 50 using do-while loop.

---
+++ javaAssignmentQuestionsTwo/src/DoWhileLoopThirtyOneToFourty/CalSumNumbers.java
@@ -0,0 +1,34 @@
+package DoWhileLoopThirtyOneToFourty;
+
+import java.util.Scanner;
+
+public class CalSumNumbers {
+	public static void main(String[] args) {
+
+		// 34. Calculate sum of numbers from 1 to 50 using do-while loop.
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number : ");
+		
+		int a = 1;
+		int b = 0;
+		
+		do
+		{
+			a = a +b;
+			b++;
+		}while(b <= 20);
+		System.out.println("Sum : ");
+		// simple way
+
+//		int i = 1;
+//		int num = 0;
+//		
+//		do
+//		{
+//			num = num + i;
+//			i++;
+//		}while(i <= 50);
+//		System.out.println("Sum : "+num);
+	}
+
+}
