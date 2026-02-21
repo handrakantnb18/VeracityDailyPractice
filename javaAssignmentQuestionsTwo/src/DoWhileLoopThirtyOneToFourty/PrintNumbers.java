ac89f81113d0e486df975107620019380e8f2e34
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-21 22:23:43 +0530

    Print numbers from 1 to 10 using do-while loop.

---
+++ javaAssignmentQuestionsTwo/src/DoWhileLoopThirtyOneToFourty/PrintNumbers.java
@@ -0,0 +1,18 @@
+package DoWhileLoopThirtyOneToFourty;
+
+public class PrintNumbers {
+
+	public static void main(String[] args) {
+
+		// 31. Print numbers from 1 to 10 using do-while loop.
+
+		int num = 1;
+
+		do {
+
+			System.out.println(num);
+			num++;
+		} while (num <= 10);
+
+	}
+}
