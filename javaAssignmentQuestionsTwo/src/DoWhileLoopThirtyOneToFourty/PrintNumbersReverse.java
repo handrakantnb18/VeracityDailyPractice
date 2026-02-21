ecaedd9c4693c4ae57083f11abda2b9c91c301b7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-21 22:30:07 +0530

    simple ways Print numbers from 10 to 1 using

--- javaAssignmentQuestionsTwo/src/DoWhileLoopThirtyOneToFourty/PrintNumbersReverse.java
+++ javaAssignmentQuestionsTwo/src/DoWhileLoopThirtyOneToFourty/PrintNumbersReverse.java
@@ -8,15 +8,26 @@ public class PrintNumbersReverse {
 		
 		// 32. Print numbers from 10 to 1 using do-while loop.
 
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter number : ");
-		
-		int num = sc.nextInt();
-		
-		do
-		{
-			System.out.println(num);
-			num--;
-		}while(num >= 1);
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter number : ");
+//		
+//		int num = sc.nextInt();
+//		
+//		do
+//		{
+//			System.out.println(num);
+//			num--;
+//		}while(num >= 1);
+//	}
+	
+	// simple ways
+	
+	int i = 10;
+	
+	do
+	{
+		System.out.println(i);
+		i--;
+	}while(i >= 1);
 	}
 }
