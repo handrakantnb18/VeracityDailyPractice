8156b93a3f0d259527006a518c51ba2b240294d0
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-22 15:33:40 +0530

    14. Write a program to check if a number is zero or non-zero.

--- javafullstack/src/com/javapractice/day2/assignment2/CheckZeroOrNonZero.java
+++ javafullstack/src/com/javapractice/day2/assignment2/CheckZeroOrNonZero.java
@@ -1,20 +1,35 @@
 package com.javapractice.day2.assignment2;
 
+import java.util.Scanner;
+
 public class CheckZeroOrNonZero {
 
 	public static void main(String[] args) {
 		
 		// 14. Write a program to check if a number is zero or non-zero.
 
-		int num = 10;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number : ");
+		
+		int num = sc.nextInt();
 		
 		if(num <= 0)
 		{
-			System.out.println("Number is Zero.");
+			System.out.println("Number is zero");
 		}
 		else
 		{
-			System.out.println("Number is non-zero.");
+			System.out.println("Number is non-zero");
 		}
+//		int num = 10;
+//		
+//		if(num <= 0)
+//		{
+//			System.out.println("Number is Zero.");
+//		}
+//		else
+//		{
+//			System.out.println("Number is non-zero.");
+//		}
 	}
 }
