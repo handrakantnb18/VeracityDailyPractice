7710be95855cd6e9746f54472129ef31c3b89e95
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-22 14:28:21 +0530

    50. Print diamond star pattern using nested loops.  uuper pyramid

---
+++ javaAssignmentQuestionsTwo/src/NestedLoopFourtyOneToFifty/PrintDiamondStarPattern.java
@@ -0,0 +1,46 @@
+package NestedLoopFourtyOneToFifty;
+
+import java.util.Scanner;
+
+public class PrintDiamondStarPattern {
+
+	public static void main(String[] args) {
+		
+		// 50. Print diamond star pattern using nested loops.
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number : ");
+		
+		int a = sc.nextInt();
+		
+		for (int i =1; i <= a; i++)
+		{
+			
+			for (int j = 1; j <= a - i; j++)
+			{
+				
+				for (int k = 1; k <= (2 * i - 1); k++)
+				{
+					System.out.print("* ");
+				}
+				
+				System.out.println();
+			}
+			
+//			for (int i = a - 1; i >= 1; i--)
+//			{
+//				
+//				for (int j = 1; j <= a - i; j++)
+//				{
+//					System.out.print(" ");
+//				}
+//				
+//				for (int k = 1; k <= (2 * i - 1); k++)
+//				{
+//					System.out.print("* ");
+//				}
+//				System.out.println();
+//			}
+		}
+	}
+}
