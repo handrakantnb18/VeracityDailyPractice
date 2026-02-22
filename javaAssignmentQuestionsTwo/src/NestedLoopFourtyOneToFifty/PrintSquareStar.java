2b8d8aaef5744191336888464fe3d496124e0368
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-22 11:35:10 +0530

    41. Print square star pattern using nested loops.

--- javaAssignmentQuestionsTwo/src/NestedLoopFourtyOneToFifty/PrintSquareStar.java
+++ javaAssignmentQuestionsTwo/src/NestedLoopFourtyOneToFifty/PrintSquareStar.java
@@ -1,22 +1,37 @@
 package NestedLoopFourtyOneToFifty;
 
+import java.util.Scanner;
+
 public class PrintSquareStar {
 
 	public static void main(String[] args) {
 		
 		// 41. Print square star pattern using nested loops.
 
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Enter number : ");
 		
-		// simple Square pattern in nested loop
+		int num = sc.nextInt();
 		
-		for (int i = 1; i <= 5; i++)
+		for(int i = 1; i <= num; i++)
 		{
-			for (int j = 1; j <= 5; j++)
+			for (int j = 1; j <= num; j++)
 			{
-				System.out.print("*");
+				System.out.print("* ");
 			}
 			System.out.println();
 		}
+		
+		// simple Square pattern in nested loop
+		
+//		for (int i = 1; i <= 5; i++)
+//		{
+//			for (int j = 1; j <= 5; j++)
+//			{
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
 	}
 }
