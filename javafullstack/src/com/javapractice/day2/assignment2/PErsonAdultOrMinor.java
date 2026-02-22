e0ed0f6468c16e43f7d2d31cfcb1bafb65dea61c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-22 15:06:50 +0530

    13. Write a program to check if a person is adult or minor. using Scanner class

--- javafullstack/src/com/javapractice/day2/assignment2/PErsonAdultOrMinor.java
+++ javafullstack/src/com/javapractice/day2/assignment2/PErsonAdultOrMinor.java
@@ -1,11 +1,26 @@
 package com.javapractice.day2.assignment2;
 
+import java.util.Scanner;
+
 public class PErsonAdultOrMinor {
 
 	public static void main(String[] args) {
 		// 13. Write a program to check if a person is adult or minor.
 
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number : ");
+		
+		int age = sc.nextInt();
+		
+		if(age >= 18)
+		{
+			System.out.println("Person is a Adult.");
+		}
+		else
+		{
+			System.out.println("Person is Minor");
+		}
 		
 //		int age = 18;
 //		
