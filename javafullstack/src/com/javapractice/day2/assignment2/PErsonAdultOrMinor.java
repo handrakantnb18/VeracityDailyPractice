37a8b55b3a227c4c30fd9d0e8d39490f3d1251d1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-22 15:04:21 +0530

    13. Write a program to check if a person is adult or minor. simple ways to check

---
+++ javafullstack/src/com/javapractice/day2/assignment2/PErsonAdultOrMinor.java
@@ -0,0 +1,20 @@
+package com.javapractice.day2.assignment2;
+
+public class PErsonAdultOrMinor {
+
+	public static void main(String[] args) {
+		// 13. Write a program to check if a person is adult or minor.
+
+		
+		
+//		int age = 18;
+//		
+//		if(age >= 18)
+//		{
+//			System.out.println("Person is Adult.");
+//		}else
+//		{
+//			System.out.println("Person is Minor.");
+//		}
+	}
+}
