5a456755ac5d6ce059d3b2b35ce9a5c4be6ce396
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-22 14:58:11 +0530

    12. Write a program to check if a number is positive or negative. check the using Scanner class

--- javafullstack/src/com/javapractice/day2/assignment2/CheckNumberPositiveNegative.java
+++ javafullstack/src/com/javapractice/day2/assignment2/CheckNumberPositiveNegative.java
@@ -1,12 +1,20 @@
 package com.javapractice.day2.assignment2;
 
+import java.util.Scanner;
+
 public class CheckNumberPositiveNegative {
 
 	public static void main(String[] args) {
 		
 		// 12. Write a program to check if a number is positive or negative.
 
-		int num = 10;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter number : ");
+		
+		int num = sc.nextInt();
+		
+		// int num = 10;
 		
 		if (num >= 0)
 		{
