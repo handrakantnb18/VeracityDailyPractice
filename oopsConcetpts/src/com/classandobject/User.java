d68ae0d2d3a1f0d5bb459588522c1784b7eb9e06
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-24 22:34:55 +0530

    static variable, instance variable, local variable created

---
+++ oopsConcetpts/src/com/classandobject/User.java
@@ -0,0 +1,21 @@
+package com.classandobject;
+
+public class User {
+
+	// create static variable
+	
+	int id; // instance variable
+	
+	static String name = "Pune"; // static variable
+	
+	// method
+	void add (int a) // local variable
+	{
+		
+		int x;  // local variable
+	}
+	public static void main(String[] args) {
+		
+		// we will create variable
+	}
+}
