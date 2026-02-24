d37cece505e356546b88d998d99040245562f74b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-24 20:48:11 +0530

    we will create multiple objects in single class

---
+++ oopsConcetpts/src/com/classandobject/Student.java
@@ -0,0 +1,23 @@
+package com.classandobject;
+
+public class Student {
+
+	// variables 
+	
+	int id;
+	String name;
+	
+	public static void main(String[] args) {
+		
+		// create object  
+		// we will create multiple objects in single class
+		
+		Student s1 = new Student();
+		
+		Student s2 = new Student();
+		
+		Student s3 = new Student();
+		
+		
+	}
+}
