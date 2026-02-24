d63fa3b67bfcd998142cc15b89628b99b72d803c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-24 20:22:14 +0530

    employee class for the methods and create objects and class for simple aways

---
+++ oopsConcetpts/src/com/oopsconceptsFirst/Employee.java
@@ -0,0 +1,41 @@
+package com.oopsconceptsFirst;
+
+public class Employee {
+
+	
+//	int id = 101;
+//	String name = "Vijay";
+//	
+	
+	int id;
+	String name;
+	
+	public static void main(String[] args) {
+		
+		Employee employee = new Employee();
+		
+		employee.id = 101;
+		employee.name = "Vijay";
+		
+		System.out.println(employee.id);
+		System.out.println(employee.name);
+		
+		System.out.println(employee.id+ ", "+employee.name);
+		
+		
+//		Employee e1 = new Employee();
+//		
+//		Employee e2 = new Employee();
+//		
+//		Employee e3 = new Employee();
+//		
+//		Employee e4 = new Employee();
+//		
+//		Employee e5 = new Employee();
+//		
+//		Employee e6 = new Employee();
+//		
+//		Employee e7 = new Employee();
+//		
+	}
+}
