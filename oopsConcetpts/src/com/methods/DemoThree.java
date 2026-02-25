643c44e6538ca53786693c807fc3ee938ef63059
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-25 16:41:55 +0530

    3) method  with return type and no parameter

---
+++ oopsConcetpts/src/com/methods/DemoThree.java
@@ -0,0 +1,33 @@
+package com.methods;
+
+public class DemoThree {
+
+	// 3) method with return type and no parameter
+	// return type is depends on the value we want to return from the method
+
+	int add() {
+		return 100;
+	}
+	
+	String get()
+	{
+		return "Chandrakant";
+	}
+	
+	int take()
+	{
+		return 100 * 100;
+	}
+	
+	int x()
+	{
+		return 100 * 100 / 20;
+	}
+
+	public static void main(String[] args) {
+		DemoThree d3 = new DemoThree();
+
+		int i = d3.x();
+		System.out.println(i);
+	}
+}
