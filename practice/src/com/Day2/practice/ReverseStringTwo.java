4276feab538c4e798e9af58282050617000822d3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-26 23:03:23 +0530

    Reverse String using scanner class

---
+++ practice/src/com/Day2/practice/ReverseStringTwo.java
@@ -0,0 +1,25 @@
+package com.Day2.practice;
+
+import java.util.Scanner;
+
+public class ReverseStringTwo {
+
+	public static void main(String[] args) {
+		// Reverse String
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter String : ");
+		
+		String str = sc.nextLine();
+		
+		String reverse = " ";
+		
+		for(int i = str.length() - 1; i >= 0; i--)
+		{
+			reverse = reverse + str.charAt(i);
+			
+		}
+		
+		System.out.println("Reverse string  : "+reverse);
+	}
+}
