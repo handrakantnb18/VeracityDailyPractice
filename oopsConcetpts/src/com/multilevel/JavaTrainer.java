32b77234bf34e8abae89c0a178af268a4ed08691
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-27 10:35:27 +0530

    JavaTrainer class created Constructor and method to make secure and only 5 line of code generated

--- oopsConcetpts/src/com/multilevel/JavaTrainer.java
+++ oopsConcetpts/src/com/multilevel/JavaTrainer.java
@@ -4,23 +4,46 @@ public class JavaTrainer extends Trainer{
 
 	String porject;
 	
+	void setJavaTrainerData(String project)
+	{
+		this.porject = project;
+	}
+	
+	void showJavaTrainer()
+	{
+		System.out.println("Project     : "+porject);
+	}
+	
 	public static void main(String[] args) {
 		
 		JavaTrainer j = new JavaTrainer();
 		
-		// using fields to create simple access
-		j.name = "Chandraaknt";
-		j.age= 25;
-		j.address = "Pune";
-		j.trainerId = 1011;
-		j.subject = "java";
-		j.porject = "Java Project";
+		j.setPersonData("Chandraknt", 20, "Pune");
+		j.showPerson();
 		
-		System.out.println(j.name);
-		System.out.println(j.age);
-		System.out.println(j.address);
-		System.out.println(j.trainerId);
-		System.out.println(j.subject);
-		System.out.println(j.porject);
+		j.setTrainerData(11111, "Chandra B");
+		j.showTrainer();
+		
+		j.setJavaTrainerData("Java Project");
+		j.showJavaTrainer();
+		
+		// using fields to create simple access
+//		j.name = "Chandraaknt";
+//		j.age= 25;
+//		j.address = "Pune";
+		
+//		j.trainerId = 1011;
+//		j.subject = "java";
+		
+		// j.porject = "Java Project";
+		
+//		System.out.println(j.name);
+//		System.out.println(j.age);
+//		System.out.println(j.address);
+//		
+//		System.out.println(j.trainerId);
+//		System.out.println(j.subject);
+		
+		// System.out.println(j.porject);
 	}
 }
