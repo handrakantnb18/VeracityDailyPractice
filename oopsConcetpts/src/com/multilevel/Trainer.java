b8cff66e829bdd39cbcdf28a1d7e7bd2a8fe3073
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-27 10:34:30 +0530

    Trainer class created Constructor and method to make secure data data minimized

--- oopsConcetpts/src/com/multilevel/Trainer.java
+++ oopsConcetpts/src/com/multilevel/Trainer.java
@@ -4,4 +4,16 @@ public class Trainer extends Person{
 
 	int trainerId;
 	String subject;
+	
+	void setTrainerData(int trinerid, String subject)
+	{
+		this.trainerId = trinerid;
+		this.subject = subject;
+	}
+	
+	void showTrainer()
+	{
+		System.out.println("Trainer id  : "+trainerId);
+		System.out.println("Subject     : "+subject);
+	}
 }
