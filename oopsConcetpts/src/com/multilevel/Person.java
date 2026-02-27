1e2cb4392bb187fd2370071ab028c7355d873575
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-27 10:33:49 +0530

    Person class created Constructor and method to make secure data

--- oopsConcetpts/src/com/multilevel/Person.java
+++ oopsConcetpts/src/com/multilevel/Person.java
@@ -7,4 +7,18 @@ public class Person {
 	int age;
 	String address;
 	
+	void setPersonData(String name, int age, String address)
+	{
+		this.name = name;
+		this.age = age;
+		this.address = address;
+	}
+	
+	void showPerson()
+	{
+		System.out.println("Name        : "+name);
+		System.out.println("Age         : "+age);
+		System.out.println("Address     : "+address);
+	}
+	
 }
