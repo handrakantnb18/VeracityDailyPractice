f07d4734e4ded9c50019bd62514b6956aa2f29bd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-27 09:33:49 +0530

    Inheritance Employee first program, simple taking varables

---
+++ oopsConcetpts/src/com/inheritance/Employee.java
@@ -0,0 +1,17 @@
+package com.inheritance;
+
+public class Employee {
+
+	int employeeId;
+	String employeeName;
+	
+	void work()
+	{
+		System.out.println("Employee id");
+	}
+	
+	void eat()
+	{
+		System.out.println("Employee name.");
+	}
+}
