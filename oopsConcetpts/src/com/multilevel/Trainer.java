8bbfd395a88fd01d16dbd6dfe175a63653ae6703
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-27 11:28:14 +0530

    Trainer constructor call and super keyword to access super class variables and this keyword to access current property one line of code done

--- oopsConcetpts/src/com/multilevel/Trainer.java
+++ oopsConcetpts/src/com/multilevel/Trainer.java
@@ -5,12 +5,21 @@ public class Trainer extends Person{
 	int trainerId;
 	String subject;
 	
-	void setTrainerData(int trinerid, String subject, String name, int age, String address)
+	Trainer(int trainerId, String subject, String name, int age, String address)
 	{
-		this.trainerId = trinerid;
+		super(name, age, address);
+		this.trainerId = trainerId;
 		this.subject = subject;
-		super.setPersonData(name, age, address);
+		//System.out.println("Trainer 1 param constructor.");
 	}
+	
+	
+//	void setTrainerData(int trinerid, String subject, String name, int age, String address)
+//	{
+//		this.trainerId = trinerid;
+//		this.subject = subject;
+//		super.setPersonData(name, age, address);
+//	}
 	
 	void showTrainer()
 	{
