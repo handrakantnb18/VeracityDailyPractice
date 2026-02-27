6255f2822590b3fad61aa70c7a902e19b197b9ed
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-27 10:51:59 +0530

    JavaTrainer method used to minimize complex data and 3 line of code done

--- oopsConcetpts/src/com/multilevel/Trainer.java
+++ oopsConcetpts/src/com/multilevel/Trainer.java
@@ -5,14 +5,16 @@ public class Trainer extends Person{
 	int trainerId;
 	String subject;
 	
-	void setTrainerData(int trinerid, String subject)
+	void setTrainerData(int trinerid, String subject, String name, int age, String address)
 	{
 		this.trainerId = trinerid;
 		this.subject = subject;
+		super.setPersonData(name, age, address);
 	}
 	
 	void showTrainer()
 	{
+		showPerson();
 		System.out.println("Trainer id  : "+trainerId);
 		System.out.println("Subject     : "+subject);
 	}
