0028abff958b758ec82d576912bb5d35d08f8906
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-02 12:19:56 +0530

    Sequence of parameter is different

--- oopsConcetpts/src/com/compiletimepolymorphism/Student.java
+++ oopsConcetpts/src/com/compiletimepolymorphism/Student.java
@@ -24,16 +24,27 @@ public class Student {
 //	
 	// type of parameter different
 	
-	void study(int i)
+//	void study(int i)
+//	{
+//		System.out.println("Type of parameter int");
+//	}
+//	
+//	void study(String a)
+//	{
+//		System.out.println("Type of parameter String");
+//	}
+	
+	// Sequence of parameter is different
+	
+	void study(int i, String a)
 	{
-		System.out.println("Type of parameter int");
+		System.out.println("Sequence of parameter is diffrent");
 	}
 	
-	void study(String a)
+	void study(String a, int i)
 	{
-		System.out.println("Type of parameter String");
+		System.out.println("Sequence changed");
 	}
-	
 	
 	public static void main(String[] args) {
 		Student s1 = new Student();
@@ -41,8 +52,11 @@ public class Student {
 //		s1.study(18);
 //		s1.study(13, 06);
 		
-		s1.study(18);
-		s1.study("ABC");
-    
+//		s1.study(18);
+//		s1.study("ABC");
+//    
+		
+		s1.study(18, "Chandraknt");
+		s1.study("Bhosale", 06);
 	}
 }
