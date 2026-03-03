99d8b0200e6bfb3ac6f843f0d4c02f30feee869d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-03 12:01:28 +0530

    access by  using the child class object

--- oopsConcetpts/src/com/B/User.java
+++ oopsConcetpts/src/com/B/User.java
@@ -2,21 +2,27 @@ package com.B;
 
 import com.A.Student;
 
-public class User {
+public class User extends Student {
 	
 	// object access by using import package
 	
 	public static void main(String[] args) {
 		
-		Student s1 = new Student();
+		// Student s1 = new Student();
 		
 		// accessed different class and package variables
 		
-		System.out.println(s1.id);
+		// System.out.println(s1.id);
 		
 		// access different package public keyword
 		
-		s1.m1();
+		// s1.m1();
+		
+		
+		 // access by  using the child class object
+		
+		User u = new User();
+		System.out.println(u.id);
 	}
 
 }
