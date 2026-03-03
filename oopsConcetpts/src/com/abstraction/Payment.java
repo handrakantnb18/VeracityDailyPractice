88e1f5dc321fe3e1e17ffdf3c753cba2529d3ccd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-03 14:21:41 +0530

    abstract method pay

---
+++ oopsConcetpts/src/com/abstraction/Payment.java
@@ -0,0 +1,17 @@
+package com.abstraction;
+
+public abstract class Payment {
+
+	// abstract method pay
+	
+	public Payment()
+	{
+		System.out.println("Payment constructor");
+	}
+	// abstract void payAmount();
+	
+	abstract void pay(double amount);
+	
+	abstract void UPIPayment1(double amount);
+	
+}
