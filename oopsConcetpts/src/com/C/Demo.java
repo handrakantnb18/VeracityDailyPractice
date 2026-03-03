ddf02b57ae81a17ec691884b4341f37c55ffba96
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-03 12:16:08 +0530

    final variable can not access other variables

---
+++ oopsConcetpts/src/com/C/Demo.java
@@ -0,0 +1,17 @@
+package com.C;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		// final variable can not access other variables
+		
+		final int id = 111;
+		
+		System.out.println(id);
+		
+		// id = 123;
+		
+		// System.out.println(id);
+	}
+}
