6553c6dd50661a0e150573e2066f8e6b533a02e0
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-03 11:25:38 +0530

    object access by same package but different class

---
+++ oopsConcetpts/src/com/A/Teacher.java
@@ -0,0 +1,12 @@
+package com.A;
+
+public class Teacher {
+
+	// object access by same package but different class
+	
+	public static void main(String[] args) {
+		
+		Student s2 = new Student();
+		
+	}
+}
