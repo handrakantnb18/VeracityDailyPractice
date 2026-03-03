0fcad94a2c1cef670ff571db3ed9dbcc333254b1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-03 12:31:04 +0530

    final method not override

---
+++ oopsConcetpts/src/com/C/Test.java
@@ -0,0 +1,23 @@
+package com.C;
+
+public class Test extends Demo {
+
+	
+	// final method not override
+	
+	// @Override
+//	void house()
+//	{
+//		
+//	}
+//	
+	
+	void work()
+	{
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
