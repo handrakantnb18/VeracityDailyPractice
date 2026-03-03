291d8261df87aa05c6367460a25a568c55e18903
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-03 11:24:09 +0530

    public access modifiers we can use with class, variable, method, constructor

---
+++ oopsConcetpts/src/com/A/Student.java
@@ -0,0 +1,15 @@
+package com.A;
+
+public class Student {
+
+	// public access modifiers we can use with class, variable, method, constructor
+	
+	public int id = 18;
+	
+	
+	public static void main(String[] args) {
+		
+		Student s1 = new Student();
+		System.out.println(s1.id);
+	}
+}
