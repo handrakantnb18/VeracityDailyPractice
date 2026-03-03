5f60ca79b29163161b87ec07e7e145cd764daa1f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-03 11:30:56 +0530

    access by id different packages

--- oopsConcetpts/src/com/A/Student.java
+++ oopsConcetpts/src/com/A/Student.java
@@ -4,12 +4,15 @@ public class Student {
 
 	// public access modifiers we can use with class, variable, method, constructor
 	
+	// access by id different packages
+	
 	public int id = 18;
 	
 	
 	public static void main(String[] args) {
 		
 		Student s1 = new Student();
+		
 		System.out.println(s1.id);
 	}
 }
