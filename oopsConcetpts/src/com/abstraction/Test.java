7acef715dfeebc436191f3e7e9cf6e99a28cdd18
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-03 14:07:47 +0530

    access abstract parent class to child class

--- oopsConcetpts/src/com/abstraction/Test.java
+++ oopsConcetpts/src/com/abstraction/Test.java
@@ -5,6 +5,8 @@ public class Test extends Student {
 	
 	// abstract methods are override
 	
+	// access abstract parent class to child class
+	
 	@Override
 	void login(int x)
 	{
@@ -29,6 +31,11 @@ public class Test extends Student {
 		
 	}
 	
+	@Override
+	void chat(String sender, String reciever)
+	{
+		
+	}
 	public static void main(String[] args) {
 		Test t = new Test();
 		t.login(0);
