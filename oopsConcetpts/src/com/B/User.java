ed1ec05e4ed8c7e7e6996ff34ae29d1f22b6d02a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-03 11:26:43 +0530

    object access by using import package

---
+++ oopsConcetpts/src/com/B/User.java
@@ -0,0 +1,14 @@
+package com.B;
+
+import com.A.Student;
+
+public class User {
+	
+	// object access by using import package
+	
+	public static void main(String[] args) {
+		
+		Student s3 = new Student();
+	}
+
+}
