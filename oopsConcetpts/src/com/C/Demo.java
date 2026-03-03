59aa31f4ed30e78844871eaef3b773ca5e1280af
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-03 12:22:20 +0530

    final can not change once declared, class not accessible, variable not override other class and package

--- oopsConcetpts/src/com/C/Demo.java
+++ oopsConcetpts/src/com/C/Demo.java
@@ -3,8 +3,11 @@ package com.C;
 public class Demo {
 
 	// final instance variable need to assign value for variable
+	// final can not change once declared, class not accessible, variable not override other class and package
+	
 	
 	final String name = "Chandrakant";
+	
 	public static void main(String[] args) {
 		
 		// final variable can not access other variables
