01cfdaaf92ca14741e02d7069c32b022d4af457f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-04 22:13:51 +0530

    different object created acces by Student class

--- oopsConcetpts/src/com/interface2/Demo.java
+++ oopsConcetpts/src/com/interface2/Demo.java
@@ -1,5 +1,6 @@
 package com.interface2;
 
+import com.A.Student;
 import com.interface1.Test;
 
 public class Demo implements Test {
@@ -12,20 +13,32 @@ public class Demo implements Test {
 	
 	// abstract method super class access to child class
 	
+	@Override
 	public void login()
 	{
 		System.out.println("Login");
 	}
 	
+	@Override
 	public void register()
 	{
-		System.out.println("Login");
+		System.out.println("register");
 	}
 	
 	public static void main(String[] args) {
-		Demo d = new Demo();
-		d.login();
-		d.register();
+//		Demo d = new Demo();
+//		d.login();
+//		d.register();
+//		
+//		System.out.println(Demo.name);
+//		System.out.println(Demo.id);
+		
+		Test test = new Demo();
+		test.login();
+		test.register();
+		
+		// Test test1 = new Student();
+		
 	}
 	
 }
