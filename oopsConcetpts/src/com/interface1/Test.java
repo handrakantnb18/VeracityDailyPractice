484970f3d655e0a95105c0de9e8337d443868990
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-04 20:39:57 +0530

    instance method not accept

--- oopsConcetpts/src/com/interface1/Test.java
+++ oopsConcetpts/src/com/interface1/Test.java
@@ -2,6 +2,10 @@ package com.interface1;
 
 public interface Test {
 
+	public static final int id = 10; // public  static   final
+	
+	String name = "chandr"; // static
+	 
 	// constructor not accept in interface
 //	
 //	Test()
@@ -9,5 +13,17 @@ public interface Test {
 //		
 //	}
 	
+	// instance method not accept
+	
+//	void m1()
+//	{
+//		
+//	}
+//	
+	
+	
+	public static void main(String[] args) {
+		System.out.println(Test.id);
+	}
 	
 }
