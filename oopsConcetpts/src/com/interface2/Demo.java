971196f7bc35a9e88932aad86b1a303d6f773d8a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-04 21:55:48 +0530

    abstract method super class access to child class

--- oopsConcetpts/src/com/interface2/Demo.java
+++ oopsConcetpts/src/com/interface2/Demo.java
@@ -2,11 +2,30 @@ package com.interface2;
 
 import com.interface1.Test;
 
-public interface Demo {
+public class Demo implements Test {
 
 	// different package and child class access
 	
-	public static void main(String[] args) {
-		System.out.println(Test.id);
+//	public static void main(String[] args) {
+//		System.out.println(Test.id);
+//	}
+	
+	// abstract method super class access to child class
+	
+	public void login()
+	{
+		System.out.println("Login");
 	}
+	
+	public void register()
+	{
+		System.out.println("Login");
+	}
+	
+	public static void main(String[] args) {
+		Demo d = new Demo();
+		d.login();
+		d.register();
+	}
+	
 }
