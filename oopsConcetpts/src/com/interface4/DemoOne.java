1f2674bc5a96ed25f0c2b780a8c518a467bb2bd8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-05 20:53:57 +0530

    Parent class java 9 version static default private method created

---
+++ oopsConcetpts/src/com/interface4/DemoOne.java
@@ -0,0 +1,29 @@
+package com.interface4;
+
+public interface DemoOne {
+
+	static void m1()
+	{
+		System.out.println("Static method");
+		
+	}
+	
+	private static void m2()
+	{
+		m1();
+		System.out.println("private method");
+		
+	}
+	
+	// default method
+	public default void m3()
+	{
+		m2();
+		System.out.println("Default method");
+		
+	}
+	
+	public static void main(String[] args) {
+		// DemoOne.m3();
+	}
+}
