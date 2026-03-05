97c63dfcea571c5e1341ed620aabe16f283389cb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-05 14:09:33 +0530

    protected modifiers method, variables and chile class

---
+++ oopsConcetpts/src/com/accessmodifiers/User.java
@@ -0,0 +1,42 @@
+package com.accessmodifiers;
+
+public class User {
+
+	// default modifiers
+//	int id = 101;
+//	String name= "Chandrakant";
+//	
+	
+	// public modifiers
+	
+//	public int id = 101;
+//	public String name= "Chandrakant";
+//	
+	
+	// private modifiers
+//	private int id = 101;
+//	private String name= "Chandrakant";
+//	
+	// abstract void add();
+	
+	// protected modifiers
+	
+	protected int id = 101;
+	protected String name= "Chandrakant";
+	
+	
+	 protected void display()
+	{
+		 int age = 25;
+	}
+	
+	 protected User()
+	 {
+		 System.out.println("Chandraknt");
+	 }
+	 
+	
+	public static void main(String[] args) {
+		
+	}
+}
