03bb3c6c3578424acec0d565d1c75401e56ed87e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-05 14:18:04 +0530

    created variables and access by using object

---
+++ oopsConcetpts/src/com/accessmodifires1/Student.java
@@ -0,0 +1,16 @@
+package com.accessmodifires1;
+
+public class Student {
+
+	// created variables and access by using object
+	
+	int id = 01;
+	String name = "chandraknt bhosale";
+	
+	public static void main(String[] args) {
+		Student s = new Student();
+		
+		System.out.println(s.id);
+		System.out.println(s.name);
+	}
+}
