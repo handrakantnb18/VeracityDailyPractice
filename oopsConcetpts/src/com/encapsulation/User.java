fbd28175281362dd402d2dbccabd294b5998fac5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-05 22:32:33 +0530

    private child class access getter and setter methods

---
+++ oopsConcetpts/src/com/encapsulation/User.java
@@ -0,0 +1,35 @@
+package com.encapsulation;
+
+public class User{
+
+	// every class default value is Null
+	
+	private String username;
+	private String password;
+	
+	
+	// getter method for username
+	public String getUsername()
+	{
+		return username;
+	}
+	
+	// setter method username
+	
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+	
+	// getter method for password
+	public String getPassword()
+	{
+		return password;
+	}
+	
+	// setter method password
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+}
