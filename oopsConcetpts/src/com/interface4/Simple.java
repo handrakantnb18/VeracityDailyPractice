90829502e7bf449f9d7577f6932942c77dd9fcf1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-05 21:09:33 +0530

    Simple is Parent class access methods to single parent object object

--- oopsConcetpts/src/com/interface4/Simple.java
+++ oopsConcetpts/src/com/interface4/Simple.java
@@ -1,12 +1,35 @@
 package com.interface4;
 
-public class Simple implements DemoOne {
+public class Simple implements DemoOne, DemoTwo , DemoThree
+{
 
 	public static void main(String[] args) {
 		Simple s = new Simple();
 
 		s.m3();
-		
+		s.work();
+		s.login();
+		s.fun();
+
 		// DemoOne.m1();
 	}
+
+	@Override
+	public void login()
+	{
+		System.out.println("Login");
+	}
+	
+	@Override
+	public void fun() 
+	{
+		System.out.println("Run");
+	}
+	
+	@Override
+	public void work()
+	{
+		System.out.println("Work");
+	}
+
 }
