5ad5159da1b756c481d5d401c6c66ba6c73ac17e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-05 20:54:59 +0530

    child class java object call only ome method m3 9 version static default private method created

---
+++ oopsConcetpts/src/com/interface4/Simple.java
@@ -0,0 +1,12 @@
+package com.interface4;
+
+public class Simple implements DemoOne {
+
+	public static void main(String[] args) {
+		Simple s = new Simple();
+
+		s.m3();
+		
+		// DemoOne.m1();
+	}
+}
