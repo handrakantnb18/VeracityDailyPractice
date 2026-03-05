2c8e4bfd563e5adb7ad6a8f3087c7356e3fd905d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-05 14:00:12 +0530

    implements the Demo class to access abstract method

---
+++ oopsConcetpts/src/com/accessmodifiers/Simple.java
@@ -0,0 +1,24 @@
+package com.accessmodifiers;
+
+public class Simple  {
+	
+	void login()
+	{
+		
+	}
+	
+	void register()
+	{
+		
+	}
+	
+	void payment()
+	{
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
