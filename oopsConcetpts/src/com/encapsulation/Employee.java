6887c71600a2f3620ca5fafd0bb625ef592ca38e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-05 22:36:44 +0530

    Employee parent class private properties

---
+++ oopsConcetpts/src/com/encapsulation/Employee.java
@@ -0,0 +1,18 @@
+package com.encapsulation;
+
+public class Employee {
+
+	private String name;
+	
+	private String email;
+	
+	private String gender;
+	
+	private String dep;
+	
+	private String salary;
+	
+	
+	
+	
+}
