36ca938adcd2afda36fe1e77028fa92ce9407896
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-05 16:44:11 +0530

    Test is a parent class and abstract method

---
+++ oopsConcetpts/src/com/interface3/Test.java
@@ -0,0 +1,13 @@
+package com.interface3;
+
+public interface Test {
+	
+	public static final int id = 101;
+	
+	String name = "chandrakant";
+	
+	void login();
+	
+	public abstract void register();
+
+}
