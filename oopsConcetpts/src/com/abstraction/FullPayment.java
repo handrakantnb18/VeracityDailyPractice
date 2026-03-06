47bd473ef4890ae58a86610afa3eeba5800ad093
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-06 11:43:27 +0530

    upipaymet make show output overriding method and child class

--- oopsConcetpts/src/com/abstraction/FullPayment.java
+++ oopsConcetpts/src/com/abstraction/FullPayment.java
@@ -8,19 +8,21 @@ public class FullPayment extends Payment {
 	@Override
 	void pay(double amount)
 	{
-		
+		System.out.println("Pay amount is : "+amount);
 	}
 	
 	@Override
 	void UPIPayment1(double amount)
 	{
-		
+		System.out.println("UPIPayment is : "+amount);
 	}
 	
 	public static void main(String[] args) {
 		
 		FullPayment p = new FullPayment();
 		p.pay(100);
+		
 		p.UPIPayment1(5000);
+		
 	}
 }
