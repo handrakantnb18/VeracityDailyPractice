4bee23a21cf9131f441630aed2a49ba5f1fceba5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-06 10:45:35 +0530

    getter and setter method for data security private variable

---
+++ oopsConcetpts/src/com/encps/Employee.java
@@ -0,0 +1,61 @@
+package com.encps;
+
+public class Employee {
+
+	
+	private String name;
+	
+	private String address;
+	
+	private String mobileNo;
+	
+	private String dep;
+	
+	
+	// getter and setter method
+	
+	// getter method
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	// setter method
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getAddress()
+	{
+		return address;
+	}
+	
+	public void setAddress(String address) 
+	{
+		this.address = address;
+	}
+	
+	public String getMobileno()
+	{
+		return mobileNo;
+	}
+	
+	public void setMobileno(String mobileNo)
+	{
+		this.mobileNo = mobileNo;
+	}
+	
+	public String getDep()
+	{
+		return dep;
+	}
+	
+	public void setDep(String dep)
+	{
+		this.dep = dep;
+	}
+	
+}
