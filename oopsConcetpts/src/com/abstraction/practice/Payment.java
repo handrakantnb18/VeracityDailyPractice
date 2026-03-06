f391f45af53f02f0c697c91ba9a428263b599ffa
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-06 13:24:16 +0530

    Payment is super class to achive abstract methods and methods

---
+++ oopsConcetpts/src/com/abstraction/practice/Payment.java
@@ -0,0 +1,40 @@
+package com.abstraction.practice;
+
+abstract class Payment {
+
+	//int id = 10;
+	
+	//static String collageName = "Abc";
+	
+	void payment()
+	{
+		System.out.println("Abstract method");
+	}
+	
+	abstract void Login(double amount);
+	
+	abstract void register(double amount);
+	
+	abstract void work(String name);
+	
+	// constructor
+//	Payment()
+//	{
+//		
+//	}
+//	
+	// instance method
+	
+//	void add()
+//	{
+//		
+//	}
+	
+	
+	// can not create object abstact class
+	
+//	public static void main(String[] args) {
+//		Payment p = new Payment();
+//		
+//	}
+}
