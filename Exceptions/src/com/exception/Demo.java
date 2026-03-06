912270883e23ab9b3dbb4b349b88efc3a74be37e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-06 14:18:00 +0530

    Dont divide by zero problem solved by arithmatic exception

--- Exceptions/src/com/exception/Demo.java
+++ Exceptions/src/com/exception/Demo.java
@@ -4,12 +4,44 @@ public class Demo {
 
 	
 	public static void main(String[] args) {
+		// Exception handling
 		
 		System.out.println("Hello, my name is chandraknt.");
 		System.out.println(10+10);
-		System.out.println(10/0);
+		
+		try
+		{
+		System.out.println(10/0);  // problem -- solution
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("Dont divide by zero ");
+			e.getMessage();
+		}
 		System.out.println("I am from pune");
 		System.out.println("I am student");
 		System.out.println("I like play cricket");
+		
+		System.out.println("I am from pune");
+		System.out.println("I am student");
+		System.out.println("I like play cricket");
+	
+		
+		System.out.println("I am from pune");
+		System.out.println("I am student");
+		System.out.println("I like play cricket");
+	
+		System.out.println("I am from pune");
+		System.out.println("I am student");
+		System.out.println("I like play cricket");
+	
+		System.out.println("I am from pune");
+		System.out.println("I am student");
+		System.out.println("I like play cricket");
+	
+		System.out.println("I am from pune");
+		System.out.println("I am student");
+		System.out.println("I like play cricket");
+	
 	}
 }
