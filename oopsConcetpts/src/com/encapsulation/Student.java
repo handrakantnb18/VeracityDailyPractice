1e511a3f8279d16babc7c8c2264fcfd9ccf1844d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-06 09:54:14 +0530

    Student class private variables and getter and setter method

--- oopsConcetpts/src/com/encapsulation/Student.java
+++ oopsConcetpts/src/com/encapsulation/Student.java
@@ -11,4 +11,47 @@ public class Student {
 	private String marks;
 	
 	
+	// getter and setter method
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getMobileno()
+	{
+		return mobileNo;
+	}
+	
+	public void setMobileno(String mobileNo)
+	{
+		this.mobileNo = mobileNo;
+	}
+	
+	public String getAddress()
+	{
+		return address;
+	}
+	
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+	
+	public String getMarks()
+	{
+		return marks;
+	}
+	
+	public void setMarks(String marks)
+	{
+		this.marks = marks;
+	}
+	
+	
 }
