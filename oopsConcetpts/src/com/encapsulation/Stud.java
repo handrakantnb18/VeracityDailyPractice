89a6560e81fb5f235202dd3e0f691ac2a3d29897
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-06 10:11:17 +0530

    object created private variable class and access all properties

--- oopsConcetpts/src/com/encapsulation/Stud.java
+++ oopsConcetpts/src/com/encapsulation/Stud.java
@@ -2,4 +2,22 @@ package com.encapsulation;
 
 public class Stud {
 
+	// object created private variable class and access all properties
+	
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		
+		s1.setName("Priyanka");
+		System.out.println(s1.getName());
+		
+		s1.setMobileno("8799857751");
+		System.out.println(s1.getMobileno());
+		
+		s1.setAddress("Pune");
+		System.out.println(s1.getAddress());
+		
+		s1.setMarks("75");
+		System.out.println(s1.getMarks());
+		
+	}
 }
