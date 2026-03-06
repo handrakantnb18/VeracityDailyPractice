0b683eb6a97c416e321a7f14af5f28fb5881cd62
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-06 14:05:09 +0530

    Exception handling checking the exceptions to check the problem

---
+++ Exceptions/src/com/exception/Demo.java
@@ -0,0 +1,15 @@
+package com.exception;
+
+public class Demo {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, my name is chandraknt.");
+		System.out.println(10+10);
+		System.out.println(10/0);
+		System.out.println("I am from pune");
+		System.out.println("I am student");
+		System.out.println("I like play cricket");
+	}
+}
