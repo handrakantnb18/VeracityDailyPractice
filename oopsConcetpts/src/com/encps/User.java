e211c8f3a89e448689e8ff3f0b5921ac0a971e61
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-06 11:11:41 +0530

    User super class getter and setter method

---
+++ oopsConcetpts/src/com/encps/User.java
@@ -0,0 +1,60 @@
+package com.encps;
+
+public class User {
+
+	
+	private String username;
+	
+	private String password;
+	
+	private String address;
+	
+	private String mobileno;
+	
+	// getter and setter method
+	
+	// getter method we have created
+	
+	public String getUsername()
+	{
+		return username;
+	}
+	
+	// setter method
+	
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+	
+	public String getPassword()
+	{
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	public String getAddress()
+	{
+		return address;
+	}
+	
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+	
+	public String getMobileno() 
+	{
+		return mobileno;
+	}
+	
+	public void setMobileno(String mobileno)
+	{
+		this.mobileno = mobileno;
+	}
+	
+}
