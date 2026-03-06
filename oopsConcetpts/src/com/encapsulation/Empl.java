f8510b777eb740263989b8f88342474ed9363710
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-06 10:07:53 +0530

    object created private variable class and access all properties

--- oopsConcetpts/src/com/encapsulation/Empl.java
+++ oopsConcetpts/src/com/encapsulation/Empl.java
@@ -2,4 +2,25 @@ package com.encapsulation;
 
 public class Empl {
 
+	public static void main(String[] args) {
+		
+		// object created private variable class and access all properties
+		
+		Employee e = new Employee();
+		
+		e.setName("Chandrakant");
+		System.out.println(e.getName());
+		
+		e.setEmail("chandrakant18@gmail.com");
+		System.out.println(e.getEmail());
+		
+		e.setGender("Male");
+		System.out.println(e.getGender());
+		
+		e.setDep("Software");
+		System.out.println(e.getDep());
+		
+		e.setSalary("450000");
+		System.out.println(e.getSalary());
+	}
 }
