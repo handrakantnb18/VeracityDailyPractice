ddfa7b96a0b2645b93229e60356fecf0d8fbd059
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-07 12:46:08 +0530

    Exception handling

---
+++ Exceptions/src/com/exception/DemoFour.java
@@ -0,0 +1,15 @@
+package com.exception;
+
+import java.util.Scanner;
+
+public class DemoFour {
+
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter age : ");
+		
+		
+	}
+}
