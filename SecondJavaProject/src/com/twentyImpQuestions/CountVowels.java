8e299558b412fecd59592a578d0f4f77a46a3b04
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-07 22:25:47 +0530

    Count Vowels in String

---
+++ SecondJavaProject/src/com/twentyImpQuestions/CountVowels.java
@@ -0,0 +1,21 @@
+package com.twentyImpQuestions;
+
+public class CountVowels {
+
+	public static void main(String[] args) {
+		String str = "Chandrakant bhosale";
+		int count = 0;
+		
+		for(int i = 0; i < str.length(); i++)
+		{
+			char ch = str.charAt(i);
+			
+			if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'
+				|| ch=='A' || ch=='E' || ch =='I' || ch=='O' || ch=='U')
+			{
+				count++;
+			}
+		}
+		System.out.println("Vowels : "+count);
+	}
+}
