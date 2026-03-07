48525e938ebc1e21e240c628b2f92c69ccc54e84
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-07 22:48:10 +0530

    check Even Odd Number

---
+++ SecondJavaProject/src/com/twentyImpQuestions/CheckEvenOdd.java
@@ -0,0 +1,13 @@
+package com.twentyImpQuestions;
+
+public class CheckEvenOdd {
+
+	public static void main(String[] args) {
+		int num = 10;
+		
+		if(num % 2 == 0)
+			System.out.println("Even Number");
+		else
+			System.out.println("Odd Number");
+	}
+}
