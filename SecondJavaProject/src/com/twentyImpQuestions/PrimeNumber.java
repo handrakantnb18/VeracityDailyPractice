d83e09b2e250615039f79dfc4bc9b58ca9ea0063
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-07 22:12:46 +0530

    Prime number

---
+++ SecondJavaProject/src/com/twentyImpQuestions/PrimeNumber.java
@@ -0,0 +1,23 @@
+package com.twentyImpQuestions;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int num = 7;
+		boolean isPrime = true;
+		
+		for(int i = 2; i <= num/2; i++)
+		{
+			if(num % i == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+		
+		if(isPrime)
+			System.out.println("Prime number ");
+		else
+			System.out.println("Not prime number...");
+	}
+}
