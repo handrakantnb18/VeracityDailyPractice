0db9e51764e8ea4c4422747d60fd71c21dc2c78d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-08 15:34:10 +0530

    AgeNotFoundException chaild class used exception throw it is we will create own exception in try catch block and acces the property

---
+++ Exceptions/src/com/exception/Test.java
@@ -0,0 +1,42 @@
+package com.exception;
+
+import java.util.Scanner;
+
+public class Test {
+
+	
+	// AgeNotValidException
+	// AmountNotValidException
+	// UserNotValidException
+	// EmployeeNotFountException
+	// ProductNotFoundException
+	
+	void check() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter age : ");
+		int age = sc.nextInt();
+		
+		if(age < 18)
+		{
+			throw new AgeNotValidException("Age is not eligible.");
+		}
+		else
+		{
+			System.out.println("Eligible for the voting..");
+			
+		}
+			
+	}
+	public static void main(String[] args) {
+		Test t = new Test();
+		try
+		{
+			t.check();
+		}
+		catch(AgeNotValidException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+	}
+}
