3579a8050ccb93697e9bb33d26a9d09aed559984
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-08 15:45:36 +0530

    throws keyword is used to declare exception, in feature might be exception will be occures normal flow program will get destructor

---
+++ Exceptions/src/com/exception/DemoThrows.java
@@ -0,0 +1,23 @@
+package com.exception;
+
+public class DemoThrows {
+
+	void fun() throws ArithmeticException
+	{
+		
+		// yes or NO : throws
+		System.out.println(10/0);
+	}
+	
+	public static void main(String[] args) {
+		DemoThrows d = new DemoThrows();
+		try
+		{
+			d.fun();
+		}
+		catch (ArithmeticException e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+	}
+}
