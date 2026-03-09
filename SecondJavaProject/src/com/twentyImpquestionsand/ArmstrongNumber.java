76191dbefd852e26223e43b8e92e22169112640b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-09 22:59:55 +0530

    Armstrong Number

---
+++ SecondJavaProject/src/com/twentyImpquestionsand/ArmstrongNumber.java
@@ -0,0 +1,22 @@
+package com.twentyImpquestionsand;
+
+public class ArmstrongNumber {
+
+	public static void main(String[] args) {
+		int num = 153;
+		int sum = 0;
+		int temp = num;
+		
+		while (num != 0)
+		{
+			int digit = num % 10;
+			sum += digit * digit * digit;
+			num /= 10;
+		}
+		
+		if(sum == temp)
+			System.out.println("Armstrong number.");
+		else
+			System.out.println("Not Armstrong number.");
+	}
+}
