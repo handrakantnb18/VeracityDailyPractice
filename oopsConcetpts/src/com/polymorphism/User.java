db9eba27af9dfb8b0c645d8d7b5e8e84c5b67fce
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-11 13:56:59 +0530

    User is parent class methods are same name

---
+++ oopsConcetpts/src/com/polymorphism/User.java
@@ -0,0 +1,15 @@
+package com.polymorphism;
+
+public class User {
+
+	
+	void login()
+	{
+		System.out.println("1. user login");
+	}
+	
+	void register()
+	{
+		System.out.println("1. resister form");
+	}
+}
