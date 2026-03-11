7025bdb2b502c6ffce1278c636c67cdbf532f5ba
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-11 13:25:27 +0530

    method overloading method name same access parent class to child class

---
+++ oopsConcetpts/src/com/polymorphism/Payment.java
@@ -0,0 +1,14 @@
+package com.polymorphism;
+
+public class Payment {
+
+	void login()
+	{
+		System.out.println("1. Login successfuly");
+	}
+	
+	void register()
+	{
+		System.out.println("2. Login successfuly");
+	}
+}
