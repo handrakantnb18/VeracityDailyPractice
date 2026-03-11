0433a03e3294588c01f207cebee2938a8e3253b7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-11 22:01:25 +0530

    Check Palindrome String

---
+++ SecondJavaProject/src/com/java/twentyquestions/PalindromeString.java
@@ -0,0 +1,19 @@
+package com.java.twentyquestions;
+
+public class PalindromeString {
+
+	public static void main(String[] args) {
+		String str = "aaaaaa";
+		String rev = "";
+		
+		for(int i = str.length() - 1; i >= 0; i--)
+		{
+			rev += str.charAt(i);
+		}
+		
+		if(str.equals(rev))
+			System.out.println("Palindrome String.");
+		else
+			System.out.println("Not Palindrome String");
+	}
+}
