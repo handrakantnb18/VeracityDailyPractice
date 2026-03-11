e92d2705b1da016e583eb235b0d12305449aa5bb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-11 22:25:22 +0530

    Sum Of Digit

---
+++ SecondJavaProject/src/com/java/twentyquestions/SumOfDigit.java
@@ -0,0 +1,17 @@
+package com.java.twentyquestions;
+
+public class SumOfDigit {
+
+	public static void main(String[] args) {
+		int num = 1234;
+		int sum = 0;
+		
+		while(num != 0)
+		{
+			sum += num % 10;
+			num /= 10;
+		}
+		
+		System.out.println("Sum of Digits : "+sum);
+	}
+}
