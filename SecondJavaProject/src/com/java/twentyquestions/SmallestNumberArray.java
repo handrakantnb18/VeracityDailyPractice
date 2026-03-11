8da9c919e87ca2aa34751f2af2affe536f2adaa6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-11 22:10:30 +0530

    Find Smallest Number in Array

---
+++ SecondJavaProject/src/com/java/twentyquestions/SmallestNumberArray.java
@@ -0,0 +1,19 @@
+package com.java.twentyquestions;
+
+public class SmallestNumberArray {
+
+	public static void main(String[] args) {
+		int arr[] = {10, 8,25,40,45,89,15};
+		int min = arr[0];
+		
+		for(int i = 1; i < arr.length; i++)
+		{
+			if(arr[i] < min)
+			{
+				min = arr[i];
+			}
+		}
+		
+		System.out.println("Smallest Number : "+min);
+	}
+}
