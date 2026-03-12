28956ddedee4c576d0ad1e0c0c8bbcc79e568188
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-12 22:23:19 +0530

    Check Even Odd

---
+++ SecondJavaProject/src/com/fivequestionsandanswers/CheckEvenOdd.java
@@ -0,0 +1,13 @@
+package com.fivequestionsandanswers;
+
+public class CheckEvenOdd {
+
+	public static void main(String[] args) {
+		int num = 10;
+		
+		if (num % 2 == 0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+	}
+}
