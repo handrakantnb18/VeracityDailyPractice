e636399e42eb19e01b2327999d8aea16ca8e3d8a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-12 22:25:05 +0530

    Count Words String

---
+++ SecondJavaProject/src/com/fivequestionsandanswers/CountWordsString.java
@@ -0,0 +1,12 @@
+package com.fivequestionsandanswers;
+
+public class CountWordsString {
+
+	public static void main(String[] args) {
+		String str = "Priyanka chadrakant bhosale";
+		String words[] = str.split(" ");
+		
+		System.out.println("Word Count : "+words.length);
+	}
+
+}
