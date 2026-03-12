d21dfe41a7771c6f7082b3170e10b4b9267ee633
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-12 22:36:28 +0530

    Reverse String

---
+++ SecondJavaProject/src/com/fivequestionsandanswers/ReverseString.java
@@ -0,0 +1,15 @@
+package com.fivequestionsandanswers;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String name = "Chandrakant";
+		String rev = " ";
+		
+		for (int i = name.length() - 1; i >= 0; i--)
+		{
+			rev = rev + name.charAt(i);
+		}
+		System.out.println("Reverse String : "+rev);
+	}
+}
