a62b85a10f20529f7cad7b24f74eead4d38bcd9a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-12 22:22:12 +0530

    0Armstrong Number check

---
+++ SecondJavaProject/src/com/fivequestionsandanswers/ArmstrongNumber.java
@@ -0,0 +1,20 @@
+package com.fivequestionsandanswers;
+
+public class ArmstrongNumber {
+
+	public static void main(String[] args) {
+		 int num = 153;
+		 int sum = 0;
+		 int original = num;
+		 
+		 while(num != 0)
+		 {
+			 int digit = num % 10;
+			 sum += digit * digit * digit;
+			 num /= 10;
+		 }
+		 
+		 System.out.println(sum == original ? "Armstrong" : "Not Armstrong");
+		 
+	}
+}
