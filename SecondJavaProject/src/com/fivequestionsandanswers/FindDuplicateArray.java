54fe12c765bb66aee1a051529b850382127b4c97
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-12 22:28:08 +0530

    Find Duplicate Array

---
+++ SecondJavaProject/src/com/fivequestionsandanswers/FindDuplicateArray.java
@@ -0,0 +1,19 @@
+package com.fivequestionsandanswers;
+
+public class FindDuplicateArray {
+
+	public static void main(String[] args) {
+		int arr[] = {1, 2,2,3,5,8,8,7,9,1};
+		
+		for(int i = 0; i < arr.length; i++)
+		{
+			for(int j = i + 1; j <arr.length; j++)
+			{
+				if(arr[i] == arr[j]) {
+					System.out.println("Duplicates : "+arr[i]);
+				}
+			}
+		}
+	}
+
+}
