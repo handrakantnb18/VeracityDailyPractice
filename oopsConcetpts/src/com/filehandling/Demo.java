dfdc31576e08209d7f757d0047626cb63857b6ea
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-13 13:50:27 +0530

    using for loop same name multiple file created

--- oopsConcetpts/src/com/filehandling/Demo.java
+++ oopsConcetpts/src/com/filehandling/Demo.java
@@ -5,23 +5,21 @@ import java.io.IOException;
 
 public class Demo {
 
-	
 	public static void main(String[] args) {
-		File file = new File("D:\\Veracity\\File Handling\\abc.txt");
-		
-		try
-		{
-			if(file.createNewFile())
-			{
-				System.out.println("File created");
-				// create new file // compile time Exception
+
+		for (int i = 1; i <= 10; i++) {
+			File file = new File("D:\\Veracity\\File Handling\\abc '"+i+"'.txt ");
+
+			try {
+				if (file.createNewFile()) {
+					System.out.println("File created");
+					// create new file // compile time Exception
+				}
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
 			}
+
+			System.out.println("File already exists");
 		}
-		catch (IOException e)
-		{
-			System.out.println(e.getMessage());
-		}
-		
-		System.out.println("File already exists");
 	}
 }
