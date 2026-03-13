f6a54151629f5d4db6f250c8391dab2f9bb9af07
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-13 22:44:43 +0530

    CRUD operation doing

---
+++ oopsConcetpts/src/com/filehandling/CRUD.java
@@ -0,0 +1,37 @@
+package com.filehandling;
+
+public class CRUD {
+
+	void createFile()
+	{
+		
+	}
+	
+	void writeDataIntoFile()
+	{
+		
+	}
+	
+	void readDataFromFile()
+	{
+		
+	}
+	
+	void deleteFile()
+	{
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		while(true)
+		{
+			System.out.println("1.create file ");
+			System.out.println("2. WriteFile ");
+			System.out.println("3.ReadFileFromFile ");
+			System.out.println("4.DeleteFile");
+			
+		}
+	}
+	
+}
