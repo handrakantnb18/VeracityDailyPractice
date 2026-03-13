3a6aeee3785e5a29065071548cd1b5c15243a506
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-13 13:07:09 +0530

    simple is a interface only created method and extends child class

---
+++ oopsConcetpts/src/com/interfacepratice/Login.java
@@ -0,0 +1,22 @@
+package com.interfacepratice;
+
+public interface Login extends Demo {
+
+public static void main(String[] args) {
+//		Login l = new Demo();
+		
+//	
+	
+//	public void run()
+//	{
+//		
+//	}
+//	
+//	 void display()
+//	{
+//		
+//	}
+	
+	
+	}
+}
