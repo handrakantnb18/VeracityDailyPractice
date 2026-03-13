d3502b0db76180e9a597b1cc273a4f960686e165
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-13 13:57:24 +0530

    file.delete() method to delete all files

--- oopsConcetpts/src/com/filehandling/Demo.java
+++ oopsConcetpts/src/com/filehandling/Demo.java
@@ -8,18 +8,24 @@ public class Demo {
 	public static void main(String[] args) {
 
 		for (int i = 1; i <= 10; i++) {
-			File file = new File("D:\\Veracity\\File Handling\\abc '"+i+"'.txt ");
+			File file = new File("D:\\Veracity\\File Handling\\abc'"+i+"'.txt ");
 
-			try {
-				if (file.createNewFile()) {
-					System.out.println("File created");
-					// create new file // compile time Exception
-				}
-			} catch (IOException e) {
-				System.out.println(e.getMessage());
-			}
-
-			System.out.println("File already exists");
+			file.delete();
+			System.out.println("Deleted files");
+			
+//			try {
+//				
+//				
+//				if (file.createNewFile()) {
+//					System.out.println("File created");
+//					// create new file // compile time Exception
+//				}
+//			} catch (IOException e) {
+//				System.out.println(e.getMessage());
+//			}
+//
+//			System.out.println("File already exists");
 		}
+		
 	}
 }
