5615ab01baabb7c0cb2c65471c8288cfdb22e24c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-13 14:56:28 +0530

    read data from file full file access using while loop

--- oopsConcetpts/src/com/filehandling/Test.java
+++ oopsConcetpts/src/com/filehandling/Test.java
@@ -6,18 +6,25 @@ import java.io.IOException;
 public class Test {
 
 	public static void main(String[] args) {
-		
+
 		// read data from file
-		
-		try
+
+		try 
 		{
-		FileReader fr = new FileReader("D:\\Veracity\\File Handling\\student.txt");
-		
-		int i = fr.read();
-		System.out.println(i);
-		
-		}
-		catch(IOException e)
+			FileReader fr = new FileReader("D:\\Veracity\\File Handling\\student.txt");
+
+			int i = fr.read();
+			System.out.println((char) i);
+
+			// while loop
+
+			int data;
+			while ((data = fr.read()) != -1) 
+			{
+				System.out.print((char)data);
+			}
+		} 
+		catch (IOException e)
 		{
 			e.printStackTrace();
 		}
