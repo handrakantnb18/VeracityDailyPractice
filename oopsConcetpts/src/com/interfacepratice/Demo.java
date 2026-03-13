071b52251e2060bc5e2f1948ae92e6d203db0b2f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-13 13:06:10 +0530

    Demo is interface created multiple method and access in super class objects

---
+++ oopsConcetpts/src/com/interfacepratice/Demo.java
@@ -0,0 +1,24 @@
+package com.interfacepratice;
+
+public interface Demo {
+
+	
+	// static method
+	static void run()
+	{
+		
+	}
+	
+	// default methods
+	
+
+	 void work();  // abstract method
+	
+	
+	private void display()
+	{
+		
+	}
+	
+	
+}
