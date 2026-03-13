fc8ceecb8f36c6d8073d9e1d9980f1d81c5f039c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-13 13:02:13 +0530

    DemoThree is a interface only created method and created default and staic methods

---
+++ oopsConcetpts/src/com/interface6/DemoThree.java
@@ -0,0 +1,17 @@
+package com.interface6;
+
+public interface DemoThree {
+
+	void Login();
+	
+	public static void display()
+	{
+		
+		System.out.println("Display 1");
+	}
+	
+	private void displayOne()
+	{
+		System.out.println("Display Private");
+	}
+}
