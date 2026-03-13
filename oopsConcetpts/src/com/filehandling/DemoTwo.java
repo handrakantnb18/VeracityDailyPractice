f903b5e331b43fe1a624a5bec2917bff4824c20e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-13 14:17:30 +0530

    array format data stored in integer and String format

--- oopsConcetpts/src/com/filehandling/DemoTwo.java
+++ oopsConcetpts/src/com/filehandling/DemoTwo.java
@@ -7,12 +7,28 @@ public class DemoTwo {
 
 	public static void main(String[] args) {
 
-
+		int a = 10, b = 20;
+		
+		int numbers[] = {34, 23, 56, 89, 25, 58};
+		
+		String names[] = {"chandraknt", "Priyanka", "Vijay", "Dyana"};
+		
 		try
 		{
 			
-			FileWriter fw = new FileWriter("D:\\Veracity\\File Handling\\student.txt", true);
-			fw.write("\ni am from sangli");
+			FileWriter fw = new FileWriter("D:\\Veracity\\File Handling\\abc.txt", true);
+//			fw.write("\ni am from sangli");
+			
+//			for (int i = 0; i < numbers.length; i++)
+//			{
+//				fw.write("\nArrays Numbers : "+numbers[i]);
+//			}
+			
+			for (int i = 0; i < names.length; i++)
+			{
+				fw.write("\nArrays Numbers : "+names[i]);
+			}
+			
 			fw.flush();
 			System.out.println("Data addedd in file.");
 		}
