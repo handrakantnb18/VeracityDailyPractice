834c52e5b9598c6e3583e312d0720357d2292b88
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-14 22:16:55 +0530

    Sort Arrays

---
+++ SecondJavaProject/src/com/twentycoding/interview/SortArrays.java
@@ -0,0 +1,18 @@
+package com.twentycoding.interview;
+
+import java.util.Arrays;
+
+public class SortArrays {
+
+	public static void main(String[] args) {
+		
+		 int arr[] = {5, 18, 3, 8, 11, 1, 2};
+
+	        Arrays.sort(arr);
+
+	        for(int num : arr) {
+	            System.out.print(num + " ");
+	        }
+	        
+	}
+}
