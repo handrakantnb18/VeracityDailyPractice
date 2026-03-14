f0ad3f18782b97cbc37895ea65878d43af009974
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-14 22:11:00 +0530

    Swap Two Number (without third variable)

---
+++ SecondJavaProject/src/com/twentycoding/interview/SwapTwoNumber.java
@@ -0,0 +1,16 @@
+package com.twentycoding.interview;
+
+public class SwapTwoNumber {
+
+	public static void main(String[] args) {
+		
+		int a = 10, b = 20;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a = " + a + ", b = " + b);
+        
+	}
+}
