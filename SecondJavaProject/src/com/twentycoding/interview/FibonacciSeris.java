28b07eacc7335945b77f329384a36a2dc8a2862a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-14 22:07:21 +0530

    Fibonacci Seris

---
+++ SecondJavaProject/src/com/twentycoding/interview/FibonacciSeris.java
@@ -0,0 +1,17 @@
+package com.twentycoding.interview;
+
+public class FibonacciSeris {
+
+	public static void main(String[] args) {
+		
+		int n = 10, a = 0, b = 1;
+
+        for(int i = 1; i <= n; i++) {
+            System.out.println(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        
+	}
+}
