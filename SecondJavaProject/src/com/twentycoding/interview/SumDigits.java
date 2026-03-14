b581c0e125e475ba5b6a46f8ade0e30ddde5e181
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-14 22:14:17 +0530

    Sum of Digits

---
+++ SecondJavaProject/src/com/twentycoding/interview/SumDigits.java
@@ -0,0 +1,18 @@
+package com.twentycoding.interview;
+
+public class SumDigits {
+
+	public static void main(String[] args) {
+		
+		 int num = 1234;
+	        int sum = 0;
+
+	        while(num != 0) {
+	            sum += num % 10;
+	            num /= 10;
+	        }
+
+	        System.out.println("Sum of digits: " + sum);
+	        
+	}
+}
