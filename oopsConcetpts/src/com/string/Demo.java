6ae80393d69ecdd01e0d7abaece7a131eacbc75c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-15 13:29:39 +0530

    Four ways to create String and run

--- oopsConcetpts/src/com/string/Demo.java
+++ oopsConcetpts/src/com/string/Demo.java
@@ -5,10 +5,35 @@ public class Demo {
 	
 	public static void main(String[] args) {
 		
-		char c[] = {'A','B','C'};
+//		char c[] = {'A','B','C'};
+//		
+//		String s1 = new String(c);
+//		
+//		System.out.println(s1);
+//		
+//		String name = "Chandrakant";
 		
-		String s1 = new String(c);
 		
-		System.out.println(s1);
+		// String Laterals
+		
+		String name = "Chandraknt";
+		System.out.println(name);
+		
+		// String new keywords
+		
+		String s = new String("Chandrakant");
+		System.out.println(s);
+		
+		// StringBuilder
+		
+		StringBuilder sb = new StringBuilder("Priyanka");
+		System.out.println(sb);
+		
+		// StringBuffer
+		
+		StringBuffer sf = new StringBuffer("Pune");
+		System.out.println(sf);
+		
+		 
 	}
 }
