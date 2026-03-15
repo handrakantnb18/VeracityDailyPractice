0a4bfb993e2da7ab0b8cbebe1b07f324430a05a5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-15 12:11:46 +0530

    String charactor array created simple

---
+++ oopsConcetpts/src/com/string/Demo.java
@@ -0,0 +1,14 @@
+package com.string;
+
+public class Demo {
+
+	
+	public static void main(String[] args) {
+		
+		char c[] = {'A','B','C'};
+		
+		String s1 = new String(c);
+		
+		System.out.println(s1);
+	}
+}
