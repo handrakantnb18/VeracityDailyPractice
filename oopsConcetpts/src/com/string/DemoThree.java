9b211b090c719be8c6e80829a25357f6162774af
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-15 18:35:09 +0530

    string methods == method

---
+++ oopsConcetpts/src/com/string/DemoThree.java
@@ -0,0 +1,13 @@
+package com.string;
+
+public class DemoThree {
+
+	public static void main(String[] args) {
+		
+		String s1 = "Chandrakant";
+		
+		String s2 = "Chandrakant";
+		
+		System.out.println(s1 == s2);
+	}
+}
