4093e88ecb50963cf889e870ed95a1a8b88dc8c6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-15 13:41:46 +0530

    Memory representation in java

---
+++ oopsConcetpts/src/com/string/DemoTwo.java
@@ -0,0 +1,28 @@
+package com.string;
+
+public class DemoTwo {
+
+	public DemoTwo() {
+		Demo d = new Demo();
+		
+		
+	}
+	
+	int id;
+	
+	static String name;
+	
+	void m1()
+	{
+		int x;
+	}
+	
+	static void m3()
+	{
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
