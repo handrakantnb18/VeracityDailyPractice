c9629ff7b7228d0a77e4444345584c16d4f51129
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-16 22:39:10 +0530

    Collection start ArrayList simple add data

---
+++ collection/src/com/collection/Demo.java
@@ -0,0 +1,36 @@
+package com.collection;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+public static void main(String[] args) {
+		
+		
+		// wrapper class -- primitive data type
+		
+		Integer x  = 100;
+		
+		Integer y = 200;
+		
+		System.out.println(x + y);
+		
+		int a = 500;
+		
+		int b = 300;
+		
+		System.out.println(a - b);
+		
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		
+		al.add(x);
+		al.add(y);
+		al.add(a);
+		al.add(b);
+		
+		System.out.println(al);
+		
+		
+	}
+
+}
