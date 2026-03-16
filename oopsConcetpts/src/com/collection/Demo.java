1a759026ea496454eca5e7056e73c7c97fbbcdc2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-16 22:31:24 +0530

    wrapper class -- primitive data type

--- oopsConcetpts/src/com/collection/Demo.java
+++ oopsConcetpts/src/com/collection/Demo.java
@@ -4,10 +4,19 @@ public class Demo {
 
 	public static void main(String[] args) {
 		
+		
+		// wrapper class -- primitive data type
+		
 		Integer x  = 100;
 		
 		Integer y = 200;
 		
 		System.out.println(x + y);
+		
+		int a = 100;
+		
+		int b = 300;
+		
+		System.out.println(a - b);
 	}
 }
