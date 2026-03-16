81c6f6c45d973b17019a37b05a9acb86c641a1db
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-16 22:29:35 +0530

    collection start

---
+++ oopsConcetpts/src/com/collection/Demo.java
@@ -0,0 +1,13 @@
+package com.collection;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		Integer x  = 100;
+		
+		Integer y = 200;
+		
+		System.out.println(x + y);
+	}
+}
