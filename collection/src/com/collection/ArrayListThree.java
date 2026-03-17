27f599cfda040d8207ff47c95d6140f1dd183141
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-17 22:59:31 +0530

    accessed Employees private members and created advance level object passed arguments

---
+++ collection/src/com/collection/ArrayListThree.java
@@ -0,0 +1,30 @@
+package com.collection;
+
+import java.util.ArrayList;
+
+public class ArrayListThree {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Employees> employees = new ArrayList<Employees>();
+		
+		employees.add(new Employees(1, "Chandrakant", "india", "45000", "IT", "Male"));
+		employees.add(new Employees(2, "priyanka", "Pune", "550000", "IT", "Female"));
+		employees.add(new Employees(3, "Ram", "Mumbai", "250000", "HR", "Male"));
+		employees.add(new Employees(4, "vijay", "Mumbai", "270000", "HR", "Male"));
+		
+		for(Employees emp:employees)
+		{
+			System.out.println("=====================");
+			
+			System.out.println("Id      : "+emp.getId());
+			System.out.println("Name    : "+emp.getName());
+			System.out.println("Address : "+emp.getAddress());
+			System.out.println("Salary  : "+emp.getSalary());
+			System.out.println("Dept    : "+emp.getDep());
+			System.out.println("Gender  : "+emp.getGender());
+			
+			
+		}
+	}
+}
