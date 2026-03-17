0fbbe544c96a13652917fafcfadc83605a397599
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-17 10:58:11 +0530

    collection arrayList Object type, all types of data we can add

--- collection/src/com/collection/ArrayListDemo.java
+++ collection/src/com/collection/ArrayListDemo.java
@@ -9,14 +9,30 @@ public class ArrayListDemo {
 		
 		// <type> -> generics are used for types safety
 		
-		ArrayList<Integer> al = new ArrayList<Integer>();
 		
+		// ArrayList
+		
+//		ArrayList<Integer> al = new ArrayList<Integer>();
+//		
+//		al.add(100);  
+//		al.add(200);
+//		al.add(450);
+//		al.add(244);
+//		al.add(350);
+//		
+//		System.out.println(al);
+		
+		// all types of data we can add
+		
+		ArrayList<Object> al = new ArrayList<Object>();
 		al.add(100);
-		al.add(200);
-		al.add(450);
-		al.add(244);
-		al.add(350);
+		al.add("Chandr");
+		al.add(true);
+		al.add(55.45);
+		al.add('A');
 		
 		System.out.println(al);
+		
+		
 	}
 }
