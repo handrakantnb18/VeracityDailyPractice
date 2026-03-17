584dbe19084cfd67b297cfbf98ff375c1092c692
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-17 15:02:14 +0530

    toString method and getter and setter method appied

--- collection/src/com/collection/Student.java
+++ collection/src/com/collection/Student.java
@@ -2,8 +2,48 @@ package com.collection;
 
 public class Student {
 
-	int id;
-	String name;
-	String address;
+	private int id;
+	private String name;
+	private String address;
+	
+	// to convert address into string
+	// toString method
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", address=" + address + "]";
+	}
+
+
+	// 
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
 	
 }
