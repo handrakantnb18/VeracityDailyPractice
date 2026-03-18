176de4dde18e6064d7b67b43fd22f85bb9f861b8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-18 14:23:58 +0530

    BookDemo we can create arrayList to store data and 4 ways to store data

---
+++ collection/src/com/collection/assignments/BooksDemo.java
@@ -0,0 +1,14 @@
+package com.collection.assignments;
+
+import java.util.ArrayList;
+
+public class BooksDemo {
+
+	// create ArrayList and add 5 Books      iterate 4 ways
+
+	public static void main(String[] args) {
+		
+		ArrayList<Books> books = new ArrayList<Books>();
+		
+	}
+}
