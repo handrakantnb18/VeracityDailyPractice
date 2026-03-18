301efe09add36c66be3d2f415064c02da790e01b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-18 23:39:20 +0530

    Find LArgest Number

---
+++ SecondJavaProject/src/com/java/eightprogram/FindLArgestNumber.java
@@ -0,0 +1,20 @@
+package com.java.eightprogram;
+
+public class FindLArgestNumber {
+
+	public static void main(String[] args) {
+		int arr[] = {10, 22, 20, 50, 8, 40, 15, 99, 100, 500};
+		int max = arr[0];
+		
+		for(int i = 1; i < arr.length; i++)
+		{
+			if(arr[i] > max)
+			{
+				max = arr[i];
+			}
+		}
+		
+		System.out.println("Largest Number : "+ max);
+	}
+	
+}
