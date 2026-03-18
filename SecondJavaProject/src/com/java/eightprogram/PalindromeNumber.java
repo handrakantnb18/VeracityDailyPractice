c0b5a9be29d9c12be0efef2ecdcf98c4d362d788
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-18 23:42:35 +0530

    Palindrome Number

---
+++ SecondJavaProject/src/com/java/eightprogram/PalindromeNumber.java
@@ -0,0 +1,22 @@
+package com.java.eightprogram;
+
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+		int num = 121;
+		int rev = 0;
+		int temp = num;
+		
+		while(num != 0)
+		{
+			int digit = num % 10;
+			rev = rev * 10 + digit;
+			num = num / 10;
+		}
+		
+		if(temp == rev)
+			System.out.println("Palindrome Number");
+		else
+			System.out.println("Not Palindrome Number.");
+	}
+}
