8cf77cc4d6478b9629b00ab88b3e7c6141759cbb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-18 11:58:29 +0530

    ArrayList created and using for loop accessed toString method and 10 products

---
+++ collection/src/com/collection/assignments/ProductDemo.java
@@ -0,0 +1,37 @@
+package com.collection.assignments;
+
+import java.util.ArrayList;
+
+public class ProductDemo {
+
+	// product -- id, name, price, category, qty, discount
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Product> product = new ArrayList<Product>();
+		product.add(new Product(1, "Samsung Galaxy", 10000, "Mobile", 10, 10));
+		product.add(new Product(2, "Laptop", 89000, "Electronics", 15, 7));
+		product.add(new Product(3, "iPhone 17", 150000, "Mobile", 5, 10));
+		product.add(new Product(4, "OPPO", 15000, "Mobile", 10, 5));
+		product.add(new Product(5, "HP", 75000, "Laptop", 9, 9));
+		product.add(new Product(6, "Acer", 60000, "Laptop", 25, 11));
+		product.add(new Product(7, "T-shirt", 900, "Fashon", 50, 3));
+		product.add(new Product(8, "Chair Table", 8800, "Furniture", 30, 15));
+		product.add(new Product(9, "Bata", 2500, "Shoes", 90, 5));
+		product.add(new Product(10, "Head Phone", 1100, "Electronics", 60, 45));
+		
+		
+		for(Product prod1 : product)
+		{
+			System.out.println("\n===============================");
+			System.out.println("Id : "+prod1.getId());
+			System.out.println("Name : "+prod1.getName());
+			System.out.println("Price : "+prod1.getPrice());
+			System.out.println("Category : "+prod1.getCategory());
+			System.out.println("Quantity : "+prod1.getQty());
+			System.out.println("Discount : "+prod1.getDiscount());
+
+		}
+		
+	}
+}
