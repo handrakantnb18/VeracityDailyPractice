9c3b98fd9bff35cbdf3aa295ce0d4c5913e97f14
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-18 13:43:35 +0530

    Books pojo class and constructor and getter and setter created

---
+++ collection/src/com/collection/assignments/Books.java
@@ -0,0 +1,68 @@
+package com.collection.assignments;
+
+public class Books {
+
+	private int id;
+
+	private String name;
+
+	private String author;
+
+	private double price;
+
+	private String category;
+
+	
+	
+	public Books(int id, String name, String author, double price, String category) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+		this.price = price;
+		this.category = category;
+	}
+	
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+}
