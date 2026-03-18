619093342206749d218a6a7c2fb048085b686d18
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-18 22:59:49 +0530

    Employee created getter and setter methods are created

---
+++ collection/src/com/collection/assignments/Employee.java
@@ -0,0 +1,68 @@
+package com.collection.assignments;
+
+public class Employee {
+
+	private int empId;
+	
+	private String empName;
+	
+	private String dept;
+	
+	private double salary;
+	
+	private String city;
+	
+	private String mobile;
+
+	
+	
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	
+	
+}
