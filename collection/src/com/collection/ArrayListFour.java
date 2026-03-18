5aba912523bd193c2c222e8629a33111e6c13b13
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-18 22:15:32 +0530

    ArrayList, List and Arrays collection is used

---
+++ collection/src/com/collection/ArrayListFour.java
@@ -0,0 +1,20 @@
+package com.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListFour {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		
+		List<Integer> number = new ArrayList<Integer>();
+		
+		List<Integer> num = Arrays.asList(1, 2, 3, 4, 5, 6);
+		
+		System.out.println(num);
+		
+	}
+}
