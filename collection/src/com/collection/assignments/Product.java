e13681c17f18dd09ba89fd3183ef38ef341abed5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-18 11:57:37 +0530

    Product class created private variable and getter , setter method and constructor

---
+++ collection/src/com/collection/assignments/Product.java
@@ -0,0 +1,84 @@
+package com.collection.assignments;
+
+public class Product {
+
+	private int id;
+	
+	private String name;
+	
+	private double price;
+	
+	private String category;
+	
+	private int qty;
+	
+	private double discount;
+
+	
+//	@Override
+//	public String toString() {
+//		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", category=" + category + ", qty=" + qty
+//				+ ", discount=" + discount + "]";
+//	}
+
+	public Product(int id, String name, double price, String category, int qty, double discount) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.category = category;
+		this.qty = qty;
+		this.discount = discount;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+	
+	
+	
+}
