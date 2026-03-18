6b540c5f9c194a8144d32d6ef3ac4e8715f81a40
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-18 23:43:34 +0530

    Palindrome String

---
+++ SecondJavaProject/src/com/java/eightprogram/PalindromeString.java
@@ -0,0 +1,19 @@
+package com.java.eightprogram;
+
+public class PalindromeString {
+
+	public static void main(String[] args) {
+		String name = "SAAS";
+		String rev = " ";
+		
+		for(int i = name.length() - 1; i >= 0; i--)
+		{
+			rev += name.length();
+		}
+		
+		if(name.equals(rev))
+			System.out.println("Palindrome");
+		else
+			System.out.println("Not Palindrome");
+	}
+}
