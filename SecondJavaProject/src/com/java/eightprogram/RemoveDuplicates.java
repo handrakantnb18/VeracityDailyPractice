a72cf758a84c090fe0598ded4a78c2322855cbc9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-18 23:45:27 +0530

    Remove Duplicates

---
+++ SecondJavaProject/src/com/java/eightprogram/RemoveDuplicates.java
@@ -0,0 +1,20 @@
+package com.java.eightprogram;
+
+public class RemoveDuplicates {
+
+	public static void main(String[] args) {
+		String name = "chandraknt bhosale";
+		String name1 = " ";
+		
+		for(int i =0; i < name.length(); i++)
+		{
+			char ch = name.charAt(i);
+			
+			if(name1.indexOf(ch) == -1)
+			{
+				name1 += ch;
+			}
+		}
+		System.out.println(name1);
+	}
+}
