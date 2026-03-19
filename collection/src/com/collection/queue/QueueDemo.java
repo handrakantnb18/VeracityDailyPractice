ddfb1b8d4c0eb0d5979cbba6b80250a3c2696ef8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-19 23:39:34 +0530

    pop method used, pollFirst, pollLast method is ussed

--- collection/src/com/collection/queue/QueueDemo.java
+++ collection/src/com/collection/queue/QueueDemo.java
@@ -33,13 +33,19 @@ public class QueueDemo {
 		pq.add(123);
 		pq.add(-11);
 		
-		System.out.println(pq.peekFirst()); // 
+//		System.out.println(pq.peekFirst()); // get first
 		
-		System.out.println(pq.peekLast());
+//		System.out.println(pq.peekLast()); // get last
 		
-		System.out.println(pq.peek());
+//		System.out.println(pq.peek()); // get
 		
-		System.out.println(pq);
+//		System.out.println(pq);
+		
+		System.out.println(pq.pop()); // delete 
+		
+		System.out.println(pq.pollFirst()); // delete first
+		
+		System.out.println(pq.pollLast()); // delete last
 		
 	}
 }
