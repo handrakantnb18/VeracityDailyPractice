f24944663eef1941bd8bdab2522d66252dee0281
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-20 00:19:51 +0530

    StudentDemo parent class to access data

---
+++ collection/src/com/hashset/StudentDemo.java
@@ -0,0 +1,12 @@
+package com.hashset;
+
+import java.util.HashSet;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		
+		HashSet<Student> hs = new HashSet<Student>();
+		
+	}
+}
