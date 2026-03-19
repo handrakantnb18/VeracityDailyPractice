2c02a2bfb04efdccfc2b7cfbdb377451eff5b082
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-20 00:21:30 +0530

    BookDemo is a super class access data form books

---
+++ collection/src/com/hashset/BookDemo.java
@@ -0,0 +1,20 @@
+package com.hashset;
+
+import java.util.HashSet;
+
+public class BookDemo {
+
+	public static void main(String[] args) {
+		
+		HashSet<Book> hs = new HashSet<Book>();
+		
+		hs.add(new Book(1, "java", "James gosling", 500, 10));
+		hs.add(new Book(2, "Rich Dad Poor Dad", "Robert kiyosaki", 225, 25));
+		hs.add(new Book(3, "Atomatic Habits", "James Clear", 180, 11));
+		hs.add(new Book(4, "Think and Grow Rich", "Napoleon Hill", 555, 15));
+		hs.add(new Book(5, "The power of positive thinking", "Norman vincent peale", 300, 10));
+		
+		System.out.println(hs);
+		
+	}
+}
