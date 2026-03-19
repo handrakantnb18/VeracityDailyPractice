cd00c351c05f551d8a337873b41866c8a07473ff
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-19 20:43:23 +0530

    linked list list is duplicate allows, indexing allowed, insertion order is there

---
+++ collection/src/com/collection/linkedlist/LinkedListDemo.java
@@ -0,0 +1,18 @@
+package com.collection.linkedlist;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(255);
+		list.add(122);
+		list.add(25);
+		list.add(45);
+		list.add(11);
+		
+		System.out.println(list);
+	}
+}
