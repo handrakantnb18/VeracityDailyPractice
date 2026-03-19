99cffe4e1c2d916bc35d83ee73aeb91a8ae6f170
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-19 23:26:00 +0530

    priorityQueue is ti give the priority to list

---
+++ collection/src/com/collection/queue/QueueDemo.java
@@ -0,0 +1,25 @@
+package com.collection.queue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.PriorityQueue;
+
+public class QueueDemo {
+
+	public static void main(String[] args) {
+		
+		//Deque<Integer> d = new ArrayDeque<Integer>();
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		pq.add(255);
+		pq.add(333);
+		pq.add(88);
+		pq.add(456);
+		pq.add(123);
+		// pq.add(null);
+		pq.add(123);
+		
+		System.out.println(pq);
+		
+		
+	}
+}
