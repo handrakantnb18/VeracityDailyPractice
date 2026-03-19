736ea8aeb779ce301175cd8e03c9c61929adebe9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-19 22:19:29 +0530

    vector is synchronized class, vector is a thread safe

--- collection/src/com/collection/linkedlist/LinkedListDemo.java
+++ collection/src/com/collection/linkedlist/LinkedListDemo.java
@@ -1,20 +1,36 @@
 package com.collection.linkedlist;
 
 import java.util.LinkedList;
+import java.util.Vector;
 
 public class LinkedListDemo {
 
 	public static void main(String[] args) {
+		// vector is a legacy class / old class
+		// vector is synchronized class
+		// vector is a thread safe
 		
-		LinkedList<Integer> list = new LinkedList<Integer>();
-		list.add(255);
-		list.add(122);
-		list.add(25);
-		list.add(45);
-		list.add(11);
-		list.add(null);
-		list.add(null);
+		Vector<Integer> v = new Vector<Integer>();
+		v.add(255);
+		v.add(122);
+		v.add(25);
+		v.add(45);
+		v.add(11);
+		v.add(null);
+		v.add(null);
 		
-		System.out.println(list);
+		System.out.println(v);
+		
+		
+//		LinkedList<Integer> list = new LinkedList<Integer>();
+//		list.add(255);
+//		list.add(122);
+//		list.add(25);
+//		list.add(45);
+//		list.add(11);
+//		list.add(null);
+//		list.add(null);
+//		
+//		System.out.println(list);
 	}
 }
