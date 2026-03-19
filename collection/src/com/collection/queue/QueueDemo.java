ddfe864ef6749e1c59c1d2829426d24b377110c7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-19 23:30:36 +0530

    arrayDeque alloed LIFO and FIFO method

--- collection/src/com/collection/queue/QueueDemo.java
+++ collection/src/com/collection/queue/QueueDemo.java
@@ -9,7 +9,20 @@ public class QueueDemo {
 	public static void main(String[] args) {
 		
 		//Deque<Integer> d = new ArrayDeque<Integer>();
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+//		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+//		pq.add(255);
+//		pq.add(333);
+//		pq.add(88);
+//		pq.add(456);
+//		pq.add(123);
+//		// pq.add(null);
+//		pq.add(123);
+//		
+//		System.out.println(pq);
+//		
+		
+		
+		ArrayDeque<Integer> pq = new ArrayDeque<Integer>();
 		pq.add(255);
 		pq.add(333);
 		pq.add(88);
@@ -19,7 +32,6 @@ public class QueueDemo {
 		pq.add(123);
 		
 		System.out.println(pq);
-		
 		
 	}
 }
