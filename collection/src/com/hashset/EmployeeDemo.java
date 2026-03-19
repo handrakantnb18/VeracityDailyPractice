43df7645eae5db430f2cd3702c39dc1a7b8cc6ee
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-20 00:22:00 +0530

    EmployeeDemo is a super class access data form Employee

---
+++ collection/src/com/hashset/EmployeeDemo.java
@@ -0,0 +1,12 @@
+package com.hashset;
+
+import java.util.HashSet;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		
+		HashSet<Employee> hs = new HashSet<Employee>();
+		
+	}
+}
