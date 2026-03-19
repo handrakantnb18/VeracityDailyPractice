19220f48e2bd22c909c3e052f27f44b60f9cc71c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-19 22:29:04 +0530

    stack method synchronized, thread safe, LIFO method allowed

--- collection/src/com/collection/linkedlist/LinkedListDemo.java
+++ collection/src/com/collection/linkedlist/LinkedListDemo.java
@@ -1,26 +1,45 @@
 package com.collection.linkedlist;
 
 import java.util.LinkedList;
+import java.util.Stack;
 import java.util.Vector;
 
 public class LinkedListDemo {
 
 	public static void main(String[] args) {
+		
+		// stack is legacy class
+		// stack is synchronized class
+		// stack is thread safe
+		// LIFO - method is access
+		
+		Stack<Integer> s = new Stack<Integer>();
+		s.add(255);
+		s.add(122);
+		s.add(25);
+		s.add(45);
+		s.add(11);
+		s.add(null);
+		s.add(null);
+		
+		System.out.println(s);
+		
+		
 		// vector is a legacy class / old class
 		// vector is synchronized class
 		// vector is a thread safe
 		
-		Vector<Integer> v = new Vector<Integer>();
-		v.add(255);
-		v.add(122);
-		v.add(25);
-		v.add(45);
-		v.add(11);
-		v.add(null);
-		v.add(null);
-		
-		System.out.println(v);
-		
+//		Vector<Integer> v = new Vector<Integer>();
+//		v.add(255);
+//		v.add(122);
+//		v.add(25);
+//		v.add(45);
+//		v.add(11);
+//		v.add(null);
+//		v.add(null);
+//		
+//		System.out.println(v);
+//		
 		
 //		LinkedList<Integer> list = new LinkedList<Integer>();
 //		list.add(255);
