50edc9526be9e01f66a500fc7e0305e3a7ec00cf
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-20 00:18:03 +0530

    UsersDemo child class

---
+++ collection/src/com/hashset/UsersDemo.java
@@ -0,0 +1,12 @@
+package com.hashset;
+
+import java.util.HashSet;
+
+public class UsersDemo {
+
+	public static void main(String[] args) {
+		
+		HashSet<Users> hs = new HashSet<Users>();
+		
+	}
+}
