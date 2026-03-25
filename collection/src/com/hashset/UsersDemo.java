eaf168e320db24070a239010b3e10394743a3f77
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-25 21:43:25 +0530

    User object has been created

--- collection/src/com/hashset/UsersDemo.java
+++ collection/src/com/hashset/UsersDemo.java
@@ -7,6 +7,10 @@ public class UsersDemo {
 	public static void main(String[] args) {
 		
 		HashSet<Users> hs = new HashSet<Users>();
+		hs.add(new Users(1, "chandrakant", "123456", "Pune", "chandrakant@gmail.com"));
+		hs.add(new Users(2, "ram", "ram", "mumbai", "ram12@gmail.com"));
 		
+		
+		System.out.println(hs);
 	}
 }
