6c9462e3d55858e1040ca10c48d2da9c49d0617e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-26 22:07:33 +0530

    connection created successfully

--- JDBC_1/src/com/demo/Demo.java
+++ JDBC_1/src/com/demo/Demo.java
@@ -2,24 +2,28 @@ package com.demo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class Demo {
 
 	public static void main(String[] args) {
 
+		
+		
+		// Driver Class Load / Register
+		// connection is interface
+		// connection is a return type of getConnection methods
+		// DriverManager is a class
+		// getConnection method present in DriverManager class
+		// createStatement is the method of Connection interface
+		
+		
 		try {
-
-			
-			// Driver Class Load / Register
-			// connection is interface
-			// connection is a return type of getConnection methods
-			// DriverManager is a class
-			// getConnection method present in DriverManager class
 			
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "root");
 			
-			
+			Statement stmt = conn.createStatement();
 			
 			
 //			
