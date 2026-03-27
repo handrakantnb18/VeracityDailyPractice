69fa8386daa9dd482f4251f28e48e4dbe5a98e8c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-27 20:30:24 +0530

    employee update the employee

--- JDBC_4/src/com/dao/EmployeeDao.java
+++ JDBC_4/src/com/dao/EmployeeDao.java
@@ -13,9 +13,9 @@ public class EmployeeDao {
 //	}
 
 	Connection conn = com.util.JDBCUtil.createConnection();
-	
+
 	// Connection conn = null;
-	
+
 //	public static void createTable()
 //	{
 //		try
@@ -32,30 +32,55 @@ public class EmployeeDao {
 //			e.printStackTrace();
 //		}
 //	}
+
+//	public String insertEmployee(Employee employee) {
+//		try {
+//			PreparedStatement ps = conn.prepareStatement("insert into employees values(?,?,?,?,?,?)");
+//
+//			ps.setInt(1, employee.getId());
+//			ps.setString(2, employee.getName());
+//			ps.setDouble(3, employee.getSalary());
+//			ps.setString(4, employee.getGender());
+//			ps.setString(5, employee.getCity());
+//			ps.setString(6, employee.getDept());
+//
+//			ps.execute();
+//			System.out.println("Success");
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//		return "Employee inserted successfully";
+//	}
+
 	
-	
-  	public String insertEmployee(Employee employee)
-	{
-		try
-		{
-		PreparedStatement ps = conn.prepareStatement("insert into employees values(?,?,?,?,?,?)");
-		
-		ps.setInt(1, employee.getId());
-		ps.setString(2, employee.getName());
-		ps.setDouble(3, employee.getSalary());
-		ps.setString(4, employee.getGender());
-		ps.setString(5, employee.getCity());
-		ps.setString(6, employee.getDept());
-		
-		ps.execute();
-		System.out.println("Success");
-		
-		}
-		catch(Exception e)
-		{
+	public String update(Employee employee) {
+		try {
+			PreparedStatement ps = conn
+					.prepareStatement("update employees set name=?, gender=?, salary=?, city=?,dept=?, id=?");
+
+//			ps.setString(1, employee.getName());
+			ps.setString(1, employee.getGender());
+			ps.setDouble(2, employee.getSalary());
+			ps.setString(3, employee.getCity());
+			ps.setString(4, employee.getDept());
+			ps.setInt(5, employee.getId());
+			ps.setString(6, employee.getName());
+
+			
+			int value = ps.executeUpdate();
+			
+			if(value > 0)
+			{
+				return "Employee update successfully";
+			}
+			
+			
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-		return "Employee inserted successfully";
+		return null;
 	}
 }
