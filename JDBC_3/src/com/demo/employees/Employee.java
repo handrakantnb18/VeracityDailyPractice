9d996093c3dffcd512ed0f80505d23118fec2efc
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-27 10:32:00 +0530

    database connection code created

---
+++ JDBC_3/src/com/demo/employees/Employee.java
@@ -0,0 +1,41 @@
+package com.demo.employees;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import com.demo.dao.EmployeeDao;
+import com.demo.jdbcConnection.JDBCUtil;
+
+public class Employee {
+
+	// database code
+	
+		Connection conn = JDBCUtil.createConnection();
+		
+		// createTable
+		
+		public void createTable()
+		{
+			
+			try
+			{
+				
+				Statement stmt = conn.createStatement();
+				
+				stmt.execute("create table employees (id int, name varchar(34), salary double, dept varchar(34), gender varchar(34), city varchar(34))");
+				
+				System.out.println("table created");
+			
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		public static void main(String[] args) {
+			EmployeeDao dao = new EmployeeDao();
+			dao.createTable(); 
+		}
+
+}
