2ef44ee8ba5b6d664ed54a68374dad3ad1990b1b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-27 08:49:08 +0530

    multple student added at a time in same table

--- JDBC_2/src/com/demomysql/Demo.java
+++ JDBC_2/src/com/demomysql/Demo.java
@@ -17,10 +17,34 @@ public class Demo {
 //		
 		
 		Student s1 = new Student();
-		s1.setId(9);
-		s1.setName("sachin");
-		s1.setAddress("panjab");
-		s1.setMarks(89);
+		s1.setId(11);
+		s1.setName("smith");
+		s1.setAddress("sidany");
+		s1.setMarks(80);
+		
+		Student s2 = new Student();
+		s2.setId(12);
+		s2.setName("andru");
+		s2.setAddress("landon");
+		s2.setMarks(90);
+		
+		Student s3 = new Student();
+		s3.setId(13);
+		s3.setName("satish");
+		s3.setAddress("patana");
+		s3.setMarks(70);
+		
+		Student s4 = new Student();
+		s4.setId(14);
+		s4.setName("prakash");
+		s4.setAddress("jamnagar");
+		s4.setMarks(75);
+		
+		Student s5 = new Student();
+		s5.setId(15);
+		s5.setName("anil");
+		s5.setAddress("vijapur");
+		s5.setMarks(45);
 		
 		try
 		{
@@ -44,7 +68,15 @@ public class Demo {
 			
 //			stmt.execute("insert into student values('"+id+"', '"+name+"', '"+address+"', '"+marks+"')");
 			
-			stmt.execute("insert into student values('"+s1.getId()+"','"+s1.getName()+"','"+s1.getAddress()+"','"+s1.getMarks()+"')");
+//			stmt.execute("insert into student values('"+s1.getId()+"','"+s1.getName()+"','"+s1.getAddress()+"','"+s1.getMarks()+"')");
+			
+			stmt.execute("insert into student values('"+s2.getId()+"','"+s2.getName()+"','"+s2.getAddress()+"','"+s2.getMarks()+"')");
+			
+			stmt.execute("insert into student values('"+s3.getId()+"','"+s3.getName()+"','"+s3.getAddress()+"','"+s3.getMarks()+"')");
+			
+			stmt.execute("insert into student values('"+s4.getId()+"','"+s4.getName()+"','"+s4.getAddress()+"','"+s4.getMarks()+"')");
+			
+			stmt.execute("insert into student values('"+s5.getId()+"','"+s5.getName()+"','"+s5.getAddress()+"','"+s5.getMarks()+"')");
 			
 			System.out.println("table created");
 			
