77e5e3080eb2f37d006e4a67111a798e64a9f9dd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-27 20:29:41 +0530

    controll the employee for services

--- JDBC_4/src/com/controller/EmployeeController.java
+++ JDBC_4/src/com/controller/EmployeeController.java
@@ -7,9 +7,17 @@ public class EmployeeController {
 
 	EmployeeService ser = new EmployeeService();
 	
-	public String insertEmployee(Employee employee)
+//	public String insertEmployee(Employee employee)
+//	{
+//		String msg = ser.inseretEmployee(employee);
+//		
+//		return msg;
+//		
+//	}
+	
+	public String update(Employee employee)
 	{
-		String msg = ser.inseretEmployee(employee);
+		String msg  = ser.update(employee);
 		
 		return msg;
 	}
