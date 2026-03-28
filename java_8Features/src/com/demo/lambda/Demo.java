6061f0dd8272cba5be08eac2cdace1978c2725fc
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-28 20:20:01 +0530

    used return type int and added two values

--- java_8Features/src/com/demo/lambda/Demo.java
+++ java_8Features/src/com/demo/lambda/Demo.java
@@ -20,9 +20,17 @@ public class Demo {
 //		
 //		t.add(12, 20);
 		
-		Test t = (a, b) -> System.out.println(a+b);
+//		Test t = (a, b) -> System.out.println(a+b);
+//		
+//		t.add(25, 20);
 		
-		t.add(25, 20);
+		Test t = (x, y) ->
+		{
+			return x+y;
+		};
+		
+		int x = t.add(25, 25);
+		System.out.println(x);
 		
 	}
 }
