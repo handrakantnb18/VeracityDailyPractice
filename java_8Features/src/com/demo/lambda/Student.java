5c19e95387b9e06e9339c691b5059792b6340009
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-28 21:15:21 +0530

    private method and constructor and getter and setter method are created

---
+++ java_8Features/src/com/demo/lambda/Student.java
@@ -0,0 +1,31 @@
+package com.demo.lambda;
+
+public class Student {
+
+	private int id;
+	
+	private String name;
+	
+	private String address;
+
+	public Student(int id, String name, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+	
+	
+}
