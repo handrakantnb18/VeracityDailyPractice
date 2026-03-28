d55eccf871a16353fa18e7e3300b8d9c320656f1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-28 12:01:58 +0530

    calculator class to abject created and addition will be created

--- java_8Features/src/com/demo/Calculator.java
+++ java_8Features/src/com/demo/Calculator.java
@@ -2,12 +2,26 @@ package com.demo;
 
 public class Calculator {
 
-	static void calculator()
+	static void calculator(Test x)
 	{
 		
+		// addition calculate
 		
+		x.calculator(100, 20);
 		
 		// i want to call calculate method into Test
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Addition addition = new Addition();
+		
+		Calculator.calculator(addition);
+		
+		// Test x = new Addition();
+		
+		
 		
 	}
 }
