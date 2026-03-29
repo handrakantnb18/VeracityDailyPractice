82f9e107f06224dbe7ff93ce99a309d929ff7385
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-29 19:40:14 +0530

    User Main class to extends all other class

---
+++ JDBC_User/src/com/main/UserMain.java
@@ -0,0 +1,11 @@
+package com.main;
+
+public class UserMain {
+
+	// 1. User -- username, password, email, gender, dob, mobileNo, address, age
+		// insert, update, delete, get, getall
+		
+	public static void main(String[] args) {
+		
+	}
+}
