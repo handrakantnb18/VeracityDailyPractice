c473c2a77456521251221d7abd43edee538d132a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-29 21:39:58 +0530

    UserDao is insertUser into database store data

--- JDBC_User/src/com/dao/UserDao.java
+++ JDBC_User/src/com/dao/UserDao.java
@@ -1,35 +1,66 @@
 package com.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 
+import com.user.User;
 import com.util.JDBCUtil;
 
 public class UserDao {
 
-	public static void main(String[] args) {
-		UserDao dao = new UserDao();
-		
-		dao.createTable();
-	}
+//	public static void main(String[] args) {
+//		UserDao dao = new UserDao();
+//		
+//		dao.createTable();
+//	}
 	
 	Connection conn = JDBCUtil.createConnection();
 	
 	
-	public  void createTable()
+//	public  void createTable()
+//	{
+//		try
+//		{
+//			Statement stmt = conn.createStatement();
+//			
+//			stmt.execute("create table user (id int, username varchar(34), password varchar(20), email varchar(34), gender varchar(20), dob varchar(20), mobileNo varchar(20), address varchar(34), age int )");
+//			
+//			System.out.println("Table created.");
+//		}
+//		catch(Exception e)
+//		{
+//			e.printStackTrace();
+//		}
+//		
+//	}
+	
+	// insert user
+	
+	public String insertUser(User user)
 	{
 		try
 		{
-			Statement stmt = conn.createStatement();
+			PreparedStatement ps = conn.prepareStatement("insert into user values(?,?,?,?,?,?,?,?,?)");
+		
+			ps.setInt(1, user.getId());
+			ps.setString(2, user.getUsername());
+			ps.setString(3, user.getPassword());
+			ps.setString(4, user.getEmail());
+			ps.setString(5, user.getGender());
+			ps.setString(6, user.getDob());
+			ps.setString(7, user.getMobileNo());
+			ps.setString(8, user.getAddress());
+			ps.setInt(9, user.getAge());
 			
-			stmt.execute("create table user (id int, username varchar(34), password varchar(20), email varchar(34), gender varchar(20), dob varchar(20), mobileNo varchar(20), address varchar(34), age int )");
+			ps.execute();
 			
-			System.out.println("Table created.");
 		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
 		}
 		
+		return "User inserted successfuly";
 	}
 }
