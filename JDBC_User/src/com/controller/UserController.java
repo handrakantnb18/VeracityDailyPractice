9c9f3af9bb9970fcbb67e83669ca16bf2bf53078
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-29 23:01:06 +0530

    user controller by the user data deleted single user

--- JDBC_User/src/com/controller/UserController.java
+++ JDBC_User/src/com/controller/UserController.java
@@ -7,10 +7,36 @@ public class UserController {
 
 	UserService service = new UserService();
 	
+	// insertUser data
+	
 	public String insertUser(User user)
 	{
 		String msg =  service.insertUser(user);
 		
 		return msg;
 	}
+	
+	
+	// updateUser data
+	
+	public String update(User user)
+	{
+		String msg = service.update(user);
+		
+		return msg;
+	}
+	
+	// delete one user 
+	
+	public String delete(int id)
+	{
+		return service.delete(id);
+	}
+	
+	
+	
+	// get single user from table
+	
+	
+	
 }
