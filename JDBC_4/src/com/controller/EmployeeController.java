acef47d9463e20e68530409aebb6083ba8afec29
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-29 14:57:30 +0530

    String updated employees in controller

--- JDBC_4/src/com/controller/EmployeeController.java
+++ JDBC_4/src/com/controller/EmployeeController.java
@@ -5,19 +5,19 @@ import com.services.EmployeeService;
 
 public class EmployeeController {
 
-	EmployeeService ser = new EmployeeService();
+	EmployeeService service = new EmployeeService();
 	
-//	public String insertEmployee(Employee employee)
-//	{
-//		String msg = ser.inseretEmployee(employee);
-//		
-//		return msg;
-//		
-//	}
+	public String insertEmployee(Employee employee)
+	{
+		String msg = service.inseretEmployee(employee);
+		
+		return msg;
+		
+	}
 	
 	public String update(Employee employee)
 	{
-		String msg  = ser.update(employee);
+		String msg  = service.update(employee);
 		
 		return msg;
 	}
