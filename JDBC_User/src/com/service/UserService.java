c5980bf5d5e1f18e6d9941e77506d5e5c9393c32
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-29 23:04:06 +0530

    Delete user from table if no data found the error

--- JDBC_User/src/com/service/UserService.java
+++ JDBC_User/src/com/service/UserService.java
@@ -1,6 +1,8 @@
 package com.service;
 
 import com.dao.UserDao;
+import com.exception.SomethingWentWrongException;
+import com.exception.UserIsNullException;
 import com.user.User;
 
 public class UserService {
@@ -9,11 +11,46 @@ public class UserService {
 	
 	UserDao dao = new UserDao();
 	
+	// insert user check the null 
 	public String insertUser(User user)
 	{
-		
+		if(user == null)
+		{
+			throw new UserIsNullException("User is Null");
+		}
 		 String msg = dao.insertUser(user);
 		
 		return msg;
 	}
+	
+	// update user data
+	
+	public String update(User user)
+	{
+		String msg= dao.updateUser(user);
+		
+		if(msg == null)
+		{
+			throw new SomethingWentWrongException("Check your SQL query.");
+		}
+		return msg;
+	}
+	
+	
+	// Delete user from table
+	
+	public String delete(int id)
+	{
+		String msg = dao.delete(id);
+		
+		if(msg == null)
+		{
+			throw new SomethingWentWrongException("Check your SQL Query.");
+		}
+		return msg;
+	}
+	
+	// get single user from table by id
+	
+	
 }
