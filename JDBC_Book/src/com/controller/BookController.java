10e101cd8fd050f9dae8e3ae548cca4ad8b6ea8d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-30 11:54:23 +0530

    BookControll insert book info as services

---
+++ JDBC_Book/src/com/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.controller;
+
+import com.book.Book;
+import com.services.BookService;
+
+public class BookController {
+
+	BookService service = new BookService();
+	
+	// insert book info
+	
+	public String insertBook(Book book)
+	{
+		 String msg = service.insertBook(book);
+		
+		return msg;
+		
+	}
+	
+	
+	// update book info
+	
+	
+	
+}
