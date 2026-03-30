900d540c2b87fa6640a52d136d55498b0bb157f2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-30 14:22:10 +0530

    ProductController is insert product in table

---
+++ JDBC_Product/src/com/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.controller;
+
+import com.product.Product;
+import com.services.ProductServices;
+
+public class ProductController {
+
+	ProductServices service = new ProductServices();
+	
+	// insert product into table
+	
+	public String insertProduct(Product product)
+	{
+		String msg= service.insertProduct(product);
+		
+		return msg;
+	}
+	
+	
+	
+	// update product into table
+	
+	
+}
