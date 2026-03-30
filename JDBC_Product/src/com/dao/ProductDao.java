b5a1ced841ef6559616e052d56e5a6774b2e5cfe
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-30 14:23:17 +0530

    ProductDao is insert product in table preparedStatement and statements used

--- JDBC_Product/src/com/dao/ProductDao.java
+++ JDBC_Product/src/com/dao/ProductDao.java
@@ -1,34 +1,62 @@
 package com.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 
+import com.product.Product;
 import com.util.JDBCUtil;
 
 public class ProductDao {
 
 	
-	public static void main(String[] args) {
-		ProductDao dao = new ProductDao();
-		
-		dao.createTable();
-		
-	}
+//	public static void main(String[] args) {
+//		ProductDao dao = new ProductDao();
+//		
+//		dao.createTable();
+//		
+//	}
 	
 	Connection conn = JDBCUtil.createConnection();
 	
 	// create product table
 	
-	public void createTable()
+//	public void createTable()
+//	{
+//		
+//		try
+//		{
+//			Statement stmt = conn.createStatement();
+//		
+//			stmt.execute("create table product (id int, name varchar(34), price int, category varchar(50), qty int)");
+//			
+//			System.out.println("Table created.");
+//			
+//		}
+//		catch(Exception e)
+//		{
+//			e.printStackTrace();
+//		}
+//		
+//	}
+	
+	
+	// insert data into table
+	
+	public String insertProduct(Product product)
 	{
 		
 		try
 		{
-			Statement stmt = conn.createStatement();
-		
-			stmt.execute("create table product (id int, name varchar(34), price int, category varchar(50), qty int)");
+			PreparedStatement ps = conn.prepareStatement("insert into product values (?,?,?,?,?)");
 			
-			System.out.println("Table created.");
+			ps.setInt(1, product.getId());
+			ps.setString(2, product.getName());
+			ps.setInt(3, product.getPrice());
+			ps.setString(4, product.getCategory());
+			ps.setInt(5, product.getQty());
+			
+			ps.execute();
 			
 		}
 		catch(Exception e)
@@ -36,5 +64,12 @@ public class ProductDao {
 			e.printStackTrace();
 		}
 		
+		return "Product inserted in table";
 	}
+	
+	
+	
+	// update product into table
+	
+	
 }
