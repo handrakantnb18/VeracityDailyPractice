426411f1965cbca58957a8830aa776b7eee0c1e3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-31 08:56:38 +0530

    Product Controller get and getAll product used to access data

--- JDBC_Product/src/com/controller/ProductController.java
+++ JDBC_Product/src/com/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.controller;
 
+import java.util.ArrayList;
+
 import com.product.Product;
 import com.services.ProductServices;
 
@@ -27,5 +29,23 @@ public class ProductController {
 	
 	// Delete one Product into table
 	
+	public String delete(int id)
+	{
+		return service.delete(id);
+	}
 	
+	// get single product into table
+	
+	public Product get(int id)
+	{
+		return service.getProductById(id);
+	}
+	
+	
+	// getAll Product from table
+	
+	public ArrayList<Product> getAll()
+	{
+		return service.getAll();
+	}
 }
