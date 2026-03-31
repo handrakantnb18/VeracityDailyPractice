e0d15275e2b56292ba7388f99fddd89fd204cbc5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-31 13:29:18 +0530

    dificult to create generics using different interface different datatypes

--- generics/src/com/generics/practice/Test.java
+++ generics/src/com/generics/practice/Test.java
@@ -14,5 +14,31 @@ public interface Test {
 		};
 		
 		t.add(55, 25);
+		
+		Test2 t2 = (a, b) ->
+		{
+			System.out.println(a+b);
+		};
+		
+		t2.add(55.56f, 50.55f);
+		
+		Test3 t3=(a, b)->
+		{
+			System.out.println(a+b);
+		};
+		t3.add(58.89, 40.48);
+		
 	}
+}
+
+interface Test2 {
+	void add(float a, float b);
+	
+	
+}
+
+interface Test3 
+{
+	void add(double a, double b);
+	
 }
