5e799c2e5dd2ae84c556a80083122590c697f7ec
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-03-31 13:50:50 +0530

    interger and float different generics types is used working good in generics

--- generics/src/com/generics/Test.java
+++ generics/src/com/generics/Test.java
@@ -13,6 +13,19 @@ public interface Test<T> {
 		
 		d1.fun("chandrakant");
 		
+		Test<Integer> d2=(a)->
+		{
+			System.out.println(a);
+		};
+		
+		d2.fun(5000);
+		
+		Test<Float> d3=(f)->
+		{
+			System.out.println(f);
+		};
+		
+		d3.fun(55.23f);
 		
 	}
 }
