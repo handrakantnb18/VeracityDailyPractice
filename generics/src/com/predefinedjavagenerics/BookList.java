6b065700433456eb203a97d339851d6ff44dbe51
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-01 14:02:18 +0530

    single book added in the data

--- generics/src/com/predefinedjavagenerics/BookList.java
+++ generics/src/com/predefinedjavagenerics/BookList.java
@@ -1,5 +1,20 @@
 package com.predefinedjavagenerics;
 
+import java.util.function.Consumer;
+
 public class BookList {
 
+	
+	public static void main(String[] args) {
+		
+		
+		// single Book added
+		
+		Consumer<Book> b = (x)->
+		{
+			System.out.println(x);
+		};
+		
+		b.accept(new Book(1, "Java", "Gemes gosling", 500));
+	}
 }
