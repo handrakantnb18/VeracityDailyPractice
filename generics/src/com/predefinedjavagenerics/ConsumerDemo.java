7d6d30e320c519dbc70a00e0b10c16694266d56b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-01 12:38:57 +0530

    Consumer String[] array is used to fetch array

--- generics/src/com/predefinedjavagenerics/ConsumerDemo.java
+++ generics/src/com/predefinedjavagenerics/ConsumerDemo.java
@@ -15,14 +15,30 @@ public class ConsumerDemo {
 //		
 //		c.accept(500);
 		
-		Consumer<String> c1= (j)->
+//		Consumer<String> c1= (j)->
+//		{
+//			System.out.println(j);
+//			System.out.println("Hi, :"+j.toUpperCase());
+//		};
+//	
+//		c1.accept("Chandrakant");
+//		c1.accept("Priyanka");
+		
+		
+		Consumer<String[]> c2= (j)->
 		{
-			System.out.println(j);
+			for(String s:j)
+			{
+			//System.out.println(j);
+			System.out.println(s.toUpperCase());
+			}
 		};
+		
+		c2.accept(new String[] {"Ram", "Priyanka"});
 	
-		c1.accept("Chandrakant");
-		
-		
+//		c1.accept("Chandrakant");
+//		c1.accept("Priyanka");
+
 		
 	}
 	
