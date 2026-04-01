6007c05cad6bb405629be7efe0473558b6e8bbf1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-01 13:38:05 +0530

    we have created single emplyee int he consumer generics

---
+++ generics/src/com/predefinedjavagenerics/EmployeeList.java
@@ -0,0 +1,28 @@
+package com.predefinedjavagenerics;
+
+import java.util.function.Consumer;
+
+public class EmployeeList {
+
+	// 34 predifined generics
+	
+	public static void main(String[] args) {
+		
+		// Single employee
+		
+//		Consumer<Employee> c1 = (x)->
+//		{
+//			for(Employee e: x)
+//			{
+//				System.out.println(e);
+//			}
+//		};
+//		
+//		c1.accept(new Employee(1, "Priyanka", "Transport", "SP Pune"));
+//		
+		
+		// List of employees
+		
+		
+	}
+}
