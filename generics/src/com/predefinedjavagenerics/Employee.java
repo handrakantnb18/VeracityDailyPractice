cbe1ceb8a2c5af5fe0e440984e2ae6ea5f411953
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-01 13:15:35 +0530

    Employee is created private variables, toString method and constructor

---
+++ generics/src/com/predefinedjavagenerics/Employee.java
@@ -0,0 +1,28 @@
+package com.predefinedjavagenerics;
+
+public class Employee {
+
+	
+	private int id;
+	
+	private String name;
+	
+	private String dept;
+	
+	private String address;
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", dept=" + dept + ", address=" + address + "]";
+	}
+
+	public Employee(int id, String name, String dept, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dept = dept;
+		this.address = address;
+	}
+	
+	
+}
