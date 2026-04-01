966be81adfa88c63334436fe0fe1786a6e28d5d2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-01 17:04:20 +0530

    Student information stored

--- generics/src/com/predefinedjavagenerics/FunctionDemo.java
+++ generics/src/com/predefinedjavagenerics/FunctionDemo.java
@@ -30,6 +30,14 @@ public class FunctionDemo {
 		
 //		System.out.println(f.apply(100));
 		
+		Function<Student, Student> f=(a)->
+		{
+			return a;
+			// return new Student(1, "Ram", 54.60);
+		};
+		
+		System.out.println(f.apply(new Student(2, "Chandrakant", 55.50)));
+		
 		
 		
 	}
