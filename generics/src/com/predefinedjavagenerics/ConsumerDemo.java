a7e63ae03f140d725e447e81c46a35f5348e5fb7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-01 12:41:56 +0530

    int[] used to print array

--- generics/src/com/predefinedjavagenerics/ConsumerDemo.java
+++ generics/src/com/predefinedjavagenerics/ConsumerDemo.java
@@ -25,20 +25,31 @@ public class ConsumerDemo {
 //		c1.accept("Priyanka");
 		
 		
-		Consumer<String[]> c2= (j)->
-		{
-			for(String s:j)
-			{
-			//System.out.println(j);
-			System.out.println(s.toUpperCase());
-			}
-		};
-		
-		c2.accept(new String[] {"Ram", "Priyanka"});
+//		Consumer<String[]> c2= (j)->
+//		{
+//			for(String s:j)
+//			{
+//			//System.out.println(j);
+//			System.out.println(s.toUpperCase());
+//			}
+//		};
+//		
+//		c2.accept(new String[] {"Ram", "Priyanka"});
 	
 //		c1.accept("Chandrakant");
 //		c1.accept("Priyanka");
 
+		
+		Consumer<int[]> c3= (j)->
+		{
+			for(int s:j)
+			{
+				System.out.println(s);
+			}
+		};
+	
+
+		c3.accept(new int[] {1,2,3,4,5,8,9,7,6});
 		
 	}
 	
