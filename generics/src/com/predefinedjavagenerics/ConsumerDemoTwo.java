e276303b355a0c1227eab611310d2c31ac462be7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-01 12:59:16 +0530

    Print List of Student in the forEach loop

--- generics/src/com/predefinedjavagenerics/ConsumerDemoTwo.java
+++ generics/src/com/predefinedjavagenerics/ConsumerDemoTwo.java
@@ -1,16 +1,34 @@
 package com.predefinedjavagenerics;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerDemoTwo {
 
 	public static void main(String[] args) {
 		
-		Consumer<Student> c1 =(x)->
+//		Consumer<Student> c1 =(x)->
+//		{
+//				System.out.println(x);			
+//		};
+//		
+//		c1.accept(new Student(1, "Ram", 78.55));
+//		
+		
+		Consumer<List<Student>> c1 =(x)->
 		{
-				System.out.println(x);			
+			for(Student s:x)
+			{
+				System.out.println(s);
+			}
 		};
 		
-		c1.accept(new Student(1, "Ram", 78.55));
+		c1.accept(Arrays.asList(
+				new Student(1, "Ram", 78.55),
+				new Student(2, "Chandraaknt", 89.55),
+				new Student(3, "Priyanka", 85.66)));
+		
+		
 	}
 }
