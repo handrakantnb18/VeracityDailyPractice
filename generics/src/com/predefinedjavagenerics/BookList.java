06ca991ddf70ed853619f5d02cdbae74d1a31aa6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-01 14:08:21 +0530

    Multiple books are accept in using List Interface

--- generics/src/com/predefinedjavagenerics/BookList.java
+++ generics/src/com/predefinedjavagenerics/BookList.java
@@ -1,5 +1,7 @@
 package com.predefinedjavagenerics;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class BookList {
@@ -10,11 +12,31 @@ public class BookList {
 		
 		// single Book added
 		
-		Consumer<Book> b = (x)->
+//		Consumer<Book> b = (x)->
+//		{
+//			System.out.println(x);
+//		};
+//		
+//		b.accept(new Book(1, "Java", "Gemes gosling", 500));
+//		
+		
+		// multiple Books added in the list
+		
+		Consumer<List<Book>> b = (x)->
 		{
-			System.out.println(x);
+			for(Book b1: x)
+			{
+				System.out.println(b1);
+			}
 		};
 		
-		b.accept(new Book(1, "Java", "Gemes gosling", 500));
+		b.accept(Arrays.asList(new Book(1, "Think Positive", "Priyanka", 550),
+				new Book(2, "Java", "Gemes gosling", 500),
+				new Book(3, ".net", "Chandrakant", 350)));
+		
+		
+//		b.accept(new Book(1, "Java", "Gemes gosling", 500));
+		
+		
 	}
 }
