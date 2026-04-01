369955903d51bed621ac75ef7dd1517cdd2bb2bf
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-01 13:16:00 +0530

    Product is created private variables, toString method and constructor

---
+++ generics/src/com/predefinedjavagenerics/Product.java
@@ -0,0 +1,27 @@
+package com.predefinedjavagenerics;
+
+public class Product {
+
+	private int id;
+	
+	private String name;
+	
+	private int price;
+	
+	private String address;
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", address=" + address + "]";
+	}
+
+	public Product(int id, String name, int price, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.address = address;
+	}
+	
+	
+}
