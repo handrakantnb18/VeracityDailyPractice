8a6e621530d1905ee9f14a615ebceac909771992
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-01 14:28:30 +0530

    Boolean value accespt only if Integer, String float any Data type we can accept true or false

--- generics/src/com/predefinedjavagenerics/PredictGene.java
+++ generics/src/com/predefinedjavagenerics/PredictGene.java
@@ -10,13 +10,30 @@ public class PredictGene {
 		
 		// boolean value - true or false
 		
-		Predicate<Integer> p = (a)->
+//		Predicate<Integer> p = (a)->
+//		{
+//			//return a > 20;
+//			return a % 2 == 0;
+//			
+//			//return a.
+//		};
+//		
+//		boolean b = p.test(100);
+//		System.out.println(b)
+
+		Predicate<String> p = (a)->
 		{
-			//return a > 20;
-			return a % 2 == 0;
+			
+			//return a.length() > 5;
+			
+			// return a.startsWith("C");
+			// return a.equals("Chandrakant");
+			
+			return a.contains(a);
+			
 		};
 		
-		boolean b = p.test(100);
+		boolean b = p.test("Chandrakant");
 		System.out.println(b);
 	}
 }
