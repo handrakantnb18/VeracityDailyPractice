0fb5488ebc664611f76b4b8a0754dcf0eebc0e53
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-01 13:15:13 +0530

    Book is created private variables, toString method and constructor

---
+++ generics/src/com/predefinedjavagenerics/Book.java
@@ -0,0 +1,27 @@
+package com.predefinedjavagenerics;
+
+public class Book {
+
+	private int id;
+	
+	private String name;
+	
+	private String auther;
+	
+	private int price;
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", auther=" + auther + ", price=" + price + "]";
+	}
+
+	public Book(int id, String name, String auther, int price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.auther = auther;
+		this.price = price;
+	}
+	
+	
+}
