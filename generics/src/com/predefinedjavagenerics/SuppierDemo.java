7dacc1decae1730629340226a8a0b682c1e62019
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-01 14:40:44 +0530

    int[] and String[] accept in the Supplier is used

--- generics/src/com/predefinedjavagenerics/SuppierDemo.java
+++ generics/src/com/predefinedjavagenerics/SuppierDemo.java
@@ -1,5 +1,6 @@
 package com.predefinedjavagenerics;
 
+import java.util.Arrays;
 import java.util.function.Supplier;
 
 public class SuppierDemo {
@@ -9,21 +10,38 @@ public class SuppierDemo {
 		
 		 // integer wrapper class
 		
-		Supplier<Integer> a=() ->
+//		Supplier<Integer> a=() ->
+//		{
+//			return 100;
+//		};
+//		
+//		System.out.println(a.get());
+//		
+//		// String wrapper class
+//		
+//		Supplier<String> s =()->
+//		{
+//			return "Chandraknt";
+//			
+//		};
+//		
+//		System.out.println(s.get());
+		
+		Supplier<int[]> s = () ->
 		{
-			return 100;
+			return new int[] {1, 2,3,4,5,6,7};
 		};
 		
-		System.out.println(a.get());
+		System.out.println(Arrays.toString(s.get()));
 		
-		// String wrapper class
-		
-		Supplier<String> s =()->
+		Supplier<String[]> s1 = () ->
 		{
-			return "Chandraknt";
-			
+			return new String[] {"A","B","C","D","E"};
 		};
 		
-		System.out.println(s.get());
+		System.out.println(Arrays.toString(s1.get()));
+		
+		
+		
 	}
 }
