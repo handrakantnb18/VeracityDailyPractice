293102ebf133bd634e52c2e866de9aac2ac40398
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-01 13:59:27 +0530

    multiple Product added in data

--- generics/src/com/predefinedjavagenerics/ProductList.java
+++ generics/src/com/predefinedjavagenerics/ProductList.java
@@ -1,5 +1,7 @@
 package com.predefinedjavagenerics;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ProductList {
@@ -7,12 +9,32 @@ public class ProductList {
 	
 	public static void main(String[] args) {
 		
-		Consumer<Product> c1 = (a)->
-		{
-			System.out.println(a);
-		};
 		
-		c1.accept(new Product(1, "Samsung", 25000, "Pune"));
+		 // single product access
+		
+//		Consumer<Product> c1 = (a)->
+//		{
+//			System.out.println(a);
+//		};
+//		
+//		c1.accept(new Product(1, "Samsung", 25000, "Pune"));
+//		
+		
+		// multiple product accept
+		
+		Consumer<List<Product>> c1 = (a)->
+		{
+			for(Product p : a)
+			{
+				System.out.println(p);
+			}
+		};
+		c1.accept(Arrays.asList
+				(new Product(1, "OPPO", 56000, "Mumbai"),
+				new Product(2, "Samsung", 25000, "Pune"),
+				new Product(3, "MI", 8800, "Delhi")));
+	
+		
 		
 	}
 }
