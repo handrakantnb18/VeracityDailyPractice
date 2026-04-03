b21114ea5f60602afae5a6ee4492be618164dd2a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-03 16:07:57 +0530

    User byte Stirng long data types used

---
+++ firstJavaProgram/src/com/datatypes/User.java
@@ -0,0 +1,41 @@
+package com.datatypes;
+
+public class User {
+
+	public static void main(String[] args) {
+		
+		byte userId = 1;
+		String username = "Chandrakant";
+		String email = "chandrakant@gmail.com";
+		byte age = 27;
+		String address = "Pune";
+		long mobileNo = 93625412541l;
+		
+		byte userId1 = 2;
+		String username1 = "Priyanka";
+		String email1 = "priyanka@gmail.com";
+		byte age1 = 25 ;
+		String address1 = "Pune";
+		long mobileNo1 = 93625412541l;
+		
+		
+		System.out.println(" ");
+		System.out.println("User Id   : "+userId);
+		System.out.println("User Name : "+username);
+		System.out.println("Email     : "+email);
+		System.out.println("Age       : "+age);
+		System.out.println("Address   : "+address);
+		System.out.println("Mobile No : "+mobileNo);
+		System.out.println("----------------------------");
+		
+		System.out.println(" ");
+		System.out.println("User Id   : "+userId1);
+		System.out.println("User Name : "+username1);
+		System.out.println("Email     : "+email1);
+		System.out.println("Age       : "+age1);
+		System.out.println("Address   : "+address1);
+		System.out.println("Mobile No : "+mobileNo1);
+		
+		
+	}
+}
