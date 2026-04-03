4fec4ccb831ce5e6d6b16dd7f127edc32c3a1f36
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-03 16:10:01 +0530

    Employee byte String int char variables used

---
+++ firstJavaProgram/src/com/datatypes/Employee.java
@@ -0,0 +1,43 @@
+package com.datatypes;
+
+public class Employee {
+
+	
+	public static void main(String[] args) {
+		
+		byte id = 101;
+		String name = "Chandrakant";
+		float marks = 55.55f;
+		String address = "Pune";
+		boolean isMarried = true;
+		char gender ='M';
+		long mobileNo = 90230233230l;
+		
+		byte id1 = 102;
+		String name1 = "Chandrakant";
+		float marks1 = 55.55f;
+		String address1 = "Pune";
+		boolean isMarried1 = true;
+		char gender1 ='M';
+		long mobileNo1 = 90230233230l;
+		
+		System.out.println("");
+		System.out.println("Id        : "+id);
+		System.out.println("Name      : "+name);
+		System.out.println("Marks     : "+marks);
+		System.out.println("Address   : "+address);
+		System.out.println("IsMarried : "+isMarried);
+		System.out.println("Gender    : "+gender);
+		System.out.println("MobileNo  : "+mobileNo);
+		System.out.println("--------------------------");
+
+		System.out.println("Id        : "+id1);
+		System.out.println("Name      : "+name1);
+		System.out.println("Marks     : "+marks1);
+		System.out.println("Address   : "+address1);
+		System.out.println("IsMarried : "+isMarried1);
+		System.out.println("Gender    : "+gender1);
+		System.out.println("MobileNo  : "+mobileNo1);
+
+	}
+}
