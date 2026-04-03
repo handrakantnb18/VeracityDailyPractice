4917cf1ff9b98d1cb862393aa3b9ec8ffe01498e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-03 16:06:54 +0530

    Employee created simple program

---
+++ firstJavaProgram/src/com/datatypes/EmpTwo.java
@@ -0,0 +1,63 @@
+package com.datatypes;
+
+public class EmpTwo {
+
+	public static void main(String[] args) {
+		
+		int id = 1;
+		String name = "Chandrakant";
+		String dept = "IT";
+		int salary = 25000;
+		String address = "Pune";
+		byte age = 27;
+		long mobileNo = 9632587412l;
+		
+		int id2 = 2;
+		String name2 = "Priyanka";
+		String dept2 = "Transport";
+		int salary2 = 15000;
+		String address2 = "Pune";
+		byte age2 = 25;
+		long mobileNo2 = 9632587412l;
+		
+		int id3 = 3;
+		String name3 = "Ram";
+		String dept3 = "Sales";
+		int salary3 = 35000;
+		String address3 = "Pune";
+		byte age3 = 30;
+		long mobileNo3 = 9632587412l;
+		
+		System.out.println("");
+		System.out.println("ID        : "+id);
+		System.out.println("Name      : "+name);
+		System.out.println("Dept      : "+dept);
+		System.out.println("Salary    : "+salary);
+		System.out.println("Address   : "+address);
+		System.out.println("Age       : "+age);
+		System.out.println("Mobile No : "+mobileNo);
+		System.out.println("------------------------ ");
+
+		System.out.println("");
+		System.out.println("ID        : "+id2);
+		System.out.println("Name      : "+name2);
+		System.out.println("Dept      : "+dept2);
+		System.out.println("Salary    : "+salary2);
+		System.out.println("Address   : "+address2);
+		System.out.println("Age       : "+age2);
+		System.out.println("Mobile No : "+mobileNo2);
+		System.out.println("------------------------ ");
+
+		System.out.println("");
+		System.out.println("ID        : "+id3);
+		System.out.println("Name      : "+name3);
+		System.out.println("Dept      : "+dept3);
+		System.out.println("Salary    : "+salary3);
+		System.out.println("Address   : "+address3);
+		System.out.println("Age       : "+age3);
+		System.out.println("Mobile No : "+mobileNo3);
+		System.out.println("------------------------ ");
+
+
+	}
+}
