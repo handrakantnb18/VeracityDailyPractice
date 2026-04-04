71e864d24fb98e05da83c3fc47c061996458caf1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-04 13:33:57 +0530

    Even or Odd numbers print simple way

---
+++ java_8Features/src/com/streamAPI/Demo.java
@@ -0,0 +1,24 @@
+package com.streamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		List<Integer> numbers =Arrays.asList(1,2,3,4,88,54,45,3,7,9,107);
+		
+		// Even or Odd numbers print
+		for(int i:numbers)
+		{
+//			if(i % 2 == 0)
+			if(i % 2 != 0)
+			{
+				System.out.println(i);
+			}
+		}
+		
+		
+	}
+}
