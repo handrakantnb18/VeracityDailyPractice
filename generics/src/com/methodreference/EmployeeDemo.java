c6f1f2635f5a2bb9f5f239d8f33fbc0a46c91592
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-04 14:06:06 +0530

    List of Integer is print

--- generics/src/com/methodreference/EmployeeDemo.java
+++ generics/src/com/methodreference/EmployeeDemo.java
@@ -2,16 +2,26 @@ package com.methodreference;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class EmployeeDemo {
 
 	public static void main(String[] args) {
 		
-		List<Employee> emp = Arrays.asList(new Employee(1, "Chandraaknt", "Male", 2500000.00, "Pune"));
+		
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		
+		Stream<List<Integer>> stream = Stream.of(list);
+		
+		stream.forEach((i) -> System.out.println(i));
+		
+		
+		
+//		List<Employee> emp = Arrays.asList(new Employee(1, "Chandraaknt", "Male", 2500000.00, "Pune"));
 		
 		//System.out.println(emp);
 		
-		emp.forEach((i) ->System.out.println(i));
+//		emp.forEach((i) ->System.out.println(i));
 		
 	}
 }
