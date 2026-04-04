d1340f95c9dff40d22306bfbba7cf4682eb6fff2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-04 12:50:20 +0530

    Demo make diffrent method to access abstract method reference to operation

--- generics/src/com/methodreference/Demo.java
+++ generics/src/com/methodreference/Demo.java
@@ -6,6 +6,15 @@ public class Demo {
 	{
 		System.out.println("This is fun method.");
 	}
+	
+	void display()
+	{
+		int a = 100, b = 20;
+		System.out.println("Addition       : "+(a+b));
+		System.out.println("Substraction   : "+(a-b));
+		System.out.println("Division       : "+(a/b));
+		System.out.println("Multiplication : "+(a*b));
+	}
 	public static void main(String[] args) {
 		
 //		Test t =()->
@@ -15,17 +24,19 @@ public class Demo {
 //		
 //		t.add();
 		
-		// Scope resolution operator
+		// Scope resolution operator ::
 		
 //		Demo d = new Demo();
 //		
 //		Test t = d::fun; // method reference
 //		
 		
-		Test t = new Demo()::fun; // method reference
+//		Test t = new Demo()::fun; // method reference
+//		
+//		t.add();
+		
+		Test t = new Demo()::display;
 		
 		t.add();
-		
-		
 	}
 }
