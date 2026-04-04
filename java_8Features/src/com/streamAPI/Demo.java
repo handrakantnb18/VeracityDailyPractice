5089dd9a9a276ad1ce4dde9f1b58598175ba9b2e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-04 13:36:26 +0530

    using stream filter and lambda used to find even and odd numbers

--- java_8Features/src/com/streamAPI/Demo.java
+++ java_8Features/src/com/streamAPI/Demo.java
@@ -9,15 +9,19 @@ public class Demo {
 		
 		List<Integer> numbers =Arrays.asList(1,2,3,4,88,54,45,3,7,9,107);
 		
+		numbers.stream().filter(i -> i % 2 == 0).forEach(i -> System.out.println(i));
+		
+		
+		
 		// Even or Odd numbers print
-		for(int i:numbers)
-		{
-//			if(i % 2 == 0)
-			if(i % 2 != 0)
-			{
-				System.out.println(i);
-			}
-		}
+//		for(int i:numbers)
+//		{
+////			if(i % 2 == 0)
+//			if(i % 2 != 0)
+//			{
+//				System.out.println(i);
+//			}
+//		}
 		
 		
 	}
