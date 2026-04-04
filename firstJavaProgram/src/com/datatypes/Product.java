292e92aab81aac3187fc8d9886e6bfdc91c9ddcf
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-04 12:35:27 +0530

    Product to make simple program

---
+++ firstJavaProgram/src/com/datatypes/Product.java
@@ -0,0 +1,51 @@
+package com.datatypes;
+
+public class Product {
+
+	public static void main(String[] args) {
+		
+		byte id = 11;
+		String name ="OPPO";
+		int price = 25000;
+		short qty = 25;
+		char category = 'A';
+		
+		byte id2 = 12;
+		String name2 ="Samsung";
+		int price2 = 30000;
+		short qty2 = 25;
+		char category2 = 'A';
+		
+		byte id3 = 13;
+		String name3 ="OnePlus";
+		int price3 = 20000;
+		short qty3 = 25;
+		char category3 = 'A';
+		
+		
+		System.out.println("");
+		System.out.println("Id       : "+id);
+		System.out.println("Name     : "+name);
+		System.out.println("Price    : "+price);
+		System.out.println("Qty      : "+qty);
+		System.out.println("Category : "+category);
+		System.out.println("--------------------------");
+
+		System.out.println("");
+		System.out.println("Id       : "+id2);
+		System.out.println("Name     : "+name2);
+		System.out.println("Price    : "+price2);
+		System.out.println("Qty      : "+qty2);
+		System.out.println("Category : "+category2);
+		
+		System.out.println("--------------------------");
+
+		System.out.println("");
+		System.out.println("Id       : "+id3);
+		System.out.println("Name     : "+name3);
+		System.out.println("Price    : "+price3);
+		System.out.println("Qty      : "+qty3);
+		System.out.println("Category : "+category3);
+		
+	}
+}
