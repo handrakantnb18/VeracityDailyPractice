48a7bb66d53094d2e0b96aaf8834f060006404e6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-05 16:19:35 +0530

    Filter Even Numbers using Stream

--- java_8featuresCodingPro/src/com/codingInterviewQuestions/EvenNumbers.java
+++ java_8featuresCodingPro/src/com/codingInterviewQuestions/EvenNumbers.java
@@ -7,11 +7,13 @@ public class EvenNumbers {
 
 	public static void main(String[] args) {
 		
+		// Even Numbers
+		
 		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
 		
 		list.stream()
-		.filter(n -> n % 2 == 0 )
-		.forEach(System.out::println);
+		 	.filter(n -> n % 2 == 0 )
+		 	.forEach(System.out::println);
 		
 	}
 }
