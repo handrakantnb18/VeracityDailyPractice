d1e7dff5055f4275fff96e9e7bc8454d6043d920
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-05 16:19:55 +0530

    Filter Odd Numbers using Stream

---
+++ java_8featuresCodingPro/src/com/codingInterviewQuestions/OddNumbers.java
@@ -0,0 +1,19 @@
+package com.codingInterviewQuestions;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class OddNumbers {
+
+	public static void main(String[] args) {
+		
+		// Odd numbers 
+		
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
+				
+		list.stream()
+			.filter(n -> n % 2 != 0)
+			.forEach(System.out::println);
+		
+	}
+}
