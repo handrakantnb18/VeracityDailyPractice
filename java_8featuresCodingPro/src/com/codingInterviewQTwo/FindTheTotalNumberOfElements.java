b53221a24644fc170bf06931901856b3e4cefdd8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-06 22:42:33 +0530

    Find The Total Number Of Elements

---
+++ java_8featuresCodingPro/src/com/codingInterviewQTwo/FindTheTotalNumberOfElements.java
@@ -0,0 +1,17 @@
+package com.codingInterviewQTwo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FindTheTotalNumberOfElements {
+
+	public static void main(String[] args) {
+		
+		 List<Integer> myList = Arrays.asList(9,8,77,10,15,8,49,25,98,98,32,15);
+         long count =  myList.stream()
+                             .count();
+         System.out.println(count);   
+
+         
+	}
+}
