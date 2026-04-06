42f3a53b164349456a33425201c5459b3a5fba02
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-06 13:38:40 +0530

    check the discount of every elseIf statements

--- controlFlowProject/src/IFElseStatement/Discount.java
+++ controlFlowProject/src/IFElseStatement/Discount.java
@@ -9,14 +9,26 @@ public class Discount {
 		if(price > 10000)
 		{
 			System.out.println("Discount allpied 10% ");
+			double discount = price * 0.10;
+			System.out.println("Total Discount : "+discount);
+			System.out.println("Final price : "+(price - discount));
+			
 		}
 		else if(price > 7000)
 		{
 			System.out.println("Discount applied 7% ");
+			double discount = price * 0.07;
+			System.out.println("Total Discount : "+discount);
+			System.out.println("Final price : "+(price - discount));
+			
 		}
 		else if(price > 3000)
 		{
 			System.out.println("Price applied 3%");
+			double discount = price * 0.03;
+			System.out.println("Total Discount : "+discount);
+			System.out.println("Final price : "+(price - discount));
+			
 		}
 		else
 		{
