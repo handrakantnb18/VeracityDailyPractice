3f4221597fdde5ab31ae0285a95bacc71aa5bb61
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-06 22:44:37 +0530

    5. Given a list of integers, find the total number of elements present in the list using Stream functions?

--- java_8featuresCodingPro/src/com/codingInterviewQTwo/FindTheTotalNumberOfElements.java
+++ java_8featuresCodingPro/src/com/codingInterviewQTwo/FindTheTotalNumberOfElements.java
@@ -7,11 +7,14 @@ public class FindTheTotalNumberOfElements {
 
 	public static void main(String[] args) {
 		
-		 List<Integer> myList = Arrays.asList(9,8,77,10,15,8,49,25,98,98,32,15);
-         long count =  myList.stream()
-                             .count();
-         System.out.println(count);   
+//		 List<Integer> myList = Arrays.asList(9,8,77,10,15,8,49,25,98,98,32,15);
+//         long count =  myList.stream()
+//                             .count();
+//         System.out.println(count);   
 
+         int[] arr = {10,15,8,49,25,98,98,32,15};
          
+         Arrays.stream(arr).boxed().count();  
+         System.out.println();
 	}
 }
