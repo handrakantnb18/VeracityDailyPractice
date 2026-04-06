5fd909c1821f1dcc63b4f8e13fe475aa65d23677
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-06 22:12:38 +0530

    2. Given a list of integers, find out all the numbers starting with 1 using Stream functions?

--- java_8featuresCodingPro/src/com/codingInterviewQTwo/NumberStartingWithOne.java
+++ java_8featuresCodingPro/src/com/codingInterviewQTwo/NumberStartingWithOne.java
@@ -1,7 +1,9 @@
 package com.codingInterviewQTwo;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class NumberStartingWithOne {
 
@@ -13,6 +15,15 @@ public class NumberStartingWithOne {
 		.map(s -> s + " ")
 		.filter(s -> s.startsWith("1"))
 		.forEach(System.out::println);
+		
+		
+		int[] arr = {10,20,15,35,60,50,55,45};
+		List<String> list2 = Arrays.stream(arr).boxed()
+				.map(s -> s + "")
+				.filter(s -> s.startsWith("5"))
+				.collect(Collectors.toList());
+		
+		System.out.println(list2);
 		
 		
 	}
