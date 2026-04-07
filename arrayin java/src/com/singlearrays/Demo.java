f2bf77b215f460774e3413882b6ae8e6a8b2ab3c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-07 14:41:47 +0530

    char datatype access // if not present value then space value

--- arrayin java/src/com/singlearrays/Demo.java
+++ arrayin java/src/com/singlearrays/Demo.java
@@ -4,20 +4,34 @@ public class Demo {
 
 	public static void main(String[] args) {
 		
-		String names[] = new String[5];
+		// if not present value then    (Space) value
+		char c[] = new char[2];
+		c[0]='A';
+		c[1]='B';
 		
-		names[0]="Chandraknt";
-		names[1]="Ram";
-		names[2]="Priyanka";
-		names[3]="Pooja";
-		names[4]="Vijay";
+		System.out.println(c[0]);
+		System.out.println(c[1]);
 		
-		System.out.println(names[0]);
-		System.out.println(names[1]);
-		System.out.println(names[2]);
-		System.out.println(names[3]);
-		System.out.println(names[4]);
 		
+		
+		
+		// if not present value then NULL value
+//		String names[] = new String[5];
+//		
+//		names[0]="Chandraknt";
+//		names[1]="Ram";
+//		names[2]="Priyanka";
+//		names[3]="Pooja";
+//		names[4]="Vijay";
+//		
+//		System.out.println(names[0]);
+//		System.out.println(names[1]);
+//		System.out.println(names[2]);
+//		System.out.println(names[3]);
+//		System.out.println(names[4]);
+//		
+		
+		// if not present value then 0(zero) value
 		
 //		int marks[] = new int[5];
 //		
