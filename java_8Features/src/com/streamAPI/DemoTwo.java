11b614185fde3f83ac519f85eac2304a9084ad05
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-07 12:28:04 +0530

    predicate boolean value if getMatch the true or false

--- java_8Features/src/com/streamAPI/DemoTwo.java
+++ java_8Features/src/com/streamAPI/DemoTwo.java
@@ -10,12 +10,48 @@ public class DemoTwo {
 
 	public static void main(String[] args) {
 		
-		List<String> backend = Arrays.asList("core java","core java", "Advanced java", "Spring Boot");
 		
-		Set<String> set = backend.stream().collect(Collectors.toSet());
+Employee e1 = new Employee(1, "Chandrakant", "Male", 250000.00, "Pune");
+		
+		Employee e2 = new Employee(2, "Priyanka", "Female", 350000, "Pune");
+		
+		Employee e3 = new Employee(3, "Ram", "Male", 550000, "Mumbai");
+		
+		Employee e4 = new Employee(4, "Vijay", "Male", 650000, "Mumbai");
+		
+		Employee e5 = new Employee(5, "Seema", "Male", 220000, "Pune");
+		
+		List<Employee> employees = Arrays.asList(e1,e2,e3,e4,e5);
+		
+		// redicate boolean value
+		
+//		boolean anyMatch = employees.stream().anyMatch(e ->e.getCity().equals("Pune"));
+//
+//		System.out.println("Namy match in pune : "+anyMatch);
+		
+//		boolean anyMatch = employees.stream().anyMatch(e ->e.getCity().equals("Mumbai"));
+//
+//		System.out.println("Namy match in Mumbai : "+anyMatch);
 
-		set.forEach(i->System.out.println(i));
 
+		boolean anyMatch = employees.stream().anyMatch(e ->e.getCity().equals("Banglore"));
+
+		System.out.println("Namy match in Banglore : "+anyMatch);
+
+		
+//		List<String> backend = Arrays.asList("core java","core java", "Advanced java", "Spring Boot");
+//		
+//		
+//		Set<String> set = backend.stream().collect(Collectors.toSet());
+//
+//		set.forEach(i->System.out.println(i));
+
+//		Set<String> set = backend.stream().collect(Collectors.toSet());
+//
+//		set.forEach(i->System.out.println(i));
+
+		
+		
 		
 //		List<String> list = backend.stream().collect(Collectors.toList());
 		
