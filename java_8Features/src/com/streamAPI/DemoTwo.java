e5ce59868dcd67cf56bd5dd8e23f478d44c30ced
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-07 12:16:41 +0530

    stream Collect and Collectors method and access in list interface

--- java_8Features/src/com/streamAPI/DemoTwo.java
+++ java_8Features/src/com/streamAPI/DemoTwo.java
@@ -2,6 +2,7 @@ package com.streamAPI;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class DemoTwo {
@@ -10,15 +11,24 @@ public class DemoTwo {
 		
 		List<String> backend = Arrays.asList("core java", "Advanced java", "Spring Boot");
 		
-		List<String> frontend = Arrays.asList("HTML", "CSS", "Java Script");
+		List<String> list = backend.stream().collect(Collectors.toList());
 		
-		List<String> db = Arrays.asList("SQL", "MYSQL", "Oracle");
-
-		List<List<String>> asList = Arrays.asList(backend, frontend,db);
+		list.forEach(i->System.out.println(i));
 		
-		Stream<String> flatmap =  asList.stream().flatMap(e->e.stream());
 		
-		flatmap.forEach(i -> System.out.println(i));
+//		List<String> frontend = Arrays.asList("HTML", "CSS", "Java Script");
+//		
+//		List<String> db = Arrays.asList("SQL", "MYSQL", "Oracle");
+//
+//		List<List<String>> asList = Arrays.asList(backend, frontend,db);
+//		
+//		Stream<String> flatmap =  asList.stream().flatMap(e->e.stream());
+//		
+//		flatmap.forEach(i -> System.out.println(i));
+		
+		
+		
+		
 		
 		
 //		asList.stream().forEach(i -> System.out.println(i));
