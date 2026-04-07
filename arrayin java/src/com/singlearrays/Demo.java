23957127710808cffe5235776f5062ecded3f6c1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-07 14:38:40 +0530

    created String[] arrays to implements the index 0

--- arrayin java/src/com/singlearrays/Demo.java
+++ arrayin java/src/com/singlearrays/Demo.java
@@ -4,16 +4,34 @@ public class Demo {
 
 	public static void main(String[] args) {
 		
-		int marks[] = new int[5];
+		String names[] = new String[5];
 		
-		marks[0]=100;
-		marks[1]=200;
+		names[0]="Chandraknt";
+		names[1]="Ram";
+		names[2]="Priyanka";
+		names[3]="Pooja";
+		names[4]="Vijay";
 		
-		System.out.println(marks[0]);
-		System.out.println(marks[1]);
-		System.out.println(marks[2]);
-		System.out.println(marks[3]);
-		System.out.println(marks[4]);
+		System.out.println(names[0]);
+		System.out.println(names[1]);
+		System.out.println(names[2]);
+		System.out.println(names[3]);
+		System.out.println(names[4]);
+		
+		
+//		int marks[] = new int[5];
+//		
+//		marks[0]=100;
+//		marks[1]=200;
+//		marks[2]=300;
+//		marks[3]=400;
+//		marks[4]=500;
+//		
+//		System.out.println(marks[0]);
+//		System.out.println(marks[1]);
+//		System.out.println(marks[2]);
+//		System.out.println(marks[3]);
+//		System.out.println(marks[4]);
 		
 	}
 }
