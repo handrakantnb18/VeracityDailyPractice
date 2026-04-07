1ac47b66af9b56688a6e5187c2ba0d6ce155cb8e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-07 14:30:49 +0530

    Arrays start how to store arrays in row or column

---
+++ arrayin java/src/com/singlearrays/Demo.java
@@ -0,0 +1,19 @@
+package com.singlearrays;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		int marks[] = new int[5];
+		
+		marks[0]=100;
+		marks[1]=200;
+		
+		System.out.println(marks[0]);
+		System.out.println(marks[1]);
+		System.out.println(marks[2]);
+		System.out.println(marks[3]);
+		System.out.println(marks[4]);
+		
+	}
+}
