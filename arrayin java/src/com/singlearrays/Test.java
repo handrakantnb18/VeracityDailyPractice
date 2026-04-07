20a0fd2f76a0cffa89137c945e0ee6d38e6c8809
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-07 14:50:57 +0530

    array accessed by using for loop to print data

---
+++ arrayin java/src/com/singlearrays/Test.java
@@ -0,0 +1,29 @@
+package com.singlearrays;
+
+public class Test {
+
+	
+	public static void main(String[] args) {
+		
+		int num[] = new int[5];
+		
+		num[0]=10;
+		num[1]=20;
+		num[2]=30;
+		num[3]=40;
+		num[4]=50;
+		
+		for(int i=0; i<5;i++)
+		{
+			System.out.println(num[i]);
+		}
+		
+//		System.out.println(num[0]);
+//		System.out.println(num[1]);
+//		System.out.println(num[2]);
+//		System.out.println(num[3]);
+//		System.out.println(num[4]);
+		
+		
+	}
+}
