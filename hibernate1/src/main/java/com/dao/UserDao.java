f3c617c134d55af62b01859f9aa240e6433876e2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-09 22:14:35 +0530

    UserDao class to getUser from session user using id

--- hibernate1/src/main/java/com/dao/UserDao.java
+++ hibernate1/src/main/java/com/dao/UserDao.java
@@ -1,21 +1,46 @@
 package com.dao;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import com.config.HibernateCongig;
+import com.entity.User;
+
 
 public class UserDao {
 
-	public static void main(String[] args) {
 		
 		SessionFactory sf = HibernateCongig.getSessionFactory();
-
+	
+	public String saveUser(User user)
+	{
+		Session session =  sf.openSession(); // save, update, delete, get
+		
+		Transaction tr = session.beginTransaction();
+		
+//		session.save(user);
+		
+		session.persist(user);
+		
+		tr.commit(); // save the user
+		
+		session.close();
+		
+		return "User Register successfuly";
+		
 	}
 	
-//	public void saveUser()
-//	{
-//	
-//	}
+	public User getUser(int id)
+	{
+		Session session = sf.openSession();
+		
+		User user = session.get(User.class, id);
+		
+		return user;
+		
+		
+	}
 	
 	
 }
