9aa138ce2f2d758a7b4f60aa902e9bfa248358ee
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-09 14:53:50 +0530

    created SessionFactory into Hibernate configuration file to create table user

---
+++ hibernate1/src/main/java/com/dao/UserDao.java
@@ -0,0 +1,21 @@
+package com.dao;
+
+import org.hibernate.SessionFactory;
+
+import com.config.HibernateCongig;
+
+public class UserDao {
+
+	public static void main(String[] args) {
+		
+		SessionFactory sf = HibernateCongig.getSessionFactory();
+
+	}
+	
+//	public void saveUser()
+//	{
+//	
+//	}
+	
+	
+}
