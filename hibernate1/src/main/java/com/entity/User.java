8bfe74478f13ea7d5bc66f85016df6bfa1fd4b46
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-09 14:19:48 +0530

    private variables and id is a primary key in the table

--- hibernate1/src/main/java/com/entity/User.java
+++ hibernate1/src/main/java/com/entity/User.java
@@ -1,5 +1,20 @@
 package com.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
 
+	@Id
+	private String id; // can not insert duplicate value and null value
+	
+	private String username;
+	
+	private String password;
+	
+	private String address;
+	
+	
+	
 }
