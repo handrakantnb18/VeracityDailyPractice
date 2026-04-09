1f8747efe51349d43340731f3f3d4ff3f2d3f618
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-09 16:24:51 +0530

    UserOrder @Table used as a self created Table name Ordeds

--- hibernate1/src/main/java/com/entity/UserOrder.java
+++ hibernate1/src/main/java/com/entity/UserOrder.java
@@ -2,8 +2,11 @@ package com.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+// @Table(name = "Order_info")
+@Table(name = "Orders")
 public class UserOrder {
 
 	@Id
