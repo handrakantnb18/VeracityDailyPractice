f169275f5c1d7bebf9f9302cf48fdea270340710
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-09 22:15:40 +0530

    User to create toString and constructor to getUser values and default constructor created because error default constructor

--- hibernate1/src/main/java/com/entity/User.java
+++ hibernate1/src/main/java/com/entity/User.java
@@ -7,14 +7,31 @@ import jakarta.persistence.Id;
 public class User {
 
 	@Id
-	private String id; // can not insert duplicate value and null value
+	private int id; // can not insert duplicate value and null value
 	
 	private String username;
 	
 	private String password;
 	
 	private String address;
+
 	
+	public User(int id, String username, String password, String address) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.address = address;
+	}
+
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", address=" + address + "]";
+	}
 	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
 	
 }
