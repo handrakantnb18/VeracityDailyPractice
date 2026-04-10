119071785f0bf5f9d8d54b9bedd7cdc172b61b92
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-10 10:45:25 +0530

    user getter and setter method access get and set the data

--- hibernate1/src/main/java/com/entity/User.java
+++ hibernate1/src/main/java/com/entity/User.java
@@ -33,5 +33,47 @@ public class User {
 	public User() {
 		// TODO Auto-generated constructor stub
 	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
 	
 }
