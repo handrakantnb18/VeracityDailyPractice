87f7af35efe9d44e1422ed36cc76bc7604685660
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-11 16:43:07 +0530

    Update book in database using service class

--- hibernate-2Book/src/main/java/com/controller/BookController.java
+++ hibernate-2Book/src/main/java/com/controller/BookController.java
@@ -7,10 +7,18 @@ public class BookController {
 
 	BookService service = new BookService();
 	
+	// save book
 	public String save(Book book)
 	{
 		return service.save(book);
 	}
 	
+	// update book
+	public String update(Book book)
+	{
+		return service.update(book);
+	}
+	
+	// 
 	
 }
