e5cc408dbe9089105ff6db6caf3eb89cd5b06038
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-11 13:14:45 +0530

    Book is to getter and setter method to get and set data

--- hibernate-2Book/src/main/java/com/entiry/Book.java
+++ hibernate-2Book/src/main/java/com/entiry/Book.java
@@ -23,11 +23,42 @@ public class Book {
 		this.price = price;
 	}
 
+	
 	@Override
 	public String toString() {
 		return "Book [id=" + id + ", bookname=" + bookname + ", auther=" + auther + ", price=" + price + "]";
 	}
-	
-	
-	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBookname() {
+		return bookname;
+	}
+
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+
+	public String getAuther() {
+		return auther;
+	}
+
+	public void setAuther(String auther) {
+		this.auther = auther;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+		
 }
