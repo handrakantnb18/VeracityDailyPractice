fde5f8e0c67ec09a605c114b135c181cbffbdf6c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-11 16:45:47 +0530

    BookMain update book id in table

--- hibernate-2Book/src/main/java/com/main/BookMain.java
+++ hibernate-2Book/src/main/java/com/main/BookMain.java
@@ -10,14 +10,26 @@ public class BookMain {
 		BookController controller = new BookController();
 		
 		// save data in DB
-		Book book = new Book(1, "Java", "Gemse Gosling", 500);
+//		Book book = new Book(1, "Java", "Gemse Gosling", 500);
 		
-		String msg = controller.save(book);
+//		Book book = new Book(2, "C#", "ABC", 450);
+
+//		Book book = new Book(3, "HTML", "XYZ", 250);
+//
+//		String msg = controller.save(book);
+//		
+//		System.out.println(msg);
 		
-		System.out.println(msg);
-		
-		// 
+		// Update user in data
 		
 		
+//		Book book = new Book(4, "HTML", "XYZ", 250);
+//
+//		String msg = controller.update(book);
+//		
+//		System.out.println(msg);
+		
+		
+		//
 	}
 }
