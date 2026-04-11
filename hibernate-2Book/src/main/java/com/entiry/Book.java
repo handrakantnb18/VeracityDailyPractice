304767a1c51616dd06d03f531f9e8537883a2e6a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-11 13:11:06 +0530

    constructor and private variables created

---
+++ hibernate-2Book/src/main/java/com/entiry/Book.java
@@ -0,0 +1,22 @@
+package com.entiry;
+
+public class Book {
+
+	private int id;
+	
+	private String bookname;
+	
+	private String auther;
+	
+	private int price;
+
+	public Book(int id, String bookname, String auther, int price) {
+		super();
+		this.id = id;
+		this.bookname = bookname;
+		this.auther = auther;
+		this.price = price;
+	}
+	
+	
+}
