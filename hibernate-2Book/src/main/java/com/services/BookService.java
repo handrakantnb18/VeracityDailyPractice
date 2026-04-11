5540399710a5d198d39b502871967872c1a39fe1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-11 18:07:34 +0530

    BookService delete dao class to delete by id

--- hibernate-2Book/src/main/java/com/services/BookService.java
+++ hibernate-2Book/src/main/java/com/services/BookService.java
@@ -7,14 +7,21 @@ public class BookService {
 
 	BookDao dao = new BookDao();
 	
+	// save
 	public String save(Book book)
 	{
 		return dao.saveBook(book);
 	}
 	
+	// update
 	public String update(Book book)
 	{
 		return dao.updateById(book);
 	}
 	
+	// delete by id
+	public String delete(int id)
+	{
+		return dao.delete(id);
+	}
 }
