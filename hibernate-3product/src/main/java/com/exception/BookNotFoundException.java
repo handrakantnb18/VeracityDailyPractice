a9d34c0c3b6eedef57756366cc4b45af1ed4a130
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-12 18:47:38 +0530

    BookNotFoundException with super keyword access msg

---
+++ hibernate-3product/src/main/java/com/exception/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.exception;
+
+public class BookNotFoundException extends RuntimeException  {
+
+	public BookNotFoundException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+}
