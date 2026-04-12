1f414fa3f45d66f9481d92ff23f007859887e54c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-12 18:36:52 +0530

    Employee anotation entity getter and setter method

--- hibernate-4Employee/src/main/java/com/entity/Employee.java
+++ hibernate-4Employee/src/main/java/com/entity/Employee.java
@@ -1,7 +1,12 @@
 package com.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Employee {
 
+	@Id
 	private int id;
 	
 	private String empname;
@@ -11,6 +16,48 @@ public class Employee {
 	private String dept;
 	
 	private String email;
+
+	public int getId() {
+		return id;
+	}
+
+	
+	// getter and setter method
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmpname() {
+		return empname;
+	}
+
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 	
 	
