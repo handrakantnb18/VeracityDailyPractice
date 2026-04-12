bd17548eecb391aae6ef60e73a8226579f427988
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-12 19:06:37 +0530

    StreamApi to create simple List and print collection

---
+++ java_8Features/src/com/streampractice/StreamAPI.java
@@ -0,0 +1,19 @@
+package com.streampractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamAPI {
+
+	public static void main(String[] args) {
+		
+		List<Integer> l1 = new ArrayList<Integer>();
+		l1.add(10);
+		l1.add(200);
+		l1.add(300);
+		l1.add(400);
+		l1.add(500);
+		
+		System.out.println(l1);
+	}
+}
