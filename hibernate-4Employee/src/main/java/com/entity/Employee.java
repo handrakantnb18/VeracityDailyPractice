d9d68422d55aa56851692de2a6b8ee37ee035f66
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-12 18:28:25 +0530

    product anotation entity getter and setter method and constructor and toString

---
+++ hibernate-4Employee/src/main/java/com/entity/Employee.java
@@ -0,0 +1,18 @@
+package com.entity;
+
+public class Employee {
+
+	private int id;
+	
+	private String empname;
+	
+	private double salary;
+	
+	private String dept;
+	
+	private String email;
+	
+	
+	
+	
+}
