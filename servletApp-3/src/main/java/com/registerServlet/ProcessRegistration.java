77a1fa9912a37dd8dfe8205c813de4137b0bbcb5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-13 13:45:06 +0530

    User user = new User(name,email,password,gender,city); service class fetch data

--- servletApp-3/src/main/java/com/registerServlet/ProcessRegistration.java
+++ servletApp-3/src/main/java/com/registerServlet/ProcessRegistration.java
@@ -2,6 +2,9 @@ package com.registerServlet;
 
 import java.io.IOException;
 
+import com.entity.User;
+import com.service.UserService;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -31,6 +34,15 @@ public class ProcessRegistration extends HttpServlet {
 		System.out.println(gender);
 		System.out.println(city);
 		
+		User user = new User(name,email,password,gender,city);
+		
+		UserService service = new UserService();
+		
+		String msg= service.saveUser(user);
+		
+		req.setAttribute("msg", msg);
+		
+		req.getRequestDispatcher("register-form.jsp").forward(req, resp);
 	}
 	
 }
