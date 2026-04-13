24dedd21a9026e7861061c717b19f528a530d3ac
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-13 12:53:28 +0530

    get the data from HTML file into processRegister file

--- servletApp-3/src/main/java/com/registerServlet/ProcessRegistration.java
+++ servletApp-3/src/main/java/com/registerServlet/ProcessRegistration.java
@@ -3,10 +3,12 @@ package com.registerServlet;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@WebServlet("/registration")
 public class ProcessRegistration extends HttpServlet {
 
 	
@@ -22,6 +24,12 @@ public class ProcessRegistration extends HttpServlet {
 		String gender = req.getParameter("gender");
 		
 		String city = req.getParameter("city");
+		
+		System.out.println(name);
+		System.out.println(email);
+		System.out.println(password);
+		System.out.println(gender);
+		System.out.println(city);
 		
 	}
 	
