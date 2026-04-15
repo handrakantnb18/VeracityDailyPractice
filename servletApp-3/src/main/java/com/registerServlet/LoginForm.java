d4519c4206e0027424d30089230afbd39ffa2072
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-15 10:55:03 +0530

    LoginForm +//@WebServlet("/login-form") req.getRequestDispatcher("login-form.jsp").forward(req, resp);

--- servletApp-3/src/main/java/com/registerServlet/LoginForm.java
+++ servletApp-3/src/main/java/com/registerServlet/LoginForm.java
@@ -1,21 +1,23 @@
-package com.registerServlet;
-
-import java.io.IOException;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-@WebServlet("/login-form")
-public class LoginForm extends HttpServlet {
-
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	
-		req.getRequestDispatcher("login-form.jsp").forward(req, resp);
-		
-	}
-	
-}
+//package com.registerservlet;
+//
+//import java.io.IOException;
+//
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.annotation.WebServlet;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//@WebServlet("/login-form")
+//public class LoginForm extends HttpServlet {
+//
+//	@Override
+//	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		
+//		req.getRequestDispatcher("login-form.jsp").forward(req, resp);
+//
+//	}
+//}
+//
+//
+//
