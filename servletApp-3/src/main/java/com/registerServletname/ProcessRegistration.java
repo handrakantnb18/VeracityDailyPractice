328b8a64bc2a750c6e66b97d7c4ef341139a6532
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-15 11:03:11 +0530

    +//@WebServlet("/registration") ProcessRegistration req.getParameter("name");

---
+++ servletApp-3/src/main/java/com/registerServletname/ProcessRegistration.java
@@ -0,0 +1,50 @@
+//package com.registerServletname;
+//
+//import java.io.IOException;
+//
+//import com.entity.User;
+//import com.service.UserService;
+//
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.annotation.WebServlet;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//@WebServlet("/registration")
+//public class ProcessRegistration extends HttpServlet {
+//
+//	
+//	@Override
+//	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//
+//		String name = req.getParameter("name");
+//		
+//		String email = req.getParameter("email");
+//		
+//		String password = req.getParameter("password");
+//		
+//		String gender = req.getParameter("gender");
+//		
+//		String city = req.getParameter("city");
+//		
+//		System.out.println(name);
+//		System.out.println(email);
+//		System.out.println(password);
+//		System.out.println(gender);
+//		System.out.println(city);
+//		
+//		User user = new User(name,email,password,gender,city);
+//		
+//		UserService service = new UserService();
+//		
+//		String msg= service.saveUser(user);
+//		
+////		req.setAttribute("msg", msg);
+//		
+//		req.getRequestDispatcher("login.jsp").forward(req, resp);
+//	}
+//	
+//}
+
+
