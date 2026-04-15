73a865f5fc5738d302bd43407f48c199ef2cc19b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-15 11:22:46 +0530

    JDBCConnection +	private static final String url ="jdbc:mysql://localhost:3306/ServletappDb"; +		Class.forName("com.mysql.cj.jdbc.Driver");

---
+++ servletApp-4/src/main/java/com/utility/JDBCConnection.java
@@ -0,0 +1,34 @@
+package com.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCConnection {
+
+	private static final String url ="jdbc:mysql://localhost:3306/ServletappDb";
+	
+	private static final String username ="root";
+	
+	private static final String password="root";
+	
+	public static Connection getConnection()
+	{
+		Connection conn = null;
+		
+		try
+		{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		conn = DriverManager.getConnection(url, username, password);
+		
+		
+		
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+}
