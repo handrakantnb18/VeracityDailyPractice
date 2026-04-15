5f698500423d64dd19fc24bb43f36fc3c7c820a4
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-15 11:14:03 +0530

    User toString method constructor

---
+++ servletApp-4/src/main/java/com/entity/User.java
@@ -0,0 +1,74 @@
+package com.entity;
+
+public class User {
+
+	private String name;
+	
+	private String email;
+	
+	private String password;
+	
+	private String gender;
+	
+	private String city;
+
+	// toString method
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", email=" + email + ", password=" + password + ", gender=" + gender + ", city="
+				+ city + "]";
+	}
+
+	// constructor
+	public User(String name, String email, String password, String gender, String city) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.gender = gender;
+		this.city = city;
+	}
+
+	// getter and setter method
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+}
