15c1f95c3770136d0e9b0b6cc02ef5cfa0c23e2e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-17 15:04:33 +0530

    Employee class we can take the String as a using sleep() method single thread

---
+++ multithreading/src/com/demo/Employee.java
@@ -0,0 +1,33 @@
+package com.demo;
+
+public class Employee implements Runnable {
+
+	@Override
+	public void run() {
+		
+		String str = "my name is chandraknt bhosale";
+		
+		char []c = str.toCharArray();
+		
+		for(int i=0; i<c.length; i++)
+		{
+			try
+			{
+			Thread.sleep(2000);
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+			System.out.println(c[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Employee employee = new Employee();
+		
+		Thread t1 = new Thread(employee);
+		t1.start();
+	}
+}
