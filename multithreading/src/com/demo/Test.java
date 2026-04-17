f0182bb1c46ecd11ba32d2e4dac8694ec8169ca3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-17 12:01:44 +0530

    getName() to check thread by Thread Schedular which thread will be run first

--- multithreading/src/com/demo/Test.java
+++ multithreading/src/com/demo/Test.java
@@ -5,28 +5,32 @@ public class Test extends Thread {
 	@Override
 	public void run() {
 		
-		System.out.println("First main method"+Thread.currentThread().getName());
+		System.out.println("First main method : "+Thread.currentThread().getName());
 	}
 	
 	
 	public static void main(String[] args) {
 		Test t1 = new Test();
 		
+		t1.setName("Chandraaknt 1");
 //		System.out.println(t1.getName());
 		t1.start();
 		
 		Test t2 = new Test();
 		
+		t2.setName("Chandraaknt 2");
 //		System.out.println(t2.getName());
 		t2.start();
 		
 		Test t3 = new Test();
 		
+		t3.setName("Chandraaknt 3");
 //		System.out.println(t3.getName());
 		t3.start();
 		
 		Test t4 = new Test();
 		
+		t4.setName("Chandraaknt 4");
 //		System.out.println(t4.getName());
 		t4.start();
 	}
