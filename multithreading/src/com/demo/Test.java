ce63a6693d41fe8a2587d6227b82c739e086439e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-17 11:52:43 +0530

    t1.start method is used to starrt a thread at a time

--- multithreading/src/com/demo/Test.java
+++ multithreading/src/com/demo/Test.java
@@ -13,18 +13,21 @@ public class Test extends Thread {
 		Test t1 = new Test();
 		
 		System.out.println(t1.getName());
+		t1.start();
 		
 		Test t2 = new Test();
 		
 		System.out.println(t2.getName());
+		t2.start();
 		
 		Test t3 = new Test();
 		
 		System.out.println(t3.getName());
+		t3.start();
 		
 		Test t4 = new Test();
 		
 		System.out.println(t4.getName());
-		
+		t4.start();
 	}
 }
