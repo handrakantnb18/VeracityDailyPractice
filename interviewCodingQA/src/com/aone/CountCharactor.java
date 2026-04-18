d21620917d285a29bb584209fbcdc13603ca07d2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-18 22:11:55 +0530

    Count String Charactor in the declaired variable of length

--- interviewCodingQA/src/com/aone/CountCharactor.java
+++ interviewCodingQA/src/com/aone/CountCharactor.java
@@ -11,5 +11,18 @@ public class CountCharactor {
 		String str1 = "priyanka";
 		int count1 = str1.length();
 		System.out.println(count1);
+		
+		String str2 = "chandrakant Bhosale";
+		int count2 = str2.length();
+		System.out.println(count2);
+		
+		String str3 = "priyanka bhosale";
+		int count3 = str3.length();
+		System.out.println(count3);
+		
+		String str4 = "i am from pune";
+		int count4 = str4.length();
+		System.out.println(count4);
+		
 	}
 }
