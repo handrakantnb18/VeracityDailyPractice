3f6c388119c600d9546b8ea22f96baa59299b05d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-18 22:07:21 +0530

    Count String length() with count variable

---
+++ interviewCodingQA/src/com/aone/CountCharactor.java
@@ -0,0 +1,15 @@
+package com.aone;
+
+public class CountCharactor {
+
+	public static void main(String[] args) {
+		
+		String str = "chandrakant";
+		int count = str.length();
+		System.out.println(count);
+		
+		String str1 = "priyanka";
+		int count1 = str1.length();
+		System.out.println(count1);
+	}
+}
