f792e725f3104a115b85d8ca244b9681bfc85096
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-18 23:00:02 +0530

    Remove Duplicate String multiple example

--- interviewCodingQA/src/com/aone/RemoveDuplicates.java
+++ interviewCodingQA/src/com/aone/RemoveDuplicates.java
@@ -17,5 +17,36 @@ public class RemoveDuplicates {
 		System.out.println(result);
 		
 		
+		
+		// remove Duplicate String
+		
+		String str1 ="priyanka Bhosale";
+		
+		String result1 =" ";
+		
+		for(char c1 : str1.toCharArray())
+		{
+			if(result1.indexOf(c1) == -1)
+			{
+				result1 += c1;
+			}
+		}
+		System.out.println(result1);
+		
+		String str2 ="i am from Sangli";
+		
+		String result2 =" ";
+		
+		for(char c2 :str2.toCharArray())
+		{
+			if(result2.indexOf(c2) == -1)
+			{
+				result2 += c2;
+			}
+		}
+		System.out.println(result2);
+		
+		
+		
 	}
 }
