26a5104b32b475e0c8c12757ef254490a7879146
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-18 20:31:57 +0530

    String reverse created using to String str and rev variables

---
+++ interviewCodingQA/src/com/aone/ReverseString.java
@@ -0,0 +1,17 @@
+package com.aone;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+		
+		String str = "Chandraaknt";
+		String rev = "";
+		
+		for(int i= str.length() -1; i>= 0; i--)
+		{
+			rev += str.charAt(i);
+		}
+		System.out.println(rev);
+	}
+
+}
