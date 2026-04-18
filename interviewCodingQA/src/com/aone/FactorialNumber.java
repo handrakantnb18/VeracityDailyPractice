52e7afdbcf9bbe83be3e06d85f84f8bf3a537288
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-18 22:01:03 +0530

    Factorial number multiple examples

--- interviewCodingQA/src/com/aone/FactorialNumber.java
+++ interviewCodingQA/src/com/aone/FactorialNumber.java
@@ -22,5 +22,43 @@ public class FactorialNumber {
 			fact1 *= i;
 		}
 		System.out.println(fact1);
+		
+		int num2 = 7;
+		int fact2 = 1;
+		
+		for(int j=1;j<=num2;j++)
+		{
+			fact2 *= j;
+		}
+		System.out.println(fact2);
+		
+		int n=4;
+		int fa=1;
+		
+		for(int i=1;i<=n;i++)
+		{
+			fa *=i;
+		}
+		System.out.println(fa);
+		
+		
+		int a=9;
+		int b=1;
+		
+		for(int k=1;k<=a;k++)
+		{
+			b *= k;
+		}
+		System.out.println(b);
+		
+		int c=8;
+		int e=1;
+		
+		for(int p=1;p<=c;c++)
+		{
+			e *= p;
+		}
+		System.out.println(e);
+		
 	}
 }
