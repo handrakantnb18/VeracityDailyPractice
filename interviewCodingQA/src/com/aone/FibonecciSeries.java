4b89bc862a5870e4276229ad6ef16eadb56ece62
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-18 21:45:05 +0530

    Fibonecci series with swap number

---
+++ interviewCodingQA/src/com/aone/FibonecciSeries.java
@@ -0,0 +1,17 @@
+package com.aone;
+
+public class FibonecciSeries {
+
+	public static void main(String[] args) {
+		
+		int a=0,b=1;
+		
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println(a+" ");
+			int c = a+b;
+			a=b;
+			b=c;
+		}
+	}
+}
