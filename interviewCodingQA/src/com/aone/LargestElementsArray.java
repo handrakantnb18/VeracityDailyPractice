f528381fc682dd05186260853698774011506a94
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-18 23:11:43 +0530

    Largest Array Elements check

---
+++ interviewCodingQA/src/com/aone/LargestElementsArray.java
@@ -0,0 +1,19 @@
+package com.aone;
+
+public class LargestElementsArray {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {10,20,30,40,50,60};
+		int max = arr[0];
+		
+		for(int i=1; i<arr.length; i++)
+		{
+			if(arr[i] > max)
+				max = arr[i];
+		}
+		System.out.println(max);
+		
+		
+	}
+}
