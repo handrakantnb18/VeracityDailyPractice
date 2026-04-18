f384d18358e4fa17a688c5e8fb3722cd33c2607b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-18 23:03:42 +0530

    Find Duplicate Elements with Arrays

---
+++ interviewCodingQA/src/com/aone/RemoveDuplicateElements.java
@@ -0,0 +1,23 @@
+package com.aone;
+
+public class RemoveDuplicateElements {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1,2,3,4,5,6,7,8,9,1,5,9,7,3};
+		
+		for(int i=0; i<=arr.length;i++)
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i] == arr[j])
+				{
+					System.out.println(arr[i]);
+				}
+			}
+		}
+		
+		
+		
+	}
+}
