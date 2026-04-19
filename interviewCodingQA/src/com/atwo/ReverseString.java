c2ebf61ece10e0017f60e026d9214f1a7e639bd8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-19 22:34:46 +0530

    String Reverse multiple example

--- interviewCodingQA/src/com/atwo/ReverseString.java
+++ interviewCodingQA/src/com/atwo/ReverseString.java
@@ -12,5 +12,29 @@ public class ReverseString {
 			rev += str1.charAt(i);
 		}
 		System.out.println(rev);
+		
+		
+		String str2 = "priyanka Bhosale";
+		String rev2 =" ";
+		
+		for(int i= str2.length() -1; i>= 0; i--)
+		{
+			rev2 += str2.charAt(i);
+		}
+		
+		System.out.println(rev2);
+		
+		
+		String str3 = "chandraaknt bhosale";
+		
+		String rev3 = " ";
+		
+		for(int j = str3.length() -1; j >= 0; j-- )
+		{
+			rev3 += str3.charAt(j);
+		}
+		
+		System.out.println(rev3);
+		
 	}
 }
