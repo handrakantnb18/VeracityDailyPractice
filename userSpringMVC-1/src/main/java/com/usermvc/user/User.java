a69bbd685c5d80451a34023675c6a58f0ac1d5a6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-20 17:00:03 +0530

    User class toString method, constructor, getter and setter method and privaye variables

---
+++ userSpringMVC-1/src/main/java/com/usermvc/user/User.java
@@ -0,0 +1,51 @@
+package com.usermvc.user;
+
+
+public class User {
+
+	private int id;
+	
+	private String name;
+	
+	private String email;
+
+	// toString method
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
+
+	// constructor 
+	public User(int id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	// getter and setter method
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
