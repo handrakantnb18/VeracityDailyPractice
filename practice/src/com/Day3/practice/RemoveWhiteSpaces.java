54da00a2b1976ca05cc9890be6667ed2a83d60d3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-21 22:05:14 +0530

    Remove Whitespaces multiple examples we can access

--- practice/src/com/Day3/practice/RemoveWhiteSpaces.java
+++ practice/src/com/Day3/practice/RemoveWhiteSpaces.java
@@ -7,5 +7,13 @@ public class RemoveWhiteSpaces {
 		String str = " i am from pune ";
 		str = str.replaceAll("\\s", "");
 		System.out.println(str);
+		
+		String str1 = "my name is chandrakant";
+		str1 = str1.replaceAll("\\s", "");
+		System.out.println(str1);
+		
+		String str2 = "wel come to java programing";
+		str2 = str2.replaceAll("\\s", "");
+		System.out.println(str2);
 	}
 }
