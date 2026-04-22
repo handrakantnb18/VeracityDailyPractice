339d10af823d9e1a890e035c1c226edec3fb641d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-22 23:18:49 +0530

    Find Min vlaues by Integer

---
+++ practice/src/com/Day3/practice/java8/FindMin.java
@@ -0,0 +1,16 @@
+package com.Day3.practice.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FindMin {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(25,1,2,88,78,99,56,24);
+		
+		int min = list.stream().min(Integer::compare).get();
+		
+		System.out.println(min);
+	}
+}
