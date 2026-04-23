8d0f6041d5425ea49cd86c90728d789beeaaf870
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-23 22:49:43 +0530

    getter and setter method Employee class fetched

--- practice/src/com/Day4/practice/java8/Employee.java
+++ practice/src/com/Day4/practice/java8/Employee.java
@@ -22,6 +22,38 @@ public class Employee {
 		this.city = city;
 		this.dept = dept;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
 	
 	
 }
