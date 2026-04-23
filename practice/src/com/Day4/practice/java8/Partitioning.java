813da2900ed735a90b09d122398ea25d3ed4fa18
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-23 22:37:04 +0530

    Partitioning for Even or odd check number from array list

---
+++ practice/src/com/Day4/practice/java8/Partitioning.java
@@ -0,0 +1,23 @@
+package com.Day4.practice.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Partitioning {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(22,15,35,85,64,77,98);
+		
+		Map<Boolean, List<Integer>> map =
+				list.stream()
+				.collect(Collectors.partitioningBy(
+						n -> n % 2 == 0));
+		
+		System.out.println(map);
+		
+		
+	}
+}
