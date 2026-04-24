83dd0e24c37300454a452d278f8eee7d2b182ad1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-24 20:58:13 +0530

    Employee class salary variable added

--- practice/src/com/Day4/practice/java8/Employee.java
+++ practice/src/com/Day4/practice/java8/Employee.java
@@ -28,6 +28,14 @@ public class Employee {
 	}
 
 	
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
 	public int getId() {
 		return id;
 	}
