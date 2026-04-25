9cc56127e20ffb3576c7a0876e2f66defaa5f210
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-25 15:19:11 +0530

    Hello Word program

--- userSpringMVC-1/src/main/java/com/usermvc/controller/USerController.java
+++ userSpringMVC-1/src/main/java/com/usermvc/controller/USerController.java
@@ -2,4 +2,11 @@ package com.usermvc.controller;
 
 public class USerController {
 
+	public static void main(String[] args) {
+		
+//		Optional<String> name = Optional.ofNullable(null);
+
+//		System.out.println(name.orElse("Default"));
+		System.out.println("Hello word");
+	}
 }
