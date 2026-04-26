f4af3bf7764e5d5cc4f6292a4145bc1b78da6051
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-26 22:30:11 +0530

    min number maximum examples 8. Find Minimum Number

--- practice/src/com/Day5/streamAPI/MinmumNumber.java
+++ practice/src/com/Day5/streamAPI/MinmumNumber.java
@@ -15,6 +15,21 @@ public class MinmumNumber {
 		
 		System.out.println(min);
 		
+		List<Integer> list1 = Arrays.asList(55,5,78,35,46,89,100,9);
+		
+		int min1 = list1.stream()
+				.min(Integer::compare)
+				.get();
+		
+		System.out.println(min1);
+		
+		List<Integer> list2 = Arrays.asList(10,88,79,25,56,48,35);
+		
+		int min2 = list2.stream()
+				.min(Integer::compare)
+				.get();
+		
+		System.out.println(min2);
 		
 	}
 }
