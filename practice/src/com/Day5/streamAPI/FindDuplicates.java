9071d2a97b3e1280ed93d4185a5e00c93cc340bb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-26 20:37:22 +0530

    duplicate eleements multiple list set Find duplicate elements in a list using Stream API.

--- practice/src/com/Day5/streamAPI/FindDuplicates.java
+++ practice/src/com/Day5/streamAPI/FindDuplicates.java
@@ -13,8 +13,24 @@ public class FindDuplicates {
 		
 		Set<Integer> set = new HashSet<Integer>();
 		
+		System.out.println("Duplicate Elements : ");
+
 		list.stream()
 		.filter(n -> !set.add(n))
+		.forEach(System.out::println);
+		
+		System.out.println("Duplicate Elements : ");
+		List<Integer> list1 = Arrays.asList(11,23,23,55,45,55,78,89,78);
+		
+		Set<Integer> set1 = new HashSet<Integer>();
+		
+		list1.stream()
+		.filter(a ->  !set1.add(a))
+		.forEach(System.out::println);
+		
+		System.out.println("Non-Duplicate Elements : ");
+		list1.stream()
+		.filter(b -> set.add(b))
 		.forEach(System.out::println);
 		
 		
