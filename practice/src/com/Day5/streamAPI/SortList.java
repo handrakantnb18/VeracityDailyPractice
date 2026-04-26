a6691a7ddf12c9ea8f26692a678d949de5be1d41
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-26 22:05:30 +0530

    Sort List in Ascending Order Sort a list using Stream API.

--- practice/src/com/Day5/streamAPI/SortList.java
+++ practice/src/com/Day5/streamAPI/SortList.java
@@ -9,9 +9,27 @@ public class SortList {
 		
 		List<Integer> list = Arrays.asList(22,5,12,80,10,12,23,25,45,65,88,78);
 		
+		System.out.println("First List : ");
 		list.stream()
 		.sorted()
 		.forEach(System.out::println);
+		
+		System.out.println("Second list : ");
+		
+		List<Integer> list1 = Arrays.asList(55,23,88,4,56,12,10);
+		
+		list1.stream()
+		.sorted()
+		.forEach(System.out::println);
+		
+		System.out.println("Third List : ");
+		
+		List<Integer> list2 = Arrays.asList(88,77,56,12,94,10,35,25);
+		
+		list2.stream()
+		.sorted()
+		.forEach(System.out::println);
+		
 		
 	}
 }
