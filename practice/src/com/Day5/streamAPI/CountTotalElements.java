b2c761172dbaf6cb6e242708812378c9f4a1fce0
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-26 20:55:32 +0530

    multiple example Count the total number of elements in a list.

--- practice/src/com/Day5/streamAPI/CountTotalElements.java
+++ practice/src/com/Day5/streamAPI/CountTotalElements.java
@@ -13,6 +13,19 @@ public class CountTotalElements {
 		
 		System.out.println(count);
 		
+		List<String> list1 = Arrays.asList("A","B","C","D","E","F");
 		
+		long count1 = list1.stream().count();
+		
+		System.out.println(count1);
+		
+		List<String> list2 = Arrays.asList("A","B","C","D","E","F","G","H","I",
+				"J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+		
+		long count2 = list2.stream().count();
+		
+		System.out.println(count2);
+		
+				
 	}
 }
