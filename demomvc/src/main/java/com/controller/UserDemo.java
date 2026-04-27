a5ea4b9a8befe00be549fb821a786ad93bddf1ef
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-27 15:48:16 +0530

    saveUser() created to access data from Controller class by database

---
+++ demomvc/src/main/java/com/controller/UserDemo.java
@@ -0,0 +1,30 @@
+package com.controller;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class UserDemo {
+
+	// cummunication with database
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Transactional
+	public void saveUser()
+	{
+		Session session = sessionFactory.getCurrentSession();
+		
+		User u = new User();
+		u.setName("Chandrakant");
+		u.setEmail("chandrakant@gmail.com");
+		
+		session.save(u);
+		
+	}
+	
+}
