e6abc0ab6146736cf736e678f56d67a92fcddc9e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-27 16:30:57 +0530

    userDemo persist() method used because save() not accept

--- demomvc/src/main/java/com/controller/UserDemo.java
+++ demomvc/src/main/java/com/controller/UserDemo.java
@@ -5,8 +5,9 @@ import javax.transaction.Transactional;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public class UserDemo {
 
 	// cummunication with database
@@ -23,7 +24,7 @@ public class UserDemo {
 		u.setName("Chandrakant");
 		u.setEmail("chandrakant@gmail.com");
 		
-		session.save(u);
+		session.persist(u);
 		
 	}
 	
