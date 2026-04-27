4503ebdb3817204dd612e956048e3b11fad9420c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-27 14:03:16 +0530

    annotation used Entity Id and GeneratedValue

--- demomvc/src/main/java/com/controller/User.java
+++ demomvc/src/main/java/com/controller/User.java
@@ -1,12 +1,17 @@
 package com.controller;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-
+@Entity
+@Table(name="users1")
 public class User {
 
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	private String name;
