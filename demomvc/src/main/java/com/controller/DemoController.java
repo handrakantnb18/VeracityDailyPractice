2f45c9aa58a239b589875d82b192bd7d3dd47f40
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-27 15:47:06 +0530

    controller class create the two APIs

--- demomvc/src/main/java/com/controller/DemoController.java
+++ demomvc/src/main/java/com/controller/DemoController.java
@@ -1,15 +1,27 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class DemoController {
 
+	@Autowired
+	private UserDemo userDemo;
+	
 	@RequestMapping("/first")
 	public String firstDemoFunction()
 	{
 		return "firstmvcresponce";
 	}
 	
+	@RequestMapping("/second")
+	public String saveUser()
+	{
+		userDemo.saveUser();
+		return"showuser";
+		
+	}
 }
