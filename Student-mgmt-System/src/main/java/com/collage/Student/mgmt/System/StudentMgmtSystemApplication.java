4098e2325df6c9cab2e0bf2a5167d920b739ce44
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-30 14:32:01 +0530

    main method and run method

---
+++ Student-mgmt-System/src/main/java/com/collage/Student/mgmt/System/StudentMgmtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.collage.Student.mgmt.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMgmtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMgmtSystemApplication.class, args);
+	}
+
+}
