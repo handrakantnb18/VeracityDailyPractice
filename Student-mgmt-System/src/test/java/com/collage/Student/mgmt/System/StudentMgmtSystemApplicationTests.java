d6898f0dd564e22528008636436814e155f70328
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-04-30 14:33:41 +0530

    Test class created to write test cases

---
+++ Student-mgmt-System/src/test/java/com/collage/Student/mgmt/System/StudentMgmtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collage.Student.mgmt.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMgmtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
