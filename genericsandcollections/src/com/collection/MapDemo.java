3b7e4daceafa817e8dd7c64e0e8011a97493d135
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-01 23:08:56 +0530

    Map collection is used in Integer value and key pair

---
+++ genericsandcollections/src/com/collection/MapDemo.java
@@ -0,0 +1,19 @@
+package com.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapDemo {
+
+	public static void main(String[] args) {
+		
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		
+		map.put(1, 101);
+		map.put(2, 102);
+		map.put(3, 103);
+		map.put(4, 104);
+		
+		System.out.println(map);
+	}
+}
