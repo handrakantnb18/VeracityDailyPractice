f2b2d94b43097ac6783795dc560e6b81cc25dd7c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-01 22:33:04 +0530

    ArrayList in used in list interface duplicate allowd

---
+++ genericsandcollections/src/com/collection/ArrayListList.java
@@ -0,0 +1,21 @@
+package com.collection;
+
+import java.util.ArrayList;
+
+public class ArrayListList {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		list.add(1);
+		list.add(5);
+		list.add(2);
+		list.add(10);
+		list.add(6);
+		list.add(3);
+		list.add(10);
+		
+		System.out.println(list);
+	}
+}
