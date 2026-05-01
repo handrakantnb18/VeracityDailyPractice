b1a962f69617778c68f6852c2255089f72918630
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-01 22:10:03 +0530

    Set collection in the hashSet order by aschending

---
+++ genericsandcollections/src/com/collection/SetInteger.java
@@ -0,0 +1,22 @@
+package com.collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetInteger {
+
+	public static void main(String[] args) {
+		
+		Set<Integer> set = new HashSet<Integer>();
+		
+		set.add(101);
+		set.add(103);
+		set.add(102);
+		set.add(104);
+		set.add(106);
+		set.add(105);
+		
+		System.out.println(set);
+		
+	}
+}
