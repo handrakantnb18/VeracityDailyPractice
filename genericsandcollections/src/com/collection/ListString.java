283f9d60a05d17a8d2e5c8327f2136ccccdd1697
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-01 22:06:19 +0530

    String List in the collection

---
+++ genericsandcollections/src/com/collection/ListString.java
@@ -0,0 +1,22 @@
+package com.collection;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ListString {
+
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		
+		list.add("Ram");
+		list.add("Chandrakant");
+		list.add("priyanka");
+		list.add("Vijay");
+		
+		
+		System.out.println(list);
+		
+	}
+}
