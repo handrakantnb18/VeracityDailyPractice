e971aee54568987244482fd61c4c9dfdcc91b0cf
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-01 22:40:41 +0530

    LinkedList String value added and null allowed and duplicate allowed

---
+++ genericsandcollections/src/com/collection/LinkedListString.java
@@ -0,0 +1,22 @@
+package com.collection;
+
+import java.util.LinkedList;
+
+public class LinkedListString {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> list = new LinkedList<String>();
+		
+		list.add("Ram");
+		list.add("Chandraaknt");
+		list.add("Priyanka");
+		list.add("Keshav");
+		list.add(null);
+		list.add("Priyanka");
+
+		
+		System.out.println(list);
+		
+	}
+}
