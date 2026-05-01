35f014482a2688eb465b778fc9c3540fe62145a8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-01 22:02:55 +0530

    Demo added list integer

---
+++ genericsandcollections/src/com/collection/DemoList.java
@@ -0,0 +1,22 @@
+package com.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoList {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = new ArrayList<Integer>();
+		
+		list.add(101);
+		list.add(102);
+		list.add(103);
+		list.add(104);
+		list.add(105);
+		
+		System.out.println(list);
+		
+		
+	}
+}
