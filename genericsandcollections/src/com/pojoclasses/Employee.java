16b383dbc3c2add79cf44d94e69b492f0610248f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-02 21:05:07 +0530

    Employee class getter setter methods and constructor created

---
+++ genericsandcollections/src/com/pojoclasses/Employee.java
@@ -0,0 +1,48 @@
+package com.pojoclasses;
+
+public class Employee {
+
+	private int empid;
+	
+	private String empname;
+	
+	private String empcity;
+
+	@Override
+	public String toString() {
+		return "Employee [empid=" + empid + ", empname=" + empname + ", empcity=" + empcity + "]";
+	}
+
+	public Employee(int empid, String empname, String empcity) {
+		super();
+		this.empid = empid;
+		this.empname = empname;
+		this.empcity = empcity;
+	}
+
+	public int getEmpid() {
+		return empid;
+	}
+
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+
+	public String getEmpname() {
+		return empname;
+	}
+
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+
+	public String getEmpcity() {
+		return empcity;
+	}
+
+	public void setEmpcity(String empcity) {
+		this.empcity = empcity;
+	}
+	
+	
+}
