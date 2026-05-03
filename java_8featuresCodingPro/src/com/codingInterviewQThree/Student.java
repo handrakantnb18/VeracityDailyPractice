e6d700f5d0e1163b2424ae05fbd7d2807fa8b697
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-03 17:19:55 +0530

    we have created Employee class private variables and getter and setter method and toString method nad constructor

---
+++ java_8featuresCodingPro/src/com/codingInterviewQThree/Student.java
@@ -0,0 +1,59 @@
+package com.codingInterviewQThree;
+
+public class Student {
+
+	private int id;
+	
+	private String name;
+	
+	private String collage;
+	
+	private String city;
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", collage=" + collage + ", city=" + city + "]";
+	}
+
+	public Student(int id, String name, String collage, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.collage = collage;
+		this.city = city;
+	}
+
+	
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCollage() {
+		return collage;
+	}
+
+	public void setCollage(String collage) {
+		this.collage = collage;
+	}
+		
+}
