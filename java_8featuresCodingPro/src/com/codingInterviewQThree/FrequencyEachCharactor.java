d1ee738b9e8a8e97f0dffee69f44213652e86aee
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-03 15:21:15 +0530

    Q2: Find frequency of each character

---
+++ java_8featuresCodingPro/src/com/codingInterviewQThree/FrequencyEachCharactor.java
@@ -0,0 +1,21 @@
+package com.codingInterviewQThree;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FrequencyEachCharactor {
+
+	public static void main(String[] args) {
+		
+		String str1 = "Chandrakant";
+		
+		Map<Character, Long> map = 
+				str1.chars()
+				.mapToObj(c -> (char) c)
+				.collect(Collectors.groupingBy(c ->c, Collectors.counting()));
+		
+		System.out.println(map);
+		
+		
+	}
+}
