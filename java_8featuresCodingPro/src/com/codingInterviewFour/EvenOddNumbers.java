fb312c95d58ae5f6647faa011712ed2c7ca795b6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-04 22:03:21 +0530

    Count even numbers in List

---
+++ java_8featuresCodingPro/src/com/codingInterviewFour/EvenOddNumbers.java
@@ -0,0 +1,22 @@
+package com.codingInterviewFour;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EvenOddNumbers {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10,
+				11,12,13,14,15,16,17,18,19,20);
+		
+		long count =
+				list.stream()
+				.filter(n -> n % 2 == 0)
+				.count();
+		
+		System.out.println(count);
+		
+		
+	}
+}
