7dc1b0a775c883c56e4442c3f8b549b90becd4d6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-04 22:35:30 +0530

    Student class created private variables and toString method

---
+++ java_8featuresCodingPro/src/com/codingInterviewFour/Student.java
@@ -0,0 +1,19 @@
+package com.codingInterviewFour;
+
+public class Student {
+
+	private int id;
+	
+	private String name;
+	
+	private int salary;
+	
+	private String city;
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", salary=" + salary + ", city=" + city + "]";
+	}
+	
+	
+}
