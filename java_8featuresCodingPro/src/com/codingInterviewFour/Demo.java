8a9c0c292f599758fcf0b859a02b4b380f55044c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-04 21:11:17 +0530

    Find second highest number sorted

--- java_8featuresCodingPro/src/com/codingInterviewFour/Demo.java
+++ java_8featuresCodingPro/src/com/codingInterviewFour/Demo.java
@@ -1,6 +1,7 @@
 package com.codingInterviewFour;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Demo {
@@ -10,12 +11,23 @@ public class Demo {
 		List<Integer> list = Arrays.asList(10,20,30,40,45,25,50,35,88);
 		int secondHighest =
 				list.stream()
-				.sorted()
+				.sorted(Comparator.reverseOrder())
 				.skip(1)
 				.findFirst()
 				.get();
 		
 		System.out.println(secondHighest);
+		
+		List<Integer> list2 = Arrays.asList(45,25,35,85,95,65,75,55);
+		
+		int secondHighest2 =
+				list2.stream()
+				.sorted(Comparator.reverseOrder())
+				.skip(1)
+				.findFirst()
+				.get();
+		
+		System.out.println(secondHighest2);
 		
 		
 	}
