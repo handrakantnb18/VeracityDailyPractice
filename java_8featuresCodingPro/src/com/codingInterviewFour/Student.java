808eba81db5c217942086700a2d9adccfabc1e0b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-04 22:37:43 +0530

    Employee class constructor and getter and setter method created

--- java_8featuresCodingPro/src/com/codingInterviewFour/Student.java
+++ java_8featuresCodingPro/src/com/codingInterviewFour/Student.java
@@ -14,6 +14,46 @@ public class Student {
 	public String toString() {
 		return "Student [id=" + id + ", name=" + name + ", salary=" + salary + ", city=" + city + "]";
 	}
+
+	public Student(int id, String name, int salary, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.city = city;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
 	
 	
 }
