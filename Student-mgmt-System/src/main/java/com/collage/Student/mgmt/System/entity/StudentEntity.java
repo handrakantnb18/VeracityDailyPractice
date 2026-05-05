9acb005dbe0443fbf81134f60f94c66afd42ad07
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-05 12:29:16 +0530

    Student class create constructor and getter and setter methods

--- Student-mgmt-System/src/main/java/com/collage/Student/mgmt/System/entity/StudentEntity.java
+++ Student-mgmt-System/src/main/java/com/collage/Student/mgmt/System/entity/StudentEntity.java
@@ -12,6 +12,37 @@ public class StudentEntity {
 	public String toString() {
 		return "StudentEntity [studentId=" + studentId + ", studentName=" + studentName + ", emailId=" + emailId + "]";
 	}
+
+	public StudentEntity(long studentId, String studentName, String emailId) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.emailId = emailId;
+	}
+
+	public long getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(long studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
 	
 	
 
