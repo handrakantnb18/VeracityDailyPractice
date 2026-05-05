9bc98beaf7f33d896428d3c32fc1732762b65b76
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-05 12:27:47 +0530

    Student class make private variables and toString method

--- Student-mgmt-System/src/main/java/com/collage/Student/mgmt/System/entity/StudentEntity.java
+++ Student-mgmt-System/src/main/java/com/collage/Student/mgmt/System/entity/StudentEntity.java
@@ -2,5 +2,17 @@ package com.collage.Student.mgmt.System.entity;
 
 public class StudentEntity {
 	
+	private long studentId;
+	
+	private String studentName;
+	
+	private String emailId;
+
+	@Override
+	public String toString() {
+		return "StudentEntity [studentId=" + studentId + ", studentName=" + studentName + ", emailId=" + emailId + "]";
+	}
+	
+	
 
 }
