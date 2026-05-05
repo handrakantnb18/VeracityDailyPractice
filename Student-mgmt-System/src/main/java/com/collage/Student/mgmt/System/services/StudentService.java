4edabe051c3f648b82ac06f04592127f19d7046a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-05 14:17:08 +0530

    StudentService StudentRepository created

---
+++ Student-mgmt-System/src/main/java/com/collage/Student/mgmt/System/services/StudentService.java
@@ -0,0 +1,18 @@
+package com.collage.Student.mgmt.System.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.collage.Student.mgmt.System.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	//public void 
+	
+	
+	
+}
