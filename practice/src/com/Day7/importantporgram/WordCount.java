b926cb78a1c0ec9c0f4f66bb5fccc5cae12b6521
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-06 23:04:52 +0530

    Count number of words in a sentence.

---
+++ practice/src/com/Day7/importantporgram/WordCount.java
@@ -0,0 +1,11 @@
+package com.Day7.importantporgram;
+
+public class WordCount {
+
+	public static void main(String[] args) {
+        String str = "my name is chandrakant bhosale, i am from sangli";
+        String words[] = str.split(" ");
+
+        System.out.println("Word count: " + words.length);
+    }
+}
