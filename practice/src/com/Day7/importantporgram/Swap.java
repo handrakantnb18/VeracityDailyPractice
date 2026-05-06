096e5a8dae2ad65037ea3522fd098ac1ea9f45cd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-06 23:07:34 +0530

    Swap numbers without using extra variable.

---
+++ practice/src/com/Day7/importantporgram/Swap.java
@@ -0,0 +1,14 @@
+package com.Day7.importantporgram;
+
+public class Swap {
+
+	 public static void main(String[] args) {
+	        int a = 100, b = 202;
+
+	        a = a + b;
+	        b = a - b;
+	        a = a - b;
+
+	        System.out.println("a=" + a + ", b=" + b);
+	    }
+}
