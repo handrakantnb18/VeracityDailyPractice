6317654b9e908c5295cbda7727d2b4b6be3524ff
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-08 13:14:10 +0530

    Fibonacci Series find

---
+++ xjavaandjava8coding/src/com/corejava/xjavaandjava8coding/FibonacciSeries.java
@@ -0,0 +1,21 @@
+package com.corejava.xjavaandjava8coding;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		
+		int a = 0, b = 1;
+		
+		for(int i =1; i <=10; i++)
+		{
+			System.out.println(a + " ");
+			
+			int c = a+ b;
+				
+				a = b;
+				b = c;
+		}
+		
+		
+	}
+}
