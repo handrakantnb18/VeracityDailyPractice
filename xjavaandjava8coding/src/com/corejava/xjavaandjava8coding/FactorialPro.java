e85f9ddd85444f0f63ed66bf0c966c0e19916992
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-08 13:24:37 +0530

    Factorial Program multiple example

--- xjavaandjava8coding/src/com/corejava/xjavaandjava8coding/FactorialPro.java
+++ xjavaandjava8coding/src/com/corejava/xjavaandjava8coding/FactorialPro.java
@@ -15,5 +15,16 @@ public class FactorialPro {
 		System.out.println(fact);
 		
 		
+		int num1 = 9;
+		int fact1 = 1;
+		
+		for(int j = 1; j<num1; j++)
+		{
+			fact1 *= j;
+		}
+		
+		System.out.println(fact1);
+		
+		
 	}
 }
