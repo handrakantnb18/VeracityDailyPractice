fd009454cfc982628df3a5b2a4167469869d3562
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-08 17:22:15 +0530

    Sort List Ascending and Descending order

---
+++ xjavaandjava8coding/src/com/java8features/xjavaandjava8coding/FindEvenNumbers.java
@@ -0,0 +1,30 @@
+package com.java8features.xjavaandjava8coding;
+
+import java.net.Inet4Address;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindEvenNumbers {
+
+	public static void main(String[] args) {
+		
+		//  Find Even Numbers
+		
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,
+				9,10,11,12,13,14,15);
+		
+		list.stream()
+		.filter(n -> n % 2 == 0)
+		.forEach(System.out::println);
+		
+		System.out.println("----------");
+		// Find Odd Numbers
+		
+		List<Integer> list1 = Arrays.asList(1,2,3,4,5,6,7,8,
+				9,10,11,12,13,14,15,16,17,18,19,20);
+		
+		list1.stream()
+		.filter(n -> n % 2 != 0)
+		.forEach(System.out::println);
+	}
+}
