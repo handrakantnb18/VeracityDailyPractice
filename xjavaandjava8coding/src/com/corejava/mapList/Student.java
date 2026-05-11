f0cfbcadd6823f1ba96f6c3c728f00c097690c17
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-11 22:53:04 +0530

    getter and setter methods

--- xjavaandjava8coding/src/com/corejava/mapList/Student.java
+++ xjavaandjava8coding/src/com/corejava/mapList/Student.java
@@ -14,6 +14,46 @@ public class Student {
 	public String toString() {
 		return "Student [id=" + id + ", name=" + name + ", city=" + city + ", univercity=" + univercity + "]";
 	}
+
+	public Student(int id, String name, String city, String univercity) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+		this.univercity = univercity;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getUnivercity() {
+		return univercity;
+	}
+
+	public void setUnivercity(String univercity) {
+		this.univercity = univercity;
+	}
 	
 	
 }
