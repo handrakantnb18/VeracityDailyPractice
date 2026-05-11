d64998567a5275b86422a7eef35c25e38a9408bd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-11 18:06:35 +0530

    Count Characters in String

---
+++ xjavaandjava8coding/src/com/corejava/xjavaandjava8coding/CountCharactors.java
@@ -0,0 +1,15 @@
+package com.corejava.xjavaandjava8coding;
+
+public class CountCharactors {
+
+	public static void main(String[] args) {
+
+		// Count Characters in String
+
+		String str = "Chandrakant";
+
+		System.out.println(str.length());
+
+	}
+
+}
