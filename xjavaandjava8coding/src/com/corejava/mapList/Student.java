90d4920f662ba604de559a6efa2a90b653b3d762
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-11 22:51:44 +0530

    Student class making to toString method and private variables

---
+++ xjavaandjava8coding/src/com/corejava/mapList/Student.java
@@ -0,0 +1,19 @@
+package com.corejava.mapList;
+
+public class Student {
+
+	private int id;
+	
+	private String name;
+	
+	private String city;
+	
+	private String univercity;
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", city=" + city + ", univercity=" + univercity + "]";
+	}
+	
+	
+}
