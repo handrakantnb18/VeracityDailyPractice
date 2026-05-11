1e8dd914722c26cceaa597f22f0b0ff43e429c91
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-11 22:48:53 +0530

    added getter and setter method

--- xjavaandjava8coding/src/com/corejava/mapList/Product.java
+++ xjavaandjava8coding/src/com/corejava/mapList/Product.java
@@ -14,6 +14,46 @@ public class Product {
 	public String toString() {
 		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", city=" + city + "]";
 	}
+
+	public Product(int id, String name, int price, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.city = city;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
 	
 	
 }
