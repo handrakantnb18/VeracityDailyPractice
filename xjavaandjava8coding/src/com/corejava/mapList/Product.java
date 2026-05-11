6df7db89c47efdd1029634612275d819d84711a6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-11 22:22:08 +0530

    product class private variable and toString methods

---
+++ xjavaandjava8coding/src/com/corejava/mapList/Product.java
@@ -0,0 +1,19 @@
+package com.corejava.mapList;
+
+public class Product {
+
+	private int id;
+	
+	private String name;
+	
+	private int price;
+	
+	private String city;
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", city=" + city + "]";
+	}
+	
+	
+}
