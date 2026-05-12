4d88063a400dc241d17b14208dac9e3dabb83380
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-12 14:55:00 +0530

    Find Even Numbers in the list

---
+++ xjavaandjava8coding/src/com/corejav/collections/FindEvenNumbers.java
@@ -0,0 +1,18 @@
+package com.corejav.collections;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FindEvenNumbers {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(1,2,3,4,5,
+				6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+		
+		list.stream()
+		.filter(n -> n % 2 == 0)
+		.forEach(System.out::println);
+		
+	}
+}
