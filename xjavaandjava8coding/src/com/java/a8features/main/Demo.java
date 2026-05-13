02feb593e3b72601f88256eef6d59e5178befc2b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-13 22:03:51 +0530

    infinite Stream: Generate an unbounded sequence using Stream.iterate() or Stream.generate() string value in the List

---
+++ xjavaandjava8coding/src/com/java/a8features/main/Demo.java
@@ -0,0 +1,29 @@
+package com.java.a8features.main;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		// Infinite Stream: Generate an unbounded sequence using Stream.iterate() or Stream.generate()
+
+		 // 1. From a Collection
+        List<String> list = Arrays.asList("Java", "Python", "C++");
+        Stream<String> stream1 = list.stream();
+
+        // 2. From an Array
+        String[] arr = {"A", "B", "C"};
+        Stream<String> stream2 = Arrays.stream(arr);
+
+        // 3. Using Stream.of()
+        Stream<Integer> stream3 = Stream.of(1, 2, 3, 4, 5);
+
+        // 4. Infinite Stream (limit to avoid infinite loop)
+        Stream<Integer> stream4 = Stream.iterate(1, n -> n + 1).limit(5);
+        stream4.forEach(System.out::println);
+    
+	}
+}
