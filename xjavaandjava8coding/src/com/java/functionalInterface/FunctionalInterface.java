48ae6e690f6b5db3cbebbb9162cd97a5209bfea1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-13 22:17:10 +0530

    using functional interface in the lambda expression of the thread methods

---
+++ xjavaandjava8coding/src/com/java/functionalInterface/FunctionalInterface.java
@@ -0,0 +1,14 @@
+package com.java.functionalInterface;
+
+public class FunctionalInterface {
+
+	// Using a Functional Interface with a Lambda Expression
+	
+	public static void main(String[] args) {
+		
+		// Using lambda expression to implement Runnable
+        new Thread(() -> System.out.println("New thread created")).start();
+    
+		
+	}
+}
