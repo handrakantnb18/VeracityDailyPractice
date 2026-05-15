3e7d8a5dae3c48951cca5d017b74fc4eb4546fec
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-15 22:17:55 +0530

    Using Predicate Interface to Filter Strings

---
+++ xjavaandjava8coding/src/com/java/functionalInterface/SuppliersFunction.java
@@ -0,0 +1,27 @@
+package com.java.functionalInterface;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SuppliersFunction {
+
+	// Using Predicate Interface to Filter Strings
+
+	public static void main(String[] args) {
+		
+		// create a list of strings
+        List<String> n = Arrays.asList("Shivraj", "Dattaraj", "Priyanka", "Chandrakant", "Bhosale");
+
+        // declare the predicate type as string and use lambda expression to create object
+        Predicate<String> p = (s) -> s.startsWith("S");
+
+        // Iterate through the list
+        for (String st : n) {
+          
+            // call the test method
+            if (p.test(st))
+                System.out.println(st);
+        }
+	}
+}
