0e9664c6cae8b34e6b1c5e5b05c1eb9d02377a90
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-16 23:29:17 +0530

    To demonstrate how an Exception in the after function is returned and handled.

--- xjavaandjava8coding/src/com/java/functionalInterface/DemonstrateException.java
+++ xjavaandjava8coding/src/com/java/functionalInterface/DemonstrateException.java
@@ -28,6 +28,9 @@ public class DemonstrateException {
         list.add(5);
         list.add(3);
         list.add(4);
+        list.add(8);
+        list.add(6);
+        list.add(7);
 
 
         // using addThen()
