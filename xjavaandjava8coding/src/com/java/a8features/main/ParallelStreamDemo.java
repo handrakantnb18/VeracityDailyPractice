79dd847d11ce0004465b0677e12663a69ecfce62
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-16 23:06:48 +0530

    Parallel Stream Demo

--- xjavaandjava8coding/src/com/java/a8features/main/ParallelStreamDemo.java
+++ xjavaandjava8coding/src/com/java/a8features/main/ParallelStreamDemo.java
@@ -10,8 +10,18 @@ public class ParallelStreamDemo {
 		
 		List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
 
-        numbers.parallelStream().forEach(n -> System.out.println(n + " " + Thread.currentThread().getName()));
+        numbers.parallelStream()
+        .forEach(n -> System.out.println(n + " " + Thread.currentThread()
+        .getName()));
     
+        System.out.println("-------------");
+
+        List<Integer> numbers1 = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
+
+        numbers1.parallelStream()
+        .forEach(n -> System.out.println(n + " " + Thread.currentThread()
+        .getName()));
+
         
 	}
 }
