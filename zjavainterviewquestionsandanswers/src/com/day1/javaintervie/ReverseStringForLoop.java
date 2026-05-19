68c93eaca57c5534f2e85397f1f59bbd7f9452d3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-19 22:54:21 +0530

    Without built-in methods

---
+++ zjavainterviewquestionsandanswers/src/com/day1/javaintervie/ReverseStringForLoop.java
@@ -0,0 +1,16 @@
+package com.day1.javaintervie;
+
+public class ReverseStringForLoop {
+
+	public static void main(String[] args) {
+		
+		  String str = "hello";
+	        String reversed = "";
+	        
+	        for (int i = str.length() - 1; i >= 0; i--) {
+	            reversed += str.charAt(i);
+	        }
+	        
+	        System.out.println(reversed);
+	}
+}
