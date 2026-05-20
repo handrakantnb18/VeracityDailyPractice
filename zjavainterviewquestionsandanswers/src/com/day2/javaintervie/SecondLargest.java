a36b06706b04e905e2617b593e2b609845ad94ac
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-20 20:31:21 +0530

    Find the Second Largest Number : Find the second highest value in an array or list using Streams.

--- zjavainterviewquestionsandanswers/src/com/day2/javaintervie/SecondLargest.java
+++ zjavainterviewquestionsandanswers/src/com/day2/javaintervie/SecondLargest.java
@@ -3,16 +3,16 @@ package com.day2.javaintervie;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.management.RuntimeErrorException;
 
 public class SecondLargest {
 
 	public static void main(String[] args) {
-		
-		List<Integer> list = Arrays.asList(
-				10,2,45,98,35,78,16,89,34);
-		
+
+		List<Integer> list = Arrays.asList(10, 2, 45, 98, 35, 78, 16, 89, 34);
+
 		int secondHeight = 
 				list.stream()
 				.distinct()
@@ -20,9 +20,22 @@ public class SecondLargest {
 				.skip(1)
 				.findFirst()
 				.orElseThrow(() -> new RuntimeException("List does not have enough elements "));
-		
-		System.out.println("Second Largest : "+secondHeight);
-		
-		
+
+		System.out.println("Second Largest : " + secondHeight);
+
+		System.out.println("========================================");
+
+		List<Integer> list1 = Arrays.asList(12, 45, 78, 98, 65, 32, 25, 14, 36, 74, 85, 96, 99);
+
+		int secondHeighest1 =
+				list1.stream()
+				.distinct()
+				.sorted(Comparator.reverseOrder())
+				.skip(1)
+				.findFirst()
+				.orElseThrow(() -> new RuntimeException("List not find."));
+
+		System.out.println("Second Heighest : "+secondHeighest1);
+
 	}
 }
