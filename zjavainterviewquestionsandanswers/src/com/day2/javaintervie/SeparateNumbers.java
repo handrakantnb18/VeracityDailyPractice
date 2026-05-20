702d9ccbe56b808f3b527f741588e165fac98ea7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-20 12:43:58 +0530

    Separate Odd and Even Numbers : Partition a list of integers into two groups: even and odd numbers.

--- zjavainterviewquestionsandanswers/src/com/day2/javaintervie/SeparateNumbers.java
+++ zjavainterviewquestionsandanswers/src/com/day2/javaintervie/SeparateNumbers.java
@@ -19,6 +19,31 @@ public class SeparateNumbers {
 		System.out.println("Even Numbers : "+partitionaed.get(true));
 		System.out.println("Odd Numbers : "+partitionaed.get(false));
 		
+		System.out.println("======================================");
+		
+		List<Integer> list1 = Arrays.asList(
+				77,88,99,55,22,114,11,44,66,33,15,25,35,75,85,95);
+		
+		Map<Boolean, List<Integer>> map =
+				list1.stream()
+				.collect(Collectors.partitioningBy(a -> a % 2 == 0));
+		
+		System.out.println("Even Numbers : "+map.get(true));
+		System.out.println("Odd Numbers : "+map.get(false));
+		
+		
+		System.out.println("======================================");
+
+		List<Integer> list2 = Arrays.asList(
+				15,25,35,45,55,56,57,58,59,44,66,22,82,46,37);
+		
+		Map<Boolean, List<Integer>> map2 =
+				list2.stream()
+				.collect(Collectors.partitioningBy(b -> b % 2 == 0));
+		
+		System.out.println("Even Numbers : "+map2.get(true));
+		System.out.println("Odd Numbers : "+map2.get(false));
+		
 		
 	}
 }
