4d253dc702c747299e24e74428fe082e2afc405d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-20 10:42:36 +0530

    Find Duplicate Elements in a List : Given a list of integers, find all duplicate elements using the Stream API.

--- zjavainterviewquestionsandanswers/src/com/day2/javaintervie/FindDuplicates.java
+++ zjavainterviewquestionsandanswers/src/com/day2/javaintervie/FindDuplicates.java
@@ -21,6 +21,34 @@ public class FindDuplicates {
 		
 		System.out.println("Duplicate Elements : "+duplicates);
 		
+		System.out.println("=====================================");
+
+		List<Integer> list1 = Arrays.asList(
+				10,11,12,13,14,15,16,17,18,19,15,12,14,16);
+		
+		Set<Integer> uniqueElemnts1 = new HashSet<Integer>();
+		
+		Set<Integer> duplicates1 =
+				list1.stream()
+				.collect(Collectors.toSet());
+		
+		System.out.println("Duplicate Elements : "+duplicates1);
+		
+		System.out.println("=====================================");
+		
+		List<Integer> list2 = Arrays.asList(
+				10,2,20,3,5,8,6,6,8,7,9,15,15,18,4,19,18);
+		
+		Set<Integer> uniqIntegers2 = new HashSet<Integer>();
+		
+		Set<Integer> duplicates2 =
+				list2.stream()
+				.collect(Collectors.toSet());
+		
+		System.out.println("Duplicate Elements : "+duplicates2);
+		
+		
+		
 		
 	}
 }
