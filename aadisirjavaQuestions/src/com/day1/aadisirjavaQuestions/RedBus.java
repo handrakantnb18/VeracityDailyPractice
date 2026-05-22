bd0740f9582d1672822a25e4821fd59948a3d89d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-22 22:47:58 +0530

    RedBus class created toString method and private variables

---
+++ aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/RedBus.java
@@ -0,0 +1,17 @@
+package com.day1.aadisirjavaQuestions;
+
+public class RedBus {
+
+	private int id;
+	
+	private String name;
+	
+	private int tickit;
+
+	@Override
+	public String toString() {
+		return "RedBus [id=" + id + ", name=" + name + ", tickit=" + tickit + "]";
+	}
+	
+	
+}
