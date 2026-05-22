64725cf0c7bff93480f91583886763cf21196495
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-22 22:39:14 +0530

    Store class created constructor and private variables

---
+++ aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/Store.java
@@ -0,0 +1,24 @@
+package com.day1.aadisirjavaQuestions;
+
+public class Store {
+
+	private int id;
+	
+	private String name;
+	
+	private String location;
+
+	public Store(int id, String name, String location) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.location = location;
+	}
+
+	public Store() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
