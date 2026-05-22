ae25f90e58035f4eceb0ce8527aec95ff727ab4e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-22 20:58:00 +0530

    Student class created private variables and toString methods are created

---
+++ aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/Student.java
@@ -0,0 +1,17 @@
+package com.day1.aadisirjavaQuestions;
+
+public class Student {
+
+	private int id;
+	
+	private String name;
+	
+	private String city;
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+	
+	
+}
