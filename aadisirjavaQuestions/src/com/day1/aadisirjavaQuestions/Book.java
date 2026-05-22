77a6254a8ef97726a156bf3abf8f5137aeeec1f4
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-22 22:35:12 +0530

    Book class and toString method created

---
+++ aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/Book.java
@@ -0,0 +1,17 @@
+package com.day1.aadisirjavaQuestions;
+
+public class Book {
+
+	private int id;
+	
+	private String name;
+	
+	private String city;
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+	
+	
+}
