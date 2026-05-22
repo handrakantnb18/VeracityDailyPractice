ecf9622d4f88228303c99a3055d949a9c8501b54
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-22 22:45:31 +0530

    Libraries created toString and constructor method

--- aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/Librarie.java
+++ aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/Librarie.java
@@ -31,6 +31,18 @@ public class Librarie {
 	public void setBooks(int books) {
 		this.books = books;
 	}
+
+	@Override
+	public String toString() {
+		return "Librarie [id=" + id + ", name=" + name + ", books=" + books + "]";
+	}
+
+	public Librarie(int id, String name, int books) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.books = books;
+	}
 	
 	
 }
