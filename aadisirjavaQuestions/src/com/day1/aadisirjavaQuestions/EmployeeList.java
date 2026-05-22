d8f12d729b73bcd8db69efe2a560fdc3c5ba54a9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-22 23:01:45 +0530

    EmployeeList created as a map and list with key and value pair

---
+++ aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/EmployeeList.java
@@ -0,0 +1,15 @@
+package com.day1.aadisirjavaQuestions;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EmployeeList {
+
+	public static void main(String[] args) {
+		
+		Map<Integer, List<Employee>> list = new HashMap<Integer, List<Employee>>();
+		
+		
+	}
+}
