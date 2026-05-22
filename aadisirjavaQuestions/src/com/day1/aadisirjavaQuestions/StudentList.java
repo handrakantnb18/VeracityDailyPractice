7564d76d7d267caafa6860059a4909376ddf5407
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-22 22:24:52 +0530

    Create an ArrayList to store student names and display all names.

---
+++ aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/StudentList.java
@@ -0,0 +1,24 @@
+package com.day1.aadisirjavaQuestions;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StudentList {
+
+	public static void main(String[] args) {
+
+		Map<Integer, List<Student>> map = new HashMap<Integer, List<Student>>();
+
+		List<Student> list = Arrays.asList(
+				new Student(1, "Ram", "Pune"), 
+				new Student(2, "Chandrakant", "Pune")
+				);
+		
+		map.put(1, list);
+		
+		System.out.println(map);
+
+	}
+}
