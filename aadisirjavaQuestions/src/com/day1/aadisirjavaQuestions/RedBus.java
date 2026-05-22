1a6cd292deef4b5dbcfb0078fbd10ae3275b5873
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-22 22:49:04 +0530

    getter and setter method and constructor created

--- aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/RedBus.java
+++ aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/RedBus.java
@@ -12,6 +12,37 @@ public class RedBus {
 	public String toString() {
 		return "RedBus [id=" + id + ", name=" + name + ", tickit=" + tickit + "]";
 	}
+
+	public RedBus(int id, String name, int tickit) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.tickit = tickit;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getTickit() {
+		return tickit;
+	}
+
+	public void setTickit(int tickit) {
+		this.tickit = tickit;
+	}
 	
 	
 }
