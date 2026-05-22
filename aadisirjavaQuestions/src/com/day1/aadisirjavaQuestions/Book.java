5487d0775be641c22451c3ce661047d62e9d27bd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-22 22:36:43 +0530

    constructor and getter and setter method are created

--- aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/Book.java
+++ aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/Book.java
@@ -12,6 +12,38 @@ public class Book {
 	public String toString() {
 		return "Book [id=" + id + ", name=" + name + ", city=" + city + "]";
 	}
+
+	public Book(int id, String name, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
 	
 	
 }
