ab2e91ef86d5c0aa99798d7ae15a672a1c86768e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-22 22:28:31 +0530

    Employee class created private variables and toString method

---
+++ aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/Employee.java
@@ -0,0 +1,17 @@
+package com.day1.aadisirjavaQuestions;
+
+public class Employee {
+
+	private int id;
+	
+	private String name;
+	
+	private String city;
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+	
+	
+}
