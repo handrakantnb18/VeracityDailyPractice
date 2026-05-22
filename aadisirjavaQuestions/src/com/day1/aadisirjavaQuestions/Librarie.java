fcb7e1aca5c73ae8b8f91549699d7b7cc278a4e1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-22 22:43:15 +0530

    Libraries class getter and setter method created

---
+++ aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/Librarie.java
@@ -0,0 +1,36 @@
+package com.day1.aadisirjavaQuestions;
+
+public class Librarie {
+
+	private int id;
+	
+	private String name;
+	
+	private int books;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBooks() {
+		return books;
+	}
+
+	public void setBooks(int books) {
+		this.books = books;
+	}
+	
+	
+}
