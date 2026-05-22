abadf2548a4ecc5a29907558dd32e8e288223ee5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-22 22:32:12 +0530

    Product class private variables and toString method

---
+++ aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/Product.java
@@ -0,0 +1,17 @@
+package com.day1.aadisirjavaQuestions;
+
+public class Product {
+
+	private int id;
+	
+	private String name;
+	
+	private String city;
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+	
+	
+}
