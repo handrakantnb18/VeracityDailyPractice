4c1d9b6c0c2d4001a59a037ce2619983439c1684
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-22 22:33:12 +0530

    getter and setter method and constructor created

--- aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/Product.java
+++ aadisirjavaQuestions/src/com/day1/aadisirjavaQuestions/Product.java
@@ -12,6 +12,37 @@ public class Product {
 	public String toString() {
 		return "Product [id=" + id + ", name=" + name + ", city=" + city + "]";
 	}
+
+	public Product(int id, String name, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
 	
 	
 }
