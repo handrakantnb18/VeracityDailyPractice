85db84426585b5c13d421fcf10efeb79b6fd1c64
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-25 13:53:40 +0530

    5) Reverse ArrayList Write a program to reverse elements of an ArrayList

---
+++ aadisirjavaQuestions/src/com/day4/aadisirjavaQuestions/ReverseArrays.java
@@ -0,0 +1,25 @@
+package com.day4.aadisirjavaQuestions;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ReverseArrays {
+
+	public static void main(String[] args) {
+		
+		// 5) Reverse ArrayList
+		// Write a program to reverse elements of an ArrayList
+		
+		List<Integer> list = Arrays.asList( 1,2,3,4,5,
+				6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
+				);
+		
+		Collections.reverse(list);
+		
+		System.out.println("Reverse Array : "+list);
+		
+		// 2
+		
+	}
+}
