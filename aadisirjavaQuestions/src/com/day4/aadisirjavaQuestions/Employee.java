6bcc5265c6858829f286c68e28411fcbbc9986e9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-25 22:06:14 +0530

    Employee class constructor and getter setter method

--- aadisirjavaQuestions/src/com/day4/aadisirjavaQuestions/Employee.java
+++ aadisirjavaQuestions/src/com/day4/aadisirjavaQuestions/Employee.java
@@ -9,6 +9,57 @@ public class Employee {
 	private String dept;
 	
 	private int salary;
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", dept=" + dept + ", salary=" + salary + "]";
+	}
+
+	public Employee(int id, String name, String dept, int salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dept = dept;
+		this.salary = salary;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
 	
 	
 }
