b793bd9e1991fe0fe6e2fb4b1a1f49b0b911c902
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-25 13:17:45 +0530

    Write a program to sort elements of an ArrayList in ascending order.

--- aadisirjavaQuestions/src/com/day4/aadisirjavaQuestions/SortElements.java
+++ aadisirjavaQuestions/src/com/day4/aadisirjavaQuestions/SortElements.java
@@ -1,6 +1,7 @@
 package com.day4.aadisirjavaQuestions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,6 +26,13 @@ public class SortElements {
 		
 		System.out.println("Sorted numbers" +list);
 		
+		List<Integer> list1 = Arrays.asList(
+				500,90,78,65,23,15,78
+				);
+		
+		Collections.sort(list1);
+		
+		System.out.println("Sorted numbers : "+list1);
 		
 		
 	}
