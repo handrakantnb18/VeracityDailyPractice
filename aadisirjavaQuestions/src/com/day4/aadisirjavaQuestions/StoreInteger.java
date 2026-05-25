9fd2b14817d4ab8704ace77323adfe8ab8a64deb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-25 20:50:33 +0530

    10) PriorityQueue Program Write a program to store integer values using PriorityQueue.

---
+++ aadisirjavaQuestions/src/com/day4/aadisirjavaQuestions/StoreInteger.java
@@ -0,0 +1,21 @@
+package com.day4.aadisirjavaQuestions;
+
+import java.util.PriorityQueue;
+
+public class StoreInteger {
+
+	public static void main(String[] args) {
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		
+		pq.add(10);
+		pq.add(20);
+		pq.add(30);
+		pq.add(40);
+		pq.add(50);
+		
+		System.out.println("Priority Queue : "+pq);
+		
+		
+	}
+}
