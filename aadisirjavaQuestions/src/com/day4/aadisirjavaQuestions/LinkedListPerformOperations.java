2272c9fac01ac3745bbb537eea5dab418444e161
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-25 14:19:42 +0530

    6) LinkedList Example Create a LinkedList and perform add, remove, and display operations.

---
+++ aadisirjavaQuestions/src/com/day4/aadisirjavaQuestions/LinkedListPerformOperations.java
@@ -0,0 +1,22 @@
+package com.day4.aadisirjavaQuestions;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListPerformOperations {
+
+	public static void main(String[] args) {
+		
+		List<String> list = new LinkedList<String>();
+		
+		list.add("Apple");
+		list.add("Banana");
+		list.add("Mango");
+		list.add("cadbary");
+		
+		System.out.println(list);
+		
+		
+		
+	}
+}
