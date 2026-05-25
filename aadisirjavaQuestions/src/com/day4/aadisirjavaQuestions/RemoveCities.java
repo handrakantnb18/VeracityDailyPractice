5d92584ebff4f37fa8a04976be3097afcfe1a116
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-25 14:37:09 +0530

    7) Vector Example Write a program to store city names using Vector collection.

---
+++ aadisirjavaQuestions/src/com/day4/aadisirjavaQuestions/RemoveCities.java
@@ -0,0 +1,26 @@
+package com.day4.aadisirjavaQuestions;
+
+import java.util.Vector;
+
+public class RemoveCities {
+
+	public static void main(String[] args) {
+		
+		// 7) Vector Example
+		// Write a program to store city names using Vector collection.
+		
+		Vector<String> list = new Vector<String>();
+		
+		list.add("Pune");
+		list.add("Mumbai");
+		list.add("Nagpur");
+		list.add("Hydrabad");
+		list.add("Banglore");
+		
+		System.out.println("City list : "+list);
+		
+		
+		
+		
+	}
+}
