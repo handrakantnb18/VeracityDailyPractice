4b0d506ba4b276501dd70be538aa360819134ca4
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-25 22:13:46 +0530

    HashSet Example Create a HashSet to store employee names and display unique values.

--- aadisirjavaQuestions/src/com/day4/aadisirjavaQuestions/HashSetEmployee.java
+++ aadisirjavaQuestions/src/com/day4/aadisirjavaQuestions/HashSetEmployee.java
@@ -8,13 +8,14 @@ public class HashSetEmployee {
 
 	public static void main(String[] args) {
 		
-		Map<Integer, List<Employee>> list = Arrays.asList(
+		 List<Employee> list = Arrays.asList(
 				new Employee(1, "Chandrakant", "IT", 25550),
-				new Employee(2, "Priyanka", "Sales", 35550)
-				
+				new Employee(2, "Priyanka", "Sales", 35550),
+				new Employee(3, "Shivraj", "Trans", 99900)
 				);
 		
 		System.out.println(list);
+		
 		
 	}
 }
