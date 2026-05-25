b3e59a9a21eb26ef50a1fe7aef88e1de3912e041
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-25 20:41:56 +0530

    9) Queue Example Create a Queue and perform insert and delete operations.

---
+++ aadisirjavaQuestions/src/com/day4/aadisirjavaQuestions/QueueInsert.java
@@ -0,0 +1,25 @@
+package com.day4.aadisirjavaQuestions;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueInsert {
+
+	public static void main(String[] args) {
+		
+		// 9) Queue Example
+		// Create a Queue and perform insert and delete operations.
+		
+		Queue<String> q = new LinkedList<String>();
+		
+		q.add("Java");
+		q.add("Phython");
+		q.add("HTML");
+		q.add("CSS");
+		q.add("JavaScript");
+		
+		System.out.println(q);
+		
+		
+	}
+}
