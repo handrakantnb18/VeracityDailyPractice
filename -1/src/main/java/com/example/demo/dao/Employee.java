2999fe35bd1f95ff5b5179a75a4af9b2512b7245
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-26 22:06:38 +0530

    +import jakarta.persistence.Entity;+import jakarta.persistence.Id;

--- -1/src/main/java/com/example/demo/dao/Employee.java
+++ -1/src/main/java/com/example/demo/dao/Employee.java
@@ -1,7 +1,17 @@
 package com.example.demo.dao;
 
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Repository
+@Entity
+
 public class Employee {
 
+	@Id
+	
 	private int id;
 	
 	private String name;
