ecaf242e87ca06e92cc1eb532d145840b39f825d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-26 13:38:26 +0530

    Employee class checked application is working or not

---
+++ All_Projects/SpringBoot-1/src/main/java/SSPP/SpringBoot_1/Employee.java
@@ -0,0 +1,12 @@
+package SSPP.SpringBoot_1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+
+	public Employee() {
+	
+		System.err.println("Employee Constructor.");
+	}
+}
