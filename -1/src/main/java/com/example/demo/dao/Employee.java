b36d27ae14193e38f4183113798a67d4a6232104
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-26 21:48:07 +0530

    Student class created private variables and toString method created

---
+++ -1/src/main/java/com/example/demo/dao/Employee.java
@@ -0,0 +1,17 @@
+package com.example.demo.dao;
+
+public class Employee {
+
+	private int id;
+	
+	private String name;
+	
+	private  String city;
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+	
+	
+}
