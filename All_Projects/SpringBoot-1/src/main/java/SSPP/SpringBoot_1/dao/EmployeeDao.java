0c8a4e86be748d72d5b55dc1fec6ac8662c2daa5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-26 13:47:06 +0530

    Employee sub packeage created

---
+++ All_Projects/SpringBoot-1/src/main/java/SSPP/SpringBoot_1/dao/EmployeeDao.java
@@ -0,0 +1,12 @@
+package SSPP.SpringBoot_1.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployeeDao {
+
+	public EmployeeDao() {
+	
+		System.err.println("Emp Dao.");
+	}
+}
