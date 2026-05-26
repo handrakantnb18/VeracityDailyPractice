b4fabcfa9606644888b9be4196f23ab13f5e9544
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-26 11:52:46 +0530

    13) LinkedHashSet Example Create a LinkedHashSet and display insertion order.

---
+++ aadisirjavaQuestions/src/com/day4/aadisirjavaQuestions/LinkedHashSetDisplay.java
@@ -0,0 +1,25 @@
+package com.day4.aadisirjavaQuestions;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDisplay {
+
+	public static void main(String[] args) {
+		
+		// 13) LinkedHashSet Example
+		// Create a LinkedHashSet and display insertion order.
+
+		LinkedHashSet<Integer> list = new LinkedHashSet<Integer>(
+				Arrays.asList(
+						15,25,35,45,55,56,75,85,95,55,5,6,9,7
+						));
+		
+		System.out.println(list);
+		
+		// 13) LinkedHashSet Example
+		// Create a LinkedHashSet and display insertion order.
+
+		
+	}
+}
