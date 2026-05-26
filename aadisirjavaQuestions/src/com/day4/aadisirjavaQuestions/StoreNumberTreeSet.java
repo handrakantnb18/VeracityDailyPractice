a73c638f5b86a4d27e1dd596b668d49e3457a9e9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-26 11:51:53 +0530

    12) TreeSet Example Write a program to store numbers in TreeSet and display sorted output.

---
+++ aadisirjavaQuestions/src/com/day4/aadisirjavaQuestions/StoreNumberTreeSet.java
@@ -0,0 +1,44 @@
+package com.day4.aadisirjavaQuestions;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class StoreNumberTreeSet {
+
+	public static void main(String[] args) {
+		
+		//12) TreeSet Example
+		// Write a program to store numbers in TreeSet and display sorted output.
+
+		TreeSet<String> tr = new TreeSet<String>();
+		
+		tr.add("Shyam");
+		tr.add("Prakash");
+		tr.add("Sachin");
+		
+		System.out.println(tr);
+		
+		// 12) TreeSet Example
+		// Write a program to store numbers in TreeSet and display sorted output.
+
+//		TreeSet<Integer> list = new TreeSet<Integer>();
+//		
+//		list.add(10);
+//		list.add(20);
+//		list.add(30);
+//		list.add(40);
+//		list.add(50);
+//		
+//		System.out.println(list);
+//		
+		// 12) TreeSet Example
+		// Write a program to store numbers in TreeSet and display sorted output.
+
+//		TreeSet<Integer> li = new TreeSet<Integer>( Arrays.asList(
+//				10,25,15,48,45,96,35,75,55
+//				));
+//		
+//		System.out.println(li);
+		
+	}
+}
