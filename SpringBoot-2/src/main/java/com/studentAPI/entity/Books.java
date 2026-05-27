2fa23f6903a15f5028a170446e38f913e4824071
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-27 13:09:01 +0530

    private variale STS installation done

--- SpringBoot-2/src/main/java/com/studentAPI/entity/Books.java
+++ SpringBoot-2/src/main/java/com/studentAPI/entity/Books.java
@@ -1,10 +1,21 @@
 package com.studentAPI.entity;
 
+import java.awt.print.Book;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Books {
 
 	private Integer id;
@@ -15,5 +26,11 @@ public class Books {
 	
 	private Integer qty;
 	
+	public static void main(String[] args) {
+		
+		Book b = new Books(null, null, null, null);
+		b.setId(100);
+	}
+	 
 	
 }
