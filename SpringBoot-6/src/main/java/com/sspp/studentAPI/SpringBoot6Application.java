997fa75286fe7ae0cb0fdd32c4bdda214a7ef5e5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-27 21:15:09 +0530

    SprinBoot-6 project created as Spring MVC to created studentAPI

---
+++ SpringBoot-6/src/main/java/com/sspp/studentAPI/SpringBoot6Application.java
@@ -0,0 +1,13 @@
+package com.sspp.studentAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot6Application.class, args);
+	}
+
+}
---
+++ SpringBoot-6/src/test/java/com/sspp/studentAPI/SpringBoot6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sspp.studentAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
