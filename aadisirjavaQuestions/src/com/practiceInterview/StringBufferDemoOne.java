419c37018a266e55bd7802701dd2bf6f6cb7c223
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-29 21:04:42 +0530

    Because the returned values from concat() are not assigned back to s.

---
+++ aadisirjavaQuestions/src/com/practiceInterview/StringBufferDemoOne.java
@@ -0,0 +1,16 @@
+package com.practiceInterview;
+
+public class StringBufferDemoOne {
+
+	public static void main(String[] args) {
+		
+		String s = new String();
+		s = s.concat("Shivraj ");
+		s = s.concat("Priyanka ");
+		s = s.concat("Chandrakant ");
+		s = s.concat("Bhosale");
+		
+		System.out.println(s);
+		
+	}
+}
