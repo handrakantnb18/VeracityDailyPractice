ecff2ca1ba41c1e8df2ed392eeef57ffd67b1131
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-29 20:56:31 +0530

    Java Program to demonstrate use of stringbuffer

---
+++ aadisirjavaQuestions/src/com/practiceInterview/StringBufferDemo.java
@@ -0,0 +1,18 @@
+package com.practiceInterview;
+
+public class StringBufferDemo {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("Priyanka ");
+		sb.append("Shivraj ");
+		sb.append("Chandrakant ");
+		sb.append("Bhosale");
+		
+//		String msg = sb.toString();
+//		
+//		System.out.println(msg);
+//		
+		System.out.println(sb);
+	}
+}
