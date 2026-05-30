6855ac4c6a29edce3255002b3abed8c30985c8cc
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-30 23:01:45 +0530

    7) Vector Example Write a program to store city names using Vector collectio.

---
+++ zzjava8Examples/src/zzjava8Examples/VectorDemo.java
@@ -0,0 +1,21 @@
+package zzjava8Examples;
+
+import java.util.Vector;
+
+public class VectorDemo {
+
+	public static void main(String[] args) {
+		
+		Vector<String> list = new Vector<String>();
+		
+		list.add("Mumbai");
+		list.add("Pune");
+		list.add("Sangli");
+		list.add("Kolhapur");
+		list.add("Satara");
+		
+		System.out.println(list);
+		
+		
+	}
+}
