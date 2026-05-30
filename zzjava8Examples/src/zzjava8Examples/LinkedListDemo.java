2e7118f990678a06a9741cdce707ebd9a4df423f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-30 22:51:39 +0530

    6) LinkedList Example Create a LinkedList and perform add, remove, and display operations.

---
+++ zzjava8Examples/src/zzjava8Examples/LinkedListDemo.java
@@ -0,0 +1,21 @@
+package zzjava8Examples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		
+		list.add("Apple");
+		list.add("Mango");
+		list.add("Banana");
+		list.add("Orange");
+		
+		System.out.println(list);
+		
+		
+	}
+}
