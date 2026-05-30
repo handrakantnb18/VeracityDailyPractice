040992148184059eedd33058f22d0fe03048380e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-30 21:03:38 +0530

    1) ArrayList in Integer Basic Program Create an ArrayList to store student names and display all names.

--- zzjava8Examples/src/zzjava8Examples/ArrayListExample.java
+++ zzjava8Examples/src/zzjava8Examples/ArrayListExample.java
@@ -1,11 +1,12 @@
 package zzjava8Examples;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayListExample {
 
 	public static void main(String[] args) {
-		
+		 // 1
 		ArrayList<String> list1 = new ArrayList<String>();
 		
 		list1.add("Shivraj");
@@ -15,7 +16,17 @@ public class ArrayListExample {
 		
 		System.out.println("List : "+list1);
 		
+		// 2
 		
+		List<Integer> list2 = new ArrayList<Integer>();
+		
+		list2.add(100);
+		list2.add(102);
+		list2.add(103);
+		list2.add(104);
+		list2.add(105);
+		
+		System.out.println("Int List : "+list2);
 		
 	}
 }
