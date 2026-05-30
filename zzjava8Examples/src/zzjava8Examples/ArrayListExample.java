34132c07cb91b3a1c02b4fb88fa5dd5463c75a72
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-30 21:00:12 +0530

    1) ArrayList Basic Program Create an ArrayList to store student names and display all names.

---
+++ zzjava8Examples/src/zzjava8Examples/ArrayListExample.java
@@ -0,0 +1,21 @@
+package zzjava8Examples;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list1 = new ArrayList<String>();
+		
+		list1.add("Shivraj");
+		list1.add("Priyanka");
+		list1.add("Chandrakant");
+		list1.add("Bhosale");
+		
+		System.out.println("List : "+list1);
+		
+		
+		
+	}
+}
