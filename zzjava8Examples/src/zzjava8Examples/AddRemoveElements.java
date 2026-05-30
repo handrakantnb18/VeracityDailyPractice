5959fb004f5f7b6aedc6913625f86a99132d7da2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-30 21:22:49 +0530

    2) Add and Remove ElementsWrite a program to add and remove elements from an ArrayList.

---
+++ zzjava8Examples/src/zzjava8Examples/AddRemoveElements.java
@@ -0,0 +1,22 @@
+package zzjava8Examples;
+
+import java.util.ArrayList;
+
+public class AddRemoveElements {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list1 = new ArrayList<String>();
+		
+		list1.add("java");
+		list1.add(".net");
+		list1.add("HTML");
+		list1.add("CSS");
+		list1.add("Spring");
+		
+		System.out.println("Book List : "+list1);
+		
+		
+		
+	}
+}
