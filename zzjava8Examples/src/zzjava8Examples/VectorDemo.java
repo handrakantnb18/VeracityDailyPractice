b522b48d7a14b7beeafe05556e9ee9546d779003
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-30 23:03:43 +0530

    7) Vector Example Write a program to store city names using Vector collection.

--- zzjava8Examples/src/zzjava8Examples/VectorDemo.java
+++ zzjava8Examples/src/zzjava8Examples/VectorDemo.java
@@ -16,6 +16,16 @@ public class VectorDemo {
 		
 		System.out.println(list);
 		
+		Vector<Integer> list1 = new Vector<Integer>();
+		
+		list1.add(1);
+		list1.add(2);
+		list1.add(3);
+		list1.add(4);
+		list1.add(5);
+		
+		System.out.println(list1);
+		
 		
 	}
 }
