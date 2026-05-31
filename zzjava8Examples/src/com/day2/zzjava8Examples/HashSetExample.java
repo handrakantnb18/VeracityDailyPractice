0f9bd060ce17bffd45be6132864201e902862cdd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-31 11:51:22 +0530

    11) HashSet Example String Create a HashSet to store employee names and display unique values.

---
+++ zzjava8Examples/src/com/day2/zzjava8Examples/HashSetExample.java
@@ -0,0 +1,19 @@
+package com.day2.zzjava8Examples;
+
+import java.util.HashSet;
+
+public class HashSetExample {
+
+	public static void main(String[] args) {
+		
+		HashSet<String> hs = new HashSet<String>();
+		
+		hs.add("Prakash");
+		hs.add("Akash");
+		hs.add("Chandrakant");
+		hs.add("Pooja");
+		
+		System.out.println(hs);
+		
+	}
+}
