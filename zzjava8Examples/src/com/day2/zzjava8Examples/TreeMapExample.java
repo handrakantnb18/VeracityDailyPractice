f98380df21b6a27c798e799bce14e73bdc50a79a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-31 17:31:36 +0530

    16) TreeMap Example Create a TreeMap and display data in sor

---
+++ zzjava8Examples/src/com/day2/zzjava8Examples/TreeMapExample.java
@@ -0,0 +1,19 @@
+package com.day2.zzjava8Examples;
+
+import java.util.TreeMap;
+
+public class TreeMapExample {
+
+	public static void main(String[] args) {
+		
+		TreeMap<Integer, String> map = new TreeMap<Integer, String>();
+		map.put(01, "Shree");
+		map.put(02, "Ganesh");
+		map.put(03, "Vinayak");
+		map.put(04, "Ram");
+		
+		System.out.println("Employee : "+map);
+		
+		
+	}
+}
