05e4cb70e89ea14f5eefd21e8d60075c5b534fcd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-31 22:36:04 +0530

    20) Copy ArrayList : Write a program to copy elements from one ArrayList to another.

---
+++ zzjava8Examples/src/com/day2/zzjava8Examples/ArrayListCoppyElements.java
@@ -0,0 +1,26 @@
+package com.day2.zzjava8Examples;
+
+import java.util.ArrayList;
+
+public class ArrayListCoppyElements {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Samrth");
+		list.add("Datta");
+		list.add("Shiv");
+		list.add("Ram");
+		list.add("Sunil");
+		
+		ArrayList<String> dest = new ArrayList<String>();
+		
+		dest.addAll(list);
+		
+		System.out.println("List : " +list);
+		
+		System.out.println("Dest List : "+dest);
+		
+		
+	}
+}
