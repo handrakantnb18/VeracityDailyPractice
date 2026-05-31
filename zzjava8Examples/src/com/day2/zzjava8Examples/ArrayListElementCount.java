6e0244561ebfbf3922fa740a13a00fa080e77295
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-31 22:24:33 +0530

    19) Count Elements : Write a program to count total elements in an ArrayList.

---
+++ zzjava8Examples/src/com/day2/zzjava8Examples/ArrayListElementCount.java
@@ -0,0 +1,21 @@
+package com.day2.zzjava8Examples;
+
+import java.util.ArrayList;
+
+public class ArrayListElementCount {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Shree");
+		list.add("Ram");
+		list.add("Chandrakant");
+		list.add("Priya");
+		list.add("Sachin");
+		
+		int count = list.size();
+		
+		System.out.println(count);
+		
+	}
+}
