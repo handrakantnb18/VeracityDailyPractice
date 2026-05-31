89c3e01333264b8ebb54b0a07de5e57405952c60
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-31 17:49:38 +0530

    17) key and value LinkedHashMap Example Write a program using LinkedHashMap to maintain insertion order.

--- zzjava8Examples/src/com/day2/zzjava8Examples/LinkedHAshMapExample.java
+++ zzjava8Examples/src/com/day2/zzjava8Examples/LinkedHAshMapExample.java
@@ -1,6 +1,7 @@
 package com.day2.zzjava8Examples;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class LinkedHAshMapExample {
 
@@ -15,6 +16,22 @@ public class LinkedHAshMapExample {
 		
 		System.out.println("Employee : "+map);
 		
+		System.out.println("=====================");
 		
+		
+		LinkedHashMap<Integer, String> map1 = new LinkedHashMap<Integer, String>();
+		map1.put(101, "Ram");
+		map1.put(102, "Akash");
+		map1.put(103, "Prakash");
+		map1.put(104, "Anil");
+		map1.put(105, "Pooja");
+		
+		System.out.println("Empolyees : ");
+		
+		for(Map.Entry<Integer, String> entry : map1.entrySet())
+		{
+			System.out.println("Id : "+ entry.getKey() + 
+								", Name : "+entry.getValue());
+		}
 	}
 }
