da42d7257d5b935f1ec794f3b66204dfbcb059c3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-31 17:56:45 +0530

    18) Iterate Collection Write a program to iterate ArrayList using: - for loop - enhanced for loop - Iterator

--- zzjava8Examples/src/com/day2/zzjava8Examples/ArrayListExample.java
+++ zzjava8Examples/src/com/day2/zzjava8Examples/ArrayListExample.java
@@ -14,6 +14,20 @@ public class ArrayListExample {
 		
 		System.out.println("Students : "+list);
 		
+		System.out.println("========================");
+		
+		ArrayList<String> list1 = new ArrayList<String>();
+		list1.add("Priyanka");
+		list1.add("Pooja");
+		list1.add("Chandrakant");
+		list1.add("Shivraj");
+		
+		System.out.println("Students : ");
+		for(int i = 0; i < list1.size(); i++)
+		{
+			System.out.println(list1.get(i));
+		}
+		
 		
 		
 		
