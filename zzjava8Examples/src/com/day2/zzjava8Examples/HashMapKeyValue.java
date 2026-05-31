c3e969f2328a8b1ff16050002e7c07c429007e77
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-31 16:10:14 +0530

    15) Display Keys and Values Write a program to display all keys and values from HashMap.

---
+++ zzjava8Examples/src/com/day2/zzjava8Examples/HashMapKeyValue.java
@@ -0,0 +1,20 @@
+package com.day2.zzjava8Examples;
+
+import java.util.HashMap;
+
+public class HashMapKeyValue {
+
+	public static void main(String[] args) {
+		
+		HashMap<Integer, String> map = new HashMap<Integer, String>();
+		map.put(111, "Satvik");
+		map.put(112, "Samar");
+		map.put(113, "samay");
+		
+		System.out.println("Employees : "+map);
+		
+		
+	}
+	
+	
+}
