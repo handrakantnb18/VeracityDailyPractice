7d4e0e282d7e1bb16c645058348459fc7162a23f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-31 15:13:16 +0530

    14) HashMap Example String Write a program to store student id and name using HashMap.

---
+++ zzjava8Examples/src/com/day2/zzjava8Examples/HashMapExample.java
@@ -0,0 +1,20 @@
+package com.day2.zzjava8Examples;
+
+import java.util.HashMap;
+
+public class HashMapExample {
+
+	public static void main(String[] args) {
+		
+		HashMap<Integer, String> map = new HashMap<Integer, String>();
+		
+		map.put(1, "Ram");
+		map.put(2, "Akash");
+		map.put(3, "Pranita");
+		map.put(4, "Sam");
+		
+		System.out.println("Student : "+map);
+		
+		
+	}
+}
