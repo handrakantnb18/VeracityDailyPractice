1472832330eca6001f4072849da5d9ac9bda5a88
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-31 15:02:45 +0530

    13) LinkedHashSet Example String Create a LinkedHashSet and display insertion order.

---
+++ zzjava8Examples/src/com/day2/zzjava8Examples/LinkedHashSetExample.java
@@ -0,0 +1,21 @@
+package com.day2.zzjava8Examples;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetExample {
+
+	public static void main(String[] args) {
+		
+		LinkedHashSet<String> ls = new LinkedHashSet<String>();
+		
+		ls.add("Rahul");
+		ls.add("Pruthvi");
+		ls.add("Vishu");
+		ls.add("Dnyana");
+		
+		System.out.println("Sudent : "+ls);
+		
+		
+		System.out.println();
+	}
+}
