b7d6ef7d854eaacc2bc678c23d35ecb5d9e54c48
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-31 12:01:33 +0530

    12) TreeSet Example String Write a program to store numbers in TreeSet and display sorted output.

---
+++ zzjava8Examples/src/com/day2/zzjava8Examples/TreeSetExample.java
@@ -0,0 +1,20 @@
+package com.day2.zzjava8Examples;
+
+import java.util.TreeSet;
+
+public class TreeSetExample {
+
+	public static void main(String[] args) {
+		
+		TreeSet<String> ts = new TreeSet<String>();
+		ts.add("Anil");
+		ts.add("Pranil");
+		ts.add("mahesh");
+		ts.add("Rahul");
+		ts.add("Ram");
+		
+		System.out.println("Employee : "+ts);
+		
+		
+	}
+}
