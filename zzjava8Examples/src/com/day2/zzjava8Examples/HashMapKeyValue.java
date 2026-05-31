c730b0d091f769cdb563b7d4e8e3a8f9cb109901
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-05-31 16:13:17 +0530

    15) Display Keys and Values pair using for loop Write a program to display all keys and values from HashMap.

--- zzjava8Examples/src/com/day2/zzjava8Examples/HashMapKeyValue.java
+++ zzjava8Examples/src/com/day2/zzjava8Examples/HashMapKeyValue.java
@@ -1,6 +1,7 @@
 package com.day2.zzjava8Examples;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class HashMapKeyValue {
 
@@ -11,9 +12,13 @@ public class HashMapKeyValue {
 		map.put(112, "Samar");
 		map.put(113, "samay");
 		
-		System.out.println("Employees : "+map);
+		System.out.println("Employees : ");
 		
-		
+		for(Map.Entry<Integer, String> entry : map.entrySet())
+		{
+			System.out.println("Key : "+entry.getKey() + 
+					", Value : "+entry.getValue());
+		}
 	}
 	
 	
