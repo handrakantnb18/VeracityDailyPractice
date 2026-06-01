e8ec3e9ab8b7cc1cc45c7d9a15c41599b2a9955c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-01 12:53:20 +0530

    constructor and getter and setter method created

--- zzjava8Examples/src/com/day3/zzjava8Examples/Book.java
+++ zzjava8Examples/src/com/day3/zzjava8Examples/Book.java
@@ -12,6 +12,42 @@ public class Book {
 	public String toString() {
 		return "Book [id=" + id + ", name=" + name + ", qty=" + qty + "]";
 	}
+
+	public Book(int id, String name, int qty) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.qty = qty;
+	}
+
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
 	
 	
 }
