483eb9e903b3b7b867bfdc7db51d07fc9096364f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-01 12:51:12 +0530

    private variables and toString methods are created

---
+++ zzjava8Examples/src/com/day3/zzjava8Examples/Book.java
@@ -0,0 +1,17 @@
+package com.day3.zzjava8Examples;
+
+public class Book {
+
+	private int id;
+	
+	private String name;
+	
+	private int qty;
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", qty=" + qty + "]";
+	}
+	
+	
+}
