218fc2af5987954939f80aa673b44ab2f9f5324a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-01 10:33:48 +0530

    22) Find Maximum Number : Write a program to find maximum number from ArrayList.

---
+++ zzjava8Examples/src/com/day3/zzjava8Examples/FindMazimumNumber.java
@@ -0,0 +1,24 @@
+package com.day3.zzjava8Examples;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FindMazimumNumber {
+
+	public static void main(String[] args) {
+		
+		List<Integer> num1 = new ArrayList<Integer>();
+		num1.add(100);
+		num1.add(200);
+		num1.add(300);
+		num1.add(400);
+		num1.add(500);
+		
+		int max = Collections.max(num1);
+		
+		System.out.println("Maximum No : "+max);
+		
+		List<Int>
+	}
+}
