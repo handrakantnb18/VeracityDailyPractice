9c15507b711378a1bd2c3cf41048b152e7af13a1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-01 12:58:21 +0530

    23) Book Object Collection : Create a Book class and store book objects in ArrayList.

---
+++ zzjava8Examples/src/com/day3/zzjava8Examples/BookCollectionDemo.java
@@ -0,0 +1,23 @@
+package com.day3.zzjava8Examples;
+
+import java.util.ArrayList;
+
+public class BookCollectionDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Book> book = new ArrayList<Book>();
+		book.add(new Book(10, "Java", 5));
+		book.add(new Book(11, "HTML", 18));
+		book.add(new Book(12, "CSS", 27));
+		
+		System.out.println("Book : ");
+		
+		for(Book books : book)
+		{
+			System.out.println(books);
+		}
+		
+		
+	}
+}
