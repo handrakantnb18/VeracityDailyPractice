7b23280ff2e77438d364a92b0db34ccc8f237171
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-01 12:06:08 +0530

    getter and setter method and constructor created in student class

--- zzjava8Examples/src/com/day3/zzjava8Examples/Student.java
+++ zzjava8Examples/src/com/day3/zzjava8Examples/Student.java
@@ -14,6 +14,49 @@ public class Student {
 	public String toString() {
 		return "Student [id=" + id + ", name=" + name + ", marks=" + marks + ", city=" + city + "]";
 	}
-	
-	
+
+	public Student(int id, String name, double marks, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.marks = marks;
+		this.city = city;
+	}
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getMarks() {
+		return marks;
+	}
+
+	public void setMarks(double marks) {
+		this.marks = marks;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
 }
