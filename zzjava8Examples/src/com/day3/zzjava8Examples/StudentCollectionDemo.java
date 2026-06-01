4d17c605704f81ba35d1188523b9f941c1685840
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-01 12:44:34 +0530

    23) Student Object Collection : Create a Student class and store student objects in ArrayList.

---
+++ zzjava8Examples/src/com/day3/zzjava8Examples/StudentCollectionDemo.java
@@ -0,0 +1,20 @@
+package com.day3.zzjava8Examples;
+
+import java.util.ArrayList;
+
+public class StudentCollectionDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Student> list = new ArrayList<Student>();
+		
+		list.add(new Student(101, "Vijay", 55.55, "pune"));
+		list.add(new Student(102, "Chandrakant", 77.77, "pune"));
+		list.add(new Student(103, "Priyanka", 44.44, "Mumbai"));
+		list.add(new Student(104, "Shivraj", 88.88, "pune"));
+		
+		System.out.println("Students : "+list);
+		
+		
+	}
+}
