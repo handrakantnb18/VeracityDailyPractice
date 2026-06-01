843eaca1062cbdb6f3d123d61ef92eee54c723ae
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-01 12:04:24 +0530

    student class created private variables and toString method

---
+++ zzjava8Examples/src/com/day3/zzjava8Examples/Student.java
@@ -0,0 +1,19 @@
+package com.day3.zzjava8Examples;
+
+public class Student {
+
+	private int id;
+	
+	private String name;
+	
+	private double marks;
+	
+	private String city;
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", marks=" + marks + ", city=" + city + "]";
+	}
+	
+	
+}
