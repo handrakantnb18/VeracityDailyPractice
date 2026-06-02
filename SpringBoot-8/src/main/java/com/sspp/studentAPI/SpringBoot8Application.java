8d57e2ee0f49f292a315807708d7389a420c8d34
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-02 10:35:28 +0530

    Spring Boot Course Managemanet System for Student Enroll to the course

---
+++ SpringBoot-8/src/main/java/com/sspp/studentAPI/SpringBoot8Application.java
@@ -0,0 +1,13 @@
+package com.sspp.studentAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot8Application.class, args);
+	}
+
+}
