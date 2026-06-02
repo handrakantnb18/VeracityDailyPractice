dd0105b7ca6a7e643d9d3c2fd40c6190a59180d1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-02 10:41:48 +0530

    Student class we have created private variable and Entity for database table and @Data is gettere and setter methods

---
+++ SpringBoot-8/src/main/java/com/sspp/studentAPI/entity/Student.java
@@ -0,0 +1,32 @@
+package com.sspp.studentAPI.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity(name ="course_Enrolled_Students")
+@Data
+ 
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
+	
+	private String course;
+	
+	private String gender;
+	
+	private String[] timing;
+	
+	private String email;
+	
+	private String mobileNo;
+	
+	
+	
+}
