c85f9f7cc47e4d70071d0acab1e1d236e3ee3fc3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-02 12:24:10 +0530

    model.addAttribute("student", student) Model package accessed

--- SpringBoot-8/src/main/java/com/sspp/studentAPI/controller/CourseController.java
+++ SpringBoot-8/src/main/java/com/sspp/studentAPI/controller/CourseController.java
@@ -1,14 +1,23 @@
 package com.sspp.studentAPI.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sspp.studentAPI.entity.Student;
+
 
 @Controller
 public class CourseController {
 
 	@GetMapping("/enroll")
-	public String getEnrollmentForm()
+	public String getEnrollmentForm(Model model)
 	{
+		
+		Student student = new Student();
+		
+		model.addAttribute("student", student);
+		
 		return "enrollment-form";
 	}
 }
