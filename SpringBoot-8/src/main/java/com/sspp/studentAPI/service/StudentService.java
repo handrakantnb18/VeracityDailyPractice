5acacd8da4bd319a84f7d665e6a047576bb56215
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-04 10:24:25 +0530

    Optional<Student> op = repository.findById(id);

--- SpringBoot-8/src/main/java/com/sspp/studentAPI/service/StudentService.java
+++ SpringBoot-8/src/main/java/com/sspp/studentAPI/service/StudentService.java
@@ -1,16 +1,32 @@
 package com.sspp.studentAPI.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sspp.studentAPI.entity.Student;
 import com.sspp.studentAPI.repo.StudentRepository;
 
+import jakarta.persistence.Id;
+
 @Service
 public class StudentService {
 
 	@Autowired
 	StudentRepository repository;
+	
+	public Student getById(int id)
+	{
+		Student student = null;
+		Optional<Student> op = repository.findById(id);
+		if(op.isPresent())
+		{
+			student = op.get();
+		}
+		
+		return student;
+	}
 	
 	public Student saveStudent(Student student)
 	{
