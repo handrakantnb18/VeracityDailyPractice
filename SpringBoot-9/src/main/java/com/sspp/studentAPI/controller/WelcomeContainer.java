3986bebdd8a42db389d6e1eb265b25236bd372ff
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-05 22:13:51 +0530

    public String welcome(Model model), model.addAttribute

--- SpringBoot-9/src/main/java/com/sspp/studentAPI/controller/WelcomeContainer.java
+++ SpringBoot-9/src/main/java/com/sspp/studentAPI/controller/WelcomeContainer.java
@@ -1,14 +1,22 @@
 package com.sspp.studentAPI.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class WelcomeContainer {
 
 	@GetMapping({"/welcome","/home","/"})
-	public String welcome()
+	public String welcome(Model model)
 	{
+		model.addAttribute("message", "Hello Chandrakant How are you!!!");
+		model.addAttribute("names", List.of("chandrakant","Priyanka","Shivraj"));
 		return "welcome";
 	}
+	
+	
 }
