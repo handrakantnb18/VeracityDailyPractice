fe80887618435ad034cea2766770cf071a2cb604
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-05 14:36:21 +0530

    SpringBoot-9 new Thymal for frontend development project created

---
+++ SpringBoot-9/src/main/java/com/sspp/studentAPI/SpringBoot9Application.java
@@ -0,0 +1,13 @@
+package com.sspp.studentAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot9Application.class, args);
+	}
+
+}
