2f4563da31c368641e3bedcfc469d65a3001eba5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-05 13:50:43 +0530

    ContactFor private variable and @Data getter and setter method accessed

---
+++ SpringBoot-8/src/main/java/com/sspp/studentAPI/entity/ContactForm.java
@@ -0,0 +1,17 @@
+package com.sspp.studentAPI.entity;
+
+import lombok.Data;
+
+@Data
+public class ContactForm {
+
+	private String name;
+	
+	private String email;
+	
+	private String subject;
+	
+	private String message;
+	
+	
+}
