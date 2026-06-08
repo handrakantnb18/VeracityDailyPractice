d81816b665a285b5506f40aa2cc6a152e2f4eb61
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-08 21:08:12 +0530

    Spring RESt APi Project created create APIs

---
+++ SpringRest-1/src/test/java/com/sspp/studentAPI/SpringRest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sspp.studentAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
