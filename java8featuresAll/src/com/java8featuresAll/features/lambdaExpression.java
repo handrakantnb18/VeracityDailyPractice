25e84961dde2cac59da5a727b763a25896a76160
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-08 13:49:15 +0530

    List<Integer> list = Arrays.asList(1,5,6,2,3,5,7,8,89,9);

--- java8featuresAll/src/com/java8featuresAll/features/lambdaExpression.java
+++ java8featuresAll/src/com/java8featuresAll/features/lambdaExpression.java
@@ -16,6 +16,12 @@ public class lambdaExpression {
 		
 		student.forEach(students -> System.out.println(students));
 		
+		List<Integer> list = Arrays.asList(1,5,6,2,3,5,7,8,89,9);
+		
+		list.forEach(lt -> System.out.println(lt));
+		
+		
+		
 		
 		
 	}
