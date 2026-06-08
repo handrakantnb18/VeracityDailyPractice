d457c45a270f05bbdabe828a50b501585b7cfd86
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-08 22:18:04 +0530

    Calculate the sum of all numbers

---
+++ java8featuresAll/src/com/java8featuresAll/features/StreamReduceExam.java
@@ -0,0 +1,17 @@
+package com.java8featuresAll.features;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamReduceExam {
+
+	public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(
+        		1, 2, 3, 4, 5);
+
+        int sum = numbers.stream()
+                         .reduce(0, Integer::sum);
+
+        System.out.println(sum);
+    }
+}
