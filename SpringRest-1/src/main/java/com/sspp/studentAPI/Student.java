1d414835afd225582edf7d8d768061b7640e9fd5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-10 13:17:55 +0530

    getter and settrer method created

--- SpringRest-1/src/main/java/com/sspp/studentAPI/Student.java
+++ SpringRest-1/src/main/java/com/sspp/studentAPI/Student.java
@@ -19,6 +19,30 @@ public class Student {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getMarks() {
+		return marks;
+	}
+
+	public void setMarks(double marks) {
+		this.marks = marks;
+	}
 	
 	
 }
