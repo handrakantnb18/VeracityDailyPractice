b5230aacf0eea1969e92d9a7edafa082bf3ff200
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-11 21:39:44 +0530

    Odd numbers check Used for processing collections in a functional style.

---
+++ java8featuresAll/src/com/day2/java8featuresAll/features/CheckOddPrimeNumber.java
@@ -0,0 +1,18 @@
+package com.day2.java8featuresAll.features;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CheckOddPrimeNumber {
+
+	public static void main(String[] args) {
+		
+		List<Integer> numbers = Arrays.asList(1,2,3,4,5,
+				6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+
+		numbers.stream()
+		       .filter(n -> n % 2 != 0)
+		       .forEach(System.out::println);
+		
+	}
+}
