8ea2bb60c539f1831d6cc38922077ab64f4e7793
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-11 12:11:55 +0530

    toString method and getter and setter method

--- SpringRest-1/src/main/java/com/sspp/studentAPI/Employee.java
+++ SpringRest-1/src/main/java/com/sspp/studentAPI/Employee.java
@@ -19,6 +19,35 @@ public class Employee {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", salary=" + salary + "]";
+	}
 	
 	
 }
