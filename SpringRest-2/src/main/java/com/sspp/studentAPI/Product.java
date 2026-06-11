7173d23832df1b6936f37dfb15529d3f22069433
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-11 12:09:26 +0530

    product class created constructor and private variables

---
+++ SpringRest-2/src/main/java/com/sspp/studentAPI/Product.java
@@ -0,0 +1,24 @@
+package com.sspp.studentAPI;
+
+public class Product {
+
+	private int pd;
+	
+	private String pname;
+	
+	private double price;
+
+	public Product(int pd, String pname, double price) {
+		super();
+		this.pd = pd;
+		this.pname = pname;
+		this.price = price;
+	}
+
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
