e86730f742a018076dc3804b1b536ec7753f6cda
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-11 11:44:04 +0530

    SpringRest 2 project has been staretd

---
+++ SpringRest-2/src/test/java/com/sspp/studentAPI/SpringRest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sspp.studentAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
