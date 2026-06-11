c6d16c5b5f6a1f0ae314480a109acbe24c769cb3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-11 11:34:48 +0530

    Employee class created private variables and constructor created

---
+++ SpringRest-1/src/main/java/com/sspp/studentAPI/Employee.java
@@ -0,0 +1,24 @@
+package com.sspp.studentAPI;
+
+public class Employee {
+
+	private int empId;
+	
+	private String empName;
+	
+	private double salary;
+
+	public Employee(int empId, String empName, double salary) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.salary = salary;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
