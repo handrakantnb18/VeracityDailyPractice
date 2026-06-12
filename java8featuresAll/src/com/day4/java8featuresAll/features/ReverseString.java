3aa40ce3cff76135aa30566286dfd70eed50d841
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-12 22:21:54 +0530

    Write a Java program to reverse a string.

---
+++ java8featuresAll/src/com/day4/java8featuresAll/features/ReverseString.java
@@ -0,0 +1,16 @@
+package com.day4.java8featuresAll.features;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		 	String str = "Java";
+	        String reverse = "";
+
+	        for (int i = str.length() - 1; i >= 0; i--) {
+	            reverse += str.charAt(i);
+	        }
+
+	        System.out.println(reverse);
+	}
+}
