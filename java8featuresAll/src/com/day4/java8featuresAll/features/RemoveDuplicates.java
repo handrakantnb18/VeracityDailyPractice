76778ee2bed3ecab54aa978e930fb646d5f85682
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-12 22:41:28 +0530

    Remove Duplicates from Array

---
+++ java8featuresAll/src/com/day4/java8featuresAll/features/RemoveDuplicates.java
@@ -0,0 +1,20 @@
+package com.day4.java8featuresAll.features;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicates {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1,2,2,3,4,4,5};
+
+		Set<Integer> set = new LinkedHashSet<>();
+
+		for(int num : arr){
+		    set.add(num);
+		}
+
+		System.out.println(set);
+	}
+}
