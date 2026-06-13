a5cf375ea5fb516d00ad23e72907402d160ee4ac
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-13 08:52:07 +0530

    getter and setter method created

--- java8featuresAll/src/com/day3/java8featuresAll/features/Employee.java
+++ java8featuresAll/src/com/day3/java8featuresAll/features/Employee.java
@@ -25,5 +25,29 @@ public class Employee {
 		// TODO Auto-generated constructor stub
 	}
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
 		
 }
