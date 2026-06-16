fa9fd455a4c03d0f7a9c2238b8ad6aa8a3a91525
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-16 21:59:22 +0530

    Find Odd Numbers from a List

---
+++ java8featuresAll/src/com/day7/java8featuresAll/features/OddNumbers.java
@@ -0,0 +1,18 @@
+package com.day7.java8featuresAll.features;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class OddNumbers {
+
+	// Find Even Numbers from a List
+	public static void main(String[] args) {
+		
+		 List<Integer> numbers =
+		            Arrays.asList(10,15,20,25,30,35);
+
+		        numbers.stream()
+		            .filter(n -> n % 2 != 0)
+		            .forEach(System.out::println);
+	}
+}
