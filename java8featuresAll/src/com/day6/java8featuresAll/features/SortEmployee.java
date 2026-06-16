bce35afac8778aeb500d86d895bb257c2a4de620
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-16 22:17:22 +0530

    Sort a List of Employees by Salary

---
+++ java8featuresAll/src/com/day6/java8featuresAll/features/SortEmployee.java
@@ -0,0 +1,21 @@
+package com.day6.java8featuresAll.features;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortEmployee {
+
+	public static void main(String[] args) {
+		
+		List<Employee> list = Arrays.asList(
+	            new Employee(1, "chandrakant", 55000),
+	            new Employee(2, "priyanka", 75000),
+	            new Employee(3, "Shivraj", 65000)
+	        );
+
+	        list.stream()
+	            .sorted(Comparator.comparing(Employee::getSalary))
+	            .forEach(System.out::println);
+	}
+}
