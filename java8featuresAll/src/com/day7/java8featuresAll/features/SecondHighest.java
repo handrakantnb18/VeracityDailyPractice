34bafd723b12edc355b9f7e1fcebb79af2f6e4b2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-16 21:55:26 +0530

    Find the Second Highest Number

---
+++ java8featuresAll/src/com/day7/java8featuresAll/features/SecondHighest.java
@@ -0,0 +1,24 @@
+package com.day7.java8featuresAll.features;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class SecondHighest {
+
+	// Find the Second Highest Number
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list =
+                Arrays.asList(10, 20, 30, 40, 50);
+
+        int result = list.stream()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .get();
+
+        System.out.println(result);
+	}
+}
