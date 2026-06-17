a034f58e6de5af2952a94b142674e14c87ea9ea9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-17 21:41:19 +0530

    Product class private variables and getter and setter methods @Data

---
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/entity/Product.java
@@ -0,0 +1,18 @@
+package com.sspp.studentAPI.entity;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+	private Integer id;
+	
+	private String name;
+	
+	private Double price;
+	
+	private Integer qty;
+	
+	private String catagory;
+	
+}
