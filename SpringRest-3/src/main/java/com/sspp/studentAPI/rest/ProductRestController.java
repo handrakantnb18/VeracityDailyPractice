1dcf983e301c6290ebab90cda61ba02c0b306dca
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-17 21:41:39 +0530

    public void getProductById()

---
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/rest/ProductRestController.java
@@ -0,0 +1,15 @@
+package com.sspp.studentAPI.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductRestController {
+
+	@GetMapping
+	public void getProductById()
+	{
+		
+		
+	}
+}
