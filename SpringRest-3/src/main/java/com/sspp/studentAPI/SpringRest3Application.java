20256a9ee2aa668f72f44f6f753a15e92502d972
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-17 20:35:14 +0530

    new project added in to web based application

---
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/SpringRest3Application.java
@@ -0,0 +1,13 @@
+package com.sspp.studentAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRest3Application.class, args);
+	}
+
+}
---
+++ SpringRest-3/src/test/java/com/sspp/studentAPI/SpringRest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sspp.studentAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRest3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
