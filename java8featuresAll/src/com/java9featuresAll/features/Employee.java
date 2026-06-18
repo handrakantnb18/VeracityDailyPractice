6c7f721c29279176bce9c22d1397a30c69d18cc5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-18 22:22:56 +0530

    An immutable class cannot be modified after creation.

---
+++ java8featuresAll/src/com/java9featuresAll/features/Employee.java
@@ -0,0 +1,16 @@
+package com.java9featuresAll.features;
+
+public class Employee {
+
+	
+	    private final int id;
+
+	    Employee(int id) {
+	        this.id = id;
+	    }
+
+	    public int getId() {
+	        return id;
+	    }
+	}
+
