7139572aa3a96ece8a2bb91de42d2e04b355c93b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-18 22:15:59 +0530

    Find Factorial Using Recursion

---
+++ java8featuresAll/src/com/java9featuresAll/features/FindFactorial.java
@@ -0,0 +1,14 @@
+package com.java9featuresAll.features;
+
+public class FindFactorial {
+
+	public static void main(String[] args) {
+		
+//		public static int factorial(int n) {
+//		    if(n == 0 || n == 1)
+//		        return 1;
+//
+//		    return n * factorial(n - 1);
+//		}
+	}
+}
