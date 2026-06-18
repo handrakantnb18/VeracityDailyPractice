1a60472ec327ea049a0998d2a4e45d9864c1fb2f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-18 22:16:53 +0530

    Fibonacci Series

---
+++ java8featuresAll/src/com/java9featuresAll/features/FibonacciSeries.java
@@ -0,0 +1,17 @@
+package com.java9featuresAll.features;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		
+		int n = 10;
+		int a = 0, b = 1;
+
+		for(int i = 1; i <= n; i++) {
+		    System.out.print(a + " ");
+		    int temp = a + b;
+		    a = b;
+		    b = temp;
+		}
+	}
+}
