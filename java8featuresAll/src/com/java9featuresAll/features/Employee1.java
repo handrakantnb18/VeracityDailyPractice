ed57e604d91f119bedb2f45b4d3201de086690d9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-18 22:25:47 +0530

    Difference Between Comparable and Comparator

---
+++ java8featuresAll/src/com/java9featuresAll/features/Employee1.java
@@ -0,0 +1,9 @@
+package com.java9featuresAll.features;
+
+//public class Employee1 implements Comparable<Employee> {
+
+//	    public int compareTo(Employee e) {
+//	        return this.id - e.id;
+//	    }
+	//}
+
