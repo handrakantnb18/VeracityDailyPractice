e5862b7020c055bbdcdad3ebbdd15512a45f32d5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-18 22:19:54 +0530

    Sort an Array

---
+++ java8featuresAll/src/com/java9featuresAll/features/SortArray.java
@@ -0,0 +1,15 @@
+package com.java9featuresAll.features;
+
+import java.util.Arrays;
+
+public class SortArray {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {5, 2, 8, 1};
+
+		Arrays.sort(arr);
+
+		System.out.println(Arrays.toString(arr));
+	}
+}
