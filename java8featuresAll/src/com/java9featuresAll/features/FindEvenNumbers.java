36fb80272991325b28dfcda39a01c1331e4b44c5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-18 22:21:29 +0530

    Java 8 Stream Example: Find Even Numbers

---
+++ java8featuresAll/src/com/java9featuresAll/features/FindEvenNumbers.java
@@ -0,0 +1,16 @@
+package com.java9featuresAll.features;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FindEvenNumbers {
+
+	public static void main(String[] args) {
+		
+		List<Integer> nums = Arrays.asList(1,2,3,4,5,6);
+
+		nums.stream()
+		    .filter(n -> n % 2 == 0)
+		    .forEach(System.out::println);
+	}
+}
