31b43a8bf84defb018ce5ec908c320d560d1ae0c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-19 21:13:39 +0530

    Count the Number of Elements

---
+++ java8featuresAll/src/com/java10featuresAll/features/CountNumber.java
@@ -0,0 +1,19 @@
+package com.java10featuresAll.features;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CountNumber {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10,
+				11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27);
+		
+		long count = 
+				list.stream()
+				.count();
+		
+		System.out.println(count);
+	}
+}
