df040c3aea3ae9a2b7a5b5624c63ee16544e5bd2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-19 23:06:08 +0530

    getter and setter method and constructor used

--- java8featuresAll/src/com/java10featuresAll/features/Book.java
+++ java8featuresAll/src/com/java10featuresAll/features/Book.java
@@ -7,6 +7,42 @@ public class Book {
 	private String name;
 	
 	private double price;
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+
+	public Book(Integer id, String name, double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
 	
 	
 }
