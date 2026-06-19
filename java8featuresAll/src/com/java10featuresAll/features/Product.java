fa6750686903732d214965ba427de6edc0445105
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-19 23:02:33 +0530

    private variables and toString method

---
+++ java8featuresAll/src/com/java10featuresAll/features/Product.java
@@ -0,0 +1,19 @@
+package com.java10featuresAll.features;
+
+public class Product {
+
+	private Integer id;
+	
+	private String name;
+	
+	private Integer price;
+	
+	private String city;
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", city=" + city + "]";
+	}
+	
+	
+}
