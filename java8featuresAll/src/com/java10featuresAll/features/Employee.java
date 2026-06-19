de2217588cddb443055fd48f21fc043902510090
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-19 22:24:38 +0530

    getter and setter method and constructor applyed

--- java8featuresAll/src/com/java10featuresAll/features/Employee.java
+++ java8featuresAll/src/com/java10featuresAll/features/Employee.java
@@ -14,6 +14,51 @@ public class Employee {
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + ", dept=" + dept + ", salary=" + salary + "]";
 	}
+
+	public Employee(long id, String name, String dept, Integer salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dept = dept;
+		this.salary = salary;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public Integer getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
 	
 	
 }
