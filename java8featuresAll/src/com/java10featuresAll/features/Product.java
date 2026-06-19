4c26008afa47ad71aaadf953e851cedc92cadd08
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-19 23:03:26 +0530

    getter and setter methods aapplies

--- java8featuresAll/src/com/java10featuresAll/features/Product.java
+++ java8featuresAll/src/com/java10featuresAll/features/Product.java
@@ -14,6 +14,51 @@ public class Product {
 	public String toString() {
 		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", city=" + city + "]";
 	}
+
+	public Product(Integer id, String name, Integer price, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.city = city;
+	}
+
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
 	
 	
 }
