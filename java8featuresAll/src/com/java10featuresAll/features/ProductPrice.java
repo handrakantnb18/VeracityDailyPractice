32205c85148f97bdc6257aaa9f07569276f3d6de
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-19 23:10:09 +0530

    ProductPrice class accessed by getName method

---
+++ java8featuresAll/src/com/java10featuresAll/features/ProductPrice.java
@@ -0,0 +1,24 @@
+package com.java10featuresAll.features;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ProductPrice {
+
+	public static void main(String[] args) {
+		
+		List<Product> list = Arrays.asList(
+                new Product(101, "Shivraj", 150, "pune"),
+                new Product(102, "Pruthviraj", 120, "Mumbai"),
+                new Product(103, "Vishwraj", 800, "Banglore"),
+                new Product(104, "Dyanraj",100, "Chennai")
+        );
+		
+		Map<String, List<Product>> map = list.stream()
+		        .collect(Collectors.groupingBy(Product::getName));
+
+		System.out.println(map);	
+	}
+}
