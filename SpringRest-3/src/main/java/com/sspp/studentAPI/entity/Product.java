82c4407b96572c5fcdf5c90a65bd686b751b3e22
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-20 10:48:42 +0530

    default and parameterised constructor added

--- SpringRest-3/src/main/java/com/sspp/studentAPI/entity/Product.java
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/entity/Product.java
@@ -14,5 +14,20 @@ public class Product {
 	private Integer qty;
 	
 	private String catagory;
+
+	public Product(Integer id, String name, Double price, Integer qty, String catagory) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.qty = qty;
+		this.catagory = catagory;
+	}
+
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 	
 }
