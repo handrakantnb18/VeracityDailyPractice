d31613d501aba9548c58097218a0a3ecaa48cb71
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-20 10:35:05 +0530

    ProductServiceImpl implements ProductService @Override

---
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/service/impl/ProductServiceImpl.java
@@ -0,0 +1,14 @@
+package com.sspp.studentAPI.service.impl;
+
+import com.sspp.studentAPI.entity.Product;
+import com.sspp.studentAPI.service.ProductService;
+
+public class ProductServiceImpl implements ProductService {
+
+	@Override
+	public Product getProductById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
