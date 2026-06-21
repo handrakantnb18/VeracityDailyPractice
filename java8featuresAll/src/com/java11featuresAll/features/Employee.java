d9b3ff56467d17fa1bd331b5654feedd1bf4728f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-21 22:21:36 +0530

    Constructor and getter and setter methods

--- java8featuresAll/src/com/java11featuresAll/features/Employee.java
+++ java8featuresAll/src/com/java11featuresAll/features/Employee.java
@@ -2,4 +2,47 @@ package com.java11featuresAll.features;
 
 public class Employee {
 
+	private Integer id;
+	
+	private String name;
+	
+	private double salary;
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+	public Employee(Integer id, String name, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	
 }
