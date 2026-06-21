f568858ef80c8d8570d22737922622516b9085f6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-21 22:15:37 +0530

    Find First Non-Repeated Character

---
+++ java8featuresAll/src/com/java11featuresAll/features/FirstNonRepeated.java
@@ -0,0 +1,30 @@
+package com.java11featuresAll.features;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FirstNonRepeated {
+
+	public static void main(String[] args) {
+		
+		String input = "aabbcdeffeoifjsioffjoijj";
+
+        Character result =
+            input.chars()
+                 .mapToObj(c -> (char)c)
+                 .collect(Collectors.groupingBy(
+                      Function.identity(),
+                      LinkedHashMap::new,
+                      Collectors.counting()))
+                 .entrySet()
+                 .stream()
+                 .filter(e -> e.getValue() == 1)
+                 .map(Map.Entry::getKey)
+                 .findFirst()
+                 .get();
+
+        System.out.println(result);
+	}
+}
