8112f07f2c347094043a76fd8ac8c4d993231cbb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-22 12:15:17 +0530

    @NotNull(message= "Id is required") all validation created

--- SpringRest-3/src/main/java/com/sspp/studentAPI/entity/Product.java
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/entity/Product.java
@@ -1,7 +1,10 @@
 package com.sspp.studentAPI.entity;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -11,14 +14,19 @@ import lombok.Data;
 public class Product {
 
 	@Id
+	@NotNull(message= "Id is required")
 	private Integer id;
 	
+	@NotBlank(message= "Name is required")
 	private String name;
 	
+	@NotNull(message = "Price is required")
 	private Double price;
 	
+	@NotNull(message = "Qty is required")
 	private Integer qty; 
 	
+	@NotBlank(message = "Catagory is required")
 	private String catagory;
 
 	
