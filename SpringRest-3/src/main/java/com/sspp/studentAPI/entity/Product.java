6772126c57967181bb72e0c72f494d8662a5743e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-22 13:04:34 +0530

    +@Entity(name = "products") table name created as per table name id generate automatically

--- SpringRest-3/src/main/java/com/sspp/studentAPI/entity/Product.java
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/entity/Product.java
@@ -2,16 +2,21 @@ package com.sspp.studentAPI.entity;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Entity
+@Entity(name = "products")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String name;
