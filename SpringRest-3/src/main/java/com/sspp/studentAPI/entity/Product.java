22e8aae665edfb0155f3cd77ac450dec718d4ab0
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-22 11:26:23 +0530

    @AllArgsConstructor	private Integer qty;

--- SpringRest-3/src/main/java/com/sspp/studentAPI/entity/Product.java
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/entity/Product.java
@@ -2,10 +2,12 @@ package com.sspp.studentAPI.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @Entity
+@AllArgsConstructor
 public class Product {
 
 	@Id
@@ -15,19 +17,11 @@ public class Product {
 	
 	private Double price;
 	
-	private Integer qty;
+	private Integer qty; 
 	
 	private String catagory;
 
-	public Product(Integer id, String name, Double price, Integer qty, String catagory) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.price = price;
-		this.qty = qty;
-		this.catagory = catagory;
-	}
-
+	
 	public Product() {
 		super();
 		// TODO Auto-generated constructor stub
