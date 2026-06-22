49babdea453d072a6dd753d359e9f07aedc024e1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-22 12:14:47 +0530

    Map<String, String> errors = new HashMap<String, String> @ExceptionHandler(value = MethodArgumentNotValidException.class)

--- SpringRest-3/src/main/java/com/sspp/studentAPI/exception/AppException.java
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/exception/AppException.java
@@ -1,15 +1,35 @@
 package com.sspp.studentAPI.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class AppException {
 
-	@ExceptionHandler(value = Exception.class)
-	public String handleException(Exception e)
-	{
-		return e.getMessage();
-	}
+//	@ExceptionHandler(value = Exception.class)
+//	public String handleException(Exception e)
+//	{
+//		return e.getMessage();
+//	}
 	
+	@ExceptionHandler(value = MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleMethodArgumentExeption(MethodArgumentNotValidException e)
+	{
+		
+		Map<String, String> errors = new HashMap<String, String>();
+		
+		e.getBindingResult().getFieldErrors().forEach(error -> {
+			
+			errors.put(error.getField(), error.getDefaultMessage());
+			
+		});
+		
+		return new ResponseEntity<Map<String, String>>(errors, HttpStatus.BAD_REQUEST);
+	}
 }
