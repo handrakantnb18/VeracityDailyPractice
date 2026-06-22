749db6f5a0d065f24ea913d7e617006175379660
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-22 12:41:28 +0530

    +@NoArgsConstructor import lombok.NoArgsConstructor;

--- SpringRest-3/src/main/java/com/sspp/studentAPI/entity/Product.java
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/entity/Product.java
@@ -2,38 +2,25 @@ package com.sspp.studentAPI.entity;
 
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
-	@Id
-	@NotNull(message= "Id is required")
 	private Integer id;
 	
-	@NotBlank(message= "Name is required")
 	private String name;
 	
-	@NotNull(message = "Price is required")
 	private Double price;
 	
-	@NotNull(message = "Qty is required")
 	private Integer qty; 
 	
-	@NotBlank(message = "Catagory is required")
 	private String catagory;
 
-	
-	public Product() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
 	
 }
