cc5957a1314a12a702f706bc457b68d9e5e29844
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-22 10:43:27 +0530

    public ResponseEntity<Product> getProductById(@PathVariable Integer id) propessional way created

--- SpringRest-3/src/main/java/com/sspp/studentAPI/rest/ProductRestController.java
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/rest/ProductRestController.java
@@ -3,6 +3,8 @@ package com.sspp.studentAPI.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,19 +23,33 @@ public class ProductRestController {
 	ProductService productService;
 	
 	@GetMapping("/product/{id}")
-	public Product getProductById(@PathVariable Integer id)
+	public ResponseEntity<Product> getProductById(@PathVariable Integer id)
 	{
 		Product product = productService.getProductById(id);
 		
-		return product;
-	}
-	
-	@GetMapping("/products")
-	public List<Product> getAllProducts()
-	{
-		List<Product> allproducts =  productService.getAllproducts();
+		product = null;
+		return new ResponseEntity<Product>(product, HttpStatus.OK);
 		
-		return allproducts;
+		
+//		return new ResponseEntity<Product>(product, HttpStatus.CREATED);
+		
+
 	}
+
+//	@GetMapping("/product/{id}")
+//	public Product getProductById(@PathVariable Integer id)
+//	{
+//		Product product = productService.getProductById(id);
+//		
+//		return product;
+//	}
+//	
+//	@GetMapping("/products")
+//	public List<Product> getAllProducts()
+//	{
+//		List<Product> allproducts =  productService.getAllproducts();
+//		
+//		return allproducts;
+//	}
 	
 }
