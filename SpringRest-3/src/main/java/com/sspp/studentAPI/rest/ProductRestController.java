f265775135371506bfd882855ed66d602999ca86
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-22 10:23:46 +0530

    @RequestMapping("/api/v1")	@GetMapping("/products")

--- SpringRest-3/src/main/java/com/sspp/studentAPI/rest/ProductRestController.java
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/rest/ProductRestController.java
@@ -1,14 +1,18 @@
 package com.sspp.studentAPI.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sspp.studentAPI.entity.Product;
 import com.sspp.studentAPI.service.ProductService;
 
 @RestController
+@RequestMapping("/api/v1")
 public class ProductRestController {
 
 	// from browser we can send only GET method
@@ -23,4 +27,13 @@ public class ProductRestController {
 		
 		return product;
 	}
+	
+	@GetMapping("/products")
+	public List<Product> getAllProducts()
+	{
+		List<Product> allproducts =  productService.getAllproducts();
+		
+		return allproducts;
+	}
+	
 }
