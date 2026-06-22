d39d57fcd5a26b2de982114986e978ac31f08f36
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-22 09:58:54 +0530

    @Entity and @Id has been passed

--- SpringRest-3/src/main/java/com/sspp/studentAPI/entity/Product.java
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/entity/Product.java
@@ -1,10 +1,14 @@
 package com.sspp.studentAPI.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Product {
 
+	@Id
 	private Integer id;
 	
 	private String name;
