459a3fc58c4e2d7ea4bf96904649f11eeff38aea
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-22 13:47:02 +0530

    public Product getProductByName(String name) {

--- SpringRest-3/src/main/java/com/sspp/studentAPI/service/impl/ProductServiceImpl.java
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/service/impl/ProductServiceImpl.java
@@ -2,6 +2,7 @@ package com.sspp.studentAPI.service.impl;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,45 +19,31 @@ public class ProductServiceImpl implements ProductService {
 	@Autowired
 	ProductRepository productRepository;
 	
-	public List<Product> getProducts()
-	{
-		Product product1 = new Product(1,"Laptop",45000.00,1,"Elecronics");
-
-		Product product2 = new Product(2,"Mobile",25000.00,2,"Elecronics");
 		
-		Product product3 = new Product(3,"Tshirt",4500.00,10,"Cloths");
-		
-		Product product4 = new Product(4,"Pen",450.00,125,"Stationary");
-		
-		Product product5 = new Product(5,"Suger",450.00,10,"Grosary");
-		
-		List<Product> products = Arrays.asList(product1,product2,product3,product4,product5);
-		
-		return products;
-	}
-	
-	
 	@Override
 	public Product getProductById(Integer id) {
-		// TODO Auto-generated method stub
-		List<Product> allProducts = getProducts();
 		
-		for(Product product : allProducts)
+		Optional<Product> byId = productRepository.findById(id);
+		
+		if(byId.isPresent())
 		{
-			if(product.getId() == id)
-			{
-				return product;
-			}	
+			Product product = byId.get();
+			
+			return product;
 			
 		}
+		
 		throw new ProductNotFoundException("Product not found by id : "+id);
 	}
 
 
 	@Override
 	public List<Product> getAllproducts() {
-		// TODO Auto-generated method stub
-		return getProducts();
+		
+		List<Product> all = productRepository.findAll();
+		
+		return all;
+		
 	}
 
 
@@ -73,6 +60,15 @@ public class ProductServiceImpl implements ProductService {
 		
 		return "Product saved successfully.";
 		
+	}
+
+
+	@Override
+	public Product getProductByName(String name) {
+		
+		Product product = productRepository.findByName(name);
+		
+		return product;
 	}
 
 }
