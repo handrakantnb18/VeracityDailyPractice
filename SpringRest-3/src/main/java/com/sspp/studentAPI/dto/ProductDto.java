fbe215fd15fee83ed973d0de8a85c126a718b53c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-22 13:03:04 +0530

    private Integer id; droped

--- SpringRest-3/src/main/java/com/sspp/studentAPI/dto/ProductDto.java
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/dto/ProductDto.java
@@ -8,10 +8,6 @@ import lombok.Data;
 @Data
 public class ProductDto {
 
-	@Id
-	@NotNull(message= "Id is required")
-	private Integer id;
-	
 	@NotBlank(message= "Name is required")
 	private String name;
 	
