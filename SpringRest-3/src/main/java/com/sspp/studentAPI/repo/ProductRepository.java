07ecbd531a62febee55cf3ec2969373934d7c0dd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-22 13:45:45 +0530

    List<Product> findByName(String name);

--- SpringRest-3/src/main/java/com/sspp/studentAPI/repo/ProductRepository.java
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/repo/ProductRepository.java
@@ -3,8 +3,11 @@ package com.sspp.studentAPI.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sspp.studentAPI.entity.Product;
+import java.util.List;
+
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+	List<Product> findByName(String name);
 	
 }
