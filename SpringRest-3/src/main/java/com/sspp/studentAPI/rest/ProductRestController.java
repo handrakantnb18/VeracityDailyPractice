efb99971542cff35230007e7c278eca1eea24cc9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-22 09:34:23 +0530

    @Autowired Product product = productService.getProductById(id);

--- SpringRest-3/src/main/java/com/sspp/studentAPI/rest/ProductRestController.java
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/rest/ProductRestController.java
@@ -1,15 +1,24 @@
 package com.sspp.studentAPI.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sspp.studentAPI.entity.Product;
+import com.sspp.studentAPI.service.ProductService;
 
 @RestController
 public class ProductRestController {
 
+	@Autowired
+	ProductService productService;
+	
 	@GetMapping
-	public void getProductById()
+	public Product getProductById(@PathVariable Integer id)
 	{
+		Product product = productService.getProductById(id);
 		
-		
+		return product;
 	}
 }
