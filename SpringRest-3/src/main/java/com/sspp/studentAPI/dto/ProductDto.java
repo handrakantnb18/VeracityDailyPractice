78522063dd71fa333afe28e68184d71f4f2acb86
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-22 12:41:04 +0530

    ProductDto class  @NotNull(message= "Id is required") all fields

---
+++ SpringRest-3/src/main/java/com/sspp/studentAPI/dto/ProductDto.java
@@ -0,0 +1,27 @@
+package com.sspp.studentAPI.dto;
+
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class ProductDto {
+
+	@Id
+	@NotNull(message= "Id is required")
+	private Integer id;
+	
+	@NotBlank(message= "Name is required")
+	private String name;
+	
+	@NotNull(message = "Price is required")
+	private Double price;
+	
+	@NotNull(message = "Qty is required")
+	private Integer qty; 
+	
+	@NotBlank(message = "Catagory is required")
+	private String catagory;
+
+}
