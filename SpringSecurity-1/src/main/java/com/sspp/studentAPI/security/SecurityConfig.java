2519511f30b1d1d42427660d3acfad203d0a8cfb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-23 13:56:59 +0530

    encription decreption username password

--- SpringSecurity-1/src/main/java/com/sspp/studentAPI/security/SecurityConfig.java
+++ SpringSecurity-1/src/main/java/com/sspp/studentAPI/security/SecurityConfig.java
@@ -1,16 +1,32 @@
 package com.sspp.studentAPI.security;
 
+import java.text.Normalizer.Form;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
 	// users creation
+	
+	// encription 
+	// decreption password
+	
+	// encoder
+	
+	@Bean
+	public BCryptPasswordEncoder encoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
 	
 	@Bean
 	public UserDetailsService createUsers()
@@ -18,14 +34,14 @@ public class SecurityConfig {
 		UserDetails user1 = 
 				User.
 				withUsername("chandr")
-				.password("1818")
+				.password(encoder().encode("1818"))
 				.roles("USER")
 				.build();
 		 
 		UserDetails user2 = 
 				 User.
-				 withUsername("Shiv")
-				 .password("5757")
+				 withUsername("shiv")
+				 .password(encoder().encode("5757"))
 				 .roles("ADMIN")
 				 .build();
 				 
@@ -33,5 +49,17 @@ public class SecurityConfig {
 		 
 	}
 	
+	@Bean
+	public SecurityFilterChain securityLogic(HttpSecurity http)
+	{
+		
+		http.authorizeHttpRequests(auth -> {
+			
+			auth.anyRequest().authenticated();
+		}).formLogin(form -> form.permitAll()); // access for everyone
+				
+		
+		return http.build();
+	}
 	
 }
