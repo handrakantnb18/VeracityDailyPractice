0111c6c5498e661d98399f5f9a85305068fc59b3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-23 14:57:19 +0530

    private variables and @Data for getter and setter method and @Table for database table name created

--- SpringSecurity-2/src/main/java/com/sspp/studentAPI/entity/Employee.java
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/entity/Employee.java
@@ -7,10 +7,26 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
+@Data
+@Table(name = "employees")
 public class Employee {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String gender;
+	
+	@Column(unique = true)
+	private String email;
 	
 	private String mobileNo;
 	
