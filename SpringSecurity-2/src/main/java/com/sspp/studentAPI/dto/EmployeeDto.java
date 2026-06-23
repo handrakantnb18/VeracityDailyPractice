f7eb075c7a2a131eec449b7305d59850eecff369
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-23 15:03:35 +0530

    EmployeeDto for access to the Employee class

---
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/dto/EmployeeDto.java
@@ -0,0 +1,44 @@
+package com.sspp.studentAPI.dto;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+
+@Data
+public class EmployeeDto {
+
+	private String firstName;
+
+	private String lastName;
+
+	private String gender;
+
+	@Column(unique = true)
+	private String email;
+
+	private String mobileNo;
+
+	private String dep;
+
+	private Double salary;
+
+	private LocalDate joiningDate;
+
+	private LocalDate dob;
+
+	private String status;
+
+	private Boolean isMarried;
+
+	private Integer experience;
+
+	private String educationQualification;
+
+	private String address;
+
+	private Integer age;
+
+	private String destination;
+
+}
