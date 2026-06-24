e8a2fef9e7336ea532e2fcde6a5665d875c2d545
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-24 11:02:38 +0530

    @PostMapping("/sapublic EmployeeRestController(EmployeeService employeeService) {

--- SpringSecurity-2/src/main/java/com/sspp/studentAPI/rest/EmployeeRestController.java
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/rest/EmployeeRestController.java
@@ -1,8 +1,36 @@
 package com.sspp.studentAPI.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sspp.studentAPI.dto.EmployeeDto;
+import com.sspp.studentAPI.service.EmployeeService;
 
 @RestController
 public class EmployeeRestController {
 
+	// final
+	private final EmployeeService employeeService;
+	
+	// constructor
+	public EmployeeRestController(EmployeeService employeeService) {
+		
+		this.employeeService=employeeService;
+
+	}
+	
+	@PostMapping("/save")
+	public ResponseEntity<EmployeeDto> saveEmployee(@RequestBody EmployeeDto employeeDto)
+	{
+		EmployeeDto saveEmployee = employeeService.saveEmployee(employeeDto);
+		
+		return new ResponseEntity<EmployeeDto>(saveEmployee, HttpStatus.CREATED);
+		
+		
+	}
+	
+	
 }
