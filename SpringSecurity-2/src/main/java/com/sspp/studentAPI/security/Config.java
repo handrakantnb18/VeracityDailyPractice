066ac00107208008b26fce6e8dea82289c5aa89f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-24 12:50:32 +0530

    public BCryptPasswordEncoder encoder() http.authorizeHttpRequests(auth ->{

--- SpringSecurity-2/src/main/java/com/sspp/studentAPI/security/Config.java
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/security/Config.java
@@ -5,12 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class Config {
 
+	@Bean
+	public BCryptPasswordEncoder encoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http)
 	{
@@ -21,6 +28,10 @@ public class Config {
 		
 		http.httpBasic(Customizer.withDefaults()); // to send basic auth from postman
 		
+		http.authorizeHttpRequests(auth ->{
+			
+			auth.requestMatchers("/auth/create-user").permitAll();
+		});
 		return http.build();
 		
 	}
