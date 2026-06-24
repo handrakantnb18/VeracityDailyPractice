002052ef8dd7cf590630230801b8cc44a436c102
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-24 12:05:55 +0530

    POST, PUT = we have to desable CSRF  but company can not be desable use to postman

---
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/security/Config.java
@@ -0,0 +1,27 @@
+package com.sspp.studentAPI.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Config {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http)
+	{
+		// POST, PUT = we have to desable CSRF
+		// but company can not be desable use to postman
+		
+		http.csrf(csrf -> csrf.disable()); // dessable to access the postman
+		
+		http.httpBasic(Customizer.withDefaults()); // to send basic auth from postman
+		
+		return http.build();
+		
+	}
+}
