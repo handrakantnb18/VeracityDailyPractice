5cbc58908b89b125e7f2d40b217393b3690230ea
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-24 12:19:46 +0530

    BCryptPasswordEncoder bCryptPasswordEncoder;

--- SpringSecurity-2/src/main/java/com/sspp/studentAPI/rest/AuthController.java
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/rest/AuthController.java
@@ -1,5 +1,7 @@
 package com.sspp.studentAPI.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,15 @@ import com.sspp.studentAPI.entity.User;
 @RequestMapping("/auth")
 public class AuthController {
 
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	
 	@PostMapping("/create-user")
 	public void createUsers(User user)
 	{
+		// password should save in encoding format
+		
 		
 	}
 }
