8638e18945df88a83f4c44855d8341205068cbb9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-24 10:34:47 +0530

    public interface EmployeeRepository extends JpaRepository<Employee, Integer> {

--- SpringSecurity-2/src/main/java/com/sspp/studentAPI/repo/EmployeeRepository.java
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/repo/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.sspp.studentAPI.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sspp.studentAPI.entity.Employee;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	 
