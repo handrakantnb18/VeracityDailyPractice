aecb8c42a6d3f32077bb799418084d0e179ae088
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-24 10:33:35 +0530

    public EmployeeDto saveEmployee(EmployeeDto employeeDto);

---
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.sspp.studentAPI.service;
+
+import com.sspp.studentAPI.dto.EmployeeDto;
+
+public interface EmployeeService {
+
+	public EmployeeDto saveEmployee(EmployeeDto employeeDto);
+	
+	
+}
