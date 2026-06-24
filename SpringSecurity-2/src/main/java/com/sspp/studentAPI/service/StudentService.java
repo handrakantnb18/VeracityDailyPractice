21f76bcb17eee1741e3951df66ba9169ea9547bb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-24 13:24:35 +0530

    public StudentDto savedStudent(Student student);

---
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.sspp.studentAPI.service;
+
+import com.sspp.studentAPI.dto.StudentDto;
+import com.sspp.studentAPI.entity.Student;
+
+public interface StudentService {
+
+	public StudentDto savedStudent(Student student);
+	
+
+}
