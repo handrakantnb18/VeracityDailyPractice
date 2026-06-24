8a4efa7b1268f6e64778cc4e2f54a214a11962b5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-24 13:09:01 +0530

    Student class private variables created

---
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/entity/Student.java
@@ -0,0 +1,28 @@
+package com.sspp.studentAPI.entity;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+	private Integer id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String mobileNo;
+	
+	private String standers;
+	
+	private Integer age;
+	
+	private String email;
+	
+	private String address;
+	
+	private String gender;
+	
+	private String city;
+	
+}
