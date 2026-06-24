693566c564502dd5102fde95dcd01c307d1337d3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-24 13:14:21 +0530

    Student class SpringBoot annotation used Data @Entity, @Id and @Column to uniqe value for emailid

--- SpringSecurity-2/src/main/java/com/sspp/studentAPI/entity/Student.java
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/entity/Student.java
@@ -1,10 +1,20 @@
 package com.sspp.studentAPI.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
+@Entity(name = "Students")
 public class Student {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String firstName;
@@ -16,7 +26,8 @@ public class Student {
 	private String standers;
 	
 	private Integer age;
-	
+
+	@Column(unique = true)
 	private String email;
 	
 	private String address;
