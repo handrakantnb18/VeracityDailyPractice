859ed47b15d9a4cfc596d03ac6264095235fe7a2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-25 23:00:38 +0530

    Global Exception Handling

---
+++ java8featuresAll/src/com/java12featuresAll/features/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.java12featuresAll.features;
+
+//@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	public static void main(String[] args) {
+//		
+//		@ExceptionHandler(
+//		        ResourceNotFoundException.class)
+//		    public ResponseEntity<String> handle(
+//		        ResourceNotFoundException ex) {
+//
+//		        return ResponseEntity
+//		                .status(HttpStatus.NOT_FOUND)
+//		                .body(ex.getMessage());
+//		    }
+	}
+}
