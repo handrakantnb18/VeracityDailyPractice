c995cbe36a980ac05cedc1fc1ef565112b025a3b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-25 10:51:32 +0530

    Optional<User> findByUsername(String username);

--- SpringSecurity-2/src/main/java/com/sspp/studentAPI/repo/UserRepository.java
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/repo/UserRepository.java
@@ -1,10 +1,14 @@
 package com.sspp.studentAPI.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sspp.studentAPI.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
+
+	Optional<User> findByUsername(String username);
 
 	
 }
