90be719211e3f859a7e267c89310a00811c4769b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-25 14:33:07 +0530

    List<Employee> getAllEmployees();

--- SpringSecurity-2/src/main/java/com/sspp/studentAPI/service/EmployeeService.java
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/service/EmployeeService.java
@@ -1,10 +1,14 @@
 package com.sspp.studentAPI.service;
 
+import java.util.List;
+
 import com.sspp.studentAPI.dto.EmployeeDto;
+import com.sspp.studentAPI.entity.Employee;
 
 public interface EmployeeService {
 
 	public EmployeeDto saveEmployee(EmployeeDto employeeDto);
 	
+	List<Employee> getAllEmployees();
 	
 }
