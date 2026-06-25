0b8f11001e958ce9bea6b656daff80482b9e63c1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-25 14:32:30 +0530

    public ResponseEntity<List<Employee>> getAllEmployees() @GetMapping("/all")

--- SpringSecurity-2/src/main/java/com/sspp/studentAPI/rest/EmployeeRestController.java
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/rest/EmployeeRestController.java
@@ -1,13 +1,17 @@
 package com.sspp.studentAPI.rest;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sspp.studentAPI.dto.EmployeeDto;
+import com.sspp.studentAPI.entity.Employee;
 import com.sspp.studentAPI.service.EmployeeService;
 
 @RestController
@@ -34,5 +38,16 @@ public class EmployeeRestController {
 		
 	}
 	
+	// Normal User can access only
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<Employee>> getAllEmployees()
+	{
+		List<Employee> allEmployees = employeeService.getAllEmployees();
+		 
+		return new ResponseEntity<List<Employee>>(allEmployees,HttpStatus.OK);
+		
+		
+	}
 	
 }
