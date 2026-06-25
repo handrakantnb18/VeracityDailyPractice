346cd95405dd6db31363b03379c31ea69b89ef20
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-25 11:44:32 +0530

    UserRepository userRepository;

--- SpringSecurity-2/src/main/java/com/sspp/studentAPI/service/CustomerUserDetailsServices.java
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/service/CustomerUserDetailsServices.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.sspp.studentAPI.entity.User;
 import com.sspp.studentAPI.repo.UserRepository;
 
+@Service
 public class CustomerUserDetailsServices implements UserDetailsService {
 
 	@Autowired
