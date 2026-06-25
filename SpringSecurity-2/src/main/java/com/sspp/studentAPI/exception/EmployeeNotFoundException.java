4c66924fe84b1d3f83a5df0ae883322eebef340f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-25 16:15:54 +0530

    public EmployeeNotFoundException(String msg)

---
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/exception/EmployeeNotFoundException.java
@@ -0,0 +1,10 @@
+package com.sspp.studentAPI.exception;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+	public EmployeeNotFoundException(String msg)
+	{
+		super(msg);
+	}
+	
+}
