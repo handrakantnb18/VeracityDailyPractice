2a05cadba65a0c2b91ca11b777f2e02731b301fe
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-25 16:16:19 +0530

    public ResponseEntity<Employee> getEmpById(@PathVariable Integer id) @GetMapping("/byId/{id}")

--- SpringSecurity-2/src/main/java/com/sspp/studentAPI/rest/EmployeeRestController.java
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,8 +47,15 @@ public class EmployeeRestController {
 		List<Employee> allEmployees = employeeService.getAllEmployees();
 		 
 		return new ResponseEntity<List<Employee>>(allEmployees,HttpStatus.OK);
-		
-		
+			
 	}
 	
+	@GetMapping("/byId/{id}")
+	public ResponseEntity<Employee> getEmpById(@PathVariable Integer id)
+	{
+		Employee employeeById = employeeService.getEmployeeById(id);
+		 
+		return new ResponseEntity<Employee>(employeeById,HttpStatus.OK);
+		
+	}
 }
