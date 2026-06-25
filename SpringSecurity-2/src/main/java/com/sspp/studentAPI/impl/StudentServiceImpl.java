28f2dfd91f32b17fc8b3a1d8f7080715d77d66b7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-25 10:52:31 +0530

    public StudentDto savedStudent(Student student) {

---
+++ SpringSecurity-2/src/main/java/com/sspp/studentAPI/impl/StudentServiceImpl.java
@@ -0,0 +1,19 @@
+package com.sspp.studentAPI.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.sspp.studentAPI.dto.StudentDto;
+import com.sspp.studentAPI.entity.Student;
+import com.sspp.studentAPI.service.StudentService;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+	@Override
+	public StudentDto savedStudent(Student student) {
+		
+		return null;
+	}
+
+	
+}
