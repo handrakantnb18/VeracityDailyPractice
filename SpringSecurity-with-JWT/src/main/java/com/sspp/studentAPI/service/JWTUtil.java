a34b876cea60fc460b9cc430d1d4aa0c98da1dfa
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-26 13:42:51 +0530

    token created

--- SpringSecurity-with-JWT/src/main/java/com/sspp/studentAPI/service/JWTUtil.java
+++ SpringSecurity-with-JWT/src/main/java/com/sspp/studentAPI/service/JWTUtil.java
@@ -1,40 +1,58 @@
 package com.sspp.studentAPI.service;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import javax.crypto.SecretKey;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 public class JWTUtil {
 
 	// this class is responsible to generate Token, validate token 
 	
-	private static final String SECRET_KEY = "";
+	private static final String SECRET_KEY = 
+			"mysecretkeymysecretkeymysecretkey12345";
 	
 	private SecretKey getSigningkey()
 	{
-		return keys.hmacShakeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+		return Keys.hmacShakeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
 		
 	}
 	
 	// JWT - Signature , Payload , Subject
 	
+	public String extractUsername(String token)
+	{
+		return Jwts.parser()
+				.verifywith(getSigningKey())
+				.build()
+				.parseSignedClaims(token)
+				.getPayload()
+				.getSubject();
+	
+	}
 	
 	public Date extractExpiration(String token)
 	{
 		return extractClaim(token, Claims::getExpiration);
 	}
 	
-	public <T> T extractClaim(String token, FunctionClaim, T> claimsResolver)
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver)
 	{
 		final Claims claims = extractAllClaims(token);
 		return claimsResolver.apply(claim);
 	}
 	
-	private Claims extractAllClaims(String token)
+	public Claims extractAllClaims(String token)
 	{
 		return Jwts.parser()
 				.verifywith(getSigningKey())
@@ -55,6 +73,10 @@ public class JWTUtil {
 		return claimsResolver.apply(claims);
 	}
 	
+	private Claims extractAllClaims(String token)
+	{
+		
+	}
 	private Boolean isTokenExpired(String token)
 	{
 		return extractException(token).before(new Date());
