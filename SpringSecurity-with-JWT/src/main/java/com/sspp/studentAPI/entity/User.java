b3312c8b956d1d468ef48c68df55fe104090fd92
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-26 09:15:56 +0530

    Entity class User and variable created

---
+++ SpringSecurity-with-JWT/src/main/java/com/sspp/studentAPI/entity/User.java
@@ -0,0 +1,17 @@
+package com.sspp.studentAPI.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+	private Integer id;
+	
+	private String username;
+	
+	private String password;
+	
+	private String role;
+	
+	
+}
