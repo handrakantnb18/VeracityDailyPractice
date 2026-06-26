c410d8e908c5cca4a02e6d6cffdc2bc2769fd4c0
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-26 10:57:42 +0530

    public String welcomeMsg() String msg = "welcome we are Learning Spring JWT SECURITY";

---
+++ SpringSecurity-with-JWT/src/main/java/com/sspp/studentAPI/rest/WelcomeRestController.java
@@ -0,0 +1,18 @@
+package com.sspp.studentAPI.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeRestController {
+
+	@GetMapping("/")
+	public String welcomeMsg()
+	{
+		String msg = "welcome we are Learning Spring JWT SECURITY";
+	
+		return msg;
+	}
+}
