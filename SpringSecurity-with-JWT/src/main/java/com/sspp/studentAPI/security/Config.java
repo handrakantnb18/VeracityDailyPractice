46a4050436725cedca723e75db963c6b28959088
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-26 11:17:27 +0530

    CustomUserDetailsService customUserDetailsService;

--- SpringSecurity-with-JWT/src/main/java/com/sspp/studentAPI/security/Config.java
+++ SpringSecurity-with-JWT/src/main/java/com/sspp/studentAPI/security/Config.java
@@ -1,17 +1,24 @@
 package com.sspp.studentAPI.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.sspp.studentAPI.service.CustomUserDetailsService;
+
 @Configuration
 @EnableWebSecurity
 public class Config {
 
+	@Autowired
+	CustomUserDetailsService customUserDetailsService;
+	
 	@Bean
 	public BCryptPasswordEncoder encoder()
 	{
@@ -33,6 +40,14 @@ public class Config {
 		
 		return http.build();
 		
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider authenticateUser()
+	{
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider(customUserDetailsService);
+		provider.setPasswordEncoder(encoder());
 		
+		return provider;
 	}
 }
