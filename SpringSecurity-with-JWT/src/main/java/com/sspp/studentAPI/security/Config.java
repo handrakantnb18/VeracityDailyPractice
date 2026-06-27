5af801e491fd897690f01d604fd1f5c0152febec
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-27 10:51:53 +0530

    public AuthenticationManager authManager(AuthenticationConfiguration auth)

--- SpringSecurity-with-JWT/src/main/java/com/sspp/studentAPI/security/Config.java
+++ SpringSecurity-with-JWT/src/main/java/com/sspp/studentAPI/security/Config.java
@@ -3,8 +3,10 @@ package com.sspp.studentAPI.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,13 +27,20 @@ public class Config {
 		return new BCryptPasswordEncoder();
 	}
 	
+	// different
+	@Bean
+	public AuthenticationManager authManager(AuthenticationConfiguration auth)
+	{
+		return auth.getAuthenticationManager();
+	}
+	
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http)
 	{
 		
 		http.csrf(csrf->csrf.disable())
-		.authorizeHttpRequests(auth->auth.requestMatchers("/user/create")
+		.authorizeHttpRequests(auth->auth.requestMatchers("/user/create","/authenticate")
 				.permitAll()
 				.anyRequest()
 				.authenticated())
