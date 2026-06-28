48407fee7d0390c48211349ab8f9d14bc8e110d9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-28 22:53:39 +0530

    Sort Array

---
+++ java8featuresAll/src/com/java13featuresAll/features/SortArray.java
@@ -0,0 +1,16 @@
+package com.java13featuresAll.features;
+
+import java.util.Arrays;
+
+public class SortArray {
+
+	public static void main(String[] args) {
+		
+		int arr[]={5,2,8,1,3};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+        
+	}
+}
