816c927b036b9be0eed3d1ccd2b6d505af721852
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-28 22:36:44 +0530

    Reverse String without using another variable

---
+++ java8featuresAll/src/com/java13featuresAll/features/ReverseStringBuilder.java
@@ -0,0 +1,11 @@
+package com.java13featuresAll.features;
+
+public class ReverseStringBuilder {
+
+	public static void main(String[] args) {
+		
+		 String str = "chandrakant";
+	        System.out.println(new StringBuilder(str).reverse());
+	        
+	}
+}
