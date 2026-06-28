6fee93866b1779dea0f93dba49ca5e435562d17f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-28 23:04:53 +0530

    Employee Highest Salary created variables and getter and setter method created

---
+++ java8featuresAll/src/com/java13featuresAll/features/Employee.java
@@ -0,0 +1,61 @@
+package com.java13featuresAll.features;
+
+public class Employee {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String dept;
+	
+	private Double salary;
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", dept=" + dept + ", salary=" + salary + "]";
+	}
+
+	
+	public Employee(Integer id, String name, String dept, Double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dept = dept;
+		this.salary = salary;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	
+	
+}
