cefca9210494f603765173b1b4db3e6e8aa46ed2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-28 22:32:22 +0530

    Add reverse string Java interview program

---
+++ java8featuresAll/src/com/java13featuresAll/features/ReverseString.java
@@ -0,0 +1,16 @@
+package com.java13featuresAll.features;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		 String str = "Java";
+	        String rev = "";
+
+	        for (int i = str.length() - 1; i >= 0; i--) {
+	            rev += str.charAt(i);
+	        }
+
+	        System.out.println(rev);
+	}
+}
