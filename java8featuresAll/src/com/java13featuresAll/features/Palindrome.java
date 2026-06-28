17c18286dc73245f0edc5db44eec61f5078bc7fe
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-28 22:38:30 +0530

    Palindrome String

---
+++ java8featuresAll/src/com/java13featuresAll/features/Palindrome.java
@@ -0,0 +1,20 @@
+package com.java13featuresAll.features;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		
+	       String str = "ReeR";
+	        String rev = "";
+
+	        for(int i=str.length()-1;i>=0;i--){
+	            rev += str.charAt(i);
+	        }
+
+	        if(str.equals(rev))
+	            System.out.println("Palindrome");
+	        else
+	            System.out.println("Not Palindrome");
+	   
+	}
+}
