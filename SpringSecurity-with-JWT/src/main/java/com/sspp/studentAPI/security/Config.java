4ba779a83641ff8a9b560c07e3d4b525fa7194f8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-28 18:43:52 +0530

    .addFilterBefore(jwtFilter, and UsernamePasswordAuthenticationFilter.class)

--- SpringSecurity-with-JWT/src/main/java/com/sspp/studentAPI/security/Config.java
+++ SpringSecurity-with-JWT/src/main/java/com/sspp/studentAPI/security/Config.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.sspp.studentAPI.service.CustomUserDetailsService;
 import com.sspp.studentAPI.service.JWTFilter;
@@ -48,6 +50,8 @@ public class Config {
 				.permitAll()
 				.anyRequest()
 				.authenticated())
+				.addFilterBefore(jwtFilter, 
+						UsernamePasswordAuthenticationFilter.class)
 				.httpBasic(Customizer.withDefaults()); // for the postman
 			
 		
