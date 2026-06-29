4d6eae4b0c0ac071d8c320ecb84ff6d9611d09b1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-29 18:42:22 +0530

    Fibonacci Seriese checke

---
+++ java8featuresAll/src/com/java14featuresAll/features/FibonacciSeries.java
@@ -0,0 +1,20 @@
+package com.java14featuresAll.features;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		
+		int a = 0;
+		int b = 1;
+		int c;
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(a+ " ");
+			
+			c = a + b;
+			a = b;
+			b = c;
+		}
+	}
+}
