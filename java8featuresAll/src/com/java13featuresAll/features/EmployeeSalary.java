aeeff64312b441b4fa814a23f7d81d4282abc054
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-29 09:24:41 +0530

    Collection List Intaerface created as per Employee List

--- java8featuresAll/src/com/java13featuresAll/features/EmployeeSalary.java
+++ java8featuresAll/src/com/java13featuresAll/features/EmployeeSalary.java
@@ -1,11 +1,20 @@
 package com.java13featuresAll.features;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class EmployeeSalary {
 
 	public static void main(String[] args) {
 		
+		List<Employee> list = Arrays.asList(
+				new Employee(101, "chandrakant", "IT", 250000.00),
+				new Employee(102, "Priyanka", "Trans", 150000.00),
+				new Employee(103, "Shivraj", "IAS", 550000.00)
+				);
 		
-		Employee emp = employees.stream()
+		Employee emp = list.stream()
 		        .max(Comparator.comparing(Employee::getSalary))
 		        .get();
 
