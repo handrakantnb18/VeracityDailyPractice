aac9e6b388ea55f9f897f097f89f0fd9b976bc5d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-29 18:33:04 +0530

    Reverse String

---
+++ java8featuresAll/src/com/java14featuresAll/features/ReverseString.java
@@ -0,0 +1,18 @@
+package com.java14featuresAll.features;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		String str = "Chandrakant bhosale";
+		String rev ="";
+		
+		for(int i = str.length() - 1; i >= 0; i--)
+		{
+			rev += str.charAt(i);
+		}
+		
+		System.out.println(rev);
+		
+	}
+}
