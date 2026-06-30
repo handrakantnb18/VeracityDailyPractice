425de478f88fa56aae6050485be4080763278d50
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-30 19:28:10 +0530

    Count Strings Starting with "A"

---
+++ java8featuresAll/src/com/java15featuresAll/features/CountString.java
@@ -0,0 +1,22 @@
+package com.java15featuresAll.features;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CountString {
+
+	public static void main(String[] args) {
+		
+		List<String> str = Arrays.asList(
+				"Shivraj", "Priyanka","Chandrakant","Shekhabai",
+				"Namdev","Bhosale");
+		
+		long count = 
+				str.stream()
+				.filter( name -> name.startsWith("S"))
+				.count();
+		
+		System.out.println(count);
+		
+	}
+}
