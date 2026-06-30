c5e5fc134718f31184395fef7ddae5afb48449b3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-30 11:45:43 +0530

    jsp extention error

--- servlet-1/src/main/java/com/servlet/LoginServlet.java
+++ servlet-1/src/main/java/com/servlet/LoginServlet.java
@@ -1,25 +1,26 @@
-package com.servlet;
-
-import java.io.IOException;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-@WebServlet("/login")
-public class LoginServlet extends HttpServlet{
-
-	
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-//		resp.getWriter().print("<h1>Login page</h1>");
-	
-		System.out.println("Login page");
-		
-		// forword your request for to UI page
-		req.getRequestDispatcher("Login.html").forward(req, resp);
-	}
-}
+//package com.servlet;
+//
+//import java.io.IOException;
+//
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.annotation.WebServlet;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+////@WebServlet("/login")
+//@
+//public class LoginServlet extends HttpServlet{
+//
+//	
+//	@Override
+//	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		
+////		resp.getWriter().print("<h1>Login page</h1>");
+//	
+//		System.out.println("Login page");
+//		
+//		// forword your request for to UI page
+//		req.getRequestDispatcher("Login.html").forward(req, resp);
+//	}
+//}
