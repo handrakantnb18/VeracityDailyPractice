dc8b4ed5c5dab8ed1a96a92b1a479e724ae6f532
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-30 11:45:55 +0530

    jsp extention error

--- servlet-1/src/main/java/com/servlet/MyServlet.java
+++ servlet-1/src/main/java/com/servlet/MyServlet.java
@@ -1,23 +1,24 @@
-package com.servlet;
-
-import java.io.IOException;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-@WebServlet("/dashboard")
-public class MyServlet extends HttpServlet {
-
-	
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	
-		System.out.println("Servlet Method.");
-	
-		resp.getWriter().print("<h1>Servlet Responce done.</h1>");
-	}
-	
-}
+//package com.servlet;
+//
+//import java.io.IOException;
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.annotation.WebServlet;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//@WebServlet("/dashboard")
+//public class MyServlet extends HttpServlet {
+//
+//	
+//	@Override
+//	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//	
+//		System.out.println("Servlet Method.");
+//	
+//		resp.getWriter().print("<h1>Servlet Responce done.</h1>");
+//	}
+//	
+//}
