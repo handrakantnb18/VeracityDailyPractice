00dfcc3312fc062beaace781be59a1b0d88c2700
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-30 19:55:59 +0530

    Find First Non-Repeated Character

---
+++ java8featuresAll/src/com/java15featuresAll/features/NoRepeatCount.java
@@ -0,0 +1,29 @@
+package com.java15featuresAll.features;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class NoRepeatCount {
+
+	public static void main(String[] args) {
+		
+		String str = "chandrakant";
+		
+		Character ch = str.chars()
+				.mapToObj(c -> (char)c)
+				.collect(Collectors.groupingBy(Function.identity(),
+						LinkedHashMap::new,
+						Collectors.counting()))
+				.entrySet()
+				.stream()
+				.filter(e -> e.getValue() == 1)
+				.map(Map.Entry::getKey)
+				.findFirst()
+				.get();
+		
+		System.out.println(ch);
+		
+	}
+}
