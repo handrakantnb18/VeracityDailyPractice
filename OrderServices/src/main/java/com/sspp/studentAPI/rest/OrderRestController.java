26332fcb5654eaaf8d9013d51cfe9919ca5dd131
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-06-30 18:33:01 +0530

    RestTemplate restTemplate;

--- OrderServices/src/main/java/com/sspp/studentAPI/rest/OrderRestController.java
+++ OrderServices/src/main/java/com/sspp/studentAPI/rest/OrderRestController.java
@@ -14,22 +14,23 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class OrderRestController {
 
-	@Autowired
-	RestTemplate restTemplate;
+//	@Autowired
+//	RestTemplate restTemplate;
 	
 	@GetMapping("/order/{orderId}")
 	public ResponseEntity<Map<String, Object>> getOrderById(@PathVariable Integer orderId)
 	{
 		Map<String, Object> order = new LinkedHashMap<String, Object>();
 		
-		Map customer = restTemplate.getForObject("http://localhost:8080/customer/101", Map.class);
+//		Map customer = restTemplate.getForObject("http://localhost:8080/customer/101", Map.class);
 		
 		order.put("Order id ", orderId);
 		order.put("Name ", "Laptop");
 		order.put("Price ", 550000.00);
 		order.put("Qty ", 10);
 		order.put("DiliveryAddress ", "abc building, bhekrai agar, hadapsar, pune - 411028");
-		order.put("Customer", customer);
+//		order.put("Customer", customer);
+		
 		
 		return new ResponseEntity<Map<String, Object>>(order,HttpStatus.OK);
 		
